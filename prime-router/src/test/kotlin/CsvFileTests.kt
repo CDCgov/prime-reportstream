a80f054c2f9434c4279358914dec6ca4e9825549
Jim Duff <James.Duff@omb.eop.gov>
2020-11-02 08:57:16 -0500

    Fixes to use Rick's new Metadata class

--- prime-router/src/test/kotlin/CsvFileTests.kt
+++ prime-router/src/test/kotlin/CsvFileTests.kt
@@ -1,5 +1,6 @@
 package gov.cdc.prime.router
 
+import gov.cdc.prime.router.Metadata.receivers
 import java.io.File
 import java.nio.file.Files
 import java.nio.file.Path
@@ -42,12 +43,12 @@ class CsvFileTests {
     private fun transformFileAndTest(fileName: String) {
         val file = File(fileName)
         assertTrue(file.exists())
-        val schema = Schema.schemas[defaultSchema] ?: error("$defaultSchema not found.")
+        val schema = Metadata.findSchema(defaultSchema) ?: error("$defaultSchema not found.")
 
         // 1) Ingest the file
         val inputMappableTable = MappableTable(file.name, schema, file.inputStream(), MappableTable.StreamType.CSV)
         // 2) Create transformed objeccts, according to the receiver table rules
-        val outputMappableTables = inputMappableTable.routeByReceiver(Receiver.receivers)
+        val outputMappableTables = inputMappableTable.routeByReceiver(Metadata.receivers)
         assertEquals(2, outputMappableTables.size)
         // 3) Write transformed objs to files, and check they are correct
         outputMappableTables.forEach { table ->
@@ -72,16 +73,17 @@ class CsvFileTests {
 
     private fun loadTestReceivers() {
         val loadingStream = File(inputPath + "test-receivers.yml").inputStream()
-        Receiver.loadReceiversList(loadingStream)
-        assertEquals(2, Receiver.receivers.size)
-        assertEquals(2, Receiver.get("federal-test-receiver", "covid-19")?.patterns?.size)
+        Metadata.loadReceiversList(loadingStream)
+        assertEquals(2, Metadata.receivers.size)
+        assertEquals(2, Metadata.findReceiver("federal-test-receiver", "covid-19")?.patterns?.size)
     }
 
     private fun loadTestSchemas() {
-        Schema.loadSchemaCatalog(inputPath)
-        assertNotNull(Schema.schemas[defaultSchema])
-        assertEquals(7, Schema.schemas.getValue(defaultSchema).elements.size)
-        assertEquals("lab", Schema.schemas.getValue(defaultSchema).elements[0].name)
+        Metadata.loadSchemaCatalog(inputPath)
+        val schema = Metadata.findSchema(defaultSchema)
+        assertNotNull(schema);
+        assertEquals(7, schema.elements.size)
+        assertEquals("lab", schema.elements[0].name)
     }
 
 
