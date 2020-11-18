85ff6496d3ec740ecfc354fb2078cc97be3c5c40
jimduff-usds <62258514+jimduff-usds@users.noreply.github.com>
2020-11-18 12:37:52 -0500

    Added Azure Function to ingest data reports via http (#85)
    Co-authored-by: Jim Duff <James.Duff@omb.eop.gov>

---
+++ prime-router/src/main/kotlin/azure/IngestFunction.kt
@@ -0,0 +1,87 @@
+package gov.cdc.prime.router.azure
+
+import com.azure.storage.blob.BlobContainerClient
+import com.azure.storage.blob.BlobServiceClientBuilder
+import com.azure.storage.queue.QueueClient
+import com.azure.storage.queue.QueueClientBuilder
+import com.azure.storage.queue.models.QueueStorageException
+import com.microsoft.azure.functions.*
+import com.microsoft.azure.functions.annotation.AuthorizationLevel
+import com.microsoft.azure.functions.annotation.FunctionName
+import com.microsoft.azure.functions.annotation.HttpTrigger
+import com.microsoft.azure.functions.annotation.StorageAccount
+import org.apache.commons.text.TextStringBuilder
+import java.util.*
+import java.util.function.Consumer
+import java.util.logging.Logger
+
+/**
+ * Azure Functions with HTTP Trigger.
+ */
+class IngestFunction {
+
+    /**
+     * This function listens at endpoint "/api/report".
+     * Ugly invocation that actually worked for me, run in prime-router/:
+     *    curl -X POST -H "Content-Type: text/csv" --data-binary "@./src/test/unit_test_files/lab1-test_results-17-42-31.csv" "http://localhost:7071/api/report?topic=covid-19&filename=lab1-test_results-17-42-31.csv&schema-name=pdi-covid-19.schema"
+     * Which returns the following upon success:
+     *    {"filename":"lab1-test_results-17-42-31.csv","topic":"covid-19","schemaName":"pdi-covid-19.schema","action":"","blobURL":"http://azurite:10000/devstoreaccount1/ingested/lab1-test_results-17-42-31-pdi-covid-19.schema-3ddef736-55e1-4a45-ac41-f74086aaa654.csv"}
+     */
+    @FunctionName("report")
+    @StorageAccount("AzureWebJobsStorage")
+    fun run(
+        @HttpTrigger(
+            name = "req",
+            methods = [HttpMethod.POST],
+            authLevel = AuthorizationLevel.FUNCTION
+        ) request: HttpRequestMessage<String?>,
+        context: ExecutionContext
+    ): HttpResponseMessage {
+        context.logger.info("report ingest function received an upload request.")
+
+        // Create an object representing the incoming CVS file, and validate presence of required metadata.
+        val file = try {
+            IngestedFile(request, context)
+        } catch (e: Exception) {
+            val msgs = TextStringBuilder()
+            Arrays.asList(*e.suppressed).forEach(Consumer { ex: Throwable -> msgs.appendln(ex.message) })
+            context.logger.severe(msgs.toString())
+            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body(msgs.toString()).build()
+        }
+        context.logger.info("Successfully read ${file.filename}. Preparing to queue it for processing.")
+
+        // Queue the object for further processing.  Return the metadata json as a receipt to the caller.
+        return try {
+            val queueClient = createQueueClient()
+            val blobContainerClient = createBlobClient()
+            file.queueForProcessing(queueClient, blobContainerClient, context)
+            context.logger.info("Successfully queued and stored blob for processing: ${file.blobURL}")
+            request.createResponseBuilder(HttpStatus.OK).body(file.toJson()).build()
+        } catch (e: Exception) {
+              request.createResponseBuilder(HttpStatus.BAD_REQUEST)
+                .body("This CVS failed to get queued for further work.\n").build()
+        }
+    }
+
+    companion object {
+        var ingestQueueName = "ingested"
+        var ingestContainerName = "ingested"
+
+        // Connect to the ingest queue.
+        @Throws(QueueStorageException::class)
+        fun createQueueClient(): QueueClient {
+            val connectStr = System.getenv("AzureWebJobsStorage")
+            return QueueClientBuilder()
+                .connectionString(connectStr)
+                .queueName(ingestQueueName)
+                .buildClient()
+        }
+
+        // Connect to the blob container.
+        fun createBlobClient(): BlobContainerClient {
+            val connectStr = System.getenv("AzureWebJobsStorage")
+            val blobServiceClient = BlobServiceClientBuilder().connectionString(connectStr).buildClient()
+            return blobServiceClient.getBlobContainerClient(ingestContainerName)
+        }
+    }
+}
---
+++ prime-router/src/main/kotlin/azure/IngestedFile.kt
@@ -0,0 +1,139 @@
+package gov.cdc.prime.router.azure
+
+import com.azure.core.exception.HttpResponseException
+import com.azure.storage.blob.BlobContainerClient
+import com.azure.storage.queue.QueueClient
+import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.core.JsonProcessingException
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.microsoft.azure.functions.ExecutionContext
+import com.microsoft.azure.functions.HttpRequestMessage
+import org.apache.commons.io.FilenameUtils
+import org.apache.commons.lang3.StringUtils
+import java.io.ByteArrayInputStream
+import java.io.InputStream
+import java.nio.charset.Charset
+import java.util.*
+
+/*
+ * POJO that specifically represents a File sent to the Hub/Router.
+ * @todo Merge this with Rick's Report.kt
+ */
+class IngestedFile {
+    var filename: String // name of the file as passed in by the client.
+    var topic: String // eg, covid-19
+    var schemaName: String // eg, pdi-covid-19
+    var action: String // eg, "route" - what to do with this file.  Probably should be an array.
+    lateinit var blobURL: String // URL of the blob in Azure storage.
+
+    @get:JsonIgnore
+    var reportContent: String // The actual content of the csv (or other format) file.  @todo SHOULD NOT BE STRING.
+
+    /* Create an object from an incoming http POST
+     * This assumes the full body of the POST is the actual contents of the data file.
+     * Does not currently work with binary data.
+     *
+     *
+     *
+     * @throws Exception on validation errors.  Detailed error msgs are in an array returned by e.getSuppressed().
+     */
+    constructor(request: HttpRequestMessage<String?>, context: ExecutionContext?) {
+        val params = request.queryParameters
+        filename = params["filename"] ?: ""
+        topic = params["topic"] ?: ""  // ex.  'covid-19'
+        schemaName = params["schema-name"] ?: ""// ex.  'pdi-covid-19'
+        action = params["action"] ?: ""
+        // Note:  use of String will only work for text data
+        // Does not handle multipart form data right now.
+        reportContent = request.body ?:  ""
+
+        validate()
+    }
+
+    @Throws(Exception::class)
+    fun validate() {
+        val errors: MutableList<String> = ArrayList()
+        if (StringUtils.isEmpty(filename)) {
+            errors.add("Error:  Missing filename parameter")
+        }
+        if (StringUtils.isEmpty(schemaName)) {
+            errors.add("Error:  Missing schemaName parameter")
+        }
+        // @todo Should we allow an empty report data file, or is that always an error?
+        if (StringUtils.isEmpty(reportContent)) {
+            errors.add("Error:  incoming data file is empty or missing")
+        }
+        if (!errors.isEmpty()) {
+            val e = Exception()
+            for (error in errors) {
+                e.addSuppressed(Exception(error))
+            }
+            throw e
+        }
+    }
+
+    /*
+     * Queue this CVS file for further processing.  Store its metadata on the queue, and put the
+     * blob in Azure Blob Storage.
+     * @throws All kinds of unchecked exceptions if the Queue or Blob submission fails.
+     */
+    @Throws(JsonProcessingException::class)
+    fun queueForProcessing(queue: QueueClient, blobContainerClient: BlobContainerClient, context: ExecutionContext) {
+        // Create the blob first.
+        val blobFilename = createInternalFilename(filename, schemaName)
+        val blobClient = blobContainerClient.getBlobClient(blobFilename)
+        val bytes = reportContent.toByteArray(Charset.forName("UTF-8"))
+        val inputStream: InputStream = ByteArrayInputStream(bytes)
+        context.logger.info("uploading ${bytes.size} byte file to blob storage...")
+        blobClient.upload(inputStream, bytes.size.toLong(), true)
+        context.logger.info("upload done: $blobFilename")
+        blobURL = blobClient.blobUrl
+
+        // Now create the message
+        val metadataJson = toJson()
+        try {
+            context.logger.info("sending this message: $metadataJson")
+            val result = queue.sendMessage(metadataJson)
+            context.logger.info("message Id ${result.messageId} sent.")
+        } catch (e: HttpResponseException) {
+            // I can't for the life of me figure out how to fix this, so I'm temporarily ignoring it.
+            // The sendMessage *works*, its just when its constructing the return value, it gives this very weird exception
+            /* Unexpected first character (char code 0xEF), not valid in xml document: could be mangled UTF-8 BOM marker. Make sure that the Reader uses correct encoding or pass an InputStream instead
+	    */
+            context.logger.info("Caught this exception: " + e + e.message)
+            e.printStackTrace()
+            // @todo FIX THIS - IGNORING this exception.
+        }
+    }
+
+    // @todo needs error checking.
+    @Throws(JsonProcessingException::class)
+    fun toJson(): String {
+        val mapper = ObjectMapper()
+        return mapper.writeValueAsString(this)
+    }
+
+    companion object {
+        /*
+     * Generate a filename that can be used to store a blob.  Filename must be guaranteed unique, hence the UUID.
+     * I thought that was better than a timestamp, since that's not guaranteed unique if we're running high thruput.
+     *
+     * Trying to cover all possibilities here.  Even a completely empty filename will create
+     * a filename that's just a UUID.
+     *
+     * @todo Rick will need a function that takes one of these filenames and generates one to represent a
+     *   transformed file.
+     *
+     * Normal use case:    foo.csv in schema 'pdi-covid-19' becomes
+     *     foo-pdi-covid-19-08ad635e-c801-43d0-8353-eb157193d065.csv
+     */
+        fun createInternalFilename(externalFilename: String?, schemaName: String?): String {
+            val baseName = FilenameUtils.getBaseName(externalFilename)
+            val basePart = if (StringUtils.isEmpty(baseName)) "" else "$baseName-"
+            val extension = FilenameUtils.getExtension(externalFilename)
+            val extPart = if (StringUtils.isEmpty(extension)) "" else ".$extension"
+            val schemaPart = if (StringUtils.isEmpty(schemaName)) "" else "$schemaName-"
+            return basePart + schemaPart + UUID.randomUUID() + extPart
+        }
+    }
+}
