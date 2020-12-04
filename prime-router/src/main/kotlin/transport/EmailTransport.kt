2f08fe0a820cdf783bbe649d893cd54f3e90dc89
Matt Young <matthew.young-usds@bisonworks.com>
2020-12-04 08:52:39 -0700

    Email transport

--- prime-router/src/main/kotlin/transport/EmailTransport.kt
+++ prime-router/src/main/kotlin/transport/EmailTransport.kt
@@ -1,8 +1,9 @@
-package gov.cdc.prime.router.azure
+package gov.cdc.prime.router.transport
 
 import gov.cdc.prime.router.OrganizationService
+import gov.cdc.prime.router.azure.DatabaseAccess
 
-class EmailTransport : Transport {
+class EmailTransport : ITransport {
 
     override fun send(orgName: String, transport: OrganizationService.Transport, header: DatabaseAccess.Header, contents: ByteArray): Boolean {
         return true
