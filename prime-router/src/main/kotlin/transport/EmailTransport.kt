747f3c3617acb7f014cb4ff40deab8cd77cea4d3
Matt Young <matthew.young-usds@bisonworks.com>
2020-12-09 10:24:53 -0700

    changes

--- prime-router/src/main/kotlin/transport/EmailTransport.kt
+++ prime-router/src/main/kotlin/transport/EmailTransport.kt
@@ -1,5 +1,6 @@
 package gov.cdc.prime.router.transport
 
+import com.sendgrid.*
 import gov.cdc.prime.router.OrganizationService
 import gov.cdc.prime.router.azure.DatabaseAccess
 
@@ -11,6 +12,22 @@ class EmailTransport : ITransport {
         header: DatabaseAccess.Header,
         contents: ByteArray
     ): Boolean {
+
+        val sg: SendGrid = SendGrid(System.getenv("SENDGRID_API_KEY"))
+        val request = Request()
+        request.setMethod(Method.POST)
+        request.setEndpoint("mail/send")
+        request.setBody("{to: 'matthew.young@bisonworks.com' }")
+        /* 
+            from: 'qtv1@cdc.gov',
+            subject: 'Sending is fun',
+            text: 'this is text',
+            html: '<strong>this is html</strong>'
+        }")*/
+        val response = sg.api(request)
+        System.out.println(response.getStatusCode())
+        System.out.println(response.getBody())
+        System.out.println(response.getHeaders())
         return true
     }
 }
