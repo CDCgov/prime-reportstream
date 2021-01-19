28edb4a4a0cd41626220bf739c8ce97efcb7fac6
Chris Glodosky <cglodosky@cdc.gov>
2021-01-19 09:34:17 -0600

    Dockerfile and initial script for testing

---
+++ operations/README.md
@@ -0,0 +1,9 @@
+## Build the docker image
+```docker build -t terraform .```
+
+## Run the container interactively
+```docker run -it --rm --name terraform terraform```
+
+## Login to Azure CLI
+```az login```
+- Navigate to the provided login URL and input the provided token
---
+++ operations/app/src/main.tf
@@ -0,0 +1,18 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.26"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "resourcegroup" {
+  name     = "prime-dev-cglodosky"
+  location = "eastus"
+}
