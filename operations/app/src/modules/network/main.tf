ebe3891294207521b767ebee647806027758cdd2
Chris Glodosky <cglodosky@cdc.gov>
2021-01-21 16:51:06 -0600

    Added networking

---
+++ operations/app/src/modules/network/main.tf
@@ -0,0 +1,45 @@
+terraform {
+    required_version = ">= 0.14"
+}
+
+resource "azurerm_network_security_group" "nsg_public" {
+  name = "nsg_public"
+  location = var.location
+  resource_group_name = var.resource_group
+}
+
+resource "azurerm_network_security_group" "nsg_private" {
+  name = "nsg_public"
+  location = var.location
+  resource_group_name = var.resource_group
+}
+
+resource "azurerm_virtual_network" "virtual_network" {
+  name = var.name
+  location = var.location
+  resource_group_name = var.resource_group
+  address_space = ["10.0.0.0/16"]
+  dns_servers = ["10.0.0.4", "10.0.0.5"]
+
+  subnet {
+    name = "public"
+    address_prefix = "10.0.1.0/24"
+    security_group = azurerm_network_security_group.nsg_public.id
+  }
+
+  subnet {
+    name = "container"
+    address_prefix = "10.0.2.0/24"
+    security_group = azurerm_network_security_group.nsg_private.id
+  }
+
+  subnet {
+    name = "privatge"
+    address_prefix = "10.0.3.0/24"
+    security_group = azurerm_network_security_group.nsg_private.id
+  }
+
+  tags = {
+    environment = var.environment
+  }
+}
--- operations/app/src/modules/prime_data_hub/main.tf
+++ operations/app/src/modules/prime_data_hub/main.tf
@@ -9,3 +9,11 @@ module "storage" {
     name = "${var.resource_prefix}storageaccount"
     location = local.location
 }
+
+module "network" {
+    source = "../network"
+    environment = var.environment
+    resource_group = var.resource_group
+    name = "${var.resource_prefix}-vnet"
+    location = local.location
+}
