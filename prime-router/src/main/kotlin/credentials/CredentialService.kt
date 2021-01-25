77af938c0162fcaea2f8e344a2a676d075894b57
Ron Heft <Ronald.K.Heft@omb.eop.gov>
2021-01-25 16:30:30 -0500

    connectionIds should be url safe

--- prime-router/src/main/kotlin/credentials/CredentialService.kt
+++ prime-router/src/main/kotlin/credentials/CredentialService.kt
@@ -3,17 +3,27 @@ package gov.cdc.prime.router.credentials
 import org.apache.logging.log4j.kotlin.Logging
 
 abstract class CredentialService : Logging {
+    private val URL_SAFE_KEY_PATTERN = Regex("^[a-zA-Z0-9_-]*$")
+
     internal abstract fun fetchCredential(connectionId: String): Credential?
     internal abstract fun saveCredential(connectionId: String, credential: Credential)
 
     fun fetchCredential(connectionId: String, callerId: String, reason: CredentialRequestReason): Credential? {
         logger.info { "CREDENTIAL REQUEST: $callerId requested connectionId($connectionId) credential for $reason" }
+        validateConnectionId(connectionId)
         return fetchCredential(connectionId)
     }
 
     fun saveCredential(connectionId: String, credential: Credential, callerId: String) {
         logger.info { "CREDENTIAL UPDATE: $callerId updated connectionId($connectionId) credential" }
+        validateConnectionId(connectionId)
         return saveCredential(connectionId, credential)
+    }
+
+    private fun validateConnectionId(connectionId: String) {
+        if ( !URL_SAFE_KEY_PATTERN.matches(connectionId) ) {
+            throw IllegalArgumentException("connectionId must match: ${URL_SAFE_KEY_PATTERN.pattern}")
+        }
     }
 }
 
--- prime-router/src/test/kotlin/credentials/CredentialServiceTest.kt
+++ prime-router/src/test/kotlin/credentials/CredentialServiceTest.kt
@@ -1,17 +1,60 @@
 package gov.cdc.prime.router.credentials
 
+import java.lang.IllegalArgumentException
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.fail
 
 internal class CredentialServiceTest : CredentialManagement {
+
+    val VALID_CONNECTION_IDS = listOf("valid1", "35wtfsdfe4t4wr4w4343", "with_underscores-and-dashes")
+    val INVALID_CONNECTION_IDS = listOf("slashes/are/not/allowed", "no spaces", "?andotherthings")
+    val VALID_CREDENTIAL = UserPassCredential("user1", "pass1")
 
     @Test
     fun `test save and fetch`() {
         val connectionId = "id1"
-        val credential = UserPassCredential("user1", "pass1")
-        credentialService.saveCredential(connectionId, credential, "CredentialServiceTest")
+        credentialService.saveCredential(connectionId, VALID_CREDENTIAL, "CredentialServiceTest")
 
         val retVal = credentialService.fetchCredential(connectionId, "CredentialServiceTest", CredentialRequestReason.AUTOMATED_TEST)
-        assertEquals(credential, retVal, "Credential service did not return expected credential")
+        assertEquals(VALID_CREDENTIAL, retVal, "Credential service did not return expected credential")
+    }
+
+    @Test
+    fun `test fetchCredential handles valid connectionIds`() {
+        VALID_CONNECTION_IDS.forEach {
+            credentialService.fetchCredential(it, "CredentialServiceTest", CredentialRequestReason.AUTOMATED_TEST)
+        }
+    }
+
+    @Test
+    fun `test fetchCredential throws IllegalArgumentException with non-url safe connectionIds`() {
+        INVALID_CONNECTION_IDS.forEach {
+            try {
+                credentialService.fetchCredential(it, "CredentialServiceTest", CredentialRequestReason.AUTOMATED_TEST)
+                fail("IllegalArgumentException not thrown for $it")
+            } catch ( e: IllegalArgumentException ) {
+                assertEquals("connectionId must match: ^[a-zA-Z0-9_-]*\$", e.message)
+            }
+        }
+    }
+
+    @Test
+    fun `test saveCredential handles valid connectionIds`() {
+        VALID_CONNECTION_IDS.forEach {
+            credentialService.saveCredential(it, VALID_CREDENTIAL,"CredentialServiceTest")
+        }
+    }
+
+    @Test
+    fun `test saveCredential throws IllegalArgumentException with non-url safe connectionIds`() {
+        INVALID_CONNECTION_IDS.forEach {
+            try {
+                credentialService.saveCredential(it, VALID_CREDENTIAL, "CredentialServiceTest")
+                fail("IllegalArgumentException not thrown for $it")
+            } catch ( e: IllegalArgumentException ) {
+                assertEquals("connectionId must match: ^[a-zA-Z0-9_-]*\$", e.message)
+            }
+        }
     }
 }
