2f70f88a2488be2b6885f767c7ef26ac2a1ff082
Ron Heft <Ronald.K.Heft@omb.eop.gov>
2021-01-26 11:45:28 -0500

    Refactor CredentialService to use Kotlin require

--- prime-router/src/main/kotlin/credentials/CredentialService.kt
+++ prime-router/src/main/kotlin/credentials/CredentialService.kt
@@ -1,30 +1,37 @@
 package gov.cdc.prime.router.credentials
 
+import kotlinx.serialization.*
+import kotlinx.serialization.json.*
+import kotlinx.serialization.modules.*
 import org.apache.logging.log4j.kotlin.Logging
 
 abstract class CredentialService : Logging {
-    private val URL_SAFE_KEY_PATTERN = Regex("^[a-zA-Z0-9_-]*$")
+
+    companion object {
+        private val URL_SAFE_KEY_PATTERN = Regex("^[a-zA-Z0-9_-]*$")
+    }
+
+
+    /* Methods to implement in subclasses */
 
     internal abstract fun fetchCredential(connectionId: String): Credential?
     internal abstract fun saveCredential(connectionId: String, credential: Credential)
 
+
+    /* Base implementation for credentialService with validations */
+
     fun fetchCredential(connectionId: String, callerId: String, reason: CredentialRequestReason): Credential? {
+        require(URL_SAFE_KEY_PATTERN.matches(connectionId)) { "connectionId must match: ${URL_SAFE_KEY_PATTERN.pattern}" }
         logger.info { "CREDENTIAL REQUEST: $callerId requested connectionId($connectionId) credential for $reason" }
-        validateConnectionId(connectionId)
         return fetchCredential(connectionId)
     }
 
     fun saveCredential(connectionId: String, credential: Credential, callerId: String) {
+        require(URL_SAFE_KEY_PATTERN.matches(connectionId)) { "connectionId must match: ${URL_SAFE_KEY_PATTERN.pattern}" }
         logger.info { "CREDENTIAL UPDATE: $callerId updated connectionId($connectionId) credential" }
-        validateConnectionId(connectionId)
         return saveCredential(connectionId, credential)
     }
 
-    private fun validateConnectionId(connectionId: String) {
-        if ( !URL_SAFE_KEY_PATTERN.matches(connectionId) ) {
-            throw IllegalArgumentException("connectionId must match: ${URL_SAFE_KEY_PATTERN.pattern}")
-        }
-    }
 }
 
 enum class CredentialRequestReason {
