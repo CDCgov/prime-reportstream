9a1afb64fe6e11a836defb83b8e3ec80d4b9a9dd
Ron Heft <Ronald.K.Heft@omb.eop.gov>
2021-01-26 13:08:31 -0500

    Implementation of HashicorpVaultCredentialService

--- prime-router/src/main/kotlin/credentials/HashicorpVaultCredentialService.kt
+++ prime-router/src/main/kotlin/credentials/HashicorpVaultCredentialService.kt
@@ -1,12 +1,37 @@
 package gov.cdc.prime.router.credentials
 
-object HashicorpVaultCredentialService : CredentialService() {
+import com.github.kittinunf.fuel.core.FuelManager
+import org.apache.logging.log4j.kotlin.Logging
+
+object HashicorpVaultCredentialService : CredentialService(), Logging {
+
+    private val VAULT_API_ADDR: String by lazy { System.getProperty("VAULT_API_ADDR", "http://127.0.0.1:8200") }
+    private val VAULT_TOKEN: String by lazy { System.getProperty("VAULT_ROOT_TOKEN", "") }
+    private val manager: FuelManager by lazy { initVaultApi(FuelManager()) }
+
+    internal fun initVaultApi(manager: FuelManager): FuelManager {
+        // Set the default path
+        manager.basePath = "$VAULT_API_ADDR/v1/secret"
+
+        // Initialize the API token used to secure requests
+        manager.baseHeaders = mapOf("X-Vault-Token" to VAULT_TOKEN)
+
+        return manager
+    }
 
     override fun fetchCredential(connectionId: String): Credential? {
-        throw Exception("TO BE IMPLEMENTED IN #294")
+        val (request, response, result) = manager.get("/$connectionId")
+            .responseString()
+        val (data, error) = result
+        return Credential.fromJSON(data)
     }
 
     override fun saveCredential(connectionId: String, credential: Credential) {
-        throw Exception("TO BE IMPLEMENTED IN #294")
+        val (request, response, result) = manager.post("/$connectionId")
+            .body(credential.toJSON())
+            .response()
+        if ( response.statusCode != 200 ) {
+            logger.error("Failed to save credentials for: $connectionId")
+        }
     }
 }
---
+++ prime-router/src/test/kotlin/credentials/HashicorpVaultCredentialServiceTests.kt
@@ -0,0 +1,72 @@
+package gov.cdc.prime.router.credentials
+
+import com.github.kittinunf.fuel.core.FuelManager
+import com.github.kittinunf.fuel.core.Method
+import io.mockk.*
+import net.wussmann.kenneth.mockfuel.MockFuelClient
+import net.wussmann.kenneth.mockfuel.MockFuelStore
+import net.wussmann.kenneth.mockfuel.data.MockResponse
+import kotlin.test.*
+
+internal class HashicorpVaultCredentialServiceTests {
+
+    private val mockFuelStore = MockFuelStore()
+    private val credentialService = spyk(HashicorpVaultCredentialService, recordPrivateCalls = true)
+
+    @BeforeTest
+    fun setUp() {
+        every { credentialService getProperty "VAULT_API_ADDR" } returns VAULT_API_ADDR
+        every { credentialService getProperty "VAULT_TOKEN" } returns VAULT_TOKEN
+        every { credentialService getProperty "manager" } returns credentialService.initVaultApi(FuelManager().apply {
+            client = MockFuelClient(mockFuelStore)
+        })
+    }
+
+    @AfterTest
+    fun tearDown() {
+        unmockkObject(credentialService)
+        mockFuelStore.reset()
+    }
+
+    @Test
+    fun `uses Vault api to fetch a credential`() {
+        mockFuelStore.on(Method.GET, "/v1/secret/$CONNECTION_ID") {
+            MockResponse(200, """{"@type":"UserPass","user":"user","pass":"pass"}""".toByteArray())
+        }
+
+        val credential = credentialService.fetchCredential(CONNECTION_ID)
+
+        mockFuelStore.verifyRequest {
+            assertMethod(Method.GET)
+            assertPath("/v1/secret/$CONNECTION_ID")
+            assertHeader("X-Vault-Token", VAULT_TOKEN)
+        }
+
+        assertTrue(credential is UserPassCredential, "Deserialized class is not UserPassCredential")
+        assertEquals("user", credential.user, "User did not match")
+        assertEquals("pass", credential.pass, "Pass did not match")
+    }
+
+    @Test
+    fun `uses Vault api to save a credential`() {
+        mockFuelStore.on(Method.POST, "/v1/secret/$CONNECTION_ID") {
+            MockResponse(200)
+        }
+
+        credentialService.saveCredential(CONNECTION_ID, VALID_CREDENTIAL)
+
+        mockFuelStore.verifyRequest {
+            assertMethod(Method.POST)
+            assertPath("/v1/secret/$CONNECTION_ID")
+            assertHeader("X-Vault-Token", VAULT_TOKEN)
+            assertBody("""{"@type":"UserPass","user":"user1","pass":"pass1"}""")
+        }
+    }
+
+    companion object {
+        private const val VAULT_API_ADDR ="http://mock.vault.api"
+        private const val VAULT_TOKEN = "TESTING_VAULT_TOKEN"
+        private const val CONNECTION_ID = "connection-id-1"
+        private val VALID_CREDENTIAL = UserPassCredential("user1", "pass1")
+    }
+}
