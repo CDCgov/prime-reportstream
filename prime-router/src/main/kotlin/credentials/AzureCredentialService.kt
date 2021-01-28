058cbbad5b08cbd7b63931b5f2b0e929b95218b0
Ron Heft <Ronald.K.Heft@omb.eop.gov>
2021-01-28 11:28:33 -0500

    Implements Azure Key Vault version of credentialService

--- prime-router/src/main/kotlin/credentials/AzureCredentialService.kt
+++ prime-router/src/main/kotlin/credentials/AzureCredentialService.kt
@@ -1,12 +1,34 @@
 package gov.cdc.prime.router.credentials
 
+import com.azure.core.credential.TokenCredential
+import com.azure.identity.DefaultAzureCredentialBuilder
+import com.azure.security.keyvault.secrets.SecretClient
+import com.azure.security.keyvault.secrets.SecretClientBuilder
+
 object AzureCredentialService : CredentialService() {
 
+    private const val CREDENTIAL_KEY_PREFIX = "credential/"
+    private val KEY_VAULT_NAME: String by lazy { System.getenv("CREDENTIAL_KEY_VAULT_NAME") ?: "" }
+    private val secretClient by lazy { initSecretClient() }
+
+    internal fun initSecretClient(
+        secretClientBuilder: SecretClientBuilder = SecretClientBuilder(),
+        credential: TokenCredential = DefaultAzureCredentialBuilder().build()
+    ): SecretClient {
+        return secretClientBuilder
+            .vaultUrl("https://$KEY_VAULT_NAME.vault.azure.net")
+            .credential(credential)
+            .buildClient()
+    }
+
     override fun fetchCredential(connectionId: String): Credential? {
-        throw Exception("TO BE IMPLEMENTED IN #295")
+        return secretClient.getSecret("${CREDENTIAL_KEY_PREFIX}$connectionId")?.let {
+            return Credential.fromJSON(it.value)
+        }
     }
 
     override fun saveCredential(connectionId: String, credential: Credential) {
-        throw Exception("TO BE IMPLEMENTED IN #295")
+        secretClient.setSecret("${CREDENTIAL_KEY_PREFIX}$connectionId", credential.toJSON())
+            ?: throw Exception("Failed to save credentials for: $connectionId")
     }
 }
