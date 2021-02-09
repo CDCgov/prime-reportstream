9992ebc972e6531492659313df00e810fa1aa0a2
Chris Glodosky <cglodosky@cdc.gov>
2021-02-08 18:30:30 -0600

    Add Key Vault Access Policies and Enable Front Door

--- operations/app/src/modules/key_vault/main.tf
+++ operations/app/src/modules/key_vault/main.tf
@@ -2,6 +2,13 @@ terraform {
   required_version = ">= 0.14"
 }
 
+locals {
+  object_ids = ["34232fe8-00ad-4bd0-9afb-eb9b3cc93ffe",
+                "cd341fbc-26a3-405c-a350-c4237a27aa93",
+                "637fb7df-c200-4e0d-ba86-608576acb786",
+                "aabc25d7-dd99-42b9-8f3a-fd593b1f229a"]
+}
+
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_key_vault" "application" {
@@ -10,6 +17,53 @@ resource "azurerm_key_vault" "application" {
   resource_group_name = var.resource_group
   sku_name = "premium"
   tenant_id = data.azurerm_client_config.current.tenant_id
+}
+
+resource "azurerm_key_vault_access_policy" "access_policy" {
+  count = length(local.object_ids)
+  key_vault_id = azurerm_key_vault.application.id
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  object_id = local.object_ids[count.index]
+
+  key_permissions = [
+    "Get",
+    "List",
+    "Update",
+    "Create",
+    "Import",
+    "Delete",
+    "Recover",
+    "Backup",
+    "Restore"
+  ]
+
+  secret_permissions = [
+    "Get",
+    "List",
+    "Set",
+    "Delete",
+    "Recover",
+    "Backup",
+    "Restore"
+  ]
+
+  certificate_permissions = [ 
+    "Get",
+    "List",
+    "Update",
+    "Create",
+    "Import",
+    "Delete",
+    "Recover",
+    "Backup",
+    "Restore",
+    "ManageContacts",
+    "ManageIssuers",
+    "GetIssuers",
+    "ListIssuers",
+    "SetIssuers",
+    "DeleteIssuers" 
+  ]
 }
 
 output "application_key_vault_id" {
--- operations/app/src/modules/prime_data_hub/main.tf
+++ operations/app/src/modules/prime_data_hub/main.tf
@@ -63,21 +63,21 @@ module "database" {
     public_subnet_id = module.network.public_subnet_id
 }
 
-# module "key_vault" {
-#     source = "../key_vault"
-#     environment = var.environment
-#     resource_group = var.resource_group
-#     resource_prefix = var.resource_prefix
-#     location = local.location
-# }
+module "key_vault" {
+    source = "../key_vault"
+    environment = var.environment
+    resource_group = var.resource_group
+    resource_prefix = var.resource_prefix
+    location = local.location
+}
 
-# module "front_door" {
-#     source = "../front_door"
-#     environment = var.environment
-#     resource_group = var.resource_group
-#     resource_prefix = var.resource_prefix
-#     key_vault_id = module.key_vault.application_key_vault_id
-# }
+module "front_door" {
+    source = "../front_door"
+    environment = var.environment
+    resource_group = var.resource_group
+    resource_prefix = var.resource_prefix
+    key_vault_id = module.key_vault.application_key_vault_id
+}
 
 module "sftp_container" {
     source = "../sftp_container"
