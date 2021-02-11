e678584fac4a466f3660d3649cd14ff1912bec9b
Shane Holloway <shane.w.holloway@omb.eop.gov>
2021-02-11 14:17:55 -0700

    Github workflow CI/CD integration

--- frontend/ci_build.bash
+++ frontend/ci_build.bash
@@ -1,19 +1,51 @@
 #!/bin/bash
 
-echo "Building PRIME Data Hub Frontend using docker build"
-docker build -f ./Dockerfile .
-dkr_image=$(docker build -q -f ./Dockerfile .)
-echo dkr_image $dkr_image
+echo
+echo "Building PRIME Data Hub frontend using docker"
 
-# create temporary docker container to access static website files
-dkr_container=$(docker create $dkr_image)
-echo dkr_container $dkr_container
+if [[ -z "${ACR_REPO}" ]]; then
+  # build locally, but without tagging
 
-echo "Extracting static website"
-docker cp $dkr_container:/usr/app/_site/ ./dist
+  echo
+  docker build . -f ./Dockerfile
 
-# remove temporary docker container
-docker rm -v $dkr_container
+else
+  # build with tag matchin CI/CD of PRIME Data Hub
+  dkr_tag_name="${ACR_REPO}/${PREFIX}_frontend:latest"
 
-echo "Static website files are in ./dist:"
-ls ./dist
+  echo "Tagging static webserver docker image as: ${dkr_tag_name}"
+  echo
+
+  docker build . \
+    --file ./Dockerfile \
+    --target nginx_host \
+    --tag $dkr_tag_name
+
+  # TODO: push the docker image into the ACR_REPO
+  # docker push $dkr_tag_name
+fi
+
+echo
+
+
+if true; then
+  echo
+  echo "Extracting static website files from docker image"
+
+  dkr_site_image=$(docker build . -q --file ./Dockerfile --target static_site)
+
+  # create temporary docker container to access static website files
+  dkr_site_container=$(docker create $dkr_site_image)
+
+  # extracting static website into ./dist
+  docker cp $dkr_site_container:/usr/app/_site/ ./dist
+
+  # remove temporary docker container
+  dkr_site_term=$(docker rm -fv $dkr_site_container)
+
+  echo "Static website files are in ./dist:"
+  ls ./dist
+  echo
+  echo
+fi
+
