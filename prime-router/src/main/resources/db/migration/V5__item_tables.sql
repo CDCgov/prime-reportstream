644d20767f8094d87bb64b5f9c3ebf847d695208
Jim Duff <James.Duff@omb.eop.gov>
2021-02-11 13:50:37 -0500

    changed hub to prime.  Added item stored-procedure.  Created crude end-to-end-test.sh script.

--- prime-router/src/main/resources/db/migration/V5__item_tables.sql
+++ prime-router/src/main/resources/db/migration/V5__item_tables.sql
@@ -30,4 +30,23 @@ CREATE INDEX item_lineage_parent_idx ON item_lineage(parent_report_id, parent_in
 CREATE INDEX item_lineage_child_idx ON item_lineage(child_report_id, child_index);
 CREATE INDEX item_lineage_tracking_id_idx ON item_lineage(tracking_id);
 
+-- Retrieve item_lineage_id that descended from a single report.
+CREATE OR REPLACE FUNCTION item_descendants(start_report_id UUID)
+RETURNS SETOF BIGINT
+AS $$
+DECLARE
+BEGIN
+     RETURN QUERY   
+     WITH RECURSIVE tmp AS (
+          SELECT item_lineage_id as tmp_lineage_id, parent_report_id as tmp_report_id, parent_index as tmp_index
+          FROM item_lineage WHERE parent_report_id = start_report_id   
+       UNION ALL   
+          SELECT IL.item_lineage_id, IL.child_report_id, IL.child_index
+          FROM item_lineage AS IL   
+          JOIN tmp ON (tmp.tmp_report_id = IL.parent_report_id
+          AND tmp.tmp_index = IL.parent_index)
+     )
+     SELECT tmp_lineage_id FROM tmp ORDER BY tmp_lineage_id;
+END; 
+$$  LANGUAGE PLPGSQL;    
 
