081b37aee91bd92750c8c6000a4b59fd3c47492b
Matt <matthew.young@bisonworks.com>
2021-02-18 16:18:13 -0700

    clean up

---
+++ frontend/src/js/login.js
@@ -0,0 +1,33 @@
+export function login() {
+    
+  var jwt = window.sessionStorage.getItem("jwt");
+
+  console.log( `jwt = ${jwt}`)
+
+  if( jwt == null ){
+    let config = {
+    logo: 'https://logo.clearbit.com/cdc.gov',
+    language: 'en',
+    features: {
+      registration: false, // Enable self-service registration flow
+      rememberMe: false, // Setting to false will remove the checkbox to save username
+      router: true, // Leave this set to true for the API demo
+    },
+    el: "#okta-login-container",
+    baseUrl: "https://{{ env.OKTA_baseUrl }}",
+    clientId: "{{ env.OKTA_clientId }}",
+    redirectUri: "{{ env.OKTA_redirect }}",
+    authParams: {
+      issuer: "https://{{ env.OKTA_baseUrl }}/oauth2/default"
+    }
+  };
+  
+  let signInWidget = new OktaSignIn(config);
+  signInWidget
+    .showSignInToGetTokens( { scopes: ['openid', 'email', 'profile', 'simple_report'] } )
+    .then( tokens => {
+      let jwt = tokens.accessToken.value;
+      window.sessionStorage.setItem('jwt', jwt) ; 
+  });
+}
+}
