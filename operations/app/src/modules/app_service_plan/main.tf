39d5401ef54865ef4940e80a487d1ba44c9ce365
Chris Glodosky <cglodosky@cdc.gov>
2021-03-04 14:22:13 -0600

    Set reserved to true for Linux Function App

--- operations/app/src/modules/app_service_plan/main.tf
+++ operations/app/src/modules/app_service_plan/main.tf
@@ -7,6 +7,7 @@ resource "azurerm_app_service_plan" "service_plan" {
   location = var.location
   resource_group_name = var.resource_group
   kind = "Linux"
+  reserved = true
 
   sku {
     tier = "PremiumV2"
