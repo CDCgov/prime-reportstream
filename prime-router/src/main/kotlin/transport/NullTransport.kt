4c4db483138b0845772073d2f4a48981556fe056
Rick Hawes <rhawes@cdc.gov>
2021-03-06 11:55:28 -0800

    A comment

--- prime-router/src/main/kotlin/transport/NullTransport.kt
+++ prime-router/src/main/kotlin/transport/NullTransport.kt
@@ -7,6 +7,9 @@ import gov.cdc.prime.router.TransportType
 import gov.cdc.prime.router.azure.ActionHistory
 import gov.cdc.prime.router.azure.WorkflowEngine
 
+/**
+ * The Null transport is intended for testing and benchmarking purposes.
+ */
 class NullTransport : ITransport {
     override fun send(
         transportType: TransportType,
@@ -16,7 +19,7 @@ class NullTransport : ITransport {
         context: ExecutionContext,
         actionHistory: ActionHistory
     ): RetryItems? {
-        if (header.content == null) error("No content to Null for report ${header.reportFile.reportId}")
+        if (header.content == null) error("No content for report ${header.reportFile.reportId}")
         val receiver = header.receiver ?: error("No receiver defined for report ${header.reportFile.reportId}")
         val fileName = Report.formExternalFilename(header)
         val msg = "Sending to Null Transport"
