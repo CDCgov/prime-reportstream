d3b76324df7f47f973f5cfd4894823d28e5e1fbd
Matt Young <matthew.young-usds@bisonworks.com>
2021-03-22 13:51:29 -0700

    first push

--- frontend/src/js/cards.js
+++ frontend/src/js/cards.js
@@ -10,27 +10,6 @@ function fetchCards(){
             change: -897,
             pct_change: 9.6,
             data: [ 1, 2, 3, 4, 5, 4, 3 ]
-        },        
-        { 
-            id: "positive-cases",
-            title: "Cases" ,
-            subtitle: "Another metric",
-            daily: 329,
-            last: 1294,
-            positive: true,
-            change: -267,
-            pct_change: 20.6,
-            data: [ 1, 2, 3, 4, 5, 4, 3 ]
-        }, {
-            id: "facilities",
-            title: "Facilities" ,
-            subtitle: "Another metric",
-            daily: 4,
-            last: 12,
-            positive: true,
-            change: 4,
-            pct_change: 15.9,
-            data: [ 1, 2, 3, 4, 5, 4, 3 ]    
         }];
 }
 
