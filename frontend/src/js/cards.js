07478ffb839f3790004b9be580393151ce290ddf
Matt Young <matthew.young-usds@bisonworks.com>
2021-04-05 10:57:12 -0700

    changes

--- frontend/src/js/cards.js
+++ frontend/src/js/cards.js
@@ -2,17 +2,14 @@
 async function fetchCards() {
 
     let config = {
-        headers: { 
-            'Authorization': `Bearer ${window.jwt}`
-            }
+        headers: { 'Authorization': `Bearer ${window.jwt}` }
         }
 
-        const response = await Promise.all( [
-            axios.get('http://localhost:7071/api/history/summary/tests', config).then( res => res.data )
-        ] );
+    const response = await Promise.all( [
+        axios.get('http://localhost:7071/api/history/summary/tests', config).then( res => res.data )
+    ] );
 
-        console.log( response );
-        return response;
+    return response;
 };
 
 
--- frontend/src/js/reports.js
+++ frontend/src/js/reports.js
@@ -1,5 +1,3 @@
-
-
 async function fetchReports() 
 {
 
