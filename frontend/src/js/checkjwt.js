7b1f492f8e973309eaaa1d14e55a45722ed1e1bc
Matt Young <matthew.young-usds@bisonworks.com>
2021-04-13 09:40:22 -0700

    fetched org name from db

--- frontend/src/js/checkjwt.js
+++ frontend/src/js/checkjwt.js
@@ -5,8 +5,11 @@ let claims = token? JSON.parse(atob(token.split('.')[1])): null;
 if( !token || !claims || moment().isAfter( moment.unix( claims.exp ) ) )
   window.location.replace( `login.html?return=data-index.html`);
 else{
+  let config = {
+    headers: { 'Authorization': `Bearer ${window.jwt}` }
+    }
   window.org = claims.organization[0];
   window.user = claims.sub;
   window.jwt = token;
-  window.orgName = 'Pima County, AZ'
+
 }
