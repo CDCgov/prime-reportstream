ac2d3a081d0dbf29a3a8a22c038634dc08cd3823
Matt Young <matthew.young-usds@bisonworks.com>
2021-04-13 16:02:47 -0700

    facilities list

--- frontend/src/js/reports.js
+++ frontend/src/js/reports.js
@@ -3,6 +3,6 @@ async function fetchReports() {
     let baseURL = window.location.origin.includes( "localhost") ? "http://localhost:7071" :
     window.location.origin.includes( "staging" ) ? "https://staging.prime.cdc.gov" :
     "https://prime.cdc.gov";     
-    return await axios.get(`${baseURL}/api/history/report`, config);
+    return await axios.get(`${baseURL}/api/history/report`, config).then( res => res.data );
 };
 
