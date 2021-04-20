2ae6b922a4ac4e9a8ac24d65086191808597e4d0
Ron Heft <Ronald.K.Heft@omb.eop.gov>
2021-04-20 17:30:33 -0400

    Grant storage account access to endpoint subnet

--- operations/app/src/modules/storage/main.tf
+++ operations/app/src/modules/storage/main.tf
@@ -14,7 +14,7 @@ resource "azurerm_storage_account" "storage_account" {
   network_rules {
     default_action = "Deny"
     ip_rules = []
-    virtual_network_subnet_ids = [var.public_subnet_id, var.container_subnet_id]
+    virtual_network_subnet_ids = [var.public_subnet_id, var.container_subnet_id, var.endpoint_subnet_id]
   }
 
   # Required for customer-managed encryption
