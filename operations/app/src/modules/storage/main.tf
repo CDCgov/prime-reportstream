2dd88a43eb5eae84f51813112193fec029dc48b0
Ron Heft <Ronald.K.Heft@omb.eop.gov>
2021-04-20 17:29:09 -0400

    Grant endpoint subnet firewall access to partner storage

--- operations/app/src/modules/storage/main.tf
+++ operations/app/src/modules/storage/main.tf
@@ -229,7 +229,7 @@ resource "azurerm_storage_account" "storage_partner" {
   network_rules {
     default_action = "Deny"
     ip_rules = split(",", data.azurerm_key_vault_secret.hhsprotect_ip_ingress.value)
-    virtual_network_subnet_ids = [var.public_subnet_id]
+    virtual_network_subnet_ids = [var.public_subnet_id, var.endpoint_subnet_id]
   }
 
   # Required for customer-managed encryption
