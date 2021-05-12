115a06620b9d881d1858b432f94b1a96f178da28
Ron Heft <Ronald.K.Heft@omb.eop.gov>
2021-05-12 12:23:27 -0400

    Enable alerting for staging

--- operations/app/src/modules/application_insights/main.tf
+++ operations/app/src/modules/application_insights/main.tf
@@ -4,6 +4,7 @@ terraform {
 
 locals{
   ping_url = (var.environment == "prod" ? "https://prime.cdc.gov" : "https://${var.environment}.prime.cdc.gov")
+  alerting_enabled = (var.environment == "prod" || var.environment == "staging" ? 1 : 0)
 }
 
 resource "azurerm_application_insights" "app_insights" {
@@ -18,7 +19,7 @@ resource "azurerm_application_insights" "app_insights" {
 }
 
 resource "azurerm_monitor_action_group" "action_group" {
-  count = (var.environment == "prod" ? 1 : 0)
+  count = local.alerting_enabled
   name = "${var.resource_prefix}-actiongroup"
   resource_group_name = var.resource_group
   short_name = "ReportStream"
@@ -41,7 +42,7 @@ resource "azurerm_monitor_action_group" "action_group" {
 # Severity 4 - Verbose
 
 resource "azurerm_monitor_metric_alert" "availability_alert" {
-  count = (var.environment == "prod" ? 1 : 0)
+  count = local.alerting_enabled
   name = "Degraded Availability"
   resource_group_name = var.resource_group
   scopes = [azurerm_application_insights.app_insights.id]
@@ -67,7 +68,7 @@ resource "azurerm_monitor_metric_alert" "availability_alert" {
 }
 
 resource "azurerm_monitor_metric_alert" "exception_alert" {
-  count = (var.environment == "prod" ? 1 : 0)
+  count = local.alerting_enabled
   name = "Exception(s) Raised"
   resource_group_name = var.resource_group
   scopes = [azurerm_application_insights.app_insights.id]
@@ -93,7 +94,7 @@ resource "azurerm_monitor_metric_alert" "exception_alert" {
 }
 
 resource "azurerm_application_insights_web_test" "ping_test" {
-  count = (var.environment == "prod" ? 1 : 0)
+  count = local.alerting_enabled
   name = "${var.resource_prefix}-pingfunctions"
   location = var.location
   resource_group_name = var.resource_group
