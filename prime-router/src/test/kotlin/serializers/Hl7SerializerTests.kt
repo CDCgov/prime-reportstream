ab1e8785f68437b34aa51af228fe60d71bbf10a0
Carlos Felix <carlos.felix@agile6.com>
2021-05-19 14:24:16 -0400

    Updated phone unit tests

--- prime-router/src/test/kotlin/serializers/Hl7SerializerTests.kt
+++ prime-router/src/test/kotlin/serializers/Hl7SerializerTests.kt
@@ -204,7 +204,7 @@ NTE|1|L|This is a final comment|RE"""
         every { mockSegment.getField(any()) } returns arrayOf(mockFieldType)
         every { mockTerser.get("/.$hl7Field(0)-1") } returns "(555)5555555"
         phoneNumber = serializer.decodeXTNPhoneNumber(mockTerser, element)
-        assertEquals("5555555555", phoneNumber)
+        assertEquals("5555555555:1:", phoneNumber)
 
         // Phone number in newer components.  Will ignore phone number in deprecated component
         every { mockTerser.get("/.$hl7Field(0)-1") } returns "(444)4444444"
@@ -213,7 +213,7 @@ NTE|1|L|This is a final comment|RE"""
         every { mockTerser.get("/.$hl7Field(0)-6") } returns "666"
         every { mockTerser.get("/.$hl7Field(0)-7") } returns "7777777"
         phoneNumber = serializer.decodeXTNPhoneNumber(mockTerser, element)
-        assertEquals("16667777777", phoneNumber)
+        assertEquals("6667777777:1:", phoneNumber)
 
         // No type assumed to be a phone number
         every { mockTerser.get("/.$hl7Field(0)-1") } returns null
@@ -222,7 +222,7 @@ NTE|1|L|This is a final comment|RE"""
         every { mockTerser.get("/.$hl7Field(0)-6") } returns "666"
         every { mockTerser.get("/.$hl7Field(0)-7") } returns "7777777"
         phoneNumber = serializer.decodeXTNPhoneNumber(mockTerser, element)
-        assertEquals("16667777777", phoneNumber)
+        assertEquals("6667777777:1:", phoneNumber)
 
         // A Fax number is not used
         every { mockTerser.get("/.$hl7Field(0)-1") } returns null
@@ -253,7 +253,7 @@ NTE|1|L|This is a final comment|RE"""
         every { mockTerser.get("/.$hl7Field(2)-6") } returns "666"
         every { mockTerser.get("/.$hl7Field(2)-7") } returns "7777777"
         phoneNumber = serializer.decodeXTNPhoneNumber(mockTerser, element)
-        assertEquals("16667777777", phoneNumber)
+        assertEquals("6667777777:1:", phoneNumber)
     }
 
     @Test
