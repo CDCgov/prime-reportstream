a52af01196be1a92412a26f2a0863534194002d7
Thomas D <thomas.delrue@omb.eop.gov>
2021-06-14 13:16:10 -0400

    This commit adds a docker-compose configuration that lets you build the product inside a (standardized) container. The builder container has access to a PostgreSQL This also removes the docker-infrastructure docker-compose file since it is surpassed by this docker-compose.build.yml file.

---
+++ prime-router/build.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+# Wrapper script to aid in building
+
+HERE="$(dirname "$(realpath "${0}")")"
+DOCKER_COMPOSE="${HERE?}/docker-compose.build.yml"
+
+BUILDER_IMAGE_NAME="prime-router_builder"
+
+# Defaults
+ACTION=${ACTION:-builder}
+REBUILD=${REBUILD:-0}
+
+while [[ -n "${1}" && "${1:0:1}" == "-" ]]; do
+    if [[ "${1}" == "--" ]]; then
+        shift
+        break
+    fi
+
+    case "${1}" in
+    "--action" | "-a")
+        # Overwrites the default, last one wins
+        ACTION=${2}
+        shift
+        ;;
+    "--rebuild" | "-r")
+        REBUILD=1
+        ;;
+    *)
+        echo "Unrecognized switch \"${1}\""
+        exit 1
+        ;;
+    esac
+
+    shift
+done
+
+pushd "${HERE?}" 2>&1 1>/dev/null
+
+# Clean if requested
+if [[ ${REBUILD} != 0 ]]; then
+    docker image rm "${BUILDER_IMAGE_NAME?}"
+fi
+
+# Run (which will build if cleaned earlier)
+docker-compose \
+    --file "${DOCKER_COMPOSE?}" \
+    run "${ACTION?}"
+
+popd 2>&1 1>/dev/null
