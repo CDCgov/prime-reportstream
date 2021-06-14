d3166d4894be5be0f521ca74a35374ceac47f35d
Thomas D <thomas.delrue@omb.eop.gov>
2021-06-14 13:32:47 -0400

    Properly formatting the build.sh helper script

--- prime-router/build.sh
+++ prime-router/build.sh
@@ -11,39 +11,39 @@ ACTION=${ACTION:-builder}
 REBUILD=${REBUILD:-0}
 
 while [[ -n "${1}" && "${1:0:1}" == "-" ]]; do
-    if [[ "${1}" == "--" ]]; then
-        shift
-        break
-    fi
-
-    case "${1}" in
-    "--action" | "-a")
-        # Overwrites the default, last one wins
-        ACTION=${2}
-        shift
-        ;;
-    "--rebuild" | "-r")
-        REBUILD=1
-        ;;
-    *)
-        echo "Unrecognized switch \"${1}\""
-        exit 1
-        ;;
-    esac
-
+  if [[ "${1}" == "--" ]]; then
     shift
+    break
+  fi
+
+  case "${1}" in
+  "--action" | "-a")
+    # Overwrites the default, last one wins
+    ACTION=${2}
+    shift
+    ;;
+  "--rebuild" | "-r")
+    REBUILD=1
+    ;;
+  *)
+    echo "Unrecognized switch \"${1}\""
+    exit 1
+    ;;
+  esac
+
+  shift
 done
 
 pushd "${HERE?}" 2>&1 1>/dev/null
 
 # Clean if requested
 if [[ ${REBUILD} != 0 ]]; then
-    docker image rm "${BUILDER_IMAGE_NAME?}"
+  docker image rm "${BUILDER_IMAGE_NAME?}"
 fi
 
 # Run (which will build if cleaned earlier)
 docker-compose \
-    --file "${DOCKER_COMPOSE?}" \
-    run "${ACTION?}"
+  --file "${DOCKER_COMPOSE?}" \
+  run "${ACTION?}"
 
 popd 2>&1 1>/dev/null
