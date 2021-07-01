ee336e2561a087005c88af3fb20ee1b88ce20140
Thomas D <thomas.delrue@omb.eop.gov>
2021-07-01 11:22:20 -0400

    Fix devenv-infrastructure script to bring compositions up in the right order and to properly test for the desired state

--- prime-router/devenv-infrastructure.sh
+++ prime-router/devenv-infrastructure.sh
@@ -6,9 +6,9 @@
 # - down: takes the environment(s) down
 # - stay: does the same as 'up' and then drops into the builder container
 
-STATE=${1:-up}
+STATE=${1:-"up"}
 DETACH=
-if [ "${1}" = "up" ] || [ "${1}" = "stay" ]; then
+if [ "${STATE}" = "up" ] || [ "${STATE}" = "stay" ]; then
   STATE=up
   DETACH=--detach
 fi
@@ -17,12 +17,13 @@ ORPHAN_CONTAINER_WARNING_MSG="Found orphan containers ("
 REMAINING_NETWORK_WARNING_MSG="error while removing network: network prime-router_prime-router_build"
 
 # Bringing things up or down may produce some warnings, some of which we can safely ignore/prevent from being displayed
-docker-compose -f ./docker-compose.yml ${STATE?} ${DETACH?} 2>&1 |
-  grep -v "${ORPHAN_CONTAINER_WARNING_MSG?}" |
-  grep -v "${REMAINING_NETWORK_WARNING_MSG?}"
-
+# Bring up the 'build container' (which has postgresql) up first so that the dependencies can talk to it
 docker-compose -f ./docker-compose.build.yml ${STATE?} ${DETACH?} 2>&1 |
   grep -v "${ORPHAN_CONTAINER_WARNING_MSG?}"
+
+docker-compose -f ./docker-compose.yml ${STATE?} ${DETACH} 2>&1 |
+  grep -v "${ORPHAN_CONTAINER_WARNING_MSG?}" |
+  grep -v "${REMAINING_NETWORK_WARNING_MSG?}"
 
 if [ "${1}" = "stay" ]; then
   ./build.sh -- bash
