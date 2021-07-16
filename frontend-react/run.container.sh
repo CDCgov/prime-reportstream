f37bccd62fd7e8c668008e112399eb3f476ff9bb
Thomas D <thomas.delrue@omb.eop.gov>
2021-07-16 11:40:38 -0400

    frontend-react: adding sample Dockerfile, .dockerignore and a script to run the container locally

---
+++ frontend-react/run.container.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+IMG=${IMG:-prime-fe}
+TAG=${TAG:-local}
+
+pushd "$(dirname "${0}")" 1>/dev/null 2>&1
+
+docker build -t "${IMG?}:${TAG?}" .
+docker run -it -p 3000:3000 "${IMG?}:${TAG?}"
+
+popd 1>/dev/null 2>&1m
