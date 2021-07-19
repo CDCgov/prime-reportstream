06827652ac7128299e6ce80b3a043286adddff65
Tom Osborne <373474+tlosbo@users.noreply.github.com>
2021-07-19 10:46:14 -0400

    adding a constraint to force site_of_care to be null or non empty string

--- prime-router/src/main/resources/db/migration/V13__add_site_of_care_to_test_result_table.sql
+++ prime-router/src/main/resources/db/migration/V13__add_site_of_care_to_test_result_table.sql
@@ -10,4 +10,15 @@ Copy a version of this comment into the next migration
  * This SQL adds 'site_of_care' to the covid_result_metadata table
  */
 
-ALTER TABLE covid_result_metadata ADD COLUMN site_of_care VARCHAR(128);
+ALTER TABLE covid_result_metadata ADD COLUMN site_of_care VARCHAR(128) NULL;
+
+UPDATE
+    covid_result_metadata
+SET
+    site_of_care = null
+WHERE
+    TRIM(site_of_care) = '';
+
+ALTER TABLE covid_result_metadata
+    ADD CONSTRAINT ck_site_of_care
+        CHECK (site_of_care IS NULL OR LENGTH(site_of_care) > 0);
