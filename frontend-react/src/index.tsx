ff3961501f5d717b4ae13aa06c64f21c4b43c06e
Matt Young <matthew.young-usds@bisonworks.com>
2021-07-18 18:02:30 -0700

    added react-snap

--- frontend-react/src/index.tsx
+++ frontend-react/src/index.tsx
@@ -1,20 +1,28 @@
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import reportWebVitals from './reportWebVitals';
 import { CacheProvider } from 'rest-hooks';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(
-  <CacheProvider>
+import { hydrate, render } from "react-dom";
+
+const rootElement = document.getElementById("root");
+if (rootElement && rootElement.hasChildNodes()) {
+  hydrate( <CacheProvider>
     <Router>
       <App />
     </Router>
-  </CacheProvider>,
-  document.getElementById('root')
-);
+  </CacheProvider>,  document.getElementById("root"));
+} else {
+  render( <CacheProvider>
+    <Router>
+      <App />
+    </Router>
+  </CacheProvider>,  document.getElementById("root"));
+}
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
