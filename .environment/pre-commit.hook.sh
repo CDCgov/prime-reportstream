c9ec825719d2da13450ad75c3dd0830cae841109
Thomas D <thomas.delrue@omb.eop.gov>
2021-07-20 14:48:01 -0400

    Fixing pre-commit hook

--- .environment/pre-commit.hook.sh
+++ .environment/pre-commit.hook.sh
@@ -3,13 +3,15 @@ RC=0
 
 # Hop into the root of your repo
 pushd "$(dirname "${0}")/.." 1>/dev/null 2>&1
+REPO_ROOT=$(pwd)/..
 
 RC=0
 CHECKS=(
-    .environment/gitleaks/run-gitleaks.sh
+    ${REPO_ROOT}/.environment/gitleaks/run-gitleaks.sh
 )
 
 echo "> Running pre-commit hooks"
+pushd "${REPO_ROOT?}" 1>/dev/null 2>&1
 for item in ${CHECKS[*]}; do
     echo "   - ${item?}"
     ${item?}
@@ -29,6 +31,7 @@ else
     echo "ERROR>"
 fi
 
+popd 1>/dev/null 2>&1
 popd 1>/dev/null 2>&1
 
 exit ${RC?}
