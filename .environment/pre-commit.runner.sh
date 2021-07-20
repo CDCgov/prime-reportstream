9492bb59531ae86696aea5c1507ce158716666cc
Thomas D <thomas.delrue@omb.eop.gov>
2021-07-20 17:05:03 -0400

    gitleaks: pre-commit runner has better error out handling and now includes a check that the file you are running is indeed marked as +x

--- .environment/pre-commit.runner.sh
+++ .environment/pre-commit.runner.sh
@@ -15,24 +15,42 @@ CHECKS_TO_RUN=(
     ${REPO_ROOT}/.environment/gitleaks/run-gitleaks.sh
 )
 
+function error() {
+    echo "ERROR>"
+    echo "ERROR> ${1}"
+    echo "ERROR>"
+
+    return 1
+}
+
 echo "> Running pre-commit hooks"
-pushd "${REPO_ROOT?}" 1>/dev/null 2>&1
+
 for item in ${CHECKS_TO_RUN[*]}; do
     echo "   - ${item?}"
-    ${item?}
-    NEW_RC=${?}
+    if [[ ! -x "${item?}" ]]; then
+        error "The pre-commit hook script \"${item?}\" is not marked as executable."
 
-    if [ ${RC} == 0 ]; then
-        RC=${NEW_RC?}
+        # contaminate the entire run
+        RC=1
+    else
+        # Invoke the script and capture return code
+        ${item?}
+        NEW_RC=${?}
+
+        # If _our_ return code indicates success (i.e. up and until now everything went well)
+        # then use the last status code as the 'how are we doing so far'-code; which may result
+        # in a contamination.
+        # If we have encountered failures before (i.e. RC != 0), then maintain that contamination
+        if [[ ${RC} == 0 ]]; then
+            RC=${NEW_RC?}
+        fi
     fi
 done
 
 if [ ${RC?} == 0 ]; then
     echo "OK> pre-commit hooks passed!"
 else
-    echo "ERROR>"
-    echo "ERROR> One or more pre-commit hooks failed - the changes will NOT committed!"
-    echo "ERROR>"
+    error "One or more pre-commit hooks failed - the changes will NOT committed!"
 fi
 
 popd 1>/dev/null 2>&1
