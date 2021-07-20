e774ac1b7a886da31d061e895c922ecc41c1f393
Thomas D <thomas.delrue@omb.eop.gov>
2021-07-20 15:30:19 -0400

    gitleaks: enabling the pre-commit runner to run as a standalone invocation

--- .environment/pre-commit.runner.sh
+++ .environment/pre-commit.runner.sh
@@ -1,22 +1,23 @@
 #!/usr/bin/env bash
 RC=0
 
-# Hop into the root of your repo
-REPO_ROOT="$(pwd)"
-REPORTED_ROOT="$(git rev-parse --show-toplevel)"
-if [[ $? != 0 || "${REPORTED_ROOT?}" != "${REPO_ROOT}" ]]; then
-    echo "ERROR: Something is wrong, the pre-commit hook is not running at the root of your repository."
+# Figure out the root of your repo; so that you can run this manually as well
+REPO_ROOT="$(git rev-parse --show-toplevel)"
+if [[ $? != 0 ]]; then
+    echo "ERROR> You must run ${0} from within a git repository work tree!"
     exit 1
 fi
 
-RC=0
-CHECKS=(
+pushd "${REPO_ROOT?}" 1>/dev/null 2>&1
+
+# Add your executable script here if you want them part of the pre-commit hook execution
+CHECKS_TO_RUN=(
     ${REPO_ROOT}/.environment/gitleaks/run-gitleaks.sh
 )
 
 echo "> Running pre-commit hooks"
 pushd "${REPO_ROOT?}" 1>/dev/null 2>&1
-for item in ${CHECKS[*]}; do
+for item in ${CHECKS_TO_RUN[*]}; do
     echo "   - ${item?}"
     ${item?}
     NEW_RC=${?}
@@ -33,5 +34,7 @@ else
     echo "ERROR> One or more pre-commit hooks failed - NOT committed!"
     echo "ERROR>"
 fi
+
+popd 1>/dev/null 2>&1
 
 exit ${RC?}
