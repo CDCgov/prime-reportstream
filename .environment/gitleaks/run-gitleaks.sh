39f89d06c89cb8fb3939dd460f3cc95a4f6ad329
Thomas D <thomas.delrue@omb.eop.gov>
2021-07-28 13:08:18 -0400

    gitleaks: run-gitleaks can now scan _since_ a particular commit too

--- .environment/gitleaks/run-gitleaks.sh
+++ .environment/gitleaks/run-gitleaks.sh
@@ -3,11 +3,25 @@
 function usage() {
     echo "usage: ${0} [OPTION]"
     echo ""
-    echo "Runs the gitleaks container on top of your code. The default mode scans the files that are staged in git."
+    echo "Runs the gitleaks container on top of your code. The default mode (i.e. without any options) scans"
+    echo "the files that are staged in git."
     echo ""
     echo "Options:"
-    echo "    --no-git      Scans your current working directory as is (i.e. pretends it isn't a git repo)"
-    echo "    --help|-h     Shows this help and exits successfully"
+    echo "    --no-git          Scans your current working directory as is (i.e. pretends it isn't a git repo)"
+    echo "    --since <hash>    Scans the history of your repository since the given commit. The hash is inclusive."
+    echo "    --help|-h         Shows this help and exits successfully"
+    echo ""
+    echo "Examples:"
+    echo ""
+    echo "  $ ${0}"
+    echo "      Runs gitleaks over the files that are currently marked as staging"
+    echo ""
+    echo "  $ ${0} --no-git"
+    echo "      Runs gitleaks over the current state of the repository"
+    echo ""
+    echo "  $ VERBOSE=1 ${0} --since abcd1234"
+    echo "      Runs gitleaks on the commits in your repository since abcd1234 while setting the VERBOSE flag"
+    echo ""
     echo ""
 }
 
@@ -54,6 +68,27 @@ function scan_no_git() {
     return ${RC?}
 }
 
+function scan_history() {
+    # Scanning commits works backwards, you specify the youngest as 'from' and the oldest as 'to'
+    COMMIT_FROM="HEAD"
+    COMMIT_TO=${1}
+
+    docker run \
+        -v "${REPO_ROOT?}:${CONTAINER_SOURCE_LOCATION?}" \
+        "${GITLEAKS_IMG_NAME?}" \
+        --path="${CONTAINER_SOURCE_LOCATION?}" \
+        --repo-config-path="${REPO_CONFIG_PATH?}" \
+        --report="${CONTAINER_SOURCE_LOCATION?}/${REPORT_JSON?}" \
+        $(if [[ ${VERBOSE?} != 0 ]]; then echo "--verbose"; else echo ""; fi) \
+        --commit-from=${COMMIT_FROM?} \
+        --commit-to=${COMMIT_TO?} \
+        2>"${LOGFILE?}"
+
+    RC=$?
+
+    return ${RC?}
+}
+
 # Parse arguments
 HAS_UNRECOGNIZED=0
 
@@ -61,11 +96,31 @@ HAS_UNRECOGNIZED=0
 SELECTED_RUNMODE=""
 RUNMODE_STAGED_UNCOMMITTED="uncommitted"
 RUNMODE_NO_GIT="no-git"
+RUNMODE_SINCE="since"
+RUNMODE_SINCE_COMMIT=""
 while [[ ! -z "${1}" ]]; do
     case "${1}" in
-    "--no-git")
+    "--${RUNMODE_SINCE}")
         if [[ ! -z "${SELECTED_RUNMODE?}" ]]; then
-            echo "Warning: Previous run-mode '${SELECTED_RUNMODE?}' will be overridden by latest run-mode '${RUNMODE_NO_GIT?}'"
+            echo "Gitleaks> Warning: Previously specified run-mode '${SELECTED_RUNMODE?}' will be overridden by latest run-mode '${RUNMODE_SINCE?}'."
+        fi
+        SELECTED_RUNMODE="${RUNMODE_SINCE?}"
+
+        # by default, since will be the null commit but you can specify one too
+        # If you specify one, it will _not_ start with dash-dash
+        if [[ -z "${2}" || "${2:0:1}" == "--" ]]; then
+            echo "Gitleaks> ERROR: The commit hash to scan since (for '${RUNMODE_SINCE?}' mode) is not defined."
+            exit 1
+        else
+            RUNMODE_SINCE_COMMIT="${2}"
+
+            # We used an extra argument, shift that
+            shift
+        fi
+        ;;
+    "--${RUNMODE_NO_GIT}")
+        if [[ ! -z "${SELECTED_RUNMODE?}" ]]; then
+            echo "Gitleaks> Warning: Previously specified run-mode '${SELECTED_RUNMODE?}' will be overridden by latest run-mode '${RUNMODE_NO_GIT?}'."
         fi
         SELECTED_RUNMODE="${RUNMODE_NO_GIT?}"
         ;;
@@ -75,7 +130,7 @@ while [[ ! -z "${1}" ]]; do
         ;;
     *)
         # Keep collecting the unrecognized options
-        echo "ERROR> Option \"${1}\" is not a recognized option."
+        echo "Gitleaks> ERROR: Option \"${1}\" is not a recognized option."
         HAS_UNRECOGNIZED=1
         ;;
     esac
@@ -98,13 +153,17 @@ fi
 echo "Gitleaks> Scanning your suggested changes."
 RC=1 # Nothing done, fail
 case "${SELECTED_RUNMODE?}" in
-"${RUNMODE_STAGED_UNCOMMITTED?}")
-    # Default action
-    scan_uncommitted
+"${RUNMODE_SINCE?}")
+    scan_history "${RUNMODE_SINCE_COMMIT?}"
     RC=$?
     ;;
 "${RUNMODE_NO_GIT}")
     scan_no_git
+    RC=$?
+    ;;
+"${RUNMODE_STAGED_UNCOMMITTED?}")
+    # Default action
+    scan_uncommitted
     RC=$?
     ;;
 *)
