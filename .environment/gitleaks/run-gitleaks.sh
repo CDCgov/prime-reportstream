45eced2b09d7ccdc9dfcd112ba9f4b87ee6a512a
Thomas D <thomas.delrue@omb.eop.gov>
2021-07-30 14:01:02 -0400

    gitleaks: adding mode to scan since a time and using that in the run_gitleaks GitHub action
    This also makes the output of the script  a bit nicer and germane

--- .environment/gitleaks/run-gitleaks.sh
+++ .environment/gitleaks/run-gitleaks.sh
@@ -9,6 +9,7 @@ function usage() {
     echo "Options:"
     echo "    --no-git          Scans your current working directory as is (i.e. pretends it isn't a git repo)"
     echo "    --depth <int>     Scans the <int> (topologically) last commits of the repository"
+    echo "    --since <datespc> Scans the commits since a date (format: 'YYYY-mm-DD[THH:MM:SS-OFFSET]'"
     echo "    --help|-h         Shows this help and exits successfully"
     echo ""
     echo "Examples:"
@@ -49,6 +50,7 @@ LOGFILE="gitleaks.log"
 REPO_CONFIG_PATH=".environment/gitleaks/gitleaks-config.toml"
 
 function scan_uncommitted() {
+    note "Scanning your suggested changes."
     # NOTE: ironically, the switch to scan your staged (i.e. to be committed) changes is to use the --unstaged switch
     docker run \
         -v "${REPO_ROOT?}:${CONTAINER_SOURCE_LOCATION?}" \
@@ -65,6 +67,7 @@ function scan_uncommitted() {
 }
 
 function scan_no_git() {
+    note "Scanning the current state of the repository."
     docker run \
         -v "${REPO_ROOT?}:${CONTAINER_SOURCE_LOCATION?}" \
         "${GITLEAKS_IMG_NAME?}" \
@@ -82,6 +85,7 @@ function scan_no_git() {
 
 function scan_x_last_commits() {
     DEPTH=${1}
+    note "Scanning the last ${DEPTH?} commits."
 
     docker run \
         -v "${REPO_ROOT?}:${CONTAINER_SOURCE_LOCATION?}" \
@@ -98,6 +102,25 @@ function scan_x_last_commits() {
     return ${RC?}
 }
 
+function scan_since() {
+    SINCE=${1}
+    note "Scanning all commits since ${SINCE?}."
+
+    docker run \
+        -v "${REPO_ROOT?}:${CONTAINER_SOURCE_LOCATION?}" \
+        "${GITLEAKS_IMG_NAME?}" \
+        --path="${CONTAINER_SOURCE_LOCATION?}" \
+        --repo-config-path="${REPO_CONFIG_PATH?}" \
+        --report="${CONTAINER_SOURCE_LOCATION?}/${REPORT_JSON?}" \
+        $(if [[ ${VERBOSE?} != 0 ]]; then echo "--verbose"; else echo ""; fi) \
+        --commit-since "${SINCE?}" \
+        2>"${LOGFILE?}"
+
+    RC=$?
+
+    return ${RC?}
+}
+
 # Parse arguments
 HAS_UNRECOGNIZED=0
 
@@ -107,27 +130,39 @@ RUNMODE_STAGED_UNCOMMITTED="uncommitted"
 RUNMODE_NO_GIT="no-git"
 RUNMODE_DEPTH="depth"
 RUNMODE_DEPTH_VALUE=""
+RUNMODE_SINCE="since"
+RUNMODE_SINCE_VALUE=""
 while [[ ! -z "${1}" ]]; do
     case "${1}" in
-    "--${RUNMODE_DEPTH}")
+    "--${RUNMODE_DEPTH?}" | "--${RUNMODE_SINCE?}")
         if [[ ! -z "${SELECTED_RUNMODE?}" ]]; then
-            warning "The previously specified run-mode '${SELECTED_RUNMODE?}' will be overridden by the latest run-mode '${RUNMODE_DEPTH?}'."
+            warning "The previously specified run-mode '${SELECTED_RUNMODE?}' will be overridden by the latest run-mode '${1:2}'."
         fi
-        SELECTED_RUNMODE="${RUNMODE_DEPTH?}"
+        SELECTED_RUNMODE="${1:2}"
 
         if [[ -z "${2}" || "${2:0:1}" == "--" ]]; then
-            error "The depth to scan since (for '${RUNMODE_DEPTH?}' mode) is not defined."
+            error "The value for run-mode '${1:2}' is not defined."
             exit 1
         else
-            RUNMODE_DEPTH_VALUE="${2}"
+            case "${SELECTED_RUNMODE?}" in
+            "${RUNMODE_DEPTH?}")
+                RUNMODE_DEPTH_VALUE="${2}"
+                ;;
+            "${RUNMODE_SINCE?}")
+                RUNMODE_SINCE_VALUE="${2}"
+                ;;
+            *)
+                error "'${SELECTED_RUNMODE}' is not yet handled"
+                ;;
+            esac
 
             # We used an extra argument, shift that
             shift
         fi
         ;;
-    "--${RUNMODE_NO_GIT}")
+    "--${RUNMODE_NO_GIT?}")
         if [[ ! -z "${SELECTED_RUNMODE?}" ]]; then
-            warning "The previously specified run-mode '${SELECTED_RUNMODE?}' will be overridden by the latest run-mode '${RUNMODE_NO_GIT?}'."
+            warning "The previously specified run-mode '${SELECTED_RUNMODE?}' will be overridden by the latest run-mode '${1:2}'."
         fi
         SELECTED_RUNMODE="${RUNMODE_NO_GIT?}"
         ;;
@@ -157,7 +192,6 @@ if [[ -z "${SELECTED_RUNMODE?}" ]]; then
     SELECTED_RUNMODE="${RUNMODE_STAGED_UNCOMMITTED?}"
 fi
 
-note "Scanning your suggested changes."
 RC=1 # Nothing done, fail
 case "${SELECTED_RUNMODE?}" in
 "${RUNMODE_DEPTH?}")
@@ -166,6 +200,10 @@ case "${SELECTED_RUNMODE?}" in
     ;;
 "${RUNMODE_NO_GIT}")
     scan_no_git
+    RC=$?
+    ;;
+"${RUNMODE_SINCE?}")
+    scan_since "${RUNMODE_SINCE_VALUE?}"
     RC=$?
     ;;
 "${RUNMODE_STAGED_UNCOMMITTED?}")
