65d432b86fa586a201793e8b33cb4f9f77b33298
Jorge Lopez <jlopez@flexion.us>
2021-08-17 12:14:10 -0700

    Test run, git hook

--- .environment/ktlint/run-ktlintCheck.sh
+++ .environment/ktlint/run-ktlintCheck.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 echo ------------------  Checking format...
-(cd ../../prime-router && ./gradlew ktlintCheck) 2>/dev/null
+(cd ../../prime-router && ./gradlew ktlintCheck) 1>/dev/null
 echo ------------------  Checking finished
