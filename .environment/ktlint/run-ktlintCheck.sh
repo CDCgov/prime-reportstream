0486375c0a87e96edc189bbc87dbde4ad307a1fb
Jorge Lopez <jlopez@flexion.us>
2021-08-17 12:24:42 -0700

    Test Run, git hooks

--- .environment/ktlint/run-ktlintCheck.sh
+++ .environment/ktlint/run-ktlintCheck.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+REPO_ROOT="$(git rev-parse --show-toplevel)"
+
 echo Checking format...
-(cd ../../prime-router && ./gradlew ktlintCheck) 1>/dev/null
+(cd ${REPO_ROOT}/prime-router && ./gradlew ktlintCheck) 1>/dev/null
 echo Checking format finished
