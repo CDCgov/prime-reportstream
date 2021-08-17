8267fae2700a33503ff49a0350f3c0708747ff07
Jorge Lopez <jlopez@flexion.us>
2021-08-17 13:17:32 -0700

    Test run, git hook

--- .environment/ktlint/run-ktlintCheck.sh
+++ .environment/ktlint/run-ktlintCheck.sh
@@ -2,14 +2,15 @@
 
 REPO_ROOT="$(git rev-parse --show-toplevel)"
 RC=0
+errorMessage=""
 
 echo Checking format...
-erromessage=$(cd ${REPO_ROOT}/prime-router/ && ./gradlew ktlintCheck 1>/dev/null) 
+(cd ${REPO_ROOT}/prime-router/ && ./gradlew ktlintCheck 1>/dev/null) 2>$errorMessage
 echo Checking format finished
 
 
 
-if [[ ! -z "$erromessage" ]]; then
+if [[ ! -z "$erroMessage" ]]; then
     RC=1
     echo "ktlint found format violations!"
     echo $erromessage
