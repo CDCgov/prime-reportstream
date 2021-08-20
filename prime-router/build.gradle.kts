1ed59d57c15c6b03548aa3658977fef1cd59d865
Carlos Felix <carlos.felix@agile6.com>
2021-08-20 09:20:28 -0400

    Added comment about storage key in build script

--- prime-router/build.gradle.kts
+++ prime-router/build.gradle.kts
@@ -330,6 +330,7 @@ tasks.azureFunctionsPackage {
 }
 
 tasks.azureFunctionsRun {
+    // This storage account key is not a secret, just a dummy value.
     val devAzureConnectString =
         "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=" +
             "Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=" +
