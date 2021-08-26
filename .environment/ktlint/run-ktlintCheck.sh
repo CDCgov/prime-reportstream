76a0b5c980882ad7d571f0b9c91de8ef11c7e8a6
Thomas D <rwk4@cdc.gov>
2021-08-26 11:39:03 -0400

    #1963: Using a container to invoke and run ktlint with different run modes (--fix, --full, --help)

--- .environment/ktlint/run-ktlintCheck.sh
+++ .environment/ktlint/run-ktlintCheck.sh
@@ -1,39 +1,103 @@
 #!/usr/bin/env bash
 
-
-echo "checking for ktlint code violations..."
-
+KTLINT_IMAGE_NAME="reportstream-ktlint/latest"
 REPO_ROOT=$(git rev-parse --show-toplevel)
-stagedFile=.tmpStagedFiles
-unstagedFile=.tmpUnstagedFiles
-RC=0
+CONTAINER_SOURCE_LOCATION="/src"
+LOG_FILE="ktlint.log"
 
-git diff --cached --name-only --diff-filter=ACM | grep '\.kt[s"]\?$' | sort > $stagedFile
-git diff --name-only --diff-filter=ACM | grep '\.kt[s"]\?$' | sort > $unstagedFile
-
-function error() {
-    echo "Gitleaks> ERROR: Your code has ktlint format violations!"
+function usage() {
+    echo "usage: ${0} [OPTION]"
+    echo ""
+    echo "Checks your code for ktlint violations."
+    echo "By default, this script will only check files that you added or changed (staged and unstaged)."
+    echo ""
+    echo "Options:"
+    echo "    --full|-f     Scans all files (shorthand: lower-case f)"
+    echo "    --fix|-F      Fix violations (shorthand: UPPER-case F)"
+    echo "    --help|-h     Shows this help and exits successfully"
+    echo ""
+    echo "Examples:"
+    echo ""
+    echo "  $ ${0}"
+    echo "      Runs ktlint over the files that are new or modified (ACM in staging or modified)"
+    echo ""
+    echo "  $ ${0} --full"
+    echo "      Runs ktlint over all Kotlin (*.kt) files"
+    echo ""
+    echo "  $ ${0} --full --fix"
+    echo "      Uses ktlint to fix all violations it finds in all kotlin (*.kt) files"
+    echo ""
+    echo ""
 }
 
-# This returns a list of lines that are only in the staged list
-filesToCheck=$(comm -23 $stagedFile $unstagedFile | tr '\n' ' ')
+function error() {
+    echo "ktlint> ERROR: ${*}"
+}
 
-# Delete temp files
-rm -f $stagedFile
-rm -f $unstagedFile
+function warning() {
+    echo "ktlint> Warning: ${*}"
+}
 
-# Run ktlintCheck
-if [ ! -z "$filesToCheck" ]
-then
-	# ktlint needs to be installed in system 
-    # in order to run ktlint command
-    ktlint $filesToCheck
-	if [ $? -ne 0 ]; then
-    error
-    exit 1 
+function note() {
+    echo "ktlint> info: ${*}"
+}
+
+function ensure_ktlint() {
+    # note "Making sure you have the container image."
+    pushd "$(dirname "${0}")" 1>/dev/null 2>&1
+    docker build -t "${KTLINT_IMAGE_NAME?}" . 1>/dev/null
+    popd 1>/dev/null 2>&1
+}
+
+note "Checking for ktlint code violations..."
+
+DO_FORMAT=
+# Default targets, since we assume default runmode
+TARGETS=(
+    $(git diff --cached --name-only --diff-filter=ACM | grep '\.kt\(s\)\?$')
+    $(git diff --name-only --diff-filter=ACM | grep '\.kt\(s\)\?$')
+)
+
+while [[ ! -z "${1}" ]]; do
+    case "${1}" in
+    "--full" | "-f")
+        TARGETS=(
+            $(find -type f -iname "*.kt")
+            $(find -type f -iname "*.kts")
+        )
+        ;;
+    "--fix" | "-F")
+        DO_FORMAT="--format"
+        ;;
+    "--help" | "-h")
+        usage
+        exit 0
+        ;;
+    *) ;;
+
+    esac
+
+    shift
+done
+
+RC=1
+
+if [[ ${#TARGETS[@]} > 0 ]]; then
+    ensure_ktlint
+    docker run \
+        -v "${REPO_ROOT?}:${CONTAINER_SOURCE_LOCATION?}" \
+        --rm \
+        "${KTLINT_IMAGE_NAME?}" \
+        ktlint ${DO_FORMAT?} ${TARGETS[*]} | sed "s/\\${CONTAINER_SOURCE_LOCATION}/\./g" | tee "${LOG_FILE?}" | sed "s/^/    /g"
+    RC=${PIPESTATUS[0]}
+
+    if [[ ${RC?} != 0 ]]; then
+        error "You likely have ktlint violations, check the output or '${LOG_FILE?}' for more information"
+        note "You may be able to fix the violations by invoking '${0} --fix'."
     fi
-	# If no code violations are found, we add the changes
-	git add $filesToCheck
+else
+    note "Skipping this check, there are no target files to scan."
+    RC=0
 fi
 
-exit ${RC}
+exit ${RC?}
