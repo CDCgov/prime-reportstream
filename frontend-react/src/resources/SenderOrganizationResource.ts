9c1eaf6884f4fa02a5af699610c8788000b0c166
Aurora Hampton (github) <aurora.hampton@agile6.com>
2021-09-02 21:13:58 -0700

    1390 - independent okta sender permissions

---
+++ frontend-react/src/resources/SenderOrganizationResource.ts
@@ -0,0 +1,16 @@
+import AuthResource from './AuthResource';
+import SenderAuthResource from "./SenderAuthResource";
+
+export default class SenderOrganizationResource extends SenderAuthResource {
+    readonly name: string = "name";
+    readonly description: string = "description";
+    readonly jurisdiction: string = "jurisdiction";
+    readonly stateCode: string = "state"
+    readonly countyName: string = "county"
+
+    pk(){
+        return this.name;
+    }
+
+    static urlRoot = `${SenderAuthResource.getBaseUrl()}/api/settings/organizations`;
+}
