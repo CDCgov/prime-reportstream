7279c0a8f208dcdc49a6c194f83163693010eef6
Kevin Haube <thekevinhaube@gmail.com>
2021-09-10 11:53:11 -0400

    Changes tabs and filter to use receivingOrgSvc rather than sendingOrg

--- frontend-react/src/controllers/ReportController.ts
+++ frontend-react/src/controllers/ReportController.ts
@@ -1,6 +1,6 @@
 import ReportResource from "../resources/ReportResource";
 
 // This function returns a list of unique senders of any ReportResource[]
-export function getListOfSenders (reports: ReportResource[]): Set<string> {
-    return new Set(reports.map(r => r.sendingOrg));
+export function getUniqueReceiverSvc (reports: ReportResource[]): Set<string> {
+    return new Set(reports.map(r => r.receivingOrgSvc));
 }
