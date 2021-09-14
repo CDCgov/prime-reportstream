7a6ec72d449ec3a7b2216e4f4594374a792655b5
Carlos Felix <carlos.felix@agile6.com>
2021-09-14 10:09:01 -0400

    Simplified regex by only looking at filename for quick tests

--- prime-router/src/testIntegration/kotlin/quicktests/QuickSchemaTranslationTests.kt
+++ prime-router/src/testIntegration/kotlin/quicktests/QuickSchemaTranslationTests.kt
@@ -2,14 +2,15 @@ package gov.cdc.prime.router.tests.quicktests
 
 import assertk.assertThat
 import assertk.assertions.isEqualTo
-import assertk.assertions.isTrue
 import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
 import gov.cdc.prime.router.cli.ProcessData
+import org.apache.commons.io.FilenameUtils
 import org.junit.jupiter.api.DynamicTest
 import org.junit.jupiter.api.TestFactory
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.function.Executable
 import quicktests.QuickTestUtils
+import kotlin.test.assertTrue
 import kotlin.test.fail
 
 /**
@@ -189,10 +190,11 @@ class QuickSchemaTranslationTests {
         override fun execute() {
             generateData()
             assertThat(dataGenerator.outputReportFiles.size).isEqualTo(1)
-            assertThat(
-                Regex(config.expectedOutputFilePattern)
-                    .containsMatchIn(dataGenerator.outputReportFiles[0])
-            ).isTrue()
+            val filename = FilenameUtils.getName(dataGenerator.outputReportFiles[0])
+            assertTrue(
+                Regex(config.expectedOutputFilePattern).containsMatchIn(filename),
+                "Filename $filename does not match regex ${config.expectedOutputFilePattern}"
+            )
         }
     }
 }
