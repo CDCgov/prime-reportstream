773f256841c7970f7b51149c9c97dd2be94d6679
Jorge Lopez <jlopez@flexion.us>
2021-09-16 12:27:43 -0700

    Fixes ktlint code violation

--- prime-router/src/test/kotlin/ElementTests.kt
+++ prime-router/src/test/kotlin/ElementTests.kt
@@ -14,8 +14,7 @@ import java.time.format.DateTimeFormatter
 import kotlin.test.Test
 import kotlin.test.assertFails
 
-internal class
-ElementTests {
+internal class ElementTests {
 
     @Test
     fun `create element`() {
