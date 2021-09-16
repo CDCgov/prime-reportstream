0c54d4954a2e74056764aa42d7857c07a5ba2e13
Carlos Felix <carlos.felix@agile6.com>
2021-09-16 10:51:04 -0400

    Updated gradle plugin to 1.5.30

--- prime-router/build.gradle.kts
+++ prime-router/build.gradle.kts
@@ -25,7 +25,7 @@ import java.time.format.DateTimeFormatter
 import java.util.Properties
 
 plugins {
-    kotlin("jvm") version "1.5.21"
+    kotlin("jvm") version "1.5.30"
     id("org.flywaydb.flyway") version "7.14.0"
     id("nu.studer.jooq") version "6.0"
     id("com.github.johnrengelman.shadow") version "7.0.0"
