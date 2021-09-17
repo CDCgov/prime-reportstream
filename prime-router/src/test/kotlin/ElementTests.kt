ee6693d6e7895ec8d15f23338b01dc745c870100
Jorge Lopez <jlopez@flexion.us>
2021-09-17 08:05:34 -0700

    Takes out test case belonging to NH branch

--- prime-router/src/test/kotlin/ElementTests.kt
+++ prime-router/src/test/kotlin/ElementTests.kt
@@ -485,31 +485,4 @@ internal class ElementTests {
             assertThat(this.truncateIfNeeded("abcde")).isEqualTo("")
         }
     }
-
-    @Test
-    fun `test toFormatted values for UNK`() {
-        val values = ValueSet(
-            "hl70136",
-            system = ValueSet.SetSystem.HL7,
-            values = listOf(
-                ValueSet.Value(code = "Y", display = "Yes"),
-                ValueSet.Value(code = "N", display = "No"),
-                ValueSet.Value(code = "UNK", display = "Unk")
-            )
-        )
-        val element = Element(
-            "a",
-            valueSet = "test",
-            valueSetRef = values,
-            type = Element.Type.CODE,
-            altValues = listOf(
-                ValueSet.Value(code = "Y", display = "Yes"),
-                ValueSet.Value(code = "N", display = "No"),
-                ValueSet.Value(code = "UNK", display = "Unknown")
-            )
-        )
-        element.toFormatted("UNK", "\$system").run {
-            assertThat(this).isEqualTo("NULLFL")
-        }
-    }
 }
