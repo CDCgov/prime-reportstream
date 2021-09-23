733035ccabd7ed5cbbc624a1f65a4443644faa33
Kevin Haube <thekevinhaube@gmail.com>
2021-09-23 11:31:24 -0400

    Suspense added to Details page & Spinner update

--- frontend-react/src/components/Spinner.tsx
+++ frontend-react/src/components/Spinner.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
 import { SpinnerCircular } from 'spinners-react'
 
-function Spinner() {
+interface Props {
+    /* Will this spinner be a full page spinner? Marking this true will center the
+       spinner on the page */
+    fullPage?: boolean
+}
+
+const defaultStyles = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "left"
+}
+
+const fullPageStyles = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "calc(100vh - 230px)"
+}
+
+function Spinner(props: Props) {
     return (
-        <div id="spinner" className="grid-container">
+        <div style={props.fullPage ? fullPageStyles : defaultStyles} className="grid-container">
             <SpinnerCircular
                 color="rgba(57, 88, 172, 1)"
                 size="10%"
