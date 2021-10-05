ef2fd0cedf889f1ca290e12880c462f145c1604d
drew-usds <57154955+drew-usds@users.noreply.github.com>
2021-10-05 10:48:03 -0400

    add uswds logo classes to nav

--- frontend-react/src/components/ReportStreamHeader.tsx
+++ frontend-react/src/components/ReportStreamHeader.tsx
@@ -190,10 +190,14 @@ export const ReportStreamHeader = () => {
     return (
         <Header basic={true}>
             <div className="usa-nav-container">
-                <div className="usa-navbar margin-right-5">
-                    <Title>
-                        <a href="/">ReportStream</a>
-                    </Title>
+                <div className="usa-navbar">
+                    <div className="usa-logo" id="basic-logo">
+                        <Title>
+                            <em className="usa-logo__text font-sans-md">
+                                <a href="/" title="Home" aria-label="Home">ReportStream</a>
+                            </em>
+                        </Title>
+                    </div>
                     <NavMenuButton onClick={toggleMobileNav} label="Menu" />
                 </div>
                 <PrimaryNav 
