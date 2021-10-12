acbab4bd1a5dff6de063df51b2e901fa4942e415
Ron Heft <qwp3@cdc.gov>
2021-10-12 12:06:36 -0400

    Scale app service

--- operations/app/src/modules/app_service_plan/main.tf
+++ operations/app/src/modules/app_service_plan/main.tf
@@ -6,8 +6,8 @@ resource "azurerm_app_service_plan" "service_plan" {
   reserved            = true
 
   sku {
-    tier = "PremiumV2"
-    size = "P2v2"
+    tier = "PremiumV3"
+    size = "P2v3"
   }
 
   tags = {
