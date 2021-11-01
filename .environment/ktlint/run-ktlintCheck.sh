a3b488553b9078104b672b0bf861f25f77ffe4d1
Rick Hawes <rhawes@cdc.gov>
2021-11-01 09:40:46 -0700

    2824: Fix KTLint git hook on a M1 Mac (#2812)
    Use Gradle to run KTLint

--- .environment/ktlint/run-ktlintCheck.sh
+++ .environment/ktlint/run-ktlintCheck.sh
@@ -1,30 +1,22 @@
 #!/usr/bin/env bash
 
-KTLINT_IMAGE_NAME="reportstream-ktlint:latest"
-REPO_ROOT=$(git rev-parse --show-toplevel)
-CONTAINER_SOURCE_LOCATION="/src"
 LOG_FILE="ktlint.log"
 
 function usage() {
     echo "usage: ${0} [OPTION]"
     echo ""
-    echo "Checks your code for ktlint violations."
-    echo "By default, this script will only check files that you added or changed (staged and unstaged)."
+    echo "Checks prime_router for ktlint violations."
     echo ""
     echo "Options:"
-    echo "    --all|-a     Scans all files (shorthand: lower-case a)"
     echo "    --format|-F   Fix violations (shorthand: UPPER-case F)"
     echo "    --help|-h     Shows this help and exits successfully"
     echo ""
     echo "Examples:"
     echo ""
     echo "  $ ${0}"
-    echo "      Runs ktlint over the files that are new or modified (ACM in staging or modified)"
+    echo "      Runs ktlint over the files in prime_report."
     echo ""
-    echo "  $ ${0} --all"
-    echo "      Runs ktlint over all Kotlin (*.kt) files"
-    echo ""
-    echo "  $ ${0} --all --format"
+    echo "  $ ${0} --format"
     echo "      Uses ktlint to fix all violations it finds in all kotlin (*.kt) files"
     echo ""
     echo ""
@@ -42,32 +34,16 @@ function note() {
     echo "ktlint> info: ${*}"
 }
 
-function ensure_ktlint() {
-    # note "Making sure you have the container image."
-    pushd "$(dirname "${0}")" 1>/dev/null 2>&1
-    docker build -t "${KTLINT_IMAGE_NAME?}" . 1>/dev/null
-    popd 1>/dev/null 2>&1
-}
-
 note "Checking for ktlint code violations..."
 
-DO_FORMAT=
-# Default targets, since we assume default runmode
-TARGETS=(
-    $(git diff --cached --name-only --diff-filter=ACM | grep '\.kt\(s\)\?$')
-    $(git diff --name-only --diff-filter=ACM | grep '\.kt\(s\)\?$')
-)
+KTLINT_OPTIONS="ktlintCheck"
+PRIME_ROUTER_DIR="../../prime-router"
+DIR_NAME=$(dirname "$0")
 
 while [[ ! -z "${1}" ]]; do
     case "${1}" in
-    "--all" | "-a")
-        TARGETS=(
-            $(find ./ -type f -iname "*.kt")
-            $(find ./ -type f -iname "*.kts")
-        )
-        ;;
     "--format" | "-F")
-        DO_FORMAT="${1}"
+        KTLINT_OPTIONS="ktlintFormat"
         ;;
     "--help" | "-h")
         usage
@@ -81,23 +57,18 @@ while [[ ! -z "${1}" ]]; do
 done
 
 RC=1
+   
+pushd ${DIR_NAME}
+cd ${PRIME_ROUTER_DIR}
+./gradlew ${KTLINT_OPTIONS} 
+RC=${PIPESTATUS[0]}
+LINT_LOGS=$(find build/reports/ktlint -type f -name "*.txt")
+cat ${LINT_LOGS} > $LOG_FILE
 
-if [[ ${#TARGETS[@]} > 0 ]]; then
-    ensure_ktlint
-    docker run \
-        -v "${REPO_ROOT?}:${CONTAINER_SOURCE_LOCATION?}" \
-        --rm \
-        "${KTLINT_IMAGE_NAME?}" \
-        ktlint ${DO_FORMAT?} ${TARGETS[*]} | sed "s/\\${CONTAINER_SOURCE_LOCATION}/\./g" | tee "${LOG_FILE?}" | sed "s/^/    /g"
-    RC=${PIPESTATUS[0]}
-
-    if [[ ${RC?} != 0 ]]; then
-        error "You likely have ktlint violations, check the output or '${LOG_FILE?}' for more information"
-        note "You may be able to fix the violations by invoking '${0} --format'."
-    fi
-else
-    note "Skipping this check, there are no target files to scan."
-    RC=0
+if [[ ${RC?} != 0 ]]; then
+    error "You likely have ktlint violations, check the output or '${LOG_FILE?}' for more information"
+    note "You may be able to fix the violations by invoking '${0} --format'."
 fi
+popd
 
-exit ${RC?}
+exit ${RC?} 
