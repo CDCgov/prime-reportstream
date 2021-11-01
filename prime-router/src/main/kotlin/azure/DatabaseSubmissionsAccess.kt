b0c272536035ed46a9f1d5e0ee9cbc2db84f98d5
Derek Kinsman <derek@unttld.com>
2021-11-01 10:29:00 -0400

    Remove unused code, get Submissions API producing requirede results.

--- prime-router/src/main/kotlin/azure/DatabaseSubmissionsAccess.kt
+++ prime-router/src/main/kotlin/azure/DatabaseSubmissionsAccess.kt
@@ -1,7 +1,9 @@
 package gov.cdc.prime.router.azure
 
 import gov.cdc.prime.router.azure.db.Tables.ACTION
+import gov.cdc.prime.router.azure.db.enums.TaskAction
 import gov.cdc.prime.router.azure.db.tables.pojos.Action
+import gov.cdc.prime.router.azure.db.tables.records.ActionRecord
 import org.jooq.impl.DSL
 /**
  * Class to access lookup tables stored in the database.
@@ -12,14 +14,13 @@ class DatabaseSubmissionsAccess {
      */
     private val db = DatabaseAccess()
 
-    // TODO: do we actually want to return 'Action' here?
-    // TODO: needs mapping work - Jooq is not working how we need it to here
-    fun fetchSubmissions(): List<Action> {
+    fun fetchSubmissions(sendingOrg: String): List<Action> {
         var submissions = emptyList<Action>()
         db.transact { txn ->
             submissions = DSL.using(txn)
                 .selectFrom(ACTION)
-                // TODO: this is just for testing, we probably don't want to only get 10 records
+                .where(ACTION.ACTION_NAME.eq(TaskAction.receive).and(ACTION.SENDING_ORG.eq(sendingOrg)))
+                // TODO: this is just for testing
                 .limit(10)
                 .fetchInto(Action::class.java)
         }
