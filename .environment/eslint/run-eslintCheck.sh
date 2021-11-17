b2775da0bbb16d5aeced29d5bc03bc153d101ebe
Sean Pennino <sean.pennino@omb.eop.gov>
2021-11-17 10:01:21 -0800

    Updates to auto eslint check

--- .environment/eslint/run-eslintCheck.sh
+++ .environment/eslint/run-eslintCheck.sh
@@ -8,8 +8,9 @@ function js_fmt_check() {
     note "Checking JavaScript formatting."
     MODIFIED_TSX_FILES_COUNT=$(git status --porcelain | grep "\.tsx$" | wc -l)
     MODIFIED_JS_FILES_COUNT=$(git status --porcelain | grep "\.js$" | wc -l)
-    if [[ ${MODIFIED_TSX_FILES_COUNT?} != 0 ]] || [[ ${MODIFIED_JS_FILES_COUNT?} != 0 ]]; then
+    if [ ${MODIFIED_TSX_FILES_COUNT?} != 0 ] || [ ${MODIFIED_JS_FILES_COUNT?} != 0 ]; then
         cd frontend-react
+        yarn install
         npm run lint
         cd ..
     else
