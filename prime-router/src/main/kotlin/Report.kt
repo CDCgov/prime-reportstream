7b75f72426431f49b8181468b98fe102fc0404b5
Ott Sathngam Admin <Ott.Sathngam-A@macbook-pro.attlocal.net>
2021-11-17 07:52:36 -0600

    Updated the comment in getAge

--- prime-router/src/main/kotlin/Report.kt
+++ prime-router/src/main/kotlin/Report.kt
@@ -620,10 +620,10 @@ class Report : Logging {
         return if ((patient_age != null) && patient_age.all { Character.isDigit(it) } && (patient_age.toInt() > 0)) {
             patient_age
         } else {
-            /**
-             * Here, we got invalid or blank patient_age given to us.  Therefore, we will use patient date
-             * of birth and date of speciment collected to calculate the patient's age.
-             */
+            //
+            // Here, we got invalid or blank patient_age given to us.  Therefore, we will use patient date
+            // of birth and date of speciment collected to calculate the patient's age.
+            //
             try {
                 val d = LocalDate.parse(patient_dob, Element.dateFormatter)
                 if (d != null && specimenCollectionDate != null &&
