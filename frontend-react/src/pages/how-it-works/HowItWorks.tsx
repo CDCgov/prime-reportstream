4a19baf7a248b02ce7c295c093db34cff6fc6a1a
kevinhaube <khaube@fearless.tech>
2021-12-09 12:15:43 -0500

    Fixes 404 for How It Works and handles non-extended HIW paths

--- frontend-react/src/pages/how-it-works/HowItWorks.tsx
+++ frontend-react/src/pages/how-it-works/HowItWorks.tsx
@@ -1,5 +1,5 @@
 import { SideNav } from "@trussworks/react-uswds";
-import { NavLink, Route, Switch, useRouteMatch } from "react-router-dom";
+import {NavLink, Redirect, Route, Switch, useRouteMatch} from "react-router-dom";
 
 import { SecurityPractices } from "./SecurityPractices";
 import { ELRChecklist } from "./ElrChecklist";
@@ -7,6 +7,7 @@ import { GettingStarted } from "./GettingStarted";
 import { WhereWereLive } from "./WhereWereLive";
 import { WebReceiverGuide } from "./WebReceiverGuide";
 import { SystemsAndSettings } from "./SystemsAndSettings";
+import {CODES, ErrorPage} from "../error/ErrorPage";
 
 export const HowItWorks = () => {
     let { path, url } = useRouteMatch();
@@ -64,6 +65,10 @@ export const HowItWorks = () => {
                 </div>
                 <div className="tablet:grid-col-8 usa-prose rs-documentation">
                     <Switch>
+                        {/* Handles anyone going to /how-it-works without extension */}
+                        <Route exact path={path}>
+                            <Redirect push to={`${path}/getting-started`} />
+                        </Route>
                         <Route
                             path={`${path}/getting-started`}
                             component={GettingStarted}
@@ -87,6 +92,14 @@ export const HowItWorks = () => {
                         <Route
                             path={`${path}/security-practices`}
                             component={SecurityPractices}
+                        />
+                        {/* Handles any undefined route */}
+                        <Route
+                            render={() => (
+                                <ErrorPage
+                                    code={CODES.NOT_FOUND_404}
+                                />
+                            )}
                         />
                     </Switch>
                 </div>
