cc90756e48f2f92b8b42598661b4182f5752315f
Maurice Reeves <maurice.e.reeves@omb.eop.gov>
2021-12-10 15:46:52 -0500

    Adds organization name to metadata table

---
+++ prime-router/src/main/resources/db/migration/V28__add_organization_name_to_metadata_table.sql
@@ -0,0 +1,18 @@
+/*
+ * The Flyway tool applies this migration to create the database.
+ *
+ * Follow this style guide https://about.gitlab.com/handbook/business-ops/data-team/platform/sql-style-guide/
+ * use VARCHAR(63) for names in organization and schema
+ *
+ * Copy a version of this comment into the next migration
+ *
+ */
+
+
+/*
+ * Add organization name to the table. This relates to the organization name that comes from SimpleReport
+ * and is usually the school district, but it's good to track this as well
+ */
+ALTER TABLE covid_result_metadata
+    ADD organization_name VARCHAR(512) NULL
+;
