b6c80e91e4e5cbc0a22630f3817e566ea200aca8
Sean Pennino <sean.pennino@omb.eop.gov>
2021-12-13 17:51:36 -0800

    Submissions table

--- frontend-react/src/pages/Submissions.tsx
+++ frontend-react/src/pages/Submissions.tsx
@@ -1,15 +1,27 @@
 
+import { Suspense } from "react";
 import { Helmet } from "react-helmet";
 import { NetworkErrorBoundary } from "rest-hooks";
 import { ErrorPage } from "./error/ErrorPage";
 import { useOktaAuth } from "@okta/okta-react";
+import { useOrgName } from "../utils/OrganizationUtils";
 import { GLOBAL_STORAGE_KEYS } from "../components/GlobalContextProvider";
+import moment from "moment";
+
+const OrgName = () => {
+    const orgName: string = useOrgName();
+    return (
+        <span id="orgName" className="text-normal text-base">
+            {orgName}
+        </span>
+    );
+};
 
 function Submissions() {
     const { authState } = useOktaAuth();
     const organization = localStorage.getItem(GLOBAL_STORAGE_KEYS.GLOBAL_ORG);
 
-    let submissions:[]= [];
+    let submissions:[] = [];
     fetch(
         `${process.env.REACT_APP_BACKEND_URL}/api/history/${organization}/submissions`,
         {
@@ -30,7 +42,54 @@ function Submissions() {
             <Helmet>
                 <title>Submissions | {process.env.REACT_APP_TITLE}</title>
             </Helmet>
-            {submissions}
+            <section className="grid-container margin-top-5">
+                <h3 className="margin-bottom-0">
+                    <Suspense
+                        fallback={
+                            <span className="text-normal text-base">
+                                Loading Info...
+                            </span>
+                        }
+                    >
+                        <OrgName />
+                    </Suspense>
+                </h3>
+                <h1 className="margin-top-0 margin-bottom-0">COVID-19</h1>
+            </section>
+            <div className="grid-container usa-section margin-bottom-10">
+                <div className="grid-col-12">
+                    <h2>Test results</h2>
+                    <table className="usa-table usa-table--borderless prime-table" aria-label="Submission history from the last 30 days">
+                        <thead>
+                            <tr>
+                                <th scope="col">Date/time submitted</th>
+                                <th scope="col">File</th>
+                                <th scope="col">Records</th>
+                                <th scope="col">Report ID</th>
+                                <th scope="col">Warnings</th>
+                            </tr>
+                        </thead>
+                        <tbody id="tBody" className="font-mono-2xs">
+                            {submissions.map((s, i) => {
+                                return (
+                                    <tr key={"submission_" + i}>
+                                        <td scope="row">
+                                            {moment
+                                                .utc(s["createdAt"])
+                                                .local()
+                                                .format("YYYY-MM-DD HH:mm")}
+                                        </td>
+                                        <td scope="row"></td> {/* File name */}
+                                        <td scope="row">{s["reportItemCount"]}</td>
+                                        <td scope="row">{s["id"]}</td>
+                                        <td scope="row">{s["warningCount"]}</td>
+                                    </tr>
+                                );
+                            })}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </NetworkErrorBoundary>
     );
 }
