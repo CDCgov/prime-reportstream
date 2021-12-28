0da82f0c3ba5d7256939bc484b19ae4c61173f85
TomNUSDS <74203452+TomNUSDS@users.noreply.github.com>
2021-12-28 11:31:13 -0800

    Network layer refactor (#3572)
    - Refactor localStorage to use sessionStorage and scope calls.
    - Submission requests use useResource
      - Created a SubmissionResource for useResource().
      - Refactored code to use it.
    - `AuthResource` and `SenderAuthResource` uses getStoredOktaToken() instead of useOktaAuth() since React didn't like hooks being called in the class method.
    - `getStoredOrg()` now returns '' if it fails. This simply moves the `|| ''` down into getStoredOrg since everywhere that called it did this.
    - added `getStoredOktaToken()` and `setStoredOktaToken()`  to store the accessToken in sessionStorage (note: this is exactly what the okta component does, so it's no less secure)
    - Login calls `setStoredOktaToken()`
    - Changing organization causes Submissions component to refresh

--- frontend-react/src/components/GlobalContextProvider.tsx
+++ frontend-react/src/components/GlobalContextProvider.tsx
@@ -9,16 +9,20 @@ import { createContext, useContext, useState } from "react";
 enum GLOBAL_STORAGE_KEYS {
     GLOBAL_BASE = "global-context-",
     GLOBAL_ORG = "global-context-org",
+    OKTA_ACCESS_TOKEN = "global-okta-token",
 }
 
 /* INFO
    Updating this default context model will allow us to expand on our
    global context offerings. We could store all kinds of preferences
    in here!
+
+   A change to any of these will trigger components that depend on
+   them to re-render.
 */
 export const GlobalContext = createContext({
     state: {
-        organization: getStoredOrg() || "",
+        organization: getStoredOrg(),
     },
     updateOrganization: (newOrganization: string): void => {
         /* Default placeholder function model */
@@ -38,20 +42,20 @@ export function clearGlobalContext(): void {
     }
 }
 
-export function getStoredOrg(): string | undefined {
-    return sessionStorage.getItem(GLOBAL_STORAGE_KEYS.GLOBAL_ORG) || undefined;
+export function getStoredOrg(): string {
+    return sessionStorage.getItem(GLOBAL_STORAGE_KEYS.GLOBAL_ORG) || "";
 }
 
 export function setStoredOrg(org: string) {
     sessionStorage.setItem(GLOBAL_STORAGE_KEYS.GLOBAL_ORG, org);
 }
 
-function GlobalContextProvider({
+export function GlobalContextProvider({
     children,
 }: {
     children: JSX.Element[];
 }): JSX.Element {
-    const [organization, setOrganization] = useState(getStoredOrg() || "");
+    const [organization, setOrganization] = useState(getStoredOrg());
 
     const updateOrganization = (newOrganization: string): void => {
         setStoredOrg(newOrganization);
@@ -84,4 +88,10 @@ function GlobalContextProvider({
     );
 }
 
-export default GlobalContextProvider;
+export function getStoredOktaToken(): string {
+    return sessionStorage.getItem(GLOBAL_STORAGE_KEYS.OKTA_ACCESS_TOKEN) || "";
+}
+
+export function setStoredOktaToken(value: string) {
+    sessionStorage.setItem(GLOBAL_STORAGE_KEYS.OKTA_ACCESS_TOKEN, value);
+}
