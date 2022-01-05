5eb0213c5a879a70f8ebce29ddf190129214c85c
drew-usds <57154955+drew-usds@users.noreply.github.com>
2022-01-05 01:53:07 -0500

    nest json in sections, update markup to show section headers

--- frontend-react/src/pages/getting-started/testing-facilities/CsvSchemaDocumentation.tsx
+++ frontend-react/src/pages/getting-started/testing-facilities/CsvSchemaDocumentation.tsx
@@ -40,42 +40,68 @@ export const CsvSchemaDocumentation = () => {
                     <div
                         data-testid="fieldDiv"
                         key={`field-${fieldIndex}`}
-                        className="margin-bottom-6"
+                        className="margin-bottom-5 border-top-1px border-base-lighter"
                     >
-                        <h3 id={`doc-${field.colHeader}`}>
-                            {field.name}
-                            {field.required && <span className="text-normal text-xs text-secondary margin-left-2">Required</span>}
-                        </h3>
-                        <div className="margin-top-neg-1 margin-bottom-3">
-                            {field.notes?.map(
-                                (note, noteIndex) => {
-                                    return (
-                                        <p key={`value-${noteIndex}`}>{note}</p>
-                                    );
-                                }
-                            )}
-                        </div>
-                        
-                        <div className="grid-row margin-bottom-05">
-                            <div className="grid-col-4 text-base">Column header</div>
-                            <div className="grid-col-auto">{field.colHeader}</div>
-                        </div>
-                        <div className="grid-row margin-bottom-05">
-                            <div className="grid-col-4 text-base">Value type</div>
-                            <div className="grid-col-auto">{field.valueType}</div>
-                        </div>
-                        <div className="grid-row margin-bottom-05">
-                            <div className="grid-col-4 text-base">Accepted value(s)</div>
-                            <div className="grid-col-8 font-family-mono">
-                                {field.values?.map(
-                                    (value, valueIndex) => {
+                        {field.sections?.map((section, sectionIndex) => {
+                            return (
+                                <div 
+                                    key={`section-${sectionIndex}`}
+                                    className="border-top-1px border-base-lighter margin-top-5 padding-top-1"
+                                >
+                                    <h3>{section.title}</h3>
+
+                                    {section.items?.map((item, itemIndex) => {
                                         return (
-                                            <><span key={`value-${valueIndex}`}>{value}</span><br /></>
-                                        );
-                                    }
-                                )}
-                            </div>
-                        </div>
+                                            <div
+                                                key={`item-${itemIndex}`}
+                                                className="margin-top-6"
+                                            >
+                                                
+                                                <h4 id={`doc-${item.colHeader}`}>
+                                                    {item.name}
+                                                    {item.required ? <span className="text-normal bg-white border-1px border-secondary font-body-3xs padding-x-1 padding-y-05 text-secondary margin-left-2 text-ttbottom">Required</span> : <span className="text-normal bg-white border-1px border-base font-body-3xs padding-x-1 padding-y-05 text-base margin-left-2">Optional</span>}
+                                                </h4>
+                                                <div className="margin-bottom-3">
+                                                    {item.notes?.map(
+                                                        (note, noteIndex) => {
+                                                            return (
+                                                                <p key={`value-${noteIndex}`}>{note}</p>
+                                                            );
+                                                        }
+                                                    )}
+                                                </div>                        
+                                                <div className="grid-row margin-bottom-05">
+                                                    <div className="grid-col-4 text-base">Column header</div>
+                                                    <div className="grid-col-auto">{item.colHeader}</div>
+                                                </div>
+                                                <div className="grid-row margin-bottom-05 border-base-lighter border-top-1px padding-top-1">
+                                                    <div className="grid-col-4 text-base">Value type</div>
+                                                    <div className="grid-col-auto">{item.valueType}</div>
+                                                </div>
+                                                <div className="grid-row margin-bottom-05 border-base-lighter border-top-1px padding-top-1">
+                                                    <div className="grid-col-4 text-base">
+                                                        {item.acceptedFormat && <>Accepted format</>}
+                                                        {item.acceptedValues && <>Accepted value(s)</>}
+                                                        {item.acceptedExample && <>Example(s)</>}
+                                                    </div>
+                                                    <div className="grid-col-8">
+                                                        {item.values?.map(
+                                                            (value, valueIndex) => {
+                                                                return (
+                                                                    <><span key={`value-${valueIndex}`}>{value}</span><br /></>
+                                                                );
+                                                            }
+                                                        )}
+                                                    </div>
+                                                </div>
+                                                
+                                            </div>
+                                        )
+                                    })}
+                                </div>
+                            );
+                        })}
+ 
                     </div>
                 );
             })}
