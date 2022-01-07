151855bb1ec608482a53de10594a33ba306a1236
drew-usds <57154955+drew-usds@users.noreply.github.com>
2022-01-07 13:45:00 -0500

    flesh out placehoder text from csv upload guide

--- frontend-react/src/pages/getting-started/testing-facilities/CsvUploadGuide.tsx
+++ frontend-react/src/pages/getting-started/testing-facilities/CsvUploadGuide.tsx
@@ -21,7 +21,11 @@ export const CsvUploadGuide = () => {
                 <ul>
                     <li><a href="#preparing-a-csv" className="usa-link">How to prepare a CSV file for ReportStream</a></li>
                     <li><a href="#upload-a-csv" className="usa-link">How to upload a CSV file to ReportStream</a></li>
-                </ul>           
+                </ul>  
+                <p><strong>Resources</strong></p> 
+                <ul>
+                    <li><a href="/assets/csv/ReportStream-StandardCSV-ExampleData-20220107.csv" className="usa-link">ReportStream standard CSV with example data</a></li>
+                </ul>          
             </section>
             <section>
                 <h3 id="preparing-a-csv" className="font-body-lg border-top-1px border-ink margin-top-8 margin-bottom-6 padding-top-1">Preparing a CSV file</h3>
@@ -31,78 +35,70 @@ export const CsvUploadGuide = () => {
                             Download the ReportStream reference CSV and review the documentation
                         </h4>
                         <p className="margin-top-05">
-                            [If your jurisdiction already has a CSV, compare it to our reference file/documentation. If you're starting from scratch, use the reference CSV as a template].
-                        </p>
-                        <p>
-                            [Download the ReportStream reference CSV schema file LINK].
-                        </p>
-                        <p>
-                            [Review the CSV documentation].
+                            If your jurisdiction already has a set format for CSV, compare it to our <a href="/assets/csv/ReportStream-StandardCSV-ExampleData-20220107.csv" className="usa-link">standard CSV file</a> and <a href="/getting-started/testing-facilities/csv-schema"> CSV schema documentation</a>.</p>
+                            <p>If you're starting from scratch, use the standard CSV as a template.
                         </p>
                     </li>
                     <li className="usa-process-list__item">
-                        <h4 className="usa-process-list__heading">Format your CSV to match the ReportStream reference file</h4>
+                        <h4 className="usa-process-list__heading">Format your column headers to match the ReportStream standard CSV</h4>
                         <p>
-                            [Whether you're modifying an existing file or creating a new one from scratch, Include all column headers underneath “ReportStream CSV Field Names” (from the documentation)) in your CSV file, even if you don’t have the data. Make sure values are written exactly as displayed.]
-                        </p>
+                            Whether you're modifying an existing file or creating a new one from scratch, be sure to include all column headers outlined in the standard file and documentation. Do not include any additional column headers. Make sure headers are written exactly as defined.</p>
+                            <p>Include column headers even if you don’t have data for every field.</p>
                     </li>
                     <li className="usa-process-list__item">
                         <h4 className="usa-process-list__heading">Enter values into your CSV file</h4>
                         <p>
-                            [Follow instructions (from the documentation) on how to modify your existing CSV template headers with ReportStream field names, and how to enter correct values underneath each ReportStream Field ]
+                            Using the <a href="/getting-started/testing-facilities/csv-schema"> CSV schema documentation</a> as a guide, enter properly formatted values in the relevant fields.
                         </p>
-                        <p>[CSV Column Names” = Displays the column headers used by ReportStream (make sure values are written exactly as displayed below)] </p>
-                        <p>[Required fields:Displays whether a value is required (Yes) , requested (Requested) or optional (No) for the ReportStream CSV field ]</p>
-                        <p>[Data Requirements for User” = Displays a required or recommended value a user must enter to be accepted by ReportStream (our guidance considers HHS data requirements and standard data accepted by the majority of state, tribal, local, or territorial public health agencies we’ve worked with).]</p>
                     </li>
                     <li className="usa-process-list__item">
                         <h4 className="usa-process-list__heading">Export your CSV</h4>
                         <p>
-                            [Export your properly formatted CSV with filled-in data. Export as CSV, NOT XLS, XLXS, or other formats.]
+                            Export your properly formatted CSV with filled-in data.</p><p><em>Be sure to save your file as .CSV. ReportStream does not accept files saved as .XLS, .XLXS, or other formats.</em>
                         </p>
                     </li>
                 </ol>            
             </section>            
             <section>
                 <h3 id="upload-a-csv" className="font-body-lg border-top-1px border-ink margin-top-8 margin-bottom-6 padding-top-1">Uploading a CSV file</h3>
-                <p>[description of what uploading is in more detail].</p>
                 <ol className="usa-process-list">
                     <li className="usa-process-list__item">
                         <h4 className="usa-process-list__heading">
                             Log in to ReportStream
                         </h4>
-                        <p className="margin-top-05">
-                        Go to https://reportstream.cdc.gov/login
-                           
-                        </p>
-                        <ul>
-                            <li>Go to https://reportstream.cdc.gov/login</li>
+                        <ul className="margin-top-2">
+                            <li>Go to <a href="/login">https://reportstream.cdc.gov/login</a></li>
                             <li>Enter username</li>
                             <li>Enter password</li>
                             <li>Click Sign In</li>
-                        </ul>
-                        <p>
-                            [image].
-                        </p>
-                        
+                        </ul>                        
                     </li>
                     <li className="usa-process-list__item">
                         <h4 className="usa-process-list__heading">Navigate to "Upload"</h4>
-                        <p>
-                            [Foo] [image]
-                        </p>
-                    </li>
+                        <p className="margin-top-2">"Upload" can be found in the main site navigation at the top of the page.</p>
+                    </li>             
                     <li className="usa-process-list__item">
                         <h4 className="usa-process-list__heading">Select file to upload</h4>
-                        <p>
-                            [Option A: Drag your CSV file from your folder to the upload area. Option B: Click “choose from folder” to browse your computer, select CSV file and click Open. ] [Image]
-                        </p>
+                        <ul className="margin-top-2">
+                            <li><strong>Option A:</strong> Drag your CSV file from your folder to the upload area.</li>
+                            <li><strong>Option B:</strong> Click “choose from folder” to browse your computer, select CSV file and click Open.</li>
+                        </ul> 
                     </li>
                     <li className="usa-process-list__item">
                         <h4 className="usa-process-list__heading">Upload the file</h4>
-                        <p>
-                            [Click uplaod] [Image]
+                        <p className="margin-top-2">
+                            Click "Upload."
                         </p>
+                        <p>
+                            <strong>Optional: If you receive errors after clicking "Upload"</strong>
+                        </p>
+                        <p>ReportStream validates all uploaded files against the standard CSV schema. In the event any errors are detected with a file's format or data, the application will alert you to specific changes you need to make before the file can be submitted.</p>
+                        <p>Resolving errors with your CSV:</p>
+                        <ul>
+                            <li>Review the list of errors and recommended changes suggested by ReportStream. Reference the CSV schema documentation again for any adjustments to column headers and values.</li>
+                            <li>Make the recommended changes in your CSV file and re-export it as a .CSV</li>
+                            <li>Return to step 3 and upload your modified file</li>
+                        </ul>
                     </li>
                 </ol> 
             </section>
