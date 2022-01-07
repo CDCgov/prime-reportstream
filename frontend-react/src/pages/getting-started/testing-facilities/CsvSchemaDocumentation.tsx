af9c48f6d85ba545fbe3fce2aa9f8dab95a1fe23
drew-usds <57154955+drew-usds@users.noreply.github.com>
2022-01-07 12:09:00 -0500

    add toc to csv documentation

--- frontend-react/src/pages/getting-started/testing-facilities/CsvSchemaDocumentation.tsx
+++ frontend-react/src/pages/getting-started/testing-facilities/CsvSchemaDocumentation.tsx
@@ -1,7 +1,5 @@
 import { Helmet } from "react-helmet";
-import DOMPurify from "dompurify";
 
-import site from "../../../content/site.json";
 import schema from "../../../content/getting_started_csv_upload.json";
 
 /* eslint-disable jsx-a11y/anchor-has-content */
@@ -17,25 +15,38 @@ export const CsvSchemaDocumentation = () => {
             <section id="anchor-top">
                 <span className="text-base text-italic">Updated: January 2022</span>
                 <h2 className="margin-top-0 ">CSV schema documentation <span className="text-secondary bg-white border-1px border-secondary font-body-3xs padding-x-1 padding-y-05 text-secondary margin-left-2 text-ttbottom">Pilot program </span></h2>
-                <p className="usa-intro text-base">
-                   
-                    {schema.summary}
-
-                </p>
-                <p>Required: explanation of required. Files will fail to be processed if required fields are left blank or contain incorrect values.</p>
-                <p>Requested: explanation of requested</p>
-                <p>Optional: explanation of optinoal</p>
-                <p><a
-                        href={
-                            "mailto:" +
-                            DOMPurify.sanitize(site.orgs.RS.email) +
-                            "?subject=Getting started with ReportStream"
-                        }
-                        className="foo"
-                    >
-                        ReportStream schema CSV
-                    </a>
-                </p>
+                
+                <p><strong>In this guide</strong></p> 
+                <ul>
+                    {schema.fields.map((field, fieldIndex) => {
+                        return (
+                            <div
+                                key={`toc-${fieldIndex}`}
+                                className=""
+                            >
+                                {field.sections?.map((section, sectionIndex) => {
+                                    return (
+                                        <li key={`toc-${fieldIndex}-${sectionIndex}`}>
+                                            <a href={`#${section.slug}`} className="usa-link">
+                                                {section.title}
+                                            </a>
+                                        </li>
+                                    );
+                                })}
+                            </div>
+                        );
+                    })}
+                </ul>
+                <p><strong>Resources</strong></p> 
+                <ul>
+                    <li><a href="#preparing-a-csv" className="usa-link">ReportStream standard schema CSV</a></li>
+                </ul> 
+                <p><strong>About required, requested, and optional fields</strong></p> 
+                <ul>
+                    <li>Required: Files will fail to be processed if required fields are left blank or contain incorrect values.</li>
+                    <li>Requested: explanation of requested</li>
+                    <li>Optional: explanation of optinoal</li>
+                </ul>
                 
             </section>
             
@@ -54,6 +65,7 @@ export const CsvSchemaDocumentation = () => {
                                     className="border-top-1px border-ink margin-top-9"
                                 >
                                     <h3
+                                        id={`${section.slug}`}
                                         className="font-body-lg margin-y-1"
                                     >
                                         {section.title}
@@ -117,7 +129,7 @@ export const CsvSchemaDocumentation = () => {
                                 </div>
                             );
                         })}
- 
+                        <p><a href="#anchor-top" className="usa-link">Return to top</a></p>
                     </div>
                 );
             })}
