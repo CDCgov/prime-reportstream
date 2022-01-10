4ed1d4055fd4d8eab1af46f8358d630f8d1aded1
Maurice Reeves <74194189+MauriceReeves-usds@users.noreply.github.com>
2022-01-10 09:34:00 -0500

    Add flag to swap positive offset to negative offset for UTC date time (#3697)
    * Adds ability to flip a positive zero offset to a negative zero offset
    
    * Fixes ktlint violation
    
    * Adds test to the offset conversion logic
    
    * Adds flag to the translation config
    
    * Adds logic in the HL7 serializer to include offset conversion
    
    * Fix issue with edge case for offset only values and more tests
    
    * Removed old todo comment

--- prime-router/src/test/kotlin/ElementTests.kt
+++ prime-router/src/test/kotlin/ElementTests.kt
@@ -241,6 +241,82 @@ internal class ElementTests {
         two.getDate(instant, dateFormat).run {
             assertThat(this).isEqualTo("2020-12-03T13:30:30Z")
         }
+
+        // now let's check some other date formats
+        val parser = DateTimeFormatter.ofPattern(
+            "[yyyy-MM-dd'T'HH:mm:ssZ]" +
+                "[yyyy-MM-dd'T'HH:mm:ssxxx]" +
+                "[yyyy-MM-dd'T'HH:mm:ssx]"
+        )
+        // Check OffsetDateTime output format.
+        listOf(
+            "2018-12-12T13:30:30+00:00",
+            "2018-12-12T13:30:30+00",
+            "2018-12-12T13:30:30+0000",
+        ).forEach { date ->
+            val odt = parser.parseBest(date, OffsetDateTime::from, Instant::from)
+            two.getDate(odt, "$dateFormat HH:mm:ss").run {
+                assertThat(this).isEqualTo("2018-12-12 13:30:30")
+            }
+            two.getDate(odt, "$dateFormat HH:mm:ssZZZ").run {
+                assertThat(this).isEqualTo("2018-12-12 13:30:30+0000")
+            }
+            // now check converting the date time to the negative offset
+            two.getDate(odt, "$dateFormat HH:mm:ssZZZ", true).run {
+                assertThat(this).isEqualTo("2018-12-12 13:30:30-0000")
+            }
+        }
+    }
+
+    @Test
+    fun `test convert positive zero offset to negative offset`() {
+        Element("a", type = Element.Type.DATE).run {
+            // all happy path tests
+            Element.convertPositiveOffsetToNegativeOffset("2022-01-05 08:00:00").run {
+                assertThat(this).isEqualTo("2022-01-05 08:00:00")
+            }
+            Element.convertPositiveOffsetToNegativeOffset("2022-01-05 08:00:00+0000").run {
+                assertThat(this).isEqualTo("2022-01-05 08:00:00-0000")
+            }
+            Element.convertPositiveOffsetToNegativeOffset("2022-01-05 08:00:00-0000").run {
+                assertThat(this).isEqualTo("2022-01-05 08:00:00-0000")
+            }
+            Element.convertPositiveOffsetToNegativeOffset("2022-01-05 08:00:00+00").run {
+                assertThat(this).isEqualTo("2022-01-05 08:00:00-00")
+            }
+            Element.convertPositiveOffsetToNegativeOffset("2022-01-05 08:00:00+00:00").run {
+                assertThat(this).isEqualTo("2022-01-05 08:00:00-00:00")
+            }
+            // non-zero offsets
+            Element.convertPositiveOffsetToNegativeOffset("2022-01-05 08:00:00-0400").run {
+                assertThat(this).isEqualTo("2022-01-05 08:00:00-0400")
+            }
+            Element.convertPositiveOffsetToNegativeOffset("2022-01-05 08:00:00+12").run {
+                assertThat(this).isEqualTo("2022-01-05 08:00:00+12")
+            }
+            Element.convertPositiveOffsetToNegativeOffset("2022-01-05 08:00:00+03:30").run {
+                assertThat(this).isEqualTo("2022-01-05 08:00:00+03:30")
+            }
+            // some unhappy paths. Don't use these formats.
+            Element.convertPositiveOffsetToNegativeOffset("2022-01-05 08:00:00+").run {
+                assertThat(this).isEqualTo("2022-01-05 08:00:00+")
+            }
+            Element.convertPositiveOffsetToNegativeOffset("2022+01+05 08:00:00").run {
+                assertThat(this).isEqualTo("2022+01+05 08:00:00")
+            }
+            Element.convertPositiveOffsetToNegativeOffset("").run {
+                assertThat(this).isEqualTo("")
+            }
+            Element.convertPositiveOffsetToNegativeOffset("     ").run {
+                assertThat(this).isEqualTo("     ")
+            }
+            Element.convertPositiveOffsetToNegativeOffset("+0000").run {
+                assertThat(this).isEqualTo("+0000")
+            }
+            Element.convertPositiveOffsetToNegativeOffset("+0000 2022-01-05").run {
+                assertThat(this).isEqualTo("+0000 2022-01-05")
+            }
+        }
     }
 
     @Test
