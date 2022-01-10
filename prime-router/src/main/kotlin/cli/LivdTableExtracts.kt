207b452d7b24105aabafb5f86f1463d6c0cd1649
Ott Sathngam <ott.sathngam@gmail.com>
2022-01-10 17:29:11 -0600

    Add check for duplicate output file.

--- prime-router/src/main/kotlin/cli/LivdTableExtracts.kt
+++ prime-router/src/main/kotlin/cli/LivdTableExtracts.kt
@@ -1,35 +1,21 @@
 package gov.cdc.prime.router.cli
 
 import com.github.ajalt.clikt.core.CliktCommand
-<<<<<<< HEAD
-import com.github.ajalt.clikt.parameters.options.option
-import com.github.ajalt.clikt.parameters.options.required
-import org.apache.commons.io.FilenameUtils
-import org.apache.poi.hssf.usermodel.HSSFWorkbook
-import org.apache.poi.ss.usermodel.CellType
-import org.apache.poi.ss.usermodel.Row
-import org.apache.poi.ss.usermodel.Sheet
-import org.apache.poi.ss.usermodel.Workbook
-import org.apache.poi.xssf.usermodel.XSSFWorkbook
-import java.io.File
-import java.io.FileInputStream
-import java.io.FileOutputStream
-=======
 import com.github.ajalt.clikt.output.TermUi
+import com.github.ajalt.clikt.output.TermUi.prompt
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.required
-import java.io.FileInputStream
-import java.io.FileOutputStream
 import org.apache.commons.io.FilenameUtils
 import org.apache.poi.hssf.usermodel.HSSFWorkbook
 import org.apache.poi.ss.usermodel.CellType
-import org.apache.poi.ss.usermodel.Sheet
 import org.apache.poi.ss.usermodel.Row
+import org.apache.poi.ss.usermodel.Sheet
 import org.apache.poi.ss.usermodel.Workbook
 import org.apache.poi.xssf.usermodel.XSSFWorkbook
 import java.io.File
-
->>>>>>> 9780d2a7f3424144133d6e13a6a0e50d69791329
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.IOException
 
 private const val sheetName = "LOINC Mapping"
 
@@ -59,13 +45,8 @@ class LivdTableExtracks() : CliktCommand(
         metavar = "<path>",
         help = "interpret `--input` relative to this directory and file"
     ).required()
-<<<<<<< HEAD
 
     private val outputFile by option(
-=======
-    
-        private val outputFile by option(
->>>>>>> 9780d2a7f3424144133d6e13a6a0e50d69791329
         "--output-file",
         metavar = "<path>",
         help = "interpret `--output` relative to this directory and file"
@@ -84,74 +65,12 @@ class LivdTableExtracks() : CliktCommand(
          * @param inputfile is the output CSV file name.
          * @return true for success and fales for failure.
          */
-<<<<<<< HEAD
         fun extractsLivdTable(sheetName: String, inputfile: String, outputfile: String): Boolean {
             // Buffer and output file for CSV data
             val data = StringBuffer()
-            val fos = FileOutputStream(File(outputfile))
+            val fileOutputStream = FileOutputStream(File(outputfile))
 
-            val fis = FileInputStream(File(inputfile))
-            var workbook: Workbook? = null
-            val ext: String = FilenameUtils.getExtension(inputfile)
-            if (ext.equals("xlsx", ignoreCase = true)) {
-                workbook = XSSFWorkbook(fis)
-            } else if (ext.equals("xls", ignoreCase = true)) {
-                workbook = HSSFWorkbook(fis)
-            }
-
-            // Get the LOINC Mapping sheet
-            val sheet: Sheet = workbook!!.getSheet(sheetName)
-
-            val rowStart = sheet.getFirstRowNum()
-            val rowEnd = sheet.getLastRowNum()
-
-            for (rowNum in rowStart until rowEnd + 1) {
-                val r: Row = sheet.getRow(rowNum) ?: continue
-
-                val lastColumn: Short = r.lastCellNum
-                for (cn in 0 until lastColumn) {
-                    var delimiterChar = ","
-                    if (cn + 1 == lastColumn.toInt()) delimiterChar = ""
-
-                    val cell = r.getCell(cn, Row.MissingCellPolicy.RETURN_BLANK_AS_NULL)
-
-                    if (cell == null) {
-                        data.append("" + delimiterChar)
-                    } else {
-                        when (cell.cellType) {
-                            CellType.BOOLEAN -> data.append(cell.booleanCellValue.toString() + delimiterChar)
-                            CellType.NUMERIC -> data.append(cell.numericCellValue.toString() + delimiterChar)
-                            CellType.STRING -> {
-                                val stringValue = cell.stringCellValue.replace("*", "")
-                                if (stringValue.contains("\n") || stringValue.contains(",") ||
-                                    (stringValue.contains("\""))
-                                ) {
-                                    // String that contain special character(s)
-                                    data.append(
-                                        "\"" + stringValue.replace("\"", "\"\"") +
-                                            "\"" + delimiterChar
-                                    )
-                                } else {
-                                    data.append(stringValue + delimiterChar)
-                                }
-                            }
-                            CellType.BLANK -> data.append("" + delimiterChar)
-                            else -> data.append("$cell,")
-                        }
-                    }
-                }
-                data.append('\n') // appending new line after each row
-            }
-
-            fos.write(data.toString().toByteArray())
-            fos.close()
-            return true
-=======
-        fun extractsLivdTable(sheetName: String, inputfile: String, outputfile: String) : Boolean {
-            // Buffer and output file for CSV data
-                val data = StringBuffer()
-                val fos = FileOutputStream(File(outputfile))
-
+            try {
                 val fis = FileInputStream(File(inputfile))
                 var workbook: Workbook? = null
                 val ext: String = FilenameUtils.getExtension(inputfile)
@@ -159,6 +78,16 @@ class LivdTableExtracks() : CliktCommand(
                     workbook = XSSFWorkbook(fis)
                 } else if (ext.equals("xls", ignoreCase = true)) {
                     workbook = HSSFWorkbook(fis)
+                }
+
+                // Check for output file duplication
+                if (File(outputfile).exists()) {
+                    val c = prompt("$outputfile file is already existed: You want to overwrite it (y/n)?", "n")
+                    if (c?.lowercase() == "n") {
+                        return false
+                    } else {
+                        TermUi.echo("The $outputfile file is overwriting.\n")
+                    }
                 }
 
                 // Get the LOINC Mapping sheet
@@ -205,10 +134,15 @@ class LivdTableExtracks() : CliktCommand(
                     data.append('\n') // appending new line after each row
                 }
 
-                fos.write(data.toString().toByteArray())
-                fos.close()
+                fileOutputStream.write(data.toString().toByteArray())
+                fileOutputStream.close()
+
+                TermUi.echo("\tSuccess: The $outputfile file is created.")
                 return true
->>>>>>> 9780d2a7f3424144133d6e13a6a0e50d69791329
+            } catch (e: IOException) {
+                TermUi.echo("\tFailure: ${e.message}.")
+                return false
+            }
         }
     }
 }
