e5f548b6892b8b7e31846a6aeba1402dfc0e30ed
David Gage <whytheplatypus@gmail.com>
2022-01-12 09:47:52 -0500

    Update database migration number
    Correct a few interoduced warnings for a now non-null value

--- prime-router/src/testIntegration/kotlin/SimpleReportIntegrationTests.kt
+++ prime-router/src/testIntegration/kotlin/SimpleReportIntegrationTests.kt
@@ -52,7 +52,7 @@ class SimpleReportIntegrationTests {
         assertThat(readResult.errors).isEmpty()
         // I removed this test- at this time, the SimpleReport parsing does return an empty column warning.
         //        assertTrue(readResult.warnings.isEmpty())
-        val inputReport = readResult.report ?: fail()
+        val inputReport = readResult.report
         // 2) Create transformed objects, according to the receiver table rules
         val (outputReports, _) = Translator(metadata, settings).filterAndTranslateByReceiver(inputReport)
 
@@ -110,7 +110,7 @@ class SimpleReportIntegrationTests {
         val inputFileSource = FileSource(inputFilePath)
         val readResult = csvSerializer.readExternal(schema.name, inputFile.inputStream(), inputFileSource)
         assertThat(readResult.errors).isEmpty()
-        val inputReport = readResult.report ?: fail()
+        val inputReport = readResult.report
 
         // 2) Write the input report back out to a new file
         val outputFile = File(outputPath, inputReport.name)
