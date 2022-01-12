1de7a727111d4535c0167ec79ffad2e5dc1d3557
Ott Sathngam <ott.sathngam@gmail.com>
2022-01-12 17:13:24 -0600

    Update code per Carlos's comments

--- prime-router/src/main/kotlin/cli/LivdTableDownload.kt
+++ prime-router/src/main/kotlin/cli/LivdTableDownload.kt
@@ -20,23 +20,33 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook
 import java.io.File
 import java.io.FileInputStream
 import java.io.FileOutputStream
-import java.io.IOException
 import java.net.URL
 
-private const val cdcLOINCTestCodeMappingPage = "https://www.cdc.gov/csels/dls/sars-cov-2-livd-codes.html"
+/**
+ * cdcLOINCTestCodeMappingPageUrl is the CDC URL that contains the LIVD-SARS-CoV-2-yyyyMMdd.xlsx file.
+ */
+private const val cdcLOINCTestCodeMappingPageUrl = "https://www.cdc.gov/csels/dls/sars-cov-2-livd-codes.html"
+
+/**
+ * livdSARSCov2File is the prefix of the LIVD-SARS-CoV-2-yyyyMMdd.xlsx file to download.
+ */
 private const val livdSARSCov2File = "LIVD-SARS-CoV-2"
+
+/**
+ * sheetName is the sheet name with the LIVID-SARS-CoV-2-yyyyMMdd.xlsx downloaded file.
+ */
 private const val sheetName = "LOINC Mapping"
 
 /**
  * LivdTableDownload is the command line interface for the livd-table-download command. It parses the command line
  * for option given as below.
  */
-class LivdTableDownload() : CliktCommand(
+class LivdTableDownload : CliktCommand(
     name = "livd-table-download",
     help = """
     livd-table downloads the latest LOINC test data, so it can be ingested automatically. 
     
-    It looks for the LIVD-SAR-CoV-2-yyyy-MM-dd.xlsx file from $cdcLOINCTestCodeMappingPage.
+    It looks for the LIVD-SAR-CoV-2-yyyy-MM-dd.xlsx file from $cdcLOINCTestCodeMappingPageUrl.
 	If the file is found, it downloads the file into the directory specified by the --output-dir <path> option.
 	If the option is not specified, it will download the file to ./build directory.
         
@@ -50,14 +60,14 @@ class LivdTableDownload() : CliktCommand(
     private val outputDirFile by option(
         "--output-file",
         metavar = "<path/filename>",
-        help = "Directory/Filename for the CSV output file."
+        help = "Output pathname where to store the LIVD data as CSV."
     ).required()
 
     override fun run() {
         val downloadedDirFile = downloadFile(defaultOutputDir)
         if (downloadedDirFile.isEmpty()) return
 
-        extractsLivdTable(sheetName, downloadedDirFile, outputDirFile)
+        extractLivdTable(sheetName, downloadedDirFile, outputDirFile)
     }
 
     /**
@@ -72,13 +82,12 @@ class LivdTableDownload() : CliktCommand(
         //
         // Get the link to the LIVD-SARS-CoV-2-yyyy-MM-dd.xlsx file
         //
-        var livdFile = search(cdcLOINCTestCodeMappingPage, livdSARSCov2File)
+        val livdFile = search(cdcLOINCTestCodeMappingPageUrl, livdSARSCov2File)
         if (livdFile.isEmpty()) {
             TermUi.echo("\tError: unable to find LOINC code data to download!")
             return ""
-        } else {
         }
-        var livdFileUrl = "https://cdc.gov/" + livdFile.get(0)
+        val livdFileUrl = "https://cdc.gov/" + livdFile.get(0)
 
         //
         // Create the local file in the specified directory
@@ -139,94 +148,122 @@ class LivdTableDownload() : CliktCommand(
     }
 
     /**
-     * Extracts LIVD table from sheet [sheetName] of the input Excel formate [inputfile] file, converts to
-     * csv format, and output to the CSV format [outpufile] file.
+     * Extracts LIVD table from sheet [sheetName] of the input Excel format [inputfile] file, converts to
+     * csv format, and output to the CSV format [outputfile] file.
      * @param sheetName is the LOINC Mapping sheet from the downloaded LOINC data code file.
      * @param inputfile is the input Excel file name.
      * @param inputfile is the output CSV file name.
-     * @return true for success and fales for failure.
+     * @return true for success and false for failure.
      */
-    private fun extractsLivdTable(sheetName: String, inputfile: String, outputfile: String): Boolean {
+    private fun extractLivdTable(sheetName: String, inputfile: String, outputfile: String): Boolean {
+        //
+        // Check for input file exist
+        //
+        if (!File(inputfile).exists()) {
+            TermUi.echo("\tERROR: The $inputfile file does not exist.")
+        }
 
-        try {
-            val data = StringBuffer() // Buffer and output file for CSV data
-            val fileInputStream = FileInputStream(File(inputfile))
-            var workbook: Workbook? = null
-            val ext: String = FilenameUtils.getExtension(inputfile)
-            if (ext.equals("xlsx", ignoreCase = true)) {
-                workbook = XSSFWorkbook(fileInputStream)
-            } else if (ext.equals("xls", ignoreCase = true)) {
-                workbook = HSSFWorkbook(fileInputStream)
-            }
+        val data = StringBuffer() // Buffer and output file for CSV data
+        val fileInputStream = FileInputStream(File(inputfile))
+        var workbook: Workbook? = null
+        val ext: String = FilenameUtils.getExtension(inputfile)
+        if (ext.equals("xlsx", ignoreCase = true)) {
+            workbook = XSSFWorkbook(fileInputStream)
+        } else if (ext.equals("xls", ignoreCase = true)) {
+            workbook = HSSFWorkbook(fileInputStream)
+        }
 
-            // Check for output file duplication
-            if (File(outputfile).exists()) {
-                val c = TermUi.prompt("\t$outputfile file is already existed: You want to overwrite it (y/n)?", "n")
-                if (c?.lowercase() == "n") {
-                    return false
-                } else {
-                    TermUi.echo("\tThe $outputfile file is overwriting.")
-                }
-            }
-
-            val fileOutputStream = FileOutputStream(File(outputfile))
-
-            // Get the LOINC Mapping sheet
-            val sheet: Sheet? = workbook!!.getSheet(sheetName)
-            if (sheet == null) {
-                print("\tERROR: Sheet \"$sheetName\" doesn't exist in the $inputfile file.")
+        //
+        // Check for output file duplication
+        //
+        if (File(outputfile).exists()) {
+            val c = TermUi.prompt("\t$outputfile file is already existed: You want to overwrite it (y/n)?", "n")
+            if (c?.lowercase() == "n") {
                 return false
+            } else {
+                TermUi.echo("\tOverwriting the $outputfile file.")
             }
+        }
 
-            val rowStart = sheet.getFirstRowNum()
-            val rowEnd = sheet.getLastRowNum()
+        val fileOutputStream = FileOutputStream(File(outputfile))
 
-            for (rowNum in rowStart until rowEnd + 1) {
-                val row: Row = sheet.getRow(rowNum) ?: continue
-
-                val lastColumn: Short = row.lastCellNum
-                for (cn in 0 until lastColumn) {
-                    var delimiterChar = ","
-                    if (cn + 1 == lastColumn.toInt()) delimiterChar = ""
-
-                    val cell = row.getCell(cn, Row.MissingCellPolicy.RETURN_BLANK_AS_NULL)
-
-                    if (cell == null) {
-                        data.append("" + delimiterChar)
-                    } else {
-                        when (cell.cellType) {
-                            CellType.BOOLEAN -> data.append(cell.booleanCellValue.toString() + delimiterChar)
-                            CellType.NUMERIC -> data.append(cell.numericCellValue.toString() + delimiterChar)
-                            CellType.STRING -> {
-                                val stringValue = cell.stringCellValue.replace("*", "")
-                                if (stringValue.contains("\n") || stringValue.contains(",") ||
-                                    (stringValue.contains("\""))
-                                ) {
-                                    // String that contain special character(s)
-                                    data.append(
-                                        "\"" + stringValue.replace("\"", "\"\"") +
-                                            "\"" + delimiterChar
-                                    )
-                                } else {
-                                    data.append(stringValue + delimiterChar)
-                                }
-                            }
-                            CellType.BLANK -> data.append("" + delimiterChar)
-                            else -> data.append("$cell,")
-                        }
-                    }
-                }
-                data.append('\n') // End of each row
-            }
-
-            fileOutputStream.write(data.toString().toByteArray())
-            fileOutputStream.close()
-
-            TermUi.echo("\tSUCCESS: The $outputfile file is created.\n")
-            return true
-        } catch (e: IOException) {
-            TermUi.echo("\tFAILURE: ${e.message}.\n")
+        //
+        // Get the LOINC Mapping sheet
+        //
+        val sheet: Sheet? = workbook!!.getSheet(sheetName)
+        if (sheet == null) {
+            TermUi.echo("\tERROR: Sheet \"$sheetName\" doesn't exist in the $inputfile file.")
             return false
         }
+
+        val rowStart = sheet.getFirstRowNum() // Get starting row number
+        val rowEnd = sheet.getLastRowNum() // Get ending row number
+
+        //
+        // Start scan each row of the sheet.
+        //
+        for (rowNum in rowStart until rowEnd + 1) {
+            val row: Row = sheet.getRow(rowNum) ?: continue // Skip the empty row.
+
+            //
+            // Scan each column of the sheet
+            //
+            val lastColumn: Short = row.lastCellNum
+            for (cn in 0 until lastColumn) {
+                var delimiterChar = ","
+                if (cn + 1 == lastColumn.toInt()) delimiterChar = "" // Use blank delimiter after the last column
+                //
+                // Get cell object from the sheet.
+                //
+                val cell = row.getCell(cn, Row.MissingCellPolicy.RETURN_BLANK_AS_NULL)
+
+                if (cell == null) {
+                    data.append("" + delimiterChar) // Insert blank if cell is null.
+                } else {
+                    //
+                    // Fill in string csv data according the cell type.
+                    //
+                    when (cell.cellType) {
+                        CellType.BOOLEAN -> data.append(cell.booleanCellValue.toString() + delimiterChar)
+                        CellType.NUMERIC -> data.append(cell.numericCellValue.toString() + delimiterChar)
+                        CellType.STRING -> {
+                            //
+                            // Drop '*' if it is at the end of the string.'
+                            //
+                            val stringValue = if (cell.stringCellValue.last() == '*')
+                                cell.stringCellValue.dropLast(1)
+                            else
+                                cell.stringCellValue
+                            //
+                            // Add " to string that contains "string" (i.e ""string"")
+                            //
+                            if (stringValue.contains("\n") || stringValue.contains(",") ||
+                                (stringValue.contains("\""))
+                            ) {
+                                // String that contain special character(s)
+                                data.append(
+                                    "\"" + stringValue.replace("\"", "\"\"") +
+                                        "\"" + delimiterChar
+                                )
+                            } else {
+                                data.append(stringValue + delimiterChar)
+                            }
+                        }
+                        CellType.BLANK -> data.append("" + delimiterChar)
+                        else -> data.append("$cell,")
+                    }
+                }
+            }
+            data.append(System.lineSeparator()) // End of each row
+        }
+
+        //
+        // Write to CSV file.
+        //
+        fileOutputStream.write(data.toString().toByteArray())
+        fileOutputStream.close()
+
+        TermUi.echo("\tSUCCESS: The $outputfile file is created.\n")
+        return true
     }
 }
