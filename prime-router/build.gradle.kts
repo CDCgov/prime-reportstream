3bcaba59ffbe4e1d7f18496fc9c1eabaeda6e1b8
snesm <94193373+snesm@users.noreply.github.com>
2022-01-21 11:43:35 -0500

    resolve conflict
    khttp:1.0.0 removed in another PR

--- prime-router/build.gradle.kts
+++ prime-router/build.gradle.kts
@@ -645,7 +645,6 @@ dependencies {
 
     implementation("commons-net:commons-net:3.8.0")
     implementation("com.cronutils:cron-utils:9.1.6")
-    implementation("khttp:khttp:1.0.0")
     implementation("com.auth0:java-jwt:3.18.2")
     implementation("io.jsonwebtoken:jjwt-api:0.11.2")
     implementation("de.m3y.kformat:kformat:0.9")
