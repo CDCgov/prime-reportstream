cff85563ea67f7859f76bdb25d258239c142e29c
Kevin Haube <khaube@fearless.tech>
2022-01-31 09:30:05 -0500

    Updated field name to represent back-end change

--- frontend-react/src/pages/Upload.tsx
+++ frontend-react/src/pages/Upload.tsx
@@ -275,7 +275,7 @@ export const Upload = () => {
                                 return (
                                     <tr key={"error_" + i}>
                                         <td>{e["message"]}</td>
-                                        <td>{e["rows"]}</td>
+                                        <td>{e["itemNums"]}</td>
                                     </tr>
                                 );
                             })}
