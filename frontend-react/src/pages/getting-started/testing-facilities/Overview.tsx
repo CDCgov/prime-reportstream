1ec048b3cd7eed841a72e08d9795a47abf18c1fe
TomNUSDS <74203452+TomNUSDS@users.noreply.github.com>
2022-02-08 10:21:55 -0800

    Fix prettier
    `prettier --write --list-different src/**/*.{js,ts,jsx,tsx}`

--- frontend-react/src/pages/getting-started/testing-facilities/Overview.tsx
+++ frontend-react/src/pages/getting-started/testing-facilities/Overview.tsx
@@ -96,9 +96,16 @@ export const FacilitiesOverview = () => {
                             organizations, test manufacturers, and facilities
                             with advanced systems.
                         </p>
-                        <p>For more information, read the 
-                            <a href="https://view.officeapps.live.com/op/view.aspx?src=https%3A%2F%2Fraw.githubusercontent.com%2FCDCgov%2Fprime-reportstream%2Fmaster%2Fprime-router%2Fdocs%2FReportStream-Programmers-Guide-for-Senders.docx" className="usa-link" rel="noopener noreferrer" target="_blank">
-                                ReportStream Programmer's Guide for Testing Facilities and Senders (.docx)
+                        <p>
+                            For more information, read the
+                            <a
+                                href="https://view.officeapps.live.com/op/view.aspx?src=https%3A%2F%2Fraw.githubusercontent.com%2FCDCgov%2Fprime-reportstream%2Fmaster%2Fprime-router%2Fdocs%2FReportStream-Programmers-Guide-for-Senders.docx"
+                                className="usa-link"
+                                rel="noopener noreferrer"
+                                target="_blank"
+                            >
+                                ReportStream Programmer's Guide for Testing
+                                Facilities and Senders (.docx)
                             </a>
                         </p>
                     </li>
