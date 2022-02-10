ea1860ed0831da268fb2ae2a9ac778c0aa86fd36
OTT SATHNGAM <ott.sathngam@gmail.com>
2022-02-10 09:16:50 -0600

    4013: Fixed Fake Data Generator does not output valid site_of_care va… (#4229)
    * 4013: Fixed Fake Data Generator does not output valid site_of_care values
    
    * Response to Mo's comment
    
    Co-authored-by: Maurice Reeves <74194189+MauriceReeves-usds@users.noreply.github.com>

--- prime-router/src/main/kotlin/FakeReport.kt
+++ prime-router/src/main/kotlin/FakeReport.kt
@@ -44,6 +44,11 @@ class FakeDataService : Logging {
                 element.nameContains("name_of_school") -> randomChoice("", context.schoolName)
                 element.nameContains("reference_range") -> randomChoice("", "Normal", "Abnormal", "Negative")
                 element.nameContains("result_format") -> "CWE"
+                element.nameContains("site_of_care") -> randomChoice(
+                    "airport", "assisted_living", "camp", "correctional_facility", "employer", "fqhc",
+                    "government_agency", "hospice", "hospital", "k12", "lab", "nursing_home", "other",
+                    "pharmacy", "primary_care", "shelter", "treatment_center", "university", "urgent_care"
+                )
                 element.nameContains("patient_age_and_units") -> {
                     val unit = randomChoice("months", "years", "days")
                     val value = when (unit) {
