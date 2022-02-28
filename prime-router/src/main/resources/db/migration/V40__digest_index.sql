fc6a261da0de50c38fa893a7b704a1f1e7548b26
Cle Diggins <cle.a.diggins@omb.eop.gov>
2022-02-28 12:56:02 -0800

    add concurrent keyword to index create

--- prime-router/src/main/resources/db/migration/V40__digest_index.sql
+++ prime-router/src/main/resources/db/migration/V40__digest_index.sql
@@ -8,7 +8,7 @@
  *
  */
 
-CREATE INDEX IF NOT EXISTS idx_report_file_blob_digest
-    on report_file
-    using btree(blob_digest)
+CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_report_file_blob_digest
+  on report_file
+  using btree(blob_digest)
 ;
