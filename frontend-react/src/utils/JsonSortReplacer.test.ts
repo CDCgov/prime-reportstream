b422624423657684889869014b0fd7f6d6d6292f
TomNUSDS <74203452+TomNUSDS@users.noreply.github.com>
2022-03-02 12:24:53 -0800

    Update to support sorting arrays.

--- frontend-react/src/utils/JsonSortReplacer.test.ts
+++ frontend-react/src/utils/JsonSortReplacer.test.ts
@@ -5,6 +5,13 @@ test("SubmissionDates have valid format", () => {
         { c: 1, a: { d: 0, c: 1, e: { a: 0, 1: 4 } } },
         jsonSortReplacer
     );
-    debugger;
     expect(result).toBe(`{"a":{"c":1,"d":0,"e":{"1":4,"a":0}},"c":1}`);
+
+    // now try arrays with nested arrays
+    const result2 = JSON.stringify(
+        { c: 1, a: [["c", "b", "a"], 1, 3, ["cc", "bb", "aa"]] },
+        jsonSortReplacer
+    );
+    debugger;
+    expect(result2).toBe(`{"a":[1,3,["a","b","c"],["aa","bb","cc"]],"c":1}`);
 });
