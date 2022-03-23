be492b31df9a657a2f1c37457e25fb8d2bcd4ecb
Kevin Haube <khaube@fearless.tech>
2022-03-23 11:08:05 -0400

    Fixing the org mock server

--- frontend-react/src/__mocks__/OrganizationMockServer.ts
+++ frontend-react/src/__mocks__/OrganizationMockServer.ts
@@ -2,24 +2,18 @@ import { rest } from "msw";
 import { setupServer } from "msw/node";
 
 import { dummySender } from "../hooks/UseSenderMode.test";
+import { orgApi } from "../network/api/OrgApi";
 
-/* There's some .env shuffling going on that makes endpoint generation NOT match the
- * network call triggered by the test suite. Uses test.prime.cdc.gov
- * in the test renderer but the path comes out to localhost if using
- * env vars */
+const firstSender = orgApi.getSenderDetail("firstOrg", "firstSender");
+const testSender = orgApi.getSenderDetail("testOrg", "testSender");
+
 const handlers = [
-    rest.get(
-        `https://test.prime.cdc.gov/api/settings/organizations/testOrg/senders/testSender`,
-        (req, res, ctx) => {
-            return res(ctx.json(dummySender), ctx.status(200));
-        }
-    ),
-    rest.get(
-        `https://test.prime.cdc.gov/api/settings/organizations/firstOrg/senders/firstSender`,
-        (req, res, ctx) => {
-            return res(ctx.status(200));
-        }
-    ),
+    rest.get(testSender.url, (req, res, ctx) => {
+        return res(ctx.json(dummySender), ctx.status(200));
+    }),
+    rest.get(firstSender.url, (req, res, ctx) => {
+        return res(ctx.status(200));
+    }),
 ];
 
 export const orgServer = setupServer(...handlers);
