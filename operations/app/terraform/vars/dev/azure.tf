6c1fcbee8cd456d5237751efb95fc122bbf6f8eb
Josiah Siegel <josiah.siegel@semanticbits.com>
2022-03-25 15:41:45 -0400

    removed duplicate comments

--- operations/app/terraform/vars/dev/azure.tf
+++ operations/app/terraform/vars/dev/azure.tf
@@ -6,7 +6,7 @@ terraform {
       source = "hashicorp/azurerm"
       # This version must also be changed in other environments
       # 3.0 has breaking changes
-      version = ">= 2.61.0, < 3.0.0" # This version must also be changed in other environments
+      version = ">= 2.61.0, < 3.0.0"
     }
   }
 
