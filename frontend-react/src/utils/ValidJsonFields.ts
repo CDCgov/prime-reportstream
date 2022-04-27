a07f80a589574750fcf015b9e0d1b07d5968683a
Kevin Haube <khaube@fearless.tech>
2022-04-27 11:11:57 -0400

    Types to mimic settings API

---
+++ frontend-react/src/utils/ValidJsonFields.ts
@@ -0,0 +1,112 @@
+enum Jurisdiction {
+    FEDERAL = "FEDERAL",
+    STATE = "STATE",
+    COUNTY = "COUNTY",
+}
+
+enum Format {
+    CSV = "text/csv",
+    HL7 = "application/hl7-v2",
+}
+
+enum CustomerStatus {
+    INACTIVE = "inactive",
+    TESTING = "testing",
+    ACTIVE = "active",
+}
+
+enum ProcessingType {
+    SYNC = "sync",
+    ASYNC = "async",
+}
+
+enum SenderType {
+    TEST_MANUFACTURER = "testManufacturer",
+    DATA_AGGREGATOR = "dataAggregator",
+    FACILITY = "facility",
+    HOSPITAL_SYSTEM = "hospitalSystem",
+}
+
+enum PrimarySubmissionMethod {
+    AUTO = "automated",
+    MANUAL = "manual",
+}
+
+/* Alias can be updated with proper RS filter enum
+ * once #4353 is complete */
+type ReportStreamFilter = Array<string>;
+
+interface SettingMetadata {
+    version: number;
+    createdBy: string;
+    createdAt: string; // ISO string date
+}
+
+interface ReportStreamFilters {
+    topic: string;
+    jurisdictionalFilter?: ReportStreamFilter;
+    qualityFilter?: ReportStreamFilter;
+    routingFilter?: ReportStreamFilter;
+    processingModeFilter?: ReportStreamFilter;
+}
+
+interface OrganizationAPI {
+    name: string;
+    description: string;
+    jurisdiction: Jurisdiction;
+    stateCode?: string;
+    countyName?: string;
+    filters?: Array<ReportStreamFilters>;
+    meta?: SettingMetadata;
+}
+
+interface SenderAPI {
+    name: string;
+    organizationName: string;
+    format: Format;
+    topic: string;
+    customerStatus: CustomerStatus;
+    schemaName: string;
+    // keys: Array<string> (?)
+    processingType: ProcessingType;
+    allowDuplicates: boolean;
+    senderType: SenderType;
+    primarySubmissionMethod: PrimarySubmissionMethod;
+    meta?: SettingMetadata;
+}
+
+interface ReceiverAPI {
+    name: string;
+    organizationName: string;
+    description: string;
+    topic: string;
+    customerStatus: CustomerStatus;
+    // translation: any, (?)
+    jurisdictionalFilter: ReportStreamFilters;
+    qualityFilter: ReportStreamFilters;
+    routingFilter: ReportStreamFilters;
+    processingModeFilter: ReportStreamFilters;
+    reverseTheQualityFilter: boolean;
+    deidentify: boolean;
+    // timing: any (?)
+    // transportType: any (?)
+    meta?: SettingMetadata;
+}
+
+export {
+    Jurisdiction,
+    Format,
+    ProcessingType,
+    CustomerStatus,
+    SenderType,
+    PrimarySubmissionMethod,
+};
+
+export type {
+    OrganizationAPI,
+    SenderAPI,
+    ReceiverAPI,
+    SettingMetadata,
+    ReportStreamFilter,
+    ReportStreamFilters,
+};
