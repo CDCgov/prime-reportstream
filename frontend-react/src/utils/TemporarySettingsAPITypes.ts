cfbc978de354481f5820498918a68052b069ffc0
Kevin Haube <khaube@fearless.tech>
2022-04-28 13:24:55 -0400

    Converted interfaces and helper methods to class structures

--- frontend-react/src/utils/TemporarySettingsAPITypes.ts
+++ frontend-react/src/utils/TemporarySettingsAPITypes.ts
@@ -57,112 +57,6 @@ enum USTimeZone {
     CHAMORRO = "Pacific/Guam",
 }
 
-type ReportStreamFilter = Array<ReportStreamFilterDefinition>;
-
-interface Jwk {
-    kty: string;
-    use?: string;
-    keyOps?: string;
-    alg?: string;
-    x5u?: string;
-    x5c?: string;
-    x5t?: string; // certificate thumbprint
-    // Algorithm specific fields
-    n?: string; // RSA
-    e?: string; // RSA
-    d?: string; // EC and RSA private
-    crv?: string; // EC
-    p?: string; // RSA private
-    q?: string; // RSA private
-    dp?: string; // RSA private
-    dq?: string; // RSA private
-    qi?: string; // RSA private
-    x?: string; // EC
-    y?: string; // EC
-    k?: string; // symmetric key, eg oct
-}
-
-interface JwkSet {
-    scope: string;
-    keys: Array<Jwk>;
-}
-
-interface TranslatorProperties {
-    format: Format;
-    schemaName: string;
-    defaults: Map<string, string>;
-    nameFormat: string;
-    receivingOrganization?: string;
-}
-
-interface WhenEmpty {
-    action: EmptyOperation;
-    onlyOncePerDay: boolean;
-}
-
-interface Timing {
-    operation: BatchOperation;
-    numberPerDay: number;
-    initialTime: string;
-    timeZone: USTimeZone;
-    maxReportCount: number;
-    whenEmpty: WhenEmpty;
-}
-
-interface ReportStreamFilters {
-    topic: string;
-    jurisdictionalFilter?: ReportStreamFilter;
-    qualityFilter?: ReportStreamFilter;
-    routingFilter?: ReportStreamFilter;
-    processingModeFilter?: ReportStreamFilter;
-}
-
-interface OrganizationAPI {
-    name: string;
-    description: string;
-    jurisdiction: Jurisdiction;
-    stateCode?: string;
-    countyName?: string;
-    filters?: Array<ReportStreamFilters>;
-}
-
-interface SenderAPI {
-    name: string;
-    organizationName: string;
-    format: Format;
-    topic: string;
-    customerStatus: CustomerStatus;
-    schemaName: string;
-    keys: Array<JwkSet>;
-    processingType: ProcessingType;
-    allowDuplicates: boolean;
-}
-
-interface ReceiverAPI {
-    name: string;
-    organizationName: string;
-    description: string;
-    topic: string;
-    customerStatus: CustomerStatus;
-    translation: TranslatorProperties;
-    jurisdictionalFilter: ReportStreamFilters;
-    qualityFilter: ReportStreamFilters;
-    routingFilter: ReportStreamFilters;
-    processingModeFilter: ReportStreamFilters;
-    reverseTheQualityFilter: boolean;
-    deidentify: boolean;
-    timing?: Timing;
-    // transportType?: any (?)
-}
-
-type ReportStreamFieldType =
-    | "string"
-    | "boolean"
-    | "keys"
-    | "filterObj"
-    | "translation"
-    | "timing";
-
 type ReportStreamSettingsEnum =
     | "jurisdiction"
     | "format"
@@ -182,93 +76,80 @@ const getListOfEnumValues = (e: ReportStreamSettingsEnum): string[] => {
     }
 };
 
-const sampleFilterObj: ReportStreamFilters = {
-    jurisdictionalFilter: [],
-    processingModeFilter: [],
-    qualityFilter: [],
-    routingFilter: [],
-    topic: "",
-};
+abstract class SampleObject {
+    stringify() {
+        return JSON.stringify(this);
+    }
+}
 
-const sampleKeysObj: Array<JwkSet> = [
-    {
-        scope: "",
-        keys: [
-            {
-                kty: "",
-                use: "",
-                keyOps: "",
-                alg: "",
-                x5u: "",
-                x5c: "",
-                x5t: "",
-                n: "",
-                e: "",
-                d: "",
-                crv: "",
-                p: "",
-                q: "",
-                dp: "",
-                dq: "",
-                qi: "",
-                x: "",
-                y: "",
-                k: "",
-            },
-        ],
-    },
-];
+class SampleFilterObject extends SampleObject {
+    topic = "";
+    jurisdictionalFilter = [];
+    qualityFilter = [];
+    routingFilter = [];
+    processingModeFilter = [];
+}
 
-const sampleTimingObj: Timing = {
-    initialTime: "00:00",
-    maxReportCount: 365,
-    numberPerDay: 1,
-    operation: BatchOperation.MERGE,
-    timeZone: USTimeZone.ARIZONA,
-    whenEmpty: {
+class SampleJwkSet {
+    scope = "scope";
+    keys = {
+        kty: "",
+        use: "",
+        keyOps: "",
+        alg: "",
+        x5u: "",
+        x5c: "",
+        x5t: "",
+        n: "",
+        e: "",
+        d: "",
+        crv: "",
+        p: "",
+        q: "",
+        dp: "",
+        dq: "",
+        qi: "",
+        x: "",
+        y: "",
+        k: "",
+    };
+}
+
+class SampleKeysObj extends SampleObject {
+    listOfKeys = [new SampleJwkSet()];
+    stringify(): string {
+        return JSON.stringify(this.listOfKeys);
+    }
+}
+
+class SampleTimingObj extends SampleObject {
+    initialTime = "00:00";
+    maxReportCount = 365;
+    numberPerDay = 1;
+    operation = BatchOperation.MERGE;
+    timeZone = USTimeZone.ARIZONA;
+    whenEmpty = {
         action: EmptyOperation.NONE,
         onlyOncePerDay: true,
-    },
-};
+    };
+}
 
-const sampleTranslationObj: TranslatorProperties = {
-    defaults: new Map<string, string>([["", ""]]),
-    format: Format.CSV,
-    nameFormat: "",
-    receivingOrganization: "xx_phd",
-    schemaName: "schema",
-};
-
-const getSampleValue = (field: ReportStreamFieldType): string => {
-    switch (field) {
-        case "boolean":
-            return "true or false";
-        case "string":
-            return '"a string"';
-        case "filterObj":
-            return JSON.stringify(sampleFilterObj);
-        case "keys":
-            return JSON.stringify(sampleKeysObj);
-        case "timing":
-            return JSON.stringify(sampleTimingObj);
-        case "translation":
-            return JSON.stringify(sampleTranslationObj);
-    }
-};
+class SampleTranslationObj extends SampleObject {
+    defaults = new Map<string, string>([["", ""]]);
+    format = Format.CSV;
+    nameFormat = "";
+    receivingOrganization = "xx_phd";
+    schemaName = "schema";
+}
 
 export {
     Jurisdiction,
     Format,
     ProcessingType,
     CustomerStatus,
+    SampleFilterObject,
+    SampleKeysObj,
+    SampleTranslationObj,
+    SampleTimingObj,
     getListOfEnumValues,
-    getSampleValue,
-};
-
-export type {
-    OrganizationAPI,
-    SenderAPI,
-    ReceiverAPI,
-    ReportStreamFilter,
-    ReportStreamFilters,
 };
