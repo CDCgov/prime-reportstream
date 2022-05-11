cc5eb7c06b4d9a36a452b6ed59dfd23e80ef8192
Brian Gantick <briangantick@navapbc.com>
2022-05-11 10:05:04 -0400

    Add insights config

---
+++ frontend-react/src/utils/AppInsights.ts
@@ -0,0 +1,17 @@
+import { ApplicationInsights } from "@microsoft/applicationinsights-web";
+import { ReactPlugin } from "@microsoft/applicationinsights-react-js";
+import { createBrowserHistory } from "history";
+
+const browserHistory = createBrowserHistory({ basename: '' });
+const reactPlugin = new ReactPlugin();
+const appInsights = new ApplicationInsights({
+  config: {
+    instrumentationKey: process.env.REACT_APP_APPINSIGHTS_KEY,
+    extensions: [reactPlugin],
+    extensionConfig: {
+      [reactPlugin.identifier]: { history: browserHistory },
+    },
+  },
+});
+appInsights.loadAppInsights();
+export { reactPlugin, appInsights };
