c450022f4b8c75cc49a0e05a499401af27c53e0f
Carlos Felix <carlos.felix@agile6.com>
2022-05-18 09:47:49 -0400

    Refactor for unit testing

--- prime-router/src/main/kotlin/fhirengine/utils/CompareFhirData.kt
+++ prime-router/src/main/kotlin/fhirengine/utils/CompareFhirData.kt
@@ -4,6 +4,7 @@ import gov.cdc.prime.router.cli.tests.CompareData
 import org.apache.logging.log4j.kotlin.Logging
 import org.hl7.fhir.r4.model.Base
 import org.hl7.fhir.r4.model.Bundle
+import org.hl7.fhir.r4.model.Property
 import org.hl7.fhir.r4.model.Reference
 import org.hl7.fhir.r4.model.Resource
 import java.io.InputStream
@@ -45,7 +46,7 @@ class CompareFhirData(val result: CompareData.Result = CompareData.Result()) : L
             val resourceMatches = resourcesToCompare[expectedResource]?.all { actualResource ->
                 logger.info("")
                 logger.info("Comparing ${expectedResource.fhirType()}...")
-                compareResource(actualResource, expectedResource)
+                compareResource(actualResource, expectedResource, expectedResource.fhirType())
             }
             logger.info("Resource ${expectedResource.fhirType()} matches = $resourceMatches")
             isEqual = isEqual && (resourceMatches ?: true)
@@ -56,49 +57,30 @@ class CompareFhirData(val result: CompareData.Result = CompareData.Result()) : L
     private fun compareResource(
         actualResource: Base,
         expectedResource: Base,
-        parentTypePath: String = "",
+        parentTypePath: String,
         suppressOutput: Boolean = false
     ): Boolean {
         var isEqual = true
-        var thisTypePath = if (parentTypePath.isNotBlank()) "$parentTypePath." else ""
-        thisTypePath += expectedResource.fhirType()
 
         // logger.info("Comparing resource $thisTypePath ...")
-        expectedResource.children().filter {
-            // Skip any properties we want to ignore
-            !skippedResourceProperties.contains("${expectedResource.fhirType()}.${it.name}")
-        }.forEach { expectedChild ->
+        filterResourceProperties(expectedResource).forEach { expectedChild ->
             val actualChild = actualResource.getChildByName(expectedChild.name)
             if (actualChild != null) {
                 val actualValues = actualChild.values
                 val expectedValues = expectedChild.values
                 // Skip any empty expected properties
                 if (expectedValues.isNotEmpty()) {
+                    val thisTypePath = getFhirTypePath(parentTypePath, expectedChild.name)
                     // Search for a value that is the same in the list of values
                     val isPropertyEqual = expectedValues.all { expectedValue ->
                         // Note that here we look for the first good match, and note we are comparing all values which
                         // we expect only one match.
                         actualValues.any { actualValue ->
-                            when {
-                                // Dissimilar types are not a match.
-                                expectedValue.fhirType() != actualValue.fhirType() -> false
-
-                                // References
-                                expectedValue.hasType("Reference") ->
-                                    compareReference(actualValue as Reference, expectedValue as Reference, thisTypePath)
-
-                                !expectedValue.isPrimitive ->
-                                    compareResource(actualValue, expectedValue, thisTypePath, actualValues.size > 1)
-
-                                // Use the built-in equals for anything else.
-                                else -> {
-                                    expectedValue.equalsDeep(actualValue)
-                                }
-                            }
+                            compareValue(actualValue, expectedValue, thisTypePath, actualValues.size > 1)
                         }
                     }
                     if (!suppressOutput && !isPropertyEqual)
-                        logger.info("Property $thisTypePath.${expectedChild.name} does not match.")
+                        logger.info("Property $thisTypePath does not match.")
                     isEqual = isEqual && isPropertyEqual
                 }
             } else isEqual = false
@@ -111,15 +93,55 @@ class CompareFhirData(val result: CompareData.Result = CompareData.Result()) : L
         return compareResource(actualValue.resource as Base, expectedValue.resource as Base, "")
     }
 
+    private fun compareValue(
+        actualValue: Base,
+        expectedValue: Base,
+        thisTypePath: String = "",
+        suppressOutput: Boolean = false
+    ): Boolean {
+        return when {
+            // Dissimilar types are not a match.
+            expectedValue.fhirType() != actualValue.fhirType() -> false
+
+            // Dynamic values are only checked to exist
+            dynamicResourceProperties.contains(thisTypePath) -> true
+
+            // References
+            expectedValue.hasType("Reference") ->
+                compareReference(actualValue as Reference, expectedValue as Reference, thisTypePath)
+
+            !expectedValue.isPrimitive ->
+                compareResource(actualValue, expectedValue, thisTypePath, suppressOutput)
+
+            // Use the built-in equals for anything else.
+            else -> {
+                expectedValue.equalsDeep(actualValue)
+            }
+        }
+    }
+
+    internal fun filterResourceProperties(resource: Base): List<Property> {
+        return resource.children().filter {
+            // Skip any properties to be ignored
+            val isSkipped = skippedResourceProperties.contains("${resource.fhirType()}.${it.name}")
+            // Skip any resource IDs
+            val isResourceId = (resource.isResource && it.name == "id")
+            // Skip the entry property of the bundle as those resources are handled elsewhere
+            val isBundleEntry = (resource.hasType("Bundle") && it.name == "entry")
+            !(isSkipped || isResourceId || isBundleEntry)
+        }
+    }
+
+    internal fun getFhirTypePath(parentTypePath: String, valueName: String): String {
+        val parentPath = if (parentTypePath.isNotBlank()) "$parentTypePath." else ""
+        return parentPath + valueName
+    }
+
     companion object {
-        // TODO CHECK FOR DYNAMIC VALUES
-        private val skippedResourceProperties = listOf(
-            "Bundle.entry", "Bundle.timestamp", "Bundle.id",
-            "Meta.lastUpdated",
-            "MessageHeader.id",
-            "Provenance.id",
-            "Organization.id",
-            "Device.id"
+        private val skippedResourceProperties = emptyList<String>()
+        private val dynamicResourceProperties = listOf(
+            "Bundle.timestamp",
+            "Bundle.meta.lastUpdated",
         )
         private val comparedBundleEntries = listOf(
             "MessageHeader", "Provenance", "Patient", "Encounter",
