0d2025c14fca80ab4e8fe1ca63aecbb2fecb7935
Cle' Diggins <89804087+clediggins-usds@users.noreply.github.com>
2022-05-19 17:22:44 -0700

    ELR Routing (#5508)
    * Refactored FHIR code
    
    * Generate FHIR data from MSH segment
    
    * Unit tests step 1
    
    * creation of SubmissionReceiver, refactoring of validateAndMoveToProcessing
    
    * More unit tests
    
    * Removed copyrights from copied files
    
    * A few more improvements
    
    * storing data on submission receipt correctly, still need to fix unit tests and code coverage
    
    * unit tests for SubmissionReceiver
    
    * update to track on failure, update unit tests
    
    * updates from initial PR review
    
    * fixed topic format, commented out fhirFlow processing since that is not part of the scope of this task and was not working anyway
    
    * allow for mock metadata in tests for elr report and submission receiver
    
    * update comments for this PR, fix wildcard import in SenderFilesCommand
    
    Co-authored-by: Carlos Felix <carlos.felix@agile6.com>

--- prime-router/src/main/kotlin/fhirengine/azure/FHIRProcessFunctions.kt
+++ prime-router/src/main/kotlin/fhirengine/azure/FHIRProcessFunctions.kt
@@ -9,13 +9,12 @@ import gov.cdc.prime.router.InvalidReportMessage
 import gov.cdc.prime.router.azure.ActionHistory
 import gov.cdc.prime.router.azure.WorkflowEngine
 import gov.cdc.prime.router.azure.db.enums.TaskAction
-import gov.cdc.prime.router.fhirengine.engine.Message
 import gov.cdc.prime.router.fhirengine.engine.RawSubmission
 import gov.cdc.prime.router.fhirengine.translation.HL7toFhirTranslator
 import gov.cdc.prime.router.fhirengine.utils.HL7Reader
 import org.apache.logging.log4j.kotlin.Logging
 
-const val fhirProcessQueueName = "fhir-raw-received"
+const val elrProcessQueueName = "process-elr"
 
 /**
  * Functions to execution and logging of FHIR translations.
@@ -35,25 +34,26 @@ class FHIRFlowFunctions(
     @FunctionName("process-fhir")
     @StorageAccount("AzureWebJobsStorage")
     fun process(
-        @QueueTrigger(name = "message", queueName = fhirProcessQueueName)
+        @QueueTrigger(name = "message", queueName = elrProcessQueueName)
         message: String,
         // Number of times this message has been dequeued
         @BindingName("DequeueCount") dequeueCount: Int = 1,
     ) {
         logger.debug("Processing message: $message for the $dequeueCount time")
-        val messageContent = Message.deserialize(message)
-        check(messageContent is RawSubmission) {
-            "An unknown message was received by the FHIR Engine ${messageContent.javaClass.kotlin.qualifiedName}"
-        }
-
-        try {
-            processHL7(messageContent)
-        } catch (e: Exception) {
-            logger.error("Unknown error.", e)
-        }
-        actionHistory.trackActionParams(message)
-        actionHistory.trackLogs(actionLogger.logs)
-        workflowEngine.recordAction(actionHistory)
+        // TODO: This will be part of 4824 and does not work yet.
+//        val messageContent = Message.deserialize(message)
+//        check(messageContent is RawSubmission) {
+//            "An unknown message was received by the FHIR Engine ${messageContent.javaClass.kotlin.qualifiedName}"
+//        }
+//
+//        try {
+//            processHL7(messageContent)
+//        } catch (e: Exception) {
+//            logger.error("Unknown error.", e)
+//        }
+//        actionHistory.trackActionParams(message)
+//        actionHistory.trackLogs(actionLogger.logs)
+//        workflowEngine.recordAction(actionHistory)
     }
 
     /**
@@ -63,6 +63,11 @@ class FHIRFlowFunctions(
         logger.trace("Processing HL7 data for FHIR conversion.")
         try {
             val hl7messages = HL7Reader(actionLogger).getMessages(messageContent.downloadContent())
+
+            if (actionLogger.hasErrors()) {
+                throw java.lang.IllegalArgumentException(actionLogger.errors.joinToString("\n") { it.detail.message })
+            }
+
             val fhirBundles = hl7messages.map { message ->
                 HL7toFhirTranslator.getInstance().translate(message)
             }
