4520b7e6bcd3b4f759686a08c13aa08ac47b278e
Kevin Haube <khaube@fearless.tech>
2022-05-25 17:06:14 -0400

    Adds tests for nav visibility based on role

---
+++ frontend-react/src/components/header/ReportStreamHeader.test.tsx
@@ -0,0 +1,100 @@
+import * as OktaReact from "@okta/okta-react";
+import { IOktaContext } from "@okta/okta-react/bundles/types/OktaContext";
+import { screen } from "@testing-library/react";
+
+import { renderWithSession } from "../../utils/CustomRenderUtils";
+
+import { ReportStreamHeader } from "./ReportStreamHeader";
+
+const mockAuth = jest.spyOn(OktaReact, "useOktaAuth");
+const mockGetUser = jest.fn();
+
+/* Ran into test runner errors that wanted me to wrap "anything
+ * altering state" in the `act()` function, but then complained
+ * when I wrapped the ting in the act() function... so I made it
+ * disappear. */
+jest.mock("./SignInOrUser", () => ({
+    SignInOrUser: () => {
+        return null;
+    },
+}));
+
+describe("ReportStreamHeader", () => {
+    test("renders without errors", () => {
+        mockAuth.mockReturnValue({} as IOktaContext);
+        renderWithSession(<ReportStreamHeader />);
+    });
+
+    test("admins see all", async () => {
+        mockAuth.mockReturnValue({
+            //@ts-ignore
+            oktaAuth: {
+                getUser: mockGetUser.mockResolvedValue({
+                    email: "test@test.org",
+                }),
+            },
+            authState: {
+                isAuthenticated: true,
+                accessToken: {
+                    claims: {
+                        //@ts-ignore
+                        organization: ["DHPrimeAdmins"],
+                    },
+                },
+            },
+        });
+        renderWithSession(<ReportStreamHeader />);
+        expect(screen.getByText("Admin")).toBeInTheDocument();
+        expect(screen.getByText("Daily data")).toBeInTheDocument();
+        expect(screen.getByText("Upload")).toBeInTheDocument();
+        expect(screen.getByText("Submissions")).toBeInTheDocument();
+    });
+
+    test("senders see sender items and not receiver items", async () => {
+        mockAuth.mockReturnValue({
+            //@ts-ignore
+            oktaAuth: {
+                getUser: mockGetUser.mockResolvedValue({
+                    email: "test@test.org",
+                }),
+            },
+            authState: {
+                isAuthenticated: true,
+                accessToken: {
+                    claims: {
+                        //@ts-ignore
+                        organization: ["DHSender_ignore"],
+                    },
+                },
+            },
+        });
+        renderWithSession(<ReportStreamHeader />);
+        expect(screen.queryByText("Daily data")).not.toBeInTheDocument();
+        expect(screen.getByText("Upload")).toBeInTheDocument();
+        expect(screen.getByText("Submissions")).toBeInTheDocument();
+    });
+
+    test("receivers see receiver items and not sender items", async () => {
+        mockAuth.mockReturnValue({
+            //@ts-ignore
+            oktaAuth: {
+                getUser: mockGetUser.mockResolvedValue({
+                    email: "test@test.org",
+                }),
+            },
+            authState: {
+                isAuthenticated: true,
+                accessToken: {
+                    claims: {
+                        //@ts-ignore
+                        organization: ["DHmd_phd"],
+                    },
+                },
+            },
+        });
+        renderWithSession(<ReportStreamHeader />);
+        expect(screen.getByText("Daily data")).toBeInTheDocument();
+        expect(screen.queryByText("Upload")).not.toBeInTheDocument();
+        expect(screen.queryByText("Submissions")).not.toBeInTheDocument();
+    });
+});
