1e501567d5f73b6e6cd3cb4344cd2d7df1ebf55f
Kevin Haube <khaube@fearless.tech>
2022-05-26 10:04:13 -0400

    Remove new checks for sendermode hook

--- frontend-react/src/hooks/UseSenderMode.ts
+++ frontend-react/src/hooks/UseSenderMode.ts
@@ -2,13 +2,9 @@ import { useEffect, useMemo, useState } from "react";
 import axios from "axios";
 
 import { orgApi, Sender } from "../network/api/OrgApi";
-import { useSessionContext } from "../contexts/SessionContext";
-
-import { MemberType } from "./UseGroups";
 
 const useSenderMode = (defaultOrg?: string, defaultSender?: string): string => {
     const [status, setStatus] = useState<string>("active");
-    const { memberships } = useSessionContext();
     const endpoint = useMemo(() => {
         if (defaultOrg && defaultSender) {
             return orgApi.getSenderDetail(defaultOrg, defaultSender);
@@ -21,10 +17,7 @@ const useSenderMode = (defaultOrg?: string, defaultSender?: string): string => {
          * changes state in the .then() call. On teardown, this will
          * be set to false and the state will not attempt to update. */
         let isSubscribed = true;
-        if (
-            endpoint &&
-            memberships.state.active?.memberType === MemberType.SENDER
-        ) {
+        if (endpoint) {
             axios
                 .get<Sender>(endpoint.url, endpoint)
                 .then((res) =>
@@ -34,7 +27,7 @@ const useSenderMode = (defaultOrg?: string, defaultSender?: string): string => {
         return () => {
             isSubscribed = false;
         };
-    }, [endpoint, memberships.state.active?.memberType]);
+    }, [endpoint]);
 
     return status;
 };
