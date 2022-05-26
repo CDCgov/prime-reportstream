89d1301dc5ce121352342b97f176f9f14e7a9f22
Kevin Haube <khaube@fearless.tech>
2022-05-26 15:26:43 -0400

    Minor fixes

--- frontend-react/src/pages/made-for-you/MadeForYou.tsx
+++ frontend-react/src/pages/made-for-you/MadeForYou.tsx
@@ -3,8 +3,15 @@ import { MarkdownContent } from "../../components/Markdown/MarkdownContent";
 
 const MadeForYou = ({ directories }: MarkdownPageProps) => {
     return directories.forEach((dir) => {
-        dir.files.map((file) => (
-            <MarkdownContent markdownUrl={dir.getUrl(file)} />
-        ));
+        dir.files.map((fileName) => {
+            const contentURL = dir.getUrl(fileName);
+            if (contentURL !== undefined) {
+                return <MarkdownContent markdownUrl={contentURL} />;
+            } else {
+                return null;
+            }
+        });
     });
 };
+
+export default MadeForYou;
