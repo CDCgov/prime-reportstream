de796f34abb70d50a2e06a2d82fc1d96673f11d5
Kevin Haube <khaube@fearless.tech>
2022-05-31 22:58:55 -0400

    Fix up some comments in the code

--- frontend-react/src/pages/built-for-you/BuiltForYouIndex.tsx
+++ frontend-react/src/pages/built-for-you/BuiltForYouIndex.tsx
@@ -2,23 +2,21 @@ import React from "react";
 
 import { MarkdownDirectory } from "../../components/Markdown/MarkdownDirectory";
 import StaticPageFromDirectories from "../../components/Markdown/StaticPageFromDirectories";
-/* Markdown files must be imported as modules and passed along to the
- * MarkdownContent component through props. */
 import may2022 from "../../content/built-for-you/2022-may.md";
 import june2022 from "../../content/built-for-you/2022-june.md";
 import DropdownNav from "../../components/header/DropdownNav";
 
-/* This controls the content for Built For You! To add a directory:
+/* This controls the content for Built For You
  *
- * 1. copy-paste an existing directory and add it in the array where
- * you want it to show up in the side-nav. Then, alter the title,
- * slug, and files array to match your desired title, url slug, and
- * to pass in any files you wish to render on the page. */
+ * visit: https://reportstream.cdc.gov/admin/guides/create-markdown-pages
+ * for mor info on how to edit this directory */
 export const BUILT_FOR_YOU: MarkdownDirectory[] = [
     new MarkdownDirectory("June 2022", "june-2022", [june2022]),
     new MarkdownDirectory("May 2022", "may-2022", [may2022]),
 ];
 
+/* This generates the navigation item for Built For You
+ * Import and use this in ReportStreamHeader */
 export const BuiltForYouDropdown = () => {
     return (
         <DropdownNav
@@ -29,7 +27,7 @@ export const BuiltForYouDropdown = () => {
     );
 };
 
-/* Houses the routing and layout for Built For You */
+/* Creates the page and side-nav from your directory array */
 const BuiltForYouIndex = () => (
     <StaticPageFromDirectories directories={BUILT_FOR_YOU} />
 );
