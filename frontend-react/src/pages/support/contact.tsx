b56f9fcdf2df235f47685dee6edaf43a0768d14d
drew-usds <57154955+drew-usds@users.noreply.github.com>
2022-06-02 00:47:57 -0400

    adjust divs and sections to match other pages ... we have a markup problem with nested sections and usa-prose styles, but this works for now

--- frontend-react/src/pages/support/contact.tsx
+++ frontend-react/src/pages/support/contact.tsx
@@ -9,32 +9,26 @@ export const SupportContact = () => {
             <Helmet>
                 <title>Contact | {process.env.REACT_APP_TITLE}</title>
             </Helmet>
-
             <section className="grid-container tablet:margin-top-6 margin-bottom-5">
                 <div className="grid-row grid-gap">
+                    <div className="tablet:grid-col-8 usa-prose">
+                        <section>
+                            <h1 className="margin-top-0">Contact us</h1>
+                            <h2 className="usa-intro text-base">Want to get in touch with ReportStream? Email us at <a href={"mailto:" + DOMPurify.sanitize(site.orgs.RS.email)} className="usa-link">{site.orgs.RS.email}</a>.</h2>
 
-                    <div className="tablet:grid-col-8">
-                        
-                        <h1 className="margin-top-0">Contact us</h1>
-                        <h2 className="usa-intro text-base">Want to get in touch with ReportStream? Email us at <a href={"mailto:" + DOMPurify.sanitize(site.orgs.RS.email)} className="usa-link">{site.orgs.RS.email}</a>.</h2>
-
-                    </div>
-
-                    <div className="tablet:grid-col-8  margin-top-4 padding-top-2 border-top border-base-lighter usa-prose rs-documentation measure-5">
-
-                        <h3>Interested in partnering with ReportStream?</h3>
-                        <p>Read our getting started guides for <a className="usa-link" href="/getting-started/public-health-departments/overview">public health departments</a> and <a className="usa-link" href="/getting-started/testing-facilities/overview">organizations and testing facilities</a>.</p>
-                        <p>To learn more, or to begin the account registration process, email us at <a href={"mailto:" + DOMPurify.sanitize(site.orgs.RS.email)} className="usa-link">{site.orgs.RS.email}</a>.</p>
-                        
-                        <h3>Have a problem with a data you're trying to send or receive?</h3>
-                        <p>Read our guides for sending data <a className="usa-link" href="/getting-started/public-health-departments/elr-checklist">via API (ELR)</a> or <a className="usa-link" href="/getting-started/testing-facilities/csv-upload-guide">via CSV upload</a>. For all other issues, contact us at <a href={"mailto:" + DOMPurify.sanitize(site.orgs.RS.email)}
-                    className="usa-link">{site.orgs.RS.email}</a>.</p>
-          
-                        <h3>Need something else?</h3>
-                        <p>For quick answers to common questions, read our <a className="usa-link" href="/support/faq">frequently asked questions</a>.</p>
-                        <p>If you have another question, issue, or bug to report, we’re happy to help. Email us at <a href={"mailto:" + DOMPurify.sanitize(site.orgs.RS.email)}
-                    className="usa-link">{site.orgs.RS.email}</a>.</p>
-
+                            <div className="margin-top-6 border-top border-base-lighter usa-prose">&nbsp;</div>
+                                 
+                            <h2>Interested in partnering with ReportStream?</h2>
+                            <p>Read our getting started guides for <a className="usa-link" href="/getting-started/public-health-departments/overview">public health departments</a> and <a className="usa-link" href="/getting-started/testing-facilities/overview">organizations and testing facilities</a>.</p>
+                            <p>To learn more, or to begin the account registration process, email us at <a href={"mailto:" + DOMPurify.sanitize(site.orgs.RS.email)} className="usa-link">{site.orgs.RS.email}</a>.</p>
+                            
+                            <h2 className="margin-top-4">Have a problem with a data you're trying to send or receive?</h2>
+                            <p>Read our guides for sending data <a className="usa-link" href="/getting-started/public-health-departments/elr-checklist">via API (ELR)</a> or <a className="usa-link" href="/getting-started/testing-facilities/csv-upload-guide">via CSV upload</a>. For all other issues, contact us at <a href={"mailto:" + DOMPurify.sanitize(site.orgs.RS.email)} className="usa-link">{site.orgs.RS.email}</a>.</p>
+            
+                            <h2 className="margin-top-4">Need something else?</h2>
+                            <p>For quick answers to common questions, read our <a className="usa-link" href="/support/faq">frequently asked questions</a>.</p>
+                            <p>If you have another question, issue, or bug to report, we're happy to help. Email us at <a href={"mailto:" + DOMPurify.sanitize(site.orgs.RS.email)} className="usa-link">{site.orgs.RS.email}</a>.</p>                           
+                        </section>
                     </div>
                 </div>
             </section>
--- frontend-react/src/pages/support/faq.tsx
+++ frontend-react/src/pages/support/faq.tsx
@@ -4,69 +4,48 @@ export const SupportFaq = () => {
 
     return (
         <>
-
             <Helmet>
                 <title>FAQ | {process.env.REACT_APP_TITLE}</title>
             </Helmet>
-            
             <section className="grid-container tablet:margin-top-6 margin-bottom-5">
-                <div className="grid-row grid-gap"> 
-                    <div className="tablet:grid-col-8">
-                        <h1 className="margin-top-0">Frequently asked questions (FAQ) </h1>  
-                    </div>                 
-                    <div className="tablet:grid-col-8 usa-prose">                        
+                <div className="grid-row grid-gap">
+                    <div className="tablet:grid-col-8 usa-prose">
+                        <section>
+                            <h1 className="margin-top-0">Frequently asked questions (FAQ) </h1>
+                            
+                            <div className="margin-top-6 border-top border-base-lighter usa-prose">&nbsp;</div>
 
-                        <h3>I just activated my ReportStream account, why can't I log in? </h3>
+                            <h2>I just activated my ReportStream account, why can't I log in? </h2>
+                            <p>We use Okta to manage and authenticate access to our application. However, you'll log in to your user account at reportstream.cdc.gov/login.  </p>
+                            
+                            <h2 className="margin-top-4">Having problems logging in? </h2>
+                            <p>Trouble logging in to ReportStream or need to reset your password? Go to reportstream.cdc.gov/login or click the button below. </p>
 
-                        <p>We use Okta to manage and authenticate access to our application. However, you'll log in to your user account at reportstream.cdc.gov/login.  </p>
-                        
-                        <h3>Having problems logging in? </h3>
+                            <h2 className="margin-top-4">Why does my CSV file keep getting errors from ReportStream? </h2>
+                            <p><strong>Incorrect values:</strong> The most common submission errors we get from CSV upload users are incorrectly entered values under the following required data elements: Equipment Mode Name, Test Performed Code, Test Result, Patient Race, Patient Ethnicity. </p>
 
-                        <p>Trouble logging in to ReportStream or need to reset your password? Go to reportstream.cdc.gov/login or click the button below. </p>
+                            <p><strong>Blank values:</strong> Another common submission error is leaving a field blank underneath a data element that's required. Required fields with blank values can fail the file.</p>
 
-                        <p>Reset Password </p>
+                            <p>We've put together detailed instructions outlining how to prepare a CSV file using the standard schema that'll be accepted by state, tribal, local, or territorial (STLT) health departments partnered with ReportStream.</p> 
 
-                        <h3>Why does my CSV file keep getting errors from ReportStream? </h3>
+                            <h2 className="margin-top-4">Can I sort my CSV columns in any order when I submit the file to ReportStream? </h2>
+                            <p>Yes, you can organize your CSV columns in any order within your file. ReportStream looks for data values beneath the column header regardless of where it's located in the file.</p>
 
-                        <p>Incorrect values: The most common submission errors we get from CSV upload users are incorrectly entered values under the following required data elements:</p>
+                            <h2 className="margin-top-4">Which browser is recommended for using ReportStream? </h2>
+                            <p>Our application works best on a modern desktop browser (Chrome, FireFox, Safari, Edge). ReportStream doesn't support Internet Explorer 11 or below.</p>
 
-                        Equipment Mode Name 
+                            <h2 className="margin-top-4">Where does ReportStream work?</h2>
+                            <p>ReportStream is currently live or getting set up in jurisdictions across the United States. Here's a map and list of where we're live.</p> 
 
-                        Test Performed Code 
+                            <h2 className="margin-top-4">What's the difference between ReportStream and SimpleReport? </h2>
+                            <p>ReportStream and SimpleReport are sister projects that share similar resources under the Pandemic-Ready Interoperability Modernization Effort (PRIME) operated by the CDC.</p>
+                            <p>ReportStream is a cloud-based data router that aggregates COVID-19 test data reporting from healthcare clinics, hospitals, laboratories, and organizations, and delivers them to public health departments.</p>
+                            <p>SimpleReport is a cloud-based application that allows organizations who have not reported to public health in the past (e.g. school systems, jails, assisted living facilities, etc.) to submit COVID-19 rapid test results to public health. The data submitted through SimpleReport gets aggregated and routed through ReportStream to public health departments.</p>                       
 
-                        Test Result 
+                            <h2 className="margin-top-4">How much does ReportStream cost?</h2>
+                            <p>ReportStream was developed by the CDC for COVID-19 test data reporting and is 100% free.</p>
 
-                        Patient Race 
-
-                        Patient Ethnicity 
-
-                        <p>Blank values: Another common submission error is leaving a field blank underneath a data element that's required. Required fields with blank values can fail the file.</p>
-
-                        <p>We've put together detailed instructions outlining how to prepare a CSV file using the standard schema that'll be accepted by state, tribal, local, or territorial (STLT) health departments partnered with ReportStream.</p> 
-
-                        <h3>Can I sort my CSV columns in any order when I submit the file to ReportStream? </h3>
-
-                        <p>Yes, you can organize your CSV columns in any order within your file. ReportStream looks for data values beneath the column header regardless of where it's located in the file.</p>
-
-                        <h3>Which browser is recommended for using ReportStream? </h3>
-
-                        <p>Our application works best on a modern desktop browser (Chrome, FireFox, Safari, Edge). ReportStream doesn't support Internet Explorer 11 or below.</p>
-
-                        <h3>Where does ReportStream work?</h3>
-
-                        <p>ReportStream is currently live or getting set up in jurisdictions across the United States. Here's a map and list of where we're live.</p> 
-
-                        <h3>What's the difference between ReportStream and SimpleReport? </h3>
-
-                        <p>ReportStream and SimpleReport are sister projects that share similar resources under the Pandemic-Ready Interoperability Modernization Effort (PRIME) operated by the CDC.</p>
-                        
-                        <p>ReportStream is a cloud-based data router that aggregates COVID-19 test data reporting from healthcare clinics, hospitals, laboratories, and organizations, and delivers them to public health departments.</p>
-
-                        <p>SimpleReport is a cloud-based application that allows organizations who have not reported to public health in the past (e.g. school systems, jails, assisted living facilities, etc.) to submit COVID-19 rapid test results to public health. The data submitted through SimpleReport gets aggregated and routed through ReportStream to public health departments.</p>                       
-
-                        <h3>How much does ReportStream cost?</h3>
-
-                        <p>ReportStream was developed by the CDC for COVID-10 test data reporting and is 100% free. </p>
+                        </section>
                     </div>
                 </div>
             </section>
