f480cbd035849ca90ab901bc285798972c92536f
Kevin Haube <khaube@fearless.tech>
2022-06-13 15:54:34 -0400

    Adds buildEndpointUrl and createAxiosConfig

---
+++ frontend-react/src/network/api/readme.md
@@ -0,0 +1,10 @@
+# How to use the ReportStream API interface
+
+## Design
+
+The API interface behaves like an iPhone case. It's a shell that we put around `axios` for type 
+safety, configuration simplification and integrity, and to make our configurations easily testable. 
+
+### Value add
+
+
---
+++ frontend-react/src/network/api/scratch_API.ts
@@ -0,0 +1,105 @@
+import { AxiosRequestConfig, AxiosRequestHeaders, Method } from "axios";
+
+/* Available APIs from the RS API */
+export enum ApiBaseUrls {
+    HISTORY = "/api/history", // SAMPLE
+    LOOKUP_TABLES = "/api/lookuptables",
+}
+export type AvailableMethods = Method[];
+export interface Endpoint {
+    url: string;
+    methods: AvailableMethods;
+}
+/* Name your endpoints:
+ * e.g. new Map([
+ *   ["allOrgs", { url: "/organization", methods [GET] }],
+ *   ["org", { url: "/organization/:id", methods: [GET, UPDATE] }]
+ * ]) */
+export type EndpointMap = Map<string, Endpoint>;
+/* Declaration of an API */
+export interface API {
+    baseUrl: ApiBaseUrls;
+    endpoints: EndpointMap;
+}
+/* Make some headers required */
+export interface RSRequestHeaders extends AxiosRequestHeaders {}
+/* Make some fields required or overwrite types */
+export interface RSRequestConfig extends AxiosRequestConfig {
+    url: string;
+    method: Method;
+    headers: RSRequestHeaders;
+}
+/* Safe endpoint extraction */
+const extractEndpoint = (api: API, key: string): Endpoint => {
+    const endpoint: Endpoint | undefined = api.endpoints.get(key);
+    if (!endpoint)
+        throw Error(`You must provide a valid endpoint key: ${key} not found`);
+    return endpoint;
+};
+/* Handles generating the config from parameters */
+export const createAxiosConfig = <P extends RSUrlParams>(
+    api: API,
+    endpointKey: string,
+    method: Method,
+    token?: string,
+    organization?: string,
+    parameters?: P,
+    // Allows us to use more of AxiosRequestConfig if we want
+    advancedConfig?: Omit<AxiosRequestConfig, "url" | "method" | "headers">
+): RSRequestConfig => {
+    return {
+        url: buildEndpointUrl(api, endpointKey, parameters),
+        method: method,
+        headers: {
+            "authentication-type": "okta",
+            authorization: `Bearer ${token || ""}`,
+            organization: `${organization || ""}`,
+        },
+        ...advancedConfig,
+    };
+};
+
+/* Allows us to access them via string: params["id"] */
+export interface RSUrlParams {
+    [key: string]: any;
+}
+/* Called from consumer hook to build URL. Parameters for endpoints should be passed
+ * through the consumer hook and into this function when building the URL. */
+export const buildEndpointUrl = <P extends RSUrlParams>(
+    api: API,
+    endpointKey: string,
+    parameters?: P
+): string => {
+    try {
+        const endpoint = extractEndpoint(api, endpointKey);
+        /* Slashes NOT built in! declare urls with leading slash */
+        const construct = (endpointUrl: string) =>
+            `${process.env.REACT_APP_BACKEND_URL}${api.baseUrl}${endpointUrl}`;
+        if (endpoint.url.includes(":")) {
+            if (parameters === undefined) {
+                throw Error(
+                    `Parameters are required for ${endpointKey}: ${endpoint.url}`
+                );
+            } else {
+                const endpointUrl = endpoint.url
+                    /* split at every slash */
+                    .split("/")
+                    /* replace `:param` string with param value */
+                    .map(
+                        (key) =>
+                            key.includes(":")
+                                ? parameters?.[key.slice(1)] || key // for parameters w/ safe default
+                                : key // for non-parameters
+                    )
+                    /* rejoin as url string */
+                    .join("/");
+                return construct(endpointUrl);
+            }
+        } else {
+            return construct(endpoint.url);
+        }
+    } catch (e) {
+        console.error(e);
+        return "";
+    }
+};
