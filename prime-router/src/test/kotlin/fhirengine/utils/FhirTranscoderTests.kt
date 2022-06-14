b4a815fea47a883aa4ce29f9237673cfb5ff552e
Patricia Perozo <Patricia.C.Perozo@omb.eop.gov>
2022-06-14 14:33:43 -0400

    Add JSON verification and remove encode from decode test
    Addressing comments. Adding more json verification to encode test.
    Adding an encode/decode combination test.
    Removing encode from the decode test to only use a test string.

--- prime-router/src/test/kotlin/fhirengine/utils/FhirTranscoderTests.kt
+++ prime-router/src/test/kotlin/fhirengine/utils/FhirTranscoderTests.kt
@@ -5,19 +5,13 @@ import assertk.assertions.contains
 import assertk.assertions.isNotEmpty
 import assertk.assertions.isNotNull
 import org.hl7.fhir.r4.model.Bundle
-import org.junit.jupiter.api.BeforeEach
+import org.json.JSONException
+import org.json.JSONObject
 import java.util.Date
 import kotlin.test.Test
+import kotlin.test.assertTrue
 
 class FhirTranscoderTests {
-    private val testBundle = Bundle()
-    @BeforeEach
-    fun setup() {
-        testBundle.type = Bundle.BundleType.MESSAGE
-        testBundle.id = "someid"
-        testBundle.timestamp = Date()
-        testBundle.addEntry()
-    }
     @Test
     fun `test get message engine`() {
         // Just make sure we can get an engine, which means the config is setup correctly.
@@ -26,15 +20,53 @@ class FhirTranscoderTests {
 
     @Test
     fun `test default parser encode FHIR bundle`() {
+        fun isValidJSONObject(jsonVal: String): Boolean {
+            try {
+                JSONObject(jsonVal)
+            } catch (ex: JSONException) {
+                return false
+            }
+            return true
+        }
+        // Create test FHIR Bundle
+        val testBundle = Bundle()
+        testBundle.type = Bundle.BundleType.MESSAGE
+        testBundle.id = "someid"
+        testBundle.timestamp = Date()
+        testBundle.addEntry()
+
         val encodedBundle = FhirTranscoder.encode(testBundle)
+
         assertThat(encodedBundle).isNotEmpty()
-        assertThat(encodedBundle).contains(testBundle.id)
+        assertTrue { isValidJSONObject(encodedBundle) }
+
+        // Ensure contains id and type
+        assertThat(encodedBundle).contains("\"id\":\"${testBundle.id}\"")
+        assertThat(encodedBundle).contains("\"type\":\"${testBundle.type.name.lowercase()}\"")
     }
 
     @Test
     fun `test default parser decode FHIR bundle`() {
+        val encodedBundle = """
+            {"resourceType":"Bundle","id":"someid","type":"message","timestamp":"2022-06-14T12:11:50.281-04:00"}
+        """.trimIndent()
+        val decodedBundle = FhirTranscoder.decode(encodedBundle)
+        assertThat(decodedBundle).isNotNull()
+        assertThat(decodedBundle.id == "someid")
+        assertThat(decodedBundle.type == Bundle.BundleType.MESSAGE)
+    }
+    @Test
+    fun `test encoding and decoding combination`() {
+        // Create test FHIR Bundle
+        val testBundle = Bundle()
+        testBundle.type = Bundle.BundleType.MESSAGE
+        testBundle.id = "someid"
+        testBundle.timestamp = Date()
+        testBundle.addEntry()
+
         val encodedBundle = FhirTranscoder.encode(testBundle)
         val decodedBundle = FhirTranscoder.decode(encodedBundle)
+        // Ensure the decode output is the same as the encoded input
         assertThat(decodedBundle).isNotNull()
         assertThat(decodedBundle == testBundle)
     }
