11a020ed547a1021af2fa0ef7aef8901a6cf08e2
Aurora Hampton (github) <aurora.hampton@agile6.com>
2022-06-16 15:46:56 -0700

    experience/5709: update cypress to 10.1.0

--- frontend-react/cypress/support/commands.js
+++ frontend-react/cypress/support/commands.js
@@ -24,3 +24,53 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 import "@testing-library/cypress/add-commands";
+
+import {OktaAuth} from '@okta/okta-auth-js';
+
+import "cypress-localstorage-commands";
+import { authenticator } from "otplib";
+
+// Okta
+Cypress.Commands.add('loginByOktaApi', (username, password) => {
+    cy.request(
+        "POST",
+        `https://hhs-prime.oktapreview.com/api/v1/authn`,
+        {
+            username,
+            password,
+            options: {
+                multiOptionalFactorEnroll: true,
+                warnBeforePasswordExpired: true,
+            },
+        }).then(({body}) => {
+        const user = body._embedded.user
+        const config = {
+            issuer: `https://${Cypress.env('okta_domain')}/oauth2/default`,
+            clientId: Cypress.env('okta_client_id'),
+            redirectUri: 'http://localhost:3000/implicit/callback',
+            scope: ['openid', 'email', 'profile'],
+        }
+
+        const authClient = new OktaAuth(config)
+
+        return authClient.token
+            .getWithoutPrompt({sessionToken: body.sessionToken})
+            .then(({tokens}) => {
+                const userItem = {
+                    token: tokens.accessToken.value,
+                    user: {
+                        sub: user.id,
+                        email: user.profile.login,
+                        given_name: user.profile.firstName,
+                        family_name: user.profile.lastName,
+                        preferred_username: user.profile.login,
+                    },
+                }
+
+                window.localStorage.setItem('oktaCypress', JSON.stringify(userItem))
+
+                log.snapshot('after')
+                log.end()
+            });
+    });
+})
