70741346a7ebcd9a3a8b715f30daacada312b041
Aurora Hampton (github) <aurora.hampton@agile6.com>
2022-06-24 14:43:44 -0700

    experience/5709: add feature flag for Validation Service page

--- frontend-react/src/App.tsx
+++ frontend-react/src/App.tsx
@@ -39,7 +39,11 @@ import { EditSenderSettings } from "./components/Admin/EditSenderSettings";
 import "react-toastify/dist/ReactToastify.css";
 import SubmissionDetails from "./pages/submissions/SubmissionDetails";
 import { NewSetting } from "./components/Admin/NewSetting";
-import { FeatureFlagUIComponent } from "./pages/misc/FeatureFlags";
+import {
+    CheckFeatureFlag,
+    FeatureFlagName,
+    FeatureFlagUIComponent,
+} from "./pages/misc/FeatureFlags";
 import SenderModeBanner from "./components/SenderModeBanner";
 import { AdminOrgNew } from "./pages/admin/AdminOrgNew";
 import { DAPHeader } from "./components/header/DAPHeader";
@@ -167,11 +171,15 @@ const App = () => {
                                     authorize={PERMISSIONS.SENDER}
                                     component={Upload}
                                 />
-                                <AuthorizedRoute
-                                    path="/validate"
-                                    authorize={PERMISSIONS.PRIME_ADMIN}
-                                    component={Validate}
-                                />
+                                {CheckFeatureFlag(
+                                    FeatureFlagName.VALIDATION_SERVICE
+                                ) && (
+                                    <AuthorizedRoute
+                                        path="/validate"
+                                        authorize={PERMISSIONS.PRIME_ADMIN}
+                                        component={Validate}
+                                    />
+                                )}
                                 {/* TODO: AuthorizedRoute needs to take many potential auth groups.
                                  *  We should fix this when we refactor our permissions layer.
                                  */}
