5fa2c3533f899369a64fdbd35102a738edd3b059
Aurora Hampton (github) <aurora.hampton@agile6.com>
2022-06-27 13:21:01 -0700

    fix up Validate component unit test

--- frontend-react/src/pages/Validate.test.tsx
+++ frontend-react/src/pages/Validate.test.tsx
@@ -1,11 +1,27 @@
 // import { screen } from "@testing-library/react";
 
-import { render } from "@testing-library/react";
+import React, { ReactElement, Suspense } from "react";
+import { Fixture, MockResolver } from "@rest-hooks/test";
+import { CacheProvider } from "rest-hooks";
+
+import Spinner from "../components/Spinner";
+import { renderWithRouter } from "../utils/CustomRenderUtils";
 
 import Validate from "./Validate";
 
 describe("Validate", () => {
+    const renderWithResolver = (ui: ReactElement, fixtures: Fixture[]) =>
+        renderWithRouter(
+            <CacheProvider>
+                <MockResolver fixtures={fixtures}>{ui}</MockResolver>
+            </CacheProvider>
+        );
     test("Renders with no errors", () => {
-        render(<Validate />);
+        renderWithResolver(
+            <Suspense fallback={<Spinner size={"fullpage"} />}>
+                <Validate />
+            </Suspense>,
+            []
+        );
     });
 });
