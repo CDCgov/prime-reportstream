1c5e8644334d442904f4adfde7fec198a97246a5
Aurora Hampton (github) <aurora.hampton@agile6.com>
2022-06-27 15:32:02 -0700

    fix WaterApi unit test with updateSession

--- frontend-react/src/network/api/WatersApi.test.ts
+++ frontend-react/src/network/api/WatersApi.test.ts
@@ -1,7 +1,12 @@
 import { watersApi } from "./WatersApi";
 
 describe("Waters API", () => {
+    const fakeHeaders = {
+        Authorization: "Bearer [token]",
+        Organization: 'test-org'
+    };
     test("postReport", () => {
+        watersApi.updateSession(fakeHeaders);
         const endpoint = watersApi.postReport(
             "test.default",
             "test-data.hl7",
@@ -11,9 +16,9 @@ describe("Waters API", () => {
             method: "POST",
             url: `${process.env.REACT_APP_BACKEND_URL}/api/waters`,
             headers: {
-                Authorization: "Bearer ",
+                Authorization: fakeHeaders.Authorization,
                 "Content-Type": "application/hl7-v2",
-                Organization: "",
+                Organization: fakeHeaders.Organization,
                 "authentication-type": "okta",
                 client: "test.default",
                 payloadName: "test-data.hl7",
