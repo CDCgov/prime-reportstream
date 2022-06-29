1b8037611b6a845983edb0b30d9166a5341944c8
Aurora Hampton (github) <aurora.hampton@agile6.com>
2022-06-29 10:31:27 -0700

    experience/5709: use session in Validate component test

--- frontend-react/src/pages/Validate.test.tsx
+++ frontend-react/src/pages/Validate.test.tsx
@@ -1,20 +1,55 @@
 import React, { ReactElement, Suspense } from "react";
 import { Fixture, MockResolver } from "@rest-hooks/test";
 import { CacheProvider } from "rest-hooks";
+import * as OktaReact from "@okta/okta-react";
 
 import Spinner from "../components/Spinner";
-import { renderWithRouter } from "../utils/CustomRenderUtils";
+import { renderWithSession } from "../utils/CustomRenderUtils";
+import { mockSessionContext } from "../contexts/__mocks__/SessionContext";
+import { MemberType } from "../hooks/UseOktaMemberships";
 
 import Validate from "./Validate";
 
+const mockAuth = jest.spyOn(OktaReact, "useOktaAuth");
+const mockGetUser = jest.fn();
+
 describe("Validate", () => {
     const renderWithResolver = (ui: ReactElement, fixtures: Fixture[]) =>
-        renderWithRouter(
+        renderWithSession(
             <CacheProvider>
                 <MockResolver fixtures={fixtures}>{ui}</MockResolver>
             </CacheProvider>
         );
+
     test("Renders with no errors", () => {
+        mockAuth.mockReturnValue({
+            //@ts-ignore
+            oktaAuth: {
+                getUser: mockGetUser.mockResolvedValue({
+                    email: "test@test.org",
+                }),
+            },
+            authState: {
+                isAuthenticated: true,
+                accessToken: {
+                    claims: {
+                        //@ts-ignore
+                        organization: ["DHPrimeAdmins"],
+                    },
+                },
+            },
+        });
+        mockSessionContext.mockReturnValue({
+            //@ts-ignore
+            memberships: {
+                state: {
+                    active: {
+                        memberType: MemberType.PRIME_ADMIN,
+                        parsedName: "PrimeAdmins",
+                    },
+                },
+            },
+        });
         renderWithResolver(
             <Suspense fallback={<Spinner size={"fullpage"} />}>
                 <Validate />
