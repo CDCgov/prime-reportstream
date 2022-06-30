47eadabc26428168703e201914cc3f7f62702efd
Josiah Siegel <josiah.siegel@semanticbits.com>
2022-06-30 11:34:44 -0400

    demo release runner access

--- operations/app/terraform/vars/demo/variables.tf
+++ operations/app/terraform/vars/demo/variables.tf
@@ -4,3 +4,7 @@ variable "environment" {
 variable "address_id" {
   description = "Address space partial unique to environment"
 }
+variable "terraform_caller_ip_address" {
+  description = "Public IP address of users and runners that need access"
+  default     = ["162.224.209.174", "24.163.118.70", "75.191.122.59", "108.48.23.191"]
+}
