62ef5eeb44530de9da827e9c92c2ef83e46d4bc4
Luis Pabon <l.pabon@theta-llc.com>
2022-07-11 14:22:14 -0400

    Delivery facade should default to true instead of null for showFailed

--- prime-router/src/main/kotlin/history/azure/DeliveryFacade.kt
+++ prime-router/src/main/kotlin/history/azure/DeliveryFacade.kt
@@ -90,7 +90,7 @@ class DeliveryFacade(
             since,
             until,
             pageSize,
-            null,
+            true,
             DeliveryHistory::class.java
         )
     }
