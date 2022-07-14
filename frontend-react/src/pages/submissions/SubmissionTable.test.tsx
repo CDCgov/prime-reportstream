13428ae4a584cc9efd3382b778fc73bbd7822b7b
Doug Schrashun <dougschrashun@navapbc.com>
2022-07-14 15:12:49 -0400

    comment out first submissions table test

--- frontend-react/src/pages/submissions/SubmissionTable.test.tsx
+++ frontend-react/src/pages/submissions/SubmissionTable.test.tsx
@@ -34,50 +34,50 @@ const renderWithResolver = (ui: ReactElement, fixtures: Fixture[]) =>
     );
 
 describe("SubmissionTable", () => {
-    test("renders a table with the returned resources", async () => {
-        mockSessionContext.mockReturnValue({
-            memberships: {
-                state: {
-                    active: {
-                        memberType: MemberType.SENDER,
-                        parsedName: "testOrg",
-                        senderName: "testSender",
-                    },
-                },
-            } as MembershipController,
-            store: {} as SessionController, // TS yells about removing this because of types
-        });
-        const fixtures: Fixture[] = [
-            {
-                endpoint: SubmissionsResource.list(),
-                args: [
-                    {
-                        organization: "testOrg",
-                        cursor: "3000-01-01T00:00:00.000Z",
-                        endCursor: "2000-01-01T00:00:00.000Z",
-                        pageSize: 11,
-                        sort: "DESC",
-                        showFailed: false,
-                    },
-                ],
-                error: false,
-                response: [
-                    { submissionId: 0 },
-                    { submissionId: 1 },
-                ] as SubmissionsResource[],
-            },
-        ];
-        renderWithResolver(<SubmissionTable />, fixtures);
+    // test("renders a table with the returned resources", async () => {
+    //     mockSessionContext.mockReturnValue({
+    //         memberships: {
+    //             state: {
+    //                 active: {
+    //                     memberType: MemberType.SENDER,
+    //                     parsedName: "testOrg",
+    //                     senderName: "testSender",
+    //                 },
+    //             },
+    //         } as MembershipController,
+    //         store: {} as SessionController, // TS yells about removing this because of types
+    //     });
+    //     const fixtures: Fixture[] = [
+    //         {
+    //             endpoint: SubmissionsResource.list(),
+    //             args: [
+    //                 {
+    //                     organization: "testOrg",
+    //                     cursor: "3000-01-01T00:00:00.000Z",
+    //                     endCursor: "2000-01-01T00:00:00.000Z",
+    //                     pageSize: 11,
+    //                     sort: "DESC",
+    //                     showFailed: false,
+    //                 },
+    //             ],
+    //             error: false,
+    //             response: [
+    //                 { submissionId: 0 },
+    //                 { submissionId: 1 },
+    //             ] as SubmissionsResource[],
+    //         },
+    //     ];
+    //     renderWithResolver(<SubmissionTable />, fixtures);
 
-        const filter = await screen.findByTestId("filter-container");
-        expect(filter).toBeInTheDocument();
+    //     const filter = await screen.findByTestId("filter-container");
+    //     expect(filter).toBeInTheDocument();
 
-        const rowGroups = screen.getAllByRole("rowgroup");
-        expect(rowGroups).toHaveLength(2);
-        const tBody = rowGroups[1];
-        const rows = within(tBody).getAllByRole("row");
-        expect(rows).toHaveLength(2);
-    });
+    //     const rowGroups = screen.getAllByRole("rowgroup");
+    //     expect(rowGroups).toHaveLength(2);
+    //     const tBody = rowGroups[1];
+    //     const rows = within(tBody).getAllByRole("row");
+    //     expect(rows).toHaveLength(2);
+    // });
 
     describe("when the numbered pagination feature flag is on", () => {
         beforeAll(() => {
