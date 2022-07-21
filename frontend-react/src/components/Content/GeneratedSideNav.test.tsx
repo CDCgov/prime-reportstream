9fe733ad5ccd2c7a72884c2ae8daef4366b2984d
Kevin Haube <khaube@fearless.tech>
2022-07-21 11:43:13 -0400

    Refactors instantiation pattern for ContentDirectory children

--- frontend-react/src/components/Content/GeneratedSideNav.test.tsx
+++ frontend-react/src/components/Content/GeneratedSideNav.test.tsx
@@ -6,17 +6,17 @@ import GeneratedSideNav from "./GeneratedSideNav";
 import { MarkdownDirectory } from "./MarkdownDirectory";
 
 const TEST_DIRS = [
-    new MarkdownDirectory("Test Dir", "/test-dir", []),
-    new MarkdownDirectory("Another Dir", "/another-dir", []),
+    new MarkdownDirectory().setTitle("Test Dir").setSlug("test-dir"),
+    new MarkdownDirectory().setTitle("Another Dir").setSlug("another-dir"),
 ];
 
 test("GeneratedSideNav", () => {
     renderWithRouter(<GeneratedSideNav directories={TEST_DIRS} />);
     expect(screen.getByText("Test Dir")).toBeInTheDocument();
-    expect(screen.getByText("Test Dir")).toHaveAttribute("href", "/test-dir");
+    expect(screen.getByText("Test Dir")).toHaveAttribute("href", "test-dir");
     expect(screen.getByText("Another Dir")).toBeInTheDocument();
     expect(screen.getByText("Another Dir")).toHaveAttribute(
         "href",
-        "/another-dir"
+        "another-dir"
     );
 });
