4f6979280f603e0e2ded3e8ee568907b75481bc2
Stephen Nesman <94193373+snesm@users.noreply.github.com>
2022-07-27 15:05:51 -0400

    Update build.gradle.kts (#5719)
    update dokka for jsoup, force jsoup for skrapeit
    
    Co-authored-by: Brick Green <86254221+brick-green@users.noreply.github.com>

--- prime-router/build.gradle.kts
+++ prime-router/build.gradle.kts
@@ -757,6 +757,8 @@ dependencies {
     implementation("org.apache.poi:poi-ooxml:5.2.2")
     implementation("commons-io:commons-io: 2.11.0")
     implementation("com.anyascii:anyascii:0.3.1")
+// force jsoup since skrapeit-html-parser@1.2.1 has not updated
+    implementation("org.jsoup:jsoup:1.14.2")
 
     runtimeOnly("com.okta.jwt:okta-jwt-verifier-impl:0.5.1")
     runtimeOnly("com.github.kittinunf.fuel:fuel-jackson:2.3.1")
