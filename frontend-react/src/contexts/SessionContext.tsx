87b0b7f0e164c4778b77a5f612c85c723fc2878a
Doug Schrashun <dougschrashun@navapbc.com>
2022-08-12 08:25:47 -0400

    remove okta logging

--- frontend-react/src/contexts/SessionContext.tsx
+++ frontend-react/src/contexts/SessionContext.tsx
@@ -28,30 +28,6 @@ export const SessionContext = createContext<ISessionContext>({
     dispatch: () => {},
 });
 
-const setOktaListeners = (oktaAuth: OktaAuth) => {
-    // TOKEN MANAGER EVENTS
-    oktaAuth.tokenManager.on("added", () =>
-        console.log("TOKEN MANAGER: added")
-    );
-    oktaAuth.tokenManager.on("expired", () =>
-        console.log("TOKEN MANAGER: expired")
-    );
-    oktaAuth.tokenManager.on("error", () =>
-        console.log("TOKEN MANAGER: error")
-    );
-    oktaAuth.tokenManager.on("renewed", () =>
-        console.log("TOKEN MANAGER: renewed")
-    );
-    oktaAuth.tokenManager.on("removed", () =>
-        console.log("TOKEN MANAGER: removed")
-    );
-
-    // AUTH STATE MANAGER EVENTS
-    oktaAuth.authStateManager.subscribe((state) =>
-        console.log("AUTH STATE MANAGER: state change", state)
-    );
-};
-
 // accepts `oktaHook` as a parameter in order to allow mocking of this provider's okta based
 // behavior for testing. In non test cases this hook will be the `useOktaAuth` hook from
 // `okta-react`
@@ -59,20 +35,7 @@ const SessionProvider = ({
     children,
     oktaHook,
 }: React.PropsWithChildren<ISessionProviderProps>) => {
-    const { authState, oktaAuth } = oktaHook();
-
-    let oktaInitialized = false;
-
-    useEffect(() => {
-        if (oktaAuth) {
-            oktaInitialized = true;
-        }
-    }, [oktaAuth]);
-    useEffect(() => {
-        if (oktaInitialized) {
-            setOktaListeners(oktaAuth);
-        }
-    }, [oktaInitialized]);
+    const { authState } = oktaHook();
 
     const {
         state: { memberships, activeMembership },
