c401d4774a51a576b3b12e979cd66f4c34951532
Doug Schrashun <dougschrashun@navapbc.com>
2022-08-15 16:29:02 -0400

    reinstate changes not explicitly necessary for this pr

--- frontend-react/src/utils/OrganizationUtils.ts
+++ frontend-react/src/utils/OrganizationUtils.ts
@@ -1,6 +1,8 @@
 import { AccessToken, CustomUserClaims, UserClaims } from "@okta/okta-auth-js";
 
-import { PERMISSIONS } from "./PermissionsUtils";
+import { SessionStore } from "../hooks/UseSessionStorage";
+
+import { isAdmin, isReceiver, isSender, PERMISSIONS } from "./PermissionsUtils";
 
 enum RSOrgType {
     SENDER = "sender",
@@ -58,4 +60,53 @@ const parseOrgName = (group: string | undefined): string => {
     }
 };
 
-export { RSOrgType, getOktaGroups, parseOrgName };
+/* Delivers the array of organization names, or the first, parsed from Okta
+ * groups. */
+const getRSOrgs = (
+    token: AccessToken | undefined,
+    onlyType?: RSOrgType
+): string[] | string => {
+    if (!token || !token.claims) return [];
+    /* Have to convert to RSUserClaims type to get Organization claim */
+    const oktaGroups = getOktaGroups(token);
+
+    /* Consolidate all the filtering logic */
+    const filterGroups = (): string[] => {
+        switch (onlyType) {
+            /* Return those WITH sender */
+            case RSOrgType.SENDER:
+                return oktaGroups.filter((group) => isSender(group));
+            /* Return those WITHOUT sender */
+            case RSOrgType.RECEIVER:
+                return oktaGroups.filter((group) => isReceiver(group));
+            /* Return ONLY admin groups */
+            case RSOrgType.ADMIN:
+                return oktaGroups.filter((group) => isAdmin(group));
+            /* Return all groups */
+            default:
+                return oktaGroups;
+        }
+    };
+    return filterGroups().map((group) => parseOrgName(group));
+};
+
+function parseOrgs(orgs: Array<string>): Array<Partial<SessionStore>> {
+    return orgs.map((org) => {
+        // Org names are case sensitive. This condition will fail if the okta
+        // group name is not cased properly: DHSender_xyz, DHxy_phd, DHPrimeAdmin
+        if (org.includes(PERMISSIONS.SENDER)) {
+            const sender = org.split(".");
+            return {
+                org: parseOrgName(sender[0]),
+                senderName: sender[1] || "default",
+            };
+        } else {
+            return {
+                org: parseOrgName(org),
+                senderName: undefined,
+            };
+        }
+    });
+}
+
+export { RSOrgType, getOktaGroups, parseOrgName, getRSOrgs, parseOrgs };
