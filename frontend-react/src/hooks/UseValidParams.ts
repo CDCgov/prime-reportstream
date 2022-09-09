8e292d413d9b494a774cf936a9c82bcb102e8400
Kevin Haube <khaube@fearless.tech>
2022-09-09 13:54:46 -0400

    Update JSDOC and param name for useValidParams

--- frontend-react/src/hooks/UseValidParams.ts
+++ frontend-react/src/hooks/UseValidParams.ts
@@ -4,23 +4,26 @@ import { useEffect } from "react";
 import { StringIndexed } from "../utils/UsefulTypes";
 
 /** Validates your incoming url params with an array of keys given as params.
+ * All params from {@link useParams} return as possibly undefined, despite defining
+ * them as not undefined. This lets us define which to throw an error on if they are
+ * not present in the url.
  *
- * @param expectedParams {string[]} A list of string keys to check incoming params
+ * @param requiredParams {string[]} A list of string keys to check incoming params
  * */
-export const useValidParams = <T>(expectedParams: string[]): T => {
+export const useValidParams = <T>(requiredParams: string[]): T => {
     // Get all params as object
     const params = useParams();
     useEffect(() => {
         // String index the object
         const stringIndexedParams = params as StringIndexed;
-        expectedParams.forEach((param) => {
+        requiredParams.forEach((param) => {
             // Check all params sequentially
             if (stringIndexedParams?.[param] === undefined) {
                 console.error("Missing param: ", param);
                 throw Error(`Expected param at key {${param}} was undefined`);
             }
         });
-    }, [expectedParams, params]);
+    }, [requiredParams, params]);
     // Return as defined shape (this transformation makes sure returns are NOT undefined!)
     return params as unknown as T;
 };
