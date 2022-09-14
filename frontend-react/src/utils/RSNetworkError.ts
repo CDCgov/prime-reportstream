c1513c92e17801cf7737ca8edf95103e2ba14759
Kevin Haube <khaube@fearless.tech>
2022-09-14 17:20:19 -0400

    Updates to RSNetworkError

--- frontend-react/src/utils/RSNetworkError.ts
+++ frontend-react/src/utils/RSNetworkError.ts
@@ -3,6 +3,7 @@
 export enum ErrorName {
     UNAUTHORIZED = "unauthorized", //401
     NOT_FOUND = "not-found", //404
+    SERVER_ERROR = "server-error", //500-599
     // Any error thrown that cannot be parsed by RSError.parseStatus()
     UNKNOWN = "unknown-error",
     // Any generic error
@@ -15,19 +16,21 @@ export class RSNetworkError extends Error {
     /* Used for identifying unique content to display for error */
     name: ErrorName;
     /* Build a new RSNetworkError */
-    constructor(message: string, status?: number) {
+    constructor(message: string, statusCode?: number) {
         super(`(RSNetworkError) ${message}`); // Sets message
-        this.name = this.parseStatus(status); // Sets code using child's parseStatus
+        this.name = this.parseStatus(statusCode); // Sets code using child's parseStatus
         Object.setPrototypeOf(this, RSNetworkError.prototype);
     }
     /** Map response status code to error name */
-    parseStatus(status?: number) {
-        if (status === undefined) return ErrorName.UNKNOWN;
-        switch (status) {
-            case 401:
+    parseStatus(statusCode?: number) {
+        switch (true) {
+            case statusCode === 401:
                 return ErrorName.UNAUTHORIZED;
-            case 404:
+            case statusCode === 404:
                 return ErrorName.NOT_FOUND;
+            case statusCode!! >= 500 && statusCode!! <= 599:
+                return ErrorName.SERVER_ERROR;
+            case statusCode === undefined:
             default:
                 return ErrorName.UNKNOWN;
         }
