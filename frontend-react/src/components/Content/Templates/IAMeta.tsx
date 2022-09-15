579f163b827b710a5ab9b6484ef819296e8e29c2
Doug Schrashun <dougschrashun@navapbc.com>
2022-09-15 13:38:04 -0400

    cleanup

--- frontend-react/src/components/Content/Templates/IAMeta.tsx
+++ frontend-react/src/components/Content/Templates/IAMeta.tsx
@@ -1,4 +1,3 @@
-import { Helmet } from "react-helmet";
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 
--- frontend-react/src/pages/admin/AdminLastMileFailures.tsx
+++ frontend-react/src/pages/admin/AdminLastMileFailures.tsx
@@ -1,4 +1,5 @@
 import React, { Suspense } from "react";
+
 import Spinner from "../../components/Spinner";
 import HipaaNotice from "../../components/HipaaNotice";
 import { AdminLastMileFailuresTable } from "../../components/Admin/AdminLastMileFailuresTable";
