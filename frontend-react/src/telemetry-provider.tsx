bb9f952c7a93cca8cbf6d3867bab3a26f763966e
Brian Gantick <briangantick@navapbc.com>
2022-09-22 07:48:10 -0400

    fix up

--- frontend-react/src/telemetry-provider.tsx
+++ frontend-react/src/telemetry-provider.tsx
@@ -1,17 +1,16 @@
-
 import React, { Fragment, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 
 import { ai } from "./TelemetryService";
 
 const TelemetryProvider: React.FC = ({ children }) => {
-  const history = useHistory();
+    const history = useHistory();
 
-  useEffect(() => {
-    ai.initialize(history);
-  }, [history]);
+    useEffect(() => {
+        ai.initialize(history);
+    }, [history]);
 
-  return <Fragment>{children}</Fragment>;
+    return <Fragment>{children}</Fragment>;
 };
 
 export default TelemetryProvider;
