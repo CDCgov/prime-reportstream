e5012b3e592dbb67d6ad55f88ea7938fc6bbf823
Kevin Haube <khaube@fearless.tech>
2022-10-06 10:05:02 -0400

    Adds back error test for mutations

--- frontend-react/src/pages/admin/value-set-editor/ValueSetsDetail.test.tsx
+++ frontend-react/src/pages/admin/value-set-editor/ValueSetsDetail.test.tsx
@@ -34,7 +34,7 @@ const fakeMeta = {
     createdAt: "today",
     tableSha256Checksum: "sha",
 };
-
+const mockError = new RSNetworkError("test-error");
 let mockSaveData = jest.fn();
 let mockActivateTable = jest.fn();
 let mockUseValueSetsTable = jest.fn();
@@ -135,6 +135,22 @@ describe("ValueSetsDetail", () => {
 });
 
 describe("ValueSetsDetailTable", () => {
+    test("Handles fetch related errors", () => {
+        const mockSetAlert = jest.fn();
+        renderWithQueryProvider(
+            <ValueSetsDetailTable
+                valueSetName={"error"}
+                setAlert={mockSetAlert}
+                valueSetData={[]}
+                error={mockError}
+            />
+        );
+        expect(mockSetAlert).toHaveBeenCalled();
+        expect(mockSetAlert).toHaveBeenCalledWith({
+            type: "error",
+            message: "unknown-error: test-error",
+        });
+    });
     test("on row save, calls saveData and activateTable triggers with correct args", async () => {
         mockSaveData = jest.fn(() => {
             // to avoid unnecessary console error
