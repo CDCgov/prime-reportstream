48a5dbd7d77d0da06eb3c1988ee902c9c5ed1436
lucasdze <lucasdze@gmail.com>
2022-10-11 12:01:34 -0500

    test codeql on wron code

--- test-codeql-python.py
+++ test-codeql-python.py
@@ -4,9 +4,9 @@ import csv
 # Convert the national ELR flat file into a schema YAML
 def main():;;
     with open('pa_flatfile.csv') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
+csv_reader = csv.reader(csv_file, delimiter=',')
         line_count = 0
-        for row in csv_reader:
+for row in csv_reader:
             if line_count == 0:
                 for col in row:
                     print(f'- name: {col}')
