765598c92222b6c9fd56ed2eac1a56e072be4a57
Penny Lischer <penny.lischer@agile6.com>
2022-10-13 09:35:54 -0700

    Experience/6930 - Removed NetworkErrorBoundary from AdminMessageTracker.tsx

--- frontend-react/src/pages/admin/AdminMessageTracker.tsx
+++ frontend-react/src/pages/admin/AdminMessageTracker.tsx
@@ -1,29 +1,26 @@
 import React from "react";
-import { NetworkErrorBoundary } from "rest-hooks";
 
 import HipaaNotice from "../../components/HipaaNotice";
 import { MemberType } from "../../hooks/UseOktaMemberships";
 import { AuthElement } from "../../components/AuthElement";
 import { BasicHelmet } from "../../components/header/BasicHelmet";
 import { MessageTracker } from "../../components/MessageTracker/MessageTracker";
-import { ErrorPage } from "../error/ErrorPage";
+import { withCatchAndSuspense } from "../../components/RSErrorBoundary";
 
 export function AdminMessageTracker() {
     return (
-        <NetworkErrorBoundary
-            fallbackComponent={() => <ErrorPage type="page" />}
-        >
+        <>
             <BasicHelmet pageTitle="Message Id Search" />
             <MessageTracker />
             <HipaaNotice />
-        </NetworkErrorBoundary>
+        </>
     );
 }
 
 export function AdminMessageTrackerWithAuth() {
     return (
         <AuthElement
-            element={<AdminMessageTracker />}
+            element={withCatchAndSuspense(<AdminMessageTracker />)}
             requiredUserType={MemberType.PRIME_ADMIN}
         />
     );
