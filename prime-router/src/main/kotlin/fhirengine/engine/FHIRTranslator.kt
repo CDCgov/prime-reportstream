ddc21145d870daf2c5a19aae2936752395952123
Luis Pabon <l.pabon@theta-llc.com>
2022-10-14 15:01:09 -0400

    lint

--- prime-router/src/main/kotlin/fhirengine/engine/FHIRTranslator.kt
+++ prime-router/src/main/kotlin/fhirengine/engine/FHIRTranslator.kt
@@ -30,7 +30,7 @@ class FHIRTranslator(
     settings: SettingsProvider = this.settingsProviderSingleton,
     db: DatabaseAccess = this.databaseAccessSingleton,
     blob: BlobAccess = BlobAccess(),
-    queue: QueueAccess = QueueAccess,
+    queue: QueueAccess = QueueAccess
 ) : FHIREngine(metadata, settings, db, blob, queue) {
 
     /**
