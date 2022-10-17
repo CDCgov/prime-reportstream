bc18e22fe691d3b7b5a5a9b580373052a7941c58
Luis Pabon <l.pabon@theta-llc.com>
2022-10-17 12:28:21 -0400

    handle elements with valueSets differently when evaluating the string

--- prime-router/src/main/kotlin/fhirengine/translation/hl7/FhirToHl7Converter.kt
+++ prime-router/src/main/kotlin/fhirengine/translation/hl7/FhirToHl7Converter.kt
@@ -152,7 +152,8 @@ class FhirToHl7Converter(
         var retVal = ""
         run findValue@{
             element.value.forEach {
-                val value = if (it.isBlank()) ""
+                val value = if(element.valueSet.isNotEmpty()) it
+                else if (it.isBlank()) ""
                 else try {
                     FhirPathUtils.evaluateString(context, focusResource, bundle, it)
                 } catch (e: SchemaException) {
