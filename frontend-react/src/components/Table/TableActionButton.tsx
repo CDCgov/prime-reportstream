7cc52dcd45d03a12872e0fddbdf236328cd9c59a
Kevin Haube <khaube@fearless.tech>
2022-10-19 13:47:29 -0400

    Remove todo smell

--- frontend-react/src/components/Table/TableActionButton.tsx
+++ frontend-react/src/components/Table/TableActionButton.tsx
@@ -1,7 +1,6 @@
-// this button will be placed above the rendered table and on `click` will run an arbitrary function
-// passed in from the Table's parent, or an addRow function defined by the Table.
-// in order to avoid problems around timing, takes a `disabled` prop.
-// TODO: split this out of Table component
+/* this button will be placed above the rendered table and on `click` will run an arbitrary function
+ * passed in from the Table's parent, or an addRow function defined by the Table.
+ * in order to avoid problems around timing, takes a `disabled` prop. */
 import { Button } from "@trussworks/react-uswds";
 import React from "react";
 
