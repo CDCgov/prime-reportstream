4ff0690c4e47181e62148feb5e26f815ad42d41d
Kevin Haube <khaube@fearless.tech>
2022-10-20 20:35:27 -0400

    Table Refactoring: Removes old pagination and splits out subcomponents (#7066)
    * Removes non-numbered pagination
    
    * Splitting out Table subcomponents
    
    * Adds local sort tests back, name changes
    
    * Remove todo smell
    
    * fontClass prop added at Table level
    
    * Adds safety to fontClass prop value
    
    * Makes fontClass a more general tableRowsClassName
    
    Co-authored-by: TomNUSDS <tomn.usds.opensource+tomnusds@gmail.com>

--- frontend-react/src/components/Table/ColumnData.tsx
+++ frontend-react/src/components/Table/ColumnData.tsx
@@ -1,17 +1,13 @@
 import React, { ReactNode } from "react";
 import { NavLink } from "react-router-dom";
 
-import {
-    ColumnConfig,
-    LinkableColumn,
-    ActionableColumn,
-    TableRow,
-} from "./Table";
+import { ColumnConfig, LinkableColumn, ActionableColumn } from "./Table";
+import { TableRowData } from "./TableRows";
 
 export interface ColumnProps {
     rowIndex: number;
     colIndex: number;
-    rowData: TableRow;
+    rowData: TableRowData;
     columnConfig: ColumnConfig;
     editing?: boolean;
     setUpdatedRow?: (value: any, field: string) => void;
@@ -19,7 +15,7 @@ export interface ColumnProps {
 
 const showMappedFieldValue = (
     columnConfig: ColumnConfig,
-    rowData: TableRow
+    rowData: TableRowData
 ) => {
     const rawFieldValue = rowData[columnConfig.dataAttr];
     if (columnConfig.valueMap) {
