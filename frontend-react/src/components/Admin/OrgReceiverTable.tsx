ebc34ec320ecf48988f43c3541e203cae0e22eca
TomNUSDS <tomn.usds.opensource+tomnusds@gmail.com>
2022-10-25 10:47:52 -0700

    Move rarely used Delete Setting feature down into Edit page (#7069)
    Step 1 of adding Settings Check: Move Delete out of list and down into Setting Edit page

--- frontend-react/src/components/Admin/OrgReceiverTable.tsx
+++ frontend-react/src/components/Admin/OrgReceiverTable.tsx
@@ -1,12 +1,11 @@
-import { useController, useResource } from "rest-hooks";
+import { useResource } from "rest-hooks";
 import { Link, NavLink } from "react-router-dom";
-import { Button, ModalRef, ButtonGroup, Table } from "@trussworks/react-uswds";
-import React, { useRef, useState } from "react";
+import { ButtonGroup, Table } from "@trussworks/react-uswds";
+import React from "react";
 
 import OrgReceiverSettingsResource from "../../resources/OrgReceiverSettingsResource";
-import { showAlertNotification, showError } from "../AlertNotifications";
+import Spinner from "../Spinner";
 
-import { ConfirmDeleteSettingModal } from "./AdminModal";
 import { DisplayMeta } from "./DisplayMeta";
 
 interface OrgSettingsTableProps {
@@ -18,40 +17,6 @@ export function OrgReceiverTable(props: OrgSettingsTableProps) {
         OrgReceiverSettingsResource.list(),
         { orgname: props.orgname }
     );
-    const { fetch: fetchController } = useController();
-    const [deleteItemId, SetDeleteItemId] = useState("");
-    const modalRef = useRef<ModalRef>(null);
-    const ShowDeleteConfirm = (itemId: string) => {
-        SetDeleteItemId(itemId);
-        modalRef?.current?.toggleModal(undefined, true);
-    };
-    const doDeleteSetting = async () => {
-        try {
-            await fetchController(OrgReceiverSettingsResource.deleteSetting(), {
-                orgname: props.orgname,
-                receivername: deleteItemId,
-            });
-
-            // instead of refetching, just remove from list
-            const index = orgReceiverSettings.findIndex(
-                (item) => item.name === deleteItemId
-            );
-            if (index >= 0) {
-                orgReceiverSettings.splice(index, 1);
-            }
-            showAlertNotification(
-                "success",
-                `Item '${deleteItemId}' has been deleted`
-            );
-            return true;
-        } catch (e: any) {
-            console.trace(e);
-            showError(
-                `Deleting item '${deleteItemId}' failed. ${e.toString()}`
-            );
-            return false;
-        }
-    };
 
     return (
         <section
@@ -67,82 +32,71 @@ export function OrgReceiverTable(props: OrgSettingsTableProps) {
                     History
                 </Link>
             </h2>
-            <Table
-                key="orgreceiversettingstable"
-                aria-label="Organization Receivers"
-                striped
-                fullWidth
-            >
-                <thead>
-                    <tr>
-                        <th scope="col">Name</th>
-                        <th scope="col">Org Name</th>
-                        <th scope="col">Topic</th>
-                        <th scope="col">Status</th>
-                        <th scope="col">Meta</th>
-                        <th scope="col">Action</th>
-                        <th scope="col">
-                            <NavLink
-                                className="usa-button"
-                                to={`/admin/orgnewsetting/org/${props.orgname}/settingtype/receiver`}
-                                key={`receiver-create-link`}
-                            >
-                                New
-                            </NavLink>
-                        </th>
-                    </tr>
-                </thead>
-                <tbody id="tBodyOrgReceiver" className="font-mono-2xs">
-                    {orgReceiverSettings.map((eachOrgSetting, index) => (
-                        <tr
-                            key={`receiver-row-${eachOrgSetting.name}-${index}`}
-                        >
-                            <td>{eachOrgSetting.name}</td>
-                            <td>{eachOrgSetting?.organizationName || "-"}</td>
-                            <td>{eachOrgSetting.topic || ""}</td>
-                            <td>{eachOrgSetting.customerStatus || ""}</td>
-                            <td>
-                                <DisplayMeta metaObj={eachOrgSetting} />
-                            </td>
-                            <td colSpan={2}>
-                                <ButtonGroup type="segmented">
-                                    <NavLink
-                                        className="usa-button"
-                                        to={`/admin/orgreceiversettings/org/${eachOrgSetting.organizationName}/receiver/${eachOrgSetting.name}/action/edit`}
-                                        key={`receiver-edit-link-${eachOrgSetting.name}-${index}`}
-                                    >
-                                        Edit
-                                    </NavLink>
-                                    <NavLink
-                                        className="usa-button"
-                                        to={`/admin/orgreceiversettings/org/${eachOrgSetting.organizationName}/receiver/${eachOrgSetting.name}/action/clone`}
-                                        key={`receiver-clone-link-${eachOrgSetting.name}-${index}`}
-                                    >
-                                        Clone
-                                    </NavLink>
-                                    <Button
-                                        type={"button"}
-                                        onClick={() =>
-                                            ShowDeleteConfirm(
-                                                eachOrgSetting.name
-                                            )
-                                        }
-                                    >
-                                        Delete
-                                    </Button>
-                                </ButtonGroup>
-                            </td>
+            {!orgReceiverSettings ? (
+                <Spinner />
+            ) : (
+                <Table
+                    key="orgreceiversettingstable"
+                    aria-label="Organization Receivers"
+                    striped
+                    fullWidth
+                >
+                    <thead>
+                        <tr>
+                            <th scope="col">Name</th>
+                            <th scope="col">Org Name</th>
+                            <th scope="col">Topic</th>
+                            <th scope="col">Status</th>
+                            <th scope="col">Meta</th>
+                            <th scope="col">Action</th>
+                            <th scope="col">
+                                <NavLink
+                                    className="usa-button"
+                                    to={`/admin/orgnewsetting/org/${props.orgname}/settingtype/receiver`}
+                                    key={`receiver-create-link`}
+                                >
+                                    New
+                                </NavLink>
+                            </th>
                         </tr>
-                    ))}
-                </tbody>
-            </Table>
-            <ConfirmDeleteSettingModal
-                uniquid={deleteItemId}
-                onConfirm={doDeleteSetting}
-                modalRef={modalRef}
-            >
-                Delete
-            </ConfirmDeleteSettingModal>
+                    </thead>
+                    <tbody id="tBodyOrgReceiver" className="font-mono-2xs">
+                        {orgReceiverSettings.map((eachOrgSetting, index) => (
+                            <tr
+                                key={`receiver-row-${eachOrgSetting.name}-${index}`}
+                            >
+                                <td>{eachOrgSetting.name}</td>
+                                <td>
+                                    {eachOrgSetting?.organizationName || "-"}
+                                </td>
+                                <td>{eachOrgSetting.topic || ""}</td>
+                                <td>{eachOrgSetting.customerStatus || ""}</td>
+                                <td>
+                                    <DisplayMeta metaObj={eachOrgSetting} />
+                                </td>
+                                <td colSpan={2}>
+                                    <ButtonGroup type="segmented">
+                                        <NavLink
+                                            className="usa-button"
+                                            to={`/admin/orgreceiversettings/org/${eachOrgSetting.organizationName}/receiver/${eachOrgSetting.name}/action/edit`}
+                                            key={`receiver-edit-link-${eachOrgSetting.name}-${index}`}
+                                        >
+                                            Edit
+                                        </NavLink>
+                                        <NavLink
+                                            className="usa-button"
+                                            to={`/admin/orgreceiversettings/org/${eachOrgSetting.organizationName}/receiver/${eachOrgSetting.name}/action/clone`}
+                                            key={`receiver-clone-link-${eachOrgSetting.name}-${index}`}
+                                        >
+                                            Clone
+                                        </NavLink>
+                                    </ButtonGroup>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+            )}
         </section>
     );
 }
