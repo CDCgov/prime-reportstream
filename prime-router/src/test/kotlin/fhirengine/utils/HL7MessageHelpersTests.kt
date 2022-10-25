8cf899a00dd373bf068c4eeae3612a404128a676
Carlos Felix <carlos.felix@agile6.com>
2022-10-25 09:27:16 -0400

    Added decode unit test

--- prime-router/src/test/kotlin/fhirengine/utils/HL7MessageHelpersTests.kt
+++ prime-router/src/test/kotlin/fhirengine/utils/HL7MessageHelpersTests.kt
@@ -4,9 +4,12 @@ import assertk.assertThat
 import assertk.assertions.isEmpty
 import assertk.assertions.isEqualTo
 import assertk.assertions.isFailure
+import assertk.assertions.isNotEmpty
 import assertk.assertions.isNotNull
 import assertk.assertions.isTrue
 import assertk.assertions.startsWith
+import ca.uhn.hl7v2.util.Terser
+import gov.cdc.prime.router.ActionLogger
 import gov.cdc.prime.router.Hl7Configuration
 import gov.cdc.prime.router.Metadata
 import gov.cdc.prime.router.Receiver
@@ -119,6 +122,35 @@ class HL7MessageHelpersTests {
                 3 -> assertThat(s).isEqualTo(messages[1])
             }
         }
+    }
+
+    @Test
+    fun `test decoding generated batch HL7 file`() {
+        // Make sure the HAPI HL7 v2 library can decode the batch file
+        val receiver = Receiver(
+            "name", "org", Topic.FULL_ELR.json_val,
+            translation = Hl7Configuration(
+                receivingApplicationName = "appName", receivingApplicationOID = null,
+                receivingFacilityName = "facName", receivingFacilityOID = null, receivingOrganization = null,
+                messageProfileId = null
+            )
+        )
+        val hl7Message = """
+MSH|^~\&||Any facility USA^31D2827476^CLIA|0.0.0.0.1|0.0.0.0.1|20200331082902||ORU^R01^ORU_R01|63774|T|2.5.1||||||||||
+PID|1|1731-TEST734|5000104^^^^MR||TEST^RESULTS||19950615|M||U||||||S|||111-11-1111
+PV1|1|O|||||||||||||||||1731-T734-40923
+OBR|6|||40923^TONSILLITIS -neg strep screen|||19980601184619|||||||||51789||OV
+OBX|1|ST|MLI-4000.15^TEMPERATURE||97.7|deg f|||||R|||19980601184619
+        """.trimIndent()
+
+        val batchFile = HL7MessageHelpers.batchMessages(listOf(hl7Message, hl7Message), receiver)
+        val messages = HL7Reader(ActionLogger()).getMessages(batchFile)
+        assertThat(messages).isNotEmpty()
+        assertThat(messages.size).isEqualTo(2)
+        val a = Terser(messages[0])
+        print(a)
+        assertThat(Terser(messages[0]).get("/PATIENT_RESULT/PATIENT/PID-2")).isEqualTo("1731-TEST734")
+        assertThat(Terser(messages[1]).get("/PATIENT_RESULT/PATIENT/PID-2")).isEqualTo("1731-TEST734")
     }
 
     @Test
