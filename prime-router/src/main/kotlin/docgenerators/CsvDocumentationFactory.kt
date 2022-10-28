c8e16a3a32162c1091ee8d7b6d9343099215bcb8
Maurice Reeves <maurice.e.reeves@omb.eop.gov>
2022-10-28 15:22:15 -0400

    CSV data dictionary is done

--- prime-router/src/main/kotlin/docgenerators/CsvDocumentationFactory.kt
+++ prime-router/src/main/kotlin/docgenerators/CsvDocumentationFactory.kt
@@ -10,17 +10,36 @@ import java.io.FileWriter
 
 /** Writes out the schema documentation in a CSV data dictionary format */
 object CsvDocumentationFactory : DocumentationFactory(), Logging {
+    private const val delimiter = "|"
+    private val newlineRegex = "[\r\n]".toRegex()
+
+    /** Get the file extension for this type of file */
     override val fileExtension: String
         get() = "csv"
 
+    /** Collects and returns the format for the schema in the way that the output understands */
     override fun getSchemaDocumentation(schema: Schema) = sequence {
         schema.elements.sortedBy { it -> it.name }.forEach { element ->
             yield(
-                (element.name + "|" + (element.type?.name ?: ""))
+                listOf(
+                    element.name,
+                    (element.type?.name ?: ""),
+                    element.csvFields?.get(0)?.name ?: "",
+                    element.valueSet ?: "",
+                    element.hl7Field ?: "",
+                    element.documentation?.replace(newlineRegex, "") ?: "",
+                    element.table ?: "",
+                    element.tableColumn ?: "",
+                    element.mapper ?: "",
+                    element.default ?: "",
+                    element.cardinality ?: "",
+                    element.pii ?: "false"
+                ).joinToString(delimiter)
             )
         }
     }
 
+    /** Writes the documentation for the schema */
     override fun writeDocumentationForSchema(
         schema: Schema,
         outputDir: String,
@@ -38,7 +57,10 @@ object CsvDocumentationFactory : DocumentationFactory(), Logging {
         // create a csv format
         val csvFormat = CSVFormat.Builder
             .create(CSVFormat.DEFAULT)
-            .setHeader("field_name", "type")
+            .setHeader(
+                "field_name", "type", "csv_field_name", "valueSet", "hl7Field", "documentation", "table",
+                "tableColumn", "mapper", "default", "cardinality", "pii"
+            )
             .build()
         // create a writer
         val csvWriter = CSVPrinter(
