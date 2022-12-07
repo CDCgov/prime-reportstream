8fb2d1848faba27b237b781fe81211a3ab730434
Penelope Lischer <102491809+penny-lischer@users.noreply.github.com>
2022-12-07 08:53:39 -0800

    7301-Permissions/FeatureFlags at organization level. (#7555)
    * 7301-FeatureFlags at organization level.
    Creation of crud operations.
    Creation of new permission tables
    
    * 7301-FeatureFlags at organization level.
    Updated PUT route
    
    * 7301-FeatureFlags at organization level.
    Added organization_name index on permission_organization table.
    Added getAllPermissions API
    Added assignPermissionToOrganization API
    Added deletePermissionFromOrganization API

--- prime-router/build.gradle.kts
+++ prime-router/build.gradle.kts
@@ -822,4 +822,5 @@ dependencies {
     testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.25")
     testImplementation("io.ktor:ktor-client-mock:$ktorVersion")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.1")
+    implementation(kotlin("script-runtime"))
 }
