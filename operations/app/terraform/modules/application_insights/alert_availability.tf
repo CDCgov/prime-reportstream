6d157eec7a54b9d503dfe35a998f683e3f3dd46a
lucasdze <lucasdze@gmail.com>
2022-12-15 09:00:12 -0600

    add-metabase-healthcheck-to-functionapp

--- operations/app/terraform/modules/application_insights/alert_availability.tf
+++ operations/app/terraform/modules/application_insights/alert_availability.tf
@@ -97,7 +97,7 @@ resource "azurerm_application_insights_web_test" "metabase_test" {
   enabled                 = true
   retry_enabled           = true
   geo_locations           = ["us-va-ash-azr", "us-ca-sjc-azr", "us-fl-mia-edge"]
-  configuration = <<XML
+  configuration           = <<XML
 <WebTest Name="" Id="" Enabled="True" CssProjectStructure="" CssIteration="" Timeout="60" WorkItemIds="" xmlns="http://microsoft.com/schemas/VisualStudio/TeamTest/2010" Description="" CredentialUserName="" CredentialPassword="" PreAuthenticate="True" Proxy="default" StopOnError="False" RecordedResultFile="" ResultsLocale="">
       <Items>
         <Request Method="GET" Version="1.1" Url="https://${var.environment}.prime.cdc.gov/metabase/api/health" ThinkTime="0" Timeout="60" ParseDependentRequests="False" FollowRedirects="True" RecordResult="True" Cache="False" ResponseTimeGoal="0" Encoding="utf-8" ExpectedHttpStatusCode="200" ExpectedResponseUrl="" ReportingName="" IgnoreHttpStatusCode="False"/>
