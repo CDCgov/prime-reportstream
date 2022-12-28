d77011de6da51a3591046f9ea7505fc789ec486f
Kevin Haube <khaube@fearless.tech>
2022-12-28 12:59:00 -0500

    Update USExtLink and USLink to include all anchor props

--- frontend-react/src/components/USLink.tsx
+++ frontend-react/src/components/USLink.tsx
@@ -1,26 +1,39 @@
-import React, { PropsWithChildren } from "react";
+import React, { AnchorHTMLAttributes, PropsWithChildren } from "react";
 import { Link, NavLink } from "react-router-dom";
 
 // Known issue with the `PropsWithChildren generic in React 18,
 // so I wrote this in a way that we can just remove `<any>` and be
 // okay if we update our React version.
-interface USNavLinkProps extends PropsWithChildren<any> {
-    href: string;
-    className?: string;
-    activeClassName?: string;
-}
+type USNavLinkProps = PropsWithChildren<any> &
+    AnchorHTMLAttributes<any> & { className?: string };
 type USLinkProps = Omit<USNavLinkProps, "activeClassName">;
 
 /** A single link for rendering standard links */
-export const USLink = ({ children, href, className }: USLinkProps) => (
-    <Link to={href} className={`usa-link ${className}`}>
+export const USLink = ({
+    children,
+    href,
+    className,
+    rel,
+    target,
+}: USLinkProps) => (
+    <Link
+        target={target}
+        rel={rel}
+        to={href}
+        className={`usa-link ${className}`}
+    >
         {children}
     </Link>
 );
 
 /** A single link for rendering external links */
 export const USExtLink = ({ href, className, children }: USLinkProps) => (
-    <USLink href={href} className={`usa-link--external ${className}`}>
+    <USLink
+        target="_blank"
+        rel="noreferrer noopener"
+        href={href}
+        className={`usa-link--external ${className}`}
+    >
         {children}
     </USLink>
 );
