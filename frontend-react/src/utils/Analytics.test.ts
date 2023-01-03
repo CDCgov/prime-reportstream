d2124bbef346a6f5e01871e5f37565b0130c4a38
Stephen Kao <stephenkao@navapbc.com>
2023-01-03 16:59:52 -0500

    stephencommit: attempt to fix Jest test

--- frontend-react/src/utils/Analytics.test.ts
+++ frontend-react/src/utils/Analytics.test.ts
@@ -1,31 +1,17 @@
 import { getAppInsights } from "../TelemetryService";
-
 import { TrackAppInsightEvent } from "./Analytics";
 
-let mockAppInsights = jest.fn(() => {
-    console.log("Here");
-    return {
-        loadAppInsights() {},
-        trackEvent: jest.fn(),
-        trackException: jest.fn(),
-        addTelemetryInitializer: jest.fn(),
-        context: {
-            getSessionId() {
-                return "test-session-id";
-            },
-        },
-    };
-});
+const mockAppInsights = {
+    trackEvent: jest.fn(),
+};
 
 jest.mock("../TelemetryService", () => ({
     ...jest.requireActual("../TelemetryService"),
-    getAppInsights: () => mockAppInsights(),
+    getAppInsights: () => mockAppInsights,
 }));
 
 describe("Analytics", () => {
     describe("TrackAppInsightEvent", () => {
-        let appInsights = getAppInsights();
-
         test("calls trackEvent with the correct event and event data", () => {
             const eventName = "Event Name";
             const eventData = {
@@ -37,10 +23,17 @@ describe("Analytics", () => {
 
             TrackAppInsightEvent(eventName, eventData);
 
-            expect(appInsights?.trackEvent).toBeCalledWith({
-                name: eventName,
-                eventData,
-            });
+            expect(mockAppInsights.trackEvent).toBeCalledWith(
+                {
+                    name: eventName,
+                },
+                {
+                    tableFilter: {
+                        endRange: "1/20/2021",
+                        startRange: "1/1/2021",
+                    },
+                }
+            );
         });
     });
 });
