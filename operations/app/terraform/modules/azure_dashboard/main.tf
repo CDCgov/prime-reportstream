f4b122b1282ed771832d43594141d6e84050f121
lucasdze <lucasdze@gmail.com>
2023-02-01 09:37:29 -0600

    add-appinsight-dashboard-to-terraform

--- operations/app/terraform/modules/azure_dashboard/main.tf
+++ operations/app/terraform/modules/azure_dashboard/main.tf
@@ -10,24 +10,8 @@ resource "azurerm_dashboard" "az_dashboard" {
   }
   dashboard_properties = templatefile("../../../../../templates/az_dashboard_json.tpl",
     {
-      subscription_id  = data.azurerm_subscription.current.subscription_id
-      appinsights_name = "${var.resource_prefix}-appinsights"
-      resource_group_name = var.resource_group
-  })
-}
-
-resource "azurerm_dashboard" "az_perf_dashboard" {
-  name                = "TestPerformDashboard"
-  location            = var.location
-  resource_group_name = var.resource_group
-  tags = {
-    source      = "terraform"
-    environment = var.environment
-  }
-  dashboard_properties = templatefile("../../../../../templates/az_performance_dashboard_json.tpl",
-    {
-      subscription_id  = data.azurerm_subscription.current.subscription_id
-      appinsights_name = "${var.resource_prefix}-appinsights"
+      subscription_id     = data.azurerm_subscription.current.subscription_id
+      appinsights_name    = "${var.resource_prefix}-appinsights"
       resource_group_name = var.resource_group
   })
 }
