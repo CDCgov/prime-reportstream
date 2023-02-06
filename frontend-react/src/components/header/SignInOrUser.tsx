7aef74e52967df1d1d81adb58bd611a7f1af1b61
Joseph Andersen <josephandersen@navapbc.com>
2023-02-06 10:43:40 -0600

    Reduce the number of userinfo calls to Okta #8141 (#8159)
    * Reduce the number of userinfo calls to Okta #8141

--- frontend-react/src/components/header/SignInOrUser.tsx
+++ frontend-react/src/components/header/SignInOrUser.tsx
@@ -1,38 +1,22 @@
-import { useOktaAuth } from "@okta/okta-react";
 import { Button } from "@trussworks/react-uswds";
-import { useEffect, useState } from "react";
 
 import { logout } from "../../utils/UserUtils";
 import config from "../../config";
 import { USLink } from "../USLink";
+import { useSessionContext } from "../../contexts/SessionContext";
 
 const { IS_PREVIEW } = config;
 
 const SignInOrUser = () => {
-    const { oktaAuth, authState } = useOktaAuth();
-    const [user, setUser] = useState("");
+    const { user } = useSessionContext();
+    const userDisplay = user?.email ?? "unknown user";
 
-    useEffect(() => {
-        if (authState?.isAuthenticated) {
-            oktaAuth
-                .getUser()
-                .then((cl) => setUser(cl.email ? cl.email : "unknown user"));
-        }
-    });
-
-    return authState?.isAuthenticated ? (
+    return user ? (
         <div className="prime-user-account">
-            <span id="emailUser">{user}</span>
+            <span id="emailUser">{userDisplay}</span>
             <br />
             {/* Okta handles our logoutRedirectUri, this should _not_ be an anchor tag! */}
-            <Button
-                id="logout"
-                type="button"
-                unstyled
-                onClick={() => {
-                    logout(oktaAuth);
-                }}
-            >
+            <Button id="logout" type="button" unstyled onClick={logout}>
                 Log out
             </Button>
         </div>
