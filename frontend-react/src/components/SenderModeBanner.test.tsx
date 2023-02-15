2fe657d216de3ff7d68b28777c4698bc11f7c74d
Stephen Kao <stephenkao@navapbc.com>
2023-02-15 10:04:08 -0500

    Experience-8161: Remove makeOktaHook logic (#8323)
    This changeset removes the makeOktaHook logic that's fed into SessionProvider and a few test utilities.  There's a temporary workaround to fall back on an empty object in SessionProvider from the `useOktaAuth` call -- this is to account for components being rendered outside of Okta's Security Provider, in which case `useOktaAuth` calls return `null`.  This will be fixed as part of [#8292](https://app.zenhub.com/workspaces/experience-607d9d5e68b95200150fec37/issues/gh/cdcgov/prime-reportstream/8292); it'd just be easier to remove this logic beforehand.
    
    Ultimately, there's no reason for us to have to pass a stand-in for `useOktaAuth` as a prop when we could just mock it if we really need to.

--- frontend-react/src/components/SenderModeBanner.test.tsx
+++ frontend-react/src/components/SenderModeBanner.test.tsx
@@ -1,10 +1,7 @@
 import { screen } from "@testing-library/react";
 
 import { orgServer } from "../__mocks__/OrganizationMockServer";
-import {
-    makeOktaHook,
-    renderWithFullAppContext,
-} from "../utils/CustomRenderUtils";
+import { renderWithFullAppContext } from "../utils/CustomRenderUtils";
 import { mockSessionContext } from "../contexts/__mocks__/SessionContext";
 import { MemberType } from "../hooks/UseOktaMemberships";
 
@@ -33,14 +30,7 @@ describe("SenderModeBanner", () => {
             isUserReceiver: false,
             isUserSender: true,
         });
-        renderWithFullAppContext(
-            <SenderModeBanner />,
-            makeOktaHook({
-                authState: {
-                    isAuthenticated: true,
-                },
-            })
-        );
+        renderWithFullAppContext(<SenderModeBanner />);
         const text = await screen.findByText("Learn more about onboarding.");
         expect(text).toBeInTheDocument();
     });
