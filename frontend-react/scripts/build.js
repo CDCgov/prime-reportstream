66cf7cc519109ddd1eec733e015461019e0c31b8
Joseph Andersen <josephandersen@navapbc.com>
2023-02-22 12:28:03 -0600

    wait until .env is loaded before production override

--- frontend-react/scripts/build.js
+++ frontend-react/scripts/build.js
@@ -1,7 +1,3 @@
-// Do this as the first thing so that any code reading it knows the right env.
-process.env.BABEL_ENV = "production";
-process.env.NODE_ENV = "production";
-
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
 // terminate the Node.js process with a non-zero exit code.
@@ -11,6 +7,10 @@ process.on("unhandledRejection", (err) => {
 
 // Ensure environment variables are read.
 require("../config/env");
+
+// Do this as the first thing so that any code reading it knows the right env.
+process.env.BABEL_ENV = "production";
+process.env.NODE_ENV = "production";
 
 const path = require("path");
 
