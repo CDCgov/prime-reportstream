7d626ad24cdbaa350b3c8fbc707924e924458381
Penny Lischer <penny.lischer@agile6.com>
2023-03-10 10:15:32 -0800

    8395 [Manage Public Key] - Changed <div> to <GridContainer>. - Removed extra <div>

--- frontend-react/src/components/ManagePublicKey/ManagePublicKey.tsx
+++ frontend-react/src/components/ManagePublicKey/ManagePublicKey.tsx
@@ -3,20 +3,17 @@ import React from "react";
 import { AuthElement } from "../AuthElement";
 import { withCatchAndSuspense } from "../RSErrorBoundary";
 import { MemberType } from "../../hooks/UseOktaMemberships";
+import { GridContainer } from "@trussworks/react-uswds";
 
 export function ManagePublicKey() {
     return (
-        <div className="grid-container margin-bottom-5 tablet:margin-top-6">
-            <div>
-                <h1 className="margin-top-0 margin-bottom-5">
-                    Manage Public Key
-                </h1>
-                <p className="font-sans-md">
-                    Send your public key to begin the REST API authentication
-                    process.
-                </p>
-            </div>
-        </div>
+        <GridContainer className="padding-bottom-5 tablet:padding-top-6">
+            <h1 className="margin-top-0 margin-bottom-5">Manage Public Key</h1>
+            <p className="font-sans-md">
+                Send your public key to begin the REST API authentication
+                process.
+            </p>
+        </GridContainer>
     );
 }
 
