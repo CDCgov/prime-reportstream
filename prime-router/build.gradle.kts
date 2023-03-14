888a59bb2a5779124f760de021b82f32823b3448
Stephen Nesman <stephen.nesman@semanticbits.com>
2023-03-14 11:27:06 -0400

    Bump hapi-fhir-structures-r4

--- prime-router/build.gradle.kts
+++ prime-router/build.gradle.kts
@@ -746,9 +746,7 @@ dependencies {
     // Pin snakeyaml since it is getting included regardless of exclude attempts
     implementation("org.yaml:snakeyaml:1.33")
     implementation("io.github.linuxforhealth:hl7v2-fhir-converter:1.0.19")
-    implementation("ca.uhn.hapi.fhir:hapi-fhir-structures-r4:6.2.5")
-    // Pin org.hl7.fhir.utilities to ca.uhn.hapi.fhir:org.hl7.fhir.utilities@5.6.92 until ca.uhn.hapi.fhir:hapi-fhir-structures-r4 can be updated
-    implementation("ca.uhn.hapi.fhir:org.hl7.fhir.utilities:5.6.92")
+    implementation("ca.uhn.hapi.fhir:hapi-fhir-structures-r4:6.4.0")
     implementation("ca.uhn.hapi:hapi-base:2.3")
     implementation("ca.uhn.hapi:hapi-structures-v251:2.3")
     implementation("com.googlecode.libphonenumber:libphonenumber:8.13.5")
