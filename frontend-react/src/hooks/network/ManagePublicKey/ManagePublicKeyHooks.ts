8bf1854378d887b2c37cf15d772746245abd7f7a
Penny Lischer <penny.lischer@agile6.com>
2023-03-18 12:01:20 -0700

    8396 - Broke up function to check for file size and file type. - Update to css - Removed PEM logic from TemporarySettingsAPITypes.ts

--- frontend-react/src/hooks/network/ManagePublicKey/ManagePublicKeyHooks.ts
+++ frontend-react/src/hooks/network/ManagePublicKey/ManagePublicKeyHooks.ts
@@ -2,7 +2,9 @@ import {
     PAYLOAD_MAX_BYTES,
     PAYLOAD_MAX_KBYTES,
 } from "../../../utils/FileUtils";
-import { Format } from "../../../utils/TemporarySettingsAPITypes";
+
+export const CONTENT_TYPE = "application/x-x509-ca-cert";
+export const FORMAT = "PEM";
 
 export enum ManagePublicKeyAction {
     SELECT_SENDER = "selectSender",
@@ -12,21 +14,22 @@ export enum ManagePublicKeyAction {
     PUBLIC_KEY_NOT_SAVED = "publicKeyNotSaved",
 }
 
-export function validateFileSelectedState(file: File) {
+// Can this be moved and reused???
+export function validateFileType(file: File, fileType: string) {
     try {
         // look at the filename extension.
         const fileNameArray = file.name.split(".");
         const uploadFileExtension = fileNameArray[fileNameArray.length - 1];
 
-        if (uploadFileExtension.toUpperCase() !== Format.PEM) {
+        if (uploadFileExtension.toUpperCase() !== fileType) {
             return {
-                fileError: "The file type must be of type .pem",
+                fileTypeError: `The file type must be of type .'${fileType.toLowerCase()}'`,
             };
         }
 
         if (file.size > PAYLOAD_MAX_BYTES) {
             return {
-                fileError: `The file '${file.name}' is too large. The maximum file size is ${PAYLOAD_MAX_KBYTES}k`,
+                fileTypeError: `The file '${file.name}' is too large. The maximum file size is ${PAYLOAD_MAX_KBYTES}k`,
             };
         }
 
@@ -36,7 +39,26 @@ export function validateFileSelectedState(file: File) {
     } catch (err: any) {
         console.warn(err);
         return {
-            fileError: `An unexpected error happened: '${err.toString()}'`,
+            fileTypeError: `An unexpected error happened: '${err.toString()}'`,
+        };
+    }
+}
+
+export function validateFileSize(file: File) {
+    try {
+        if (file.size > PAYLOAD_MAX_BYTES) {
+            return {
+                fileSizeError: `The file '${file.name}' is too large. The maximum file size is ${PAYLOAD_MAX_KBYTES}k`,
+            };
+        }
+
+        return {
+            fileName: file.name,
+        };
+    } catch (err: any) {
+        console.warn(err);
+        return {
+            fileSizeError: `An unexpected error happened: '${err.toString()}'`,
         };
     }
 }
