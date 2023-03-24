e4249883f13c9fb851d46b11f5877fd6a5321d66
Supriya Addagada <supriya.addagada@geha.com>
2023-03-24 11:02:21 -0500

    pr_comment

---
+++ .github/actions/pr-comment/comment.js
@@ -0,0 +1,25 @@
+const github = require('@actions/github');
+
+async function run() {
+  const { issue_number, github_token, owner, repo, website_link } = process.env
+
+  const octokit = new github.GitHub(github_token);
+
+  try {
+    const { data } = await octokit.issues.createComment({
+      owner,
+      repo,
+      issue_number,
+      // We unfortunately need to use replace to get rid
+      // of extraneous double quotes
+      body: `[Review website here](${website_link.replace(/"/gs, '')})`
+    });
+
+    // for debugging, lets log the created comment
+    console.log('created comment', data);
+  } catch (err) {
+    throw err
+  }
+}
+
+run();
