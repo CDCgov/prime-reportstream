8c9dbf06787bc339f97781368a809aa161ba67a5
Supriya Addagada <supriya.addagada@geha.com>
2023-03-24 11:35:42 -0500

    comment.js

--- .github/actions/pr-comment/comment.js
+++ .github/actions/pr-comment/comment.js
@@ -1,9 +1,9 @@
-// const github = require('@actions/github');
+import { GitHub } from '@actions/github';
 
 async function run() {
   const { issue_number, github_token, owner, repo, website_link } = process.env
 
-  const octokit = new github.GitHub(github_token);
+  const octokit = new GitHub(github_token);
 
   try {
     const { data } = await octokit.issues.createComment({
