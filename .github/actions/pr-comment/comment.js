f35c41d599f1b6f82729ea79e903dac8a59f8251
Supriya Addagada <supriya.addagada@geha.com>
2023-03-24 11:50:12 -0500

    comment.js

--- .github/actions/pr-comment/comment.js
+++ .github/actions/pr-comment/comment.js
@@ -1,4 +1,4 @@
-import { GitHub } from '@actions/github';
+const github = require('@actions/github');
 
 async function run() {
   const { issue_number, github_token, owner, repo, website_link } = process.env
