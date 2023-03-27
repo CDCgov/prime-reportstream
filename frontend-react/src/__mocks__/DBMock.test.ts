41635e4b398024219bb73fe5e7b4a8996fb167b0
Joseph Andersen <josephandersen@navapbc.com>
2023-03-27 14:55:47 -0500

    test cleanup

--- frontend-react/src/__mocks__/DBMock.test.ts
+++ frontend-react/src/__mocks__/DBMock.test.ts
@@ -16,66 +16,66 @@ describe("DBMock", () => {
         reset();
     });
 
-    describe("createMany", () => {
-        test("multiple no initial", () => {
+    describe("model createMany", () => {
+        const createManyScenarios: {
+            label: string;
+            args: [number, Partial<RSOrganizationSettings>[] | undefined];
+        }[] = [
+            {
+                label: "predictably random",
+                args: [2, undefined],
+            },
+            {
+                label: "all with initial values",
+                args: [2, [{ name: "test1" }, { name: "test2" }]],
+            },
+            {
+                label: "some with initial values",
+                args: [2, [{ stateCode: "LA" }]],
+            },
+        ];
+
+        test.each(createManyScenarios)("$label", ({ args }) => {
+            const [num, initial = []] = args;
+            db.organizationSettings.createMany(...args);
+            expect(db.organizationSettings.count()).toStrictEqual(num);
+            const all = db.organizationSettings.getAll();
+            for (const [i, init] of initial.entries()) {
+                for (const [k, v] of Object.entries(init)) {
+                    // check initialized value
+                    expect(
+                        all[i][k as keyof (typeof all)[number]]
+                    ).toStrictEqual(v);
+                }
+            }
+            if (initial.length === 0) {
+                // check is random
+                expect(all[0]).not.toStrictEqual(all[1]);
+                reset();
+                db.organizationSettings.createMany(2);
+                // check is predictably random
+                expect(db.organizationSettings.getAll()).toStrictEqual(all);
+            }
+        });
+    });
+
+    describe("json serialized return methods", () => {
+        test("getAllJson", () => {
             db.organizationSettings.createMany(2);
-            expect(db.organizationSettings.count()).toStrictEqual(2);
+            const records = JSON.parse(
+                JSON.stringify(db.organizationSettings.getAll())
+            );
+            expect(db.organizationSettings.getAllJson()).toStrictEqual(records);
         });
 
-        test("multiple all initial", () => {
-            db.organizationSettings.createMany(2, [
-                {
-                    name: "test1",
-                },
-                {
-                    name: "test2",
-                },
-            ]);
-            expect(db.organizationSettings.count()).toStrictEqual(2);
-            const all = db.organizationSettings.getAll();
-            expect(all[0].name).toStrictEqual("test1");
-            expect(all[1].name).toStrictEqual("test2");
+        test("findFirstJson", () => {
+            db.organizationSettings.createMany(2);
+            const record = db.organizationSettings.getAllJson()[0];
+            const findRecord = db.organizationSettings.findFirstJson({
+                where: { name: { equals: record.name } },
+            });
+            expect(record).toStrictEqual(findRecord);
         });
-
-        test("multiple some initial", () => {
-            db.organizationSettings.createMany(2, [{ stateCode: "LA" }]);
-            expect(db.organizationSettings.count()).toStrictEqual(2);
-            const all = db.organizationSettings.getAll();
-            expect(all[0].stateCode).toStrictEqual("LA");
-        });
-    });
-
-    test("Random record creation", () => {
-        db.organizationSettings.createMany(2);
-        expect(db.organizationSettings.count()).toStrictEqual(2);
-        const records = db.organizationSettings.getAll();
-        expect(records[0]).not.toStrictEqual(records[1]);
-    });
-
-    test("Predictable random record creation", () => {
-        db.organizationSettings.createMany(2);
-        const records = db.organizationSettings.getAll();
-        reset();
-        db.organizationSettings.createMany(2);
-        expect(db.organizationSettings.getAll()).toStrictEqual(records);
-    });
-
-    // TODO: use a scenario array of function names for json tests?
-    test("getAllJson", () => {
-        db.organizationSettings.createMany(2);
-        const records = JSON.parse(
-            JSON.stringify(db.organizationSettings.getAll())
-        );
-        expect(db.organizationSettings.getAllJson()).toStrictEqual(records);
-    });
-
-    test("findFirstJson", () => {
-        db.organizationSettings.createMany(2);
-        const record = db.organizationSettings.getAllJson()[0];
-        const findRecord = db.organizationSettings.findFirstJson({
-            where: { name: { equals: record.name } },
-        });
-        expect(record).toStrictEqual(findRecord);
     });
 
     describe("enhanced rest handler", () => {
