f0dbae182b58b02a7049440ff8b52c21dea8f9e8
Michael Kalish <michael.kalish@focusconsulting.io>
2023-03-28 13:10:43 -0400

    Platform/mkalish/8472 API Keys endpoints (#8678)
    * 8472: add getApiKeys endpoint
    
    * 8472: add POST endpoint for api public keys
    
    * Add OpenAPI spec
    
    * Extract the logic for cycling keys to the JwkSet companion

--- prime-router/src/main/kotlin/tokens/Jwk.kt
+++ prime-router/src/main/kotlin/tokens/Jwk.kt
@@ -114,6 +114,17 @@ data class JwkSet(
     companion object {
 
         /**
+         *
+         * Reads the MAX_NUM_KEY_PER_SCOPE environment variable, otherwise defaulting to 10 that dictates
+         * how many keys can be linked to a scope for an organization
+         *
+         * @return the maximum number of keys can be configured for a scope
+         */
+        fun getMaximumNumberOfKeysPerScope(): Int {
+            return (System.getenv("MAX_NUM_KEY_PER_SCOPE") ?: "10").toInt()
+        }
+
+        /**
          * Copy an old set of authorizations to a new set, and add one to it, if needed.
          * This whole list of lists thing is confusing:
          * The 'orig' obj, and the return val, are list of JwkSets.   And each JwkSet has a list of Jwks.
@@ -145,6 +156,42 @@ data class JwkSet(
                 newJwkSetList.add(JwkSet(newScope, listOf(newJwk)))
             }
             return newJwkSetList
+        }
+
+        /**
+         *
+         * Adds a new key to the passed scope if the number of keys is less than the
+         * configured maximumNumberOfKeysPerScope, which is configured with
+         * an environment variable MAX_NUM_KEY_PER_SCOPE and defaults to 10.
+         *
+         * If there are already the maximum number of keys associated with the scope, the oldest (first in the list) key
+         * is dropped and the new key is added
+         *
+         * @param jwkSets - The current list of JwkSets
+         * @param scope - The scope to add the key for
+         * @param jwk - The Jwk to add to the scope
+         * @return The updated organization and keys
+         *
+         */
+        fun addKeyToScope(
+            jwkSets: List<JwkSet>?,
+            scope: String,
+            jwk: Jwk
+        ): List<JwkSet> {
+            val nullSafeJwkSets = jwkSets ?: emptyList()
+            val updatedJwkSet =
+                ((jwkSets ?: emptyList()).find { it.scope == scope } ?: JwkSet(scope, emptyList())).let { jwkSet ->
+                    {
+                        if (jwkSet.keys.size >= getMaximumNumberOfKeysPerScope()) {
+                            val updatedKeys = jwkSet.keys.drop(1) + listOf(jwk)
+                            JwkSet(scope, updatedKeys)
+                        } else {
+                            JwkSet(scope, jwkSet.keys + listOf(jwk))
+                        }
+                    }
+                }()
+
+            return nullSafeJwkSets.filter { jwkSet -> jwkSet.scope != scope } + listOf(updatedJwkSet)
         }
     }
 }
