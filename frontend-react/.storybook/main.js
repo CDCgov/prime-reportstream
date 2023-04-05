6eb3ac570b3ab96f7e6ff530c234a9a3fe11d6c8
Stephen Nesman <stephen.nesman@semanticbits.com>
2023-04-05 13:32:56 -0400

    Bump storybook

--- frontend-react/.storybook/main.js
+++ frontend-react/.storybook/main.js
@@ -1,22 +1,17 @@
 module.exports = {
-    stories: [
-        "../src/**/*.stories.mdx",
-        "../src/**/*.stories.@(js|jsx|ts|tsx)",
-    ],
-    addons: [
-        "@storybook/addon-links",
-        "@storybook/addon-essentials",
-        "@storybook/addon-interactions",
-        "@storybook/addon-a11y",
-    ],
-    framework: "@storybook/react",
-    core: {
-        builder: "@storybook/builder-webpack5",
-    },
-    refs: {
-        trussworks: {
-            title: "Trussworks Storybook",
-            url: "https://trussworks.github.io/react-uswds/",
-        },
-    },
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-a11y", "@storybook/addon-mdx-gfm"],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {}
+  },
+  refs: {
+    trussworks: {
+      title: "Trussworks Storybook",
+      url: "https://trussworks.github.io/react-uswds/"
+    }
+  },
+  docs: {
+    autodocs: true
+  }
 };
