d7c9113b52d976c58d889561fe73c9aafb695fe7
etanb <etan.berkowitz@gmail.com>
2023-04-10 15:26:21 -0700

    basics of storybook

--- frontend-react/src/components/shared/Table/Table.stories.tsx
+++ frontend-react/src/components/shared/Table/Table.stories.tsx
@@ -8,9 +8,77 @@ export default {
     component: Table,
 } as ComponentMeta<typeof Table>;
 
-export const Default: ComponentStory<typeof Table> = (args) => (
-    <Table>
-        <thead></thead>
-        <tbody></tbody>
-    </Table>
-);
+function DefaultTableContent() {
+    return (
+        <>
+            <thead>
+                <tr>
+                    <th>Shape</th>
+                    <th>Sides</th>
+                    <th>Note</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Triangle</td>
+                    <td>3</td>
+                    <td>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua.
+                    </td>
+                </tr>
+                <tr>
+                    <td>Square</td>
+                    <td>4</td>
+                    <td>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua.
+                    </td>
+                </tr>
+                <tr>
+                    <td>Pentagon</td>
+                    <td>5</td>
+                    <td>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua.
+                    </td>
+                </tr>
+            </tbody>
+        </>
+    );
+}
+
+export const Borderless: ComponentStory<typeof Table> = (args) => {
+    return (
+        <Table borderless>
+            <DefaultTableContent />
+        </Table>
+    );
+};
+
+export const Bordered: ComponentStory<typeof Table> = (args) => {
+    return (
+        <Table>
+            <DefaultTableContent />
+        </Table>
+    );
+};
+
+export const Striped: ComponentStory<typeof Table> = (args) => {
+    return (
+        <Table striped>
+            <DefaultTableContent />
+        </Table>
+    );
+};
+
+export const Scrollable: ComponentStory<typeof Table> = (args) => {
+    return (
+        <Table scrollable>
+            <DefaultTableContent />
+        </Table>
+    );
+};
