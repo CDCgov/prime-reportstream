f25ed83c1c267443ebaa296fd6fb95183d4a9d96
Supriya Addagada <supriya.addagada@geha.com>
2023-04-11 22:04:10 -0500

    stale items

--- .github/scripts/stale_items_report/StalePullRequests.ps1
+++ .github/scripts/stale_items_report/StalePullRequests.ps1
@@ -13,25 +13,25 @@ $BasicCreds = Get-BasicAuthCreds -Username "SupriyaAddagada" -Password ${ secret
 $val = Invoke-WebRequest -Uri $endpoint -Headers @{"Authorization"="Basic $BasicCreds"}
 $json = $val | ConvertFrom-JSON
 $limit = [datetime]::Now.AddDays(-90)
-write-host $json
-foreach($obj in $json)
-{
-    if($obj.lastupdated -lt $limit){
-    Write-Host "Pull request: #" + $obj.number
-    Write-Host "Title: " + $obj.title
-    Write-Host "Url: " + $obj.url
+write-host $val
+# foreach($obj in $json)
+# {
+#     if($obj.lastupdated -lt $limit){
+#     Write-Host "Pull request: #" + $obj.number
+#     Write-Host "Title: " + $obj.title
+#     Write-Host "Url: " + $obj.url
     
-    # $releaseNotes = $releaseNotes + "Body: "
-    # $obj.body.Split("`n") | ForEach { 
-    #     # ignore comments from issue templates
-    #     if($_.Trim().StartsWith("<!---") -eq $FALSE)
-    #     {
-    #         $releaseNotes = $releaseNotes + $_ + "`n" 
-    #     }
-    #  }   
-    # $releaseNotes = $releaseNotes + "`n"
+#     # $releaseNotes = $releaseNotes + "Body: "
+#     # $obj.body.Split("`n") | ForEach { 
+#     #     # ignore comments from issue templates
+#     #     if($_.Trim().StartsWith("<!---") -eq $FALSE)
+#     #     {
+#     #         $releaseNotes = $releaseNotes + $_ + "`n" 
+#     #     }
+#     #  }   
+#     # $releaseNotes = $releaseNotes + "`n"
     
-    Write-Host "User: " + $obj.user.login
-    Write-Host ""
-    }
-}
+#     Write-Host "User: " + $obj.user.login
+#     Write-Host ""
+#     }
+# }
