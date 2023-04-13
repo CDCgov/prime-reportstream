bfc9ef46fd48069851aba3efe984e566854a6764
Supriya Addagada <supriya.addagada@geha.com>
2023-04-13 18:15:25 -0500

    stale prs

--- .github/scripts/stale_items_report/staleprs.sh
+++ .github/scripts/stale_items_report/staleprs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-result="$(curl -s 'https://api.github.com/repos/CDCgov/prime-reportstream/pulls?state=open')"
+result="$(curl -i -H "Accept: application/json" -H "Content-Type: application/json" -s 'https://api.github.com/repos/CDCgov/prime-reportstream/pulls?state=open')"
 echo "result: '$result'"
 RESP=$(echo "$result" | grep -oP "^[^a-zA-Z0-9]")
 echo "RESP:'$RESP'"
+
+echo "$result" | json access_token
