4f3bf6969cbdaa7e6d96810f4c035f53753ec06c
Supriya Addagada <supriya.addagada@geha.com>
2023-04-15 01:29:30 -0500

    stale prs

--- .github/scripts/stale_items_report/staleprs.sh
+++ .github/scripts/stale_items_report/staleprs.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 result="$(curl -i -H "Accept: application/json" -H "Content-Type: application/json" -s 'https://api.github.com/repos/CDCgov/prime-reportstream/pulls?state=open')"
-echo "result: '$result'"
-items=$(echo "$result" | jq -c -r '.[]')
-for item in ${items[@]}; do
-    echo $item
-    # whatever you are trying to do ...
-done
+# echo "result: '$result'"
+
+# echo ${result[lastupdated]}
+
+# for i in "${result[@]}"
+# do
+# 	echo "${i[lastupdated]}"
+# done
+# echo $result | jq '. | map([.updated_at, .number])' | jq @sh
+
+REPORT_ID=$(jq -r ".number" $result)
+echo $REPORT_ID
+# items=$(echo "$result" | jq -c -r '.[]')
+# for item in ${items[@]}; do
+#     echo ${item[lastupdated]}
+#     # whatever you are trying to do ...
+# done
 $SHELL
