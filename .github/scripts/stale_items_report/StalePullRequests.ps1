c96f40de1b1fb18732ce7c747f56634004cb5ffe
Supriya Addagada <supriya.addagada@geha.com>
2023-04-15 22:28:25 -0500

    stale items

--- .github/scripts/stale_items_report/StalePullRequests.ps1
+++ .github/scripts/stale_items_report/StalePullRequests.ps1
@@ -36,7 +36,7 @@ $json1 = $data | ConvertTo-Json
 
 #$json1 | Out-File -FilePath "${runner.temp }\sample.json"
 
-echo "Stale_pullrequests=$data" >> $env:GITHUB_ENV
+echo "Stale_pullrequests=$data" | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
 
 echo $env:Stale_pullrequests
 #     if($obj.   -lt $limit){
