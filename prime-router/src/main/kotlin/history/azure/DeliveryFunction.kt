1c4df698a67b071f1ab5babf0b4f953dfeab60a4
Luis Pabon <l.pabon@theta-llc.com>
2023-04-21 10:36:43 -0400

    Update DeliveryFunction.kt
    Change Response type to use JSON

--- prime-router/src/main/kotlin/history/azure/DeliveryFunction.kt
+++ prime-router/src/main/kotlin/history/azure/DeliveryFunction.kt
@@ -230,7 +230,7 @@ class DeliveryFunction(
                     FacilityListApiParameters(request.queryParameters).sortColumn
                 )
 
-                HttpUtilities.okResponse(
+                HttpUtilities.okJSONResponse(
                     request,
                     mapper.writeValueAsString(
                         facilities.map {
