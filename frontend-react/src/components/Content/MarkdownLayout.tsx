1f515024733b4c6e9e2c4048919453f1fa23901b
Joseph Andersen <josephandersen@navapbc.com>
2023-04-21 13:07:30 -0500

    mdx troubles

--- frontend-react/src/components/Content/MarkdownLayout.tsx
+++ frontend-react/src/components/Content/MarkdownLayout.tsx
@@ -76,3 +76,5 @@ export function MarkdownLayout({
         </MDXProvider>
     );
 }
+
+export default MarkdownLayout;
--- frontend-react/src/pages/resources/api-programmers-guide/GettingStarted.stories.tsx
+++ frontend-react/src/pages/resources/api-programmers-guide/GettingStarted.stories.tsx
@@ -1,11 +1,12 @@
 import { ComponentMeta, ComponentStoryObj } from "@storybook/react";
 
 import Method1 from "../../../content/resources/api-programmers-guide/getting-started/Method1.mdx";
+import Method2 from "../../../content/resources/api-programmers-guide/getting-started/Method2.mdx";
 
 import { GettingStartedPage } from "./GettingStarted";
 
 export default {
-    title: "pages/resources/ReportStreamAPI",
+    title: "pages/resources/GettingStarted",
     component: GettingStartedPage,
 } as ComponentMeta<typeof GettingStartedPage>;
 
@@ -13,4 +14,8 @@ export const Default: ComponentStoryObj<typeof GettingStartedPage> = {};
 
 export const Method1Section: ComponentStoryObj<typeof Method1> = {
     render: () => <Method1 />,
+};
+
+export const Method2Section: ComponentStoryObj<typeof Method2> = {
+    render: () => <Method2 />,
 };
