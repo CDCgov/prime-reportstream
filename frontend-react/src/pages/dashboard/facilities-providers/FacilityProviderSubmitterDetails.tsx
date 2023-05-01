69e53075939e27b94d7e408a03fc3f27cfa5ade9
Penny Lischer <penny.lischer@agile6.com>
2023-05-01 09:36:01 -0700

    9076 - removed dashboard routes

--- frontend-react/src/pages/dashboard/facilities-providers/FacilityProviderSubmitterDetails.tsx
+++ frontend-react/src/pages/dashboard/facilities-providers/FacilityProviderSubmitterDetails.tsx
@@ -2,13 +2,12 @@ import { GridContainer } from "@trussworks/react-uswds";
 import { Helmet } from "react-helmet-async";
 import React from "react";
 
-import { FeatureName } from "../../../AppRouter";
-
 export function FacilityProviderSubmitterDetails() {
     return (
+        // TODO: title will be dynamic based on prop
         <GridContainer containerSize="widescreen">
             <Helmet>
-                <title>{FeatureName.FACILITY_PROVIDER_SUBMITTER_DETAILS}</title>
+                <title>Facility Details</title>
             </Helmet>
             <div>Facility, provider & submitter details</div>
         </GridContainer>
