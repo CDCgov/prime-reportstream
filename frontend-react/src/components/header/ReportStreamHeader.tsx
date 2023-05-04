beafb098da2ab38afbd2be140984903818844ce1
Penny Lischer <penny.lischer@agile6.com>
2023-05-04 09:54:34 -0700

    9077 - Removed commented code

--- frontend-react/src/components/header/ReportStreamHeader.tsx
+++ frontend-react/src/components/header/ReportStreamHeader.tsx
@@ -62,13 +62,6 @@ export const ReportStreamHeader = () => {
                 >
                     <span>{FeatureName.DAILY_DATA}</span>
                 </USNavLink>
-                // <USNavLink
-                //     href="/data-dashboard"
-                //     key="dashboard"
-                //     data-attribute="hidden"
-                // >
-                //     <span>{FeatureName.DASHBOARD}</span>
-                // </USNavLink>
             );
         }
 
