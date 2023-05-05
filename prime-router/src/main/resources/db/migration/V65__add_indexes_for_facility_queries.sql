5735ff9853b68cf29d5954cc241a35b6d4e7bc18
Luis Pabon <l.pabon@theta-llc.com>
2023-05-05 11:22:59 -0400

    Create V65__add_indexes_for_facility_queries.sql
    Adding indexes to columns sorted and filtered by the new endpoint

---
+++ prime-router/src/main/resources/db/migration/V65__add_indexes_for_facility_queries.sql
@@ -0,0 +1,34 @@
+/*
+ * The Flyway tool applies this migration to create the database.
+ *
+ * Follow this style guide https://about.gitlab.com/handbook/business-ops/data-team/platform/sql-style-guide/
+ * use VARCHAR(63) for names in organization and schema
+ *
+ * Copy a version of this comment into the next migration
+ *
+ */
+
+/*
+ * Add indexes to columns that are sorted and filtered on the Facility queries
+ * it primarily covers columns in the COVID Result Metadata table
+ * refer to this when adding Facility queries for the Universal Pipeline
+ */
+CREATE INDEX IF NOT EXISTS item_lineage_created_at
+    ON item_lineage USING btree
+    (created_at ASC NULLS LAST);
+
+CREATE INDEX item_lineage_created_at
+    ON item_lineage USING btree
+    (created_at ASC NULLS LAST);
+
+CREATE INDEX covid_result_metadata_testing_lab_name
+    ON covid_result_metadata USING btree
+    (testing_lab_name ASC NULLS LAST);
+
+CREATE INDEX covid_result_metadata_sender_id
+    ON covid_result_metadata USING btree
+    (sender_id ASC NULLS LAST);
+
+CREATE INDEX covid_result_metadata_report_id
+    ON public.covid_result_metadata USING btree
+    (report_id ASC NULLS LAST);
