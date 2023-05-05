7090c24a49dacb4571ba95c28d44503c7faccfb2
Penelope Lischer <102491809+penny-lischer@users.noreply.github.com>
2023-05-05 12:49:45 -0700

    9077 - Implement main dashboard page (#9387)
    * 9076 - Added routes and empty pages for stlt dashboard
    
    * 9076 - Commented Dashboard from header nav
    
    * 9077 - Added Dashboard Header
    
    * 9077 - Initial dashboard mocked table.
    
    * 9077 - Made TableFilters.tsx more generic
    Update styles of Dashboard.tsx
    
    * 9077 - Update to TableFilters.tsx
    
    * 9077 - Renamed SubmissionFilterProps to TableFilterProps
    
    * 9077 - Removed commented code
    
    * 9077 - Renamed Dashboard to DataDashboard
    
    * 9077 - Updates per code review

---
+++ frontend-react/src/pages/data-dashboard/DataDashboard.tsx
@@ -0,0 +1,58 @@
+import { GridContainer } from "@trussworks/react-uswds";
+import { Helmet } from "react-helmet-async";
+import React from "react";
+
+import { FeatureName } from "../../AppRouter";
+import { AuthElement } from "../../components/AuthElement";
+import { MemberType } from "../../hooks/UseOktaMemberships";
+import { USNavLink } from "../../components/USLink";
+import { withCatchAndSuspense } from "../../components/RSErrorBoundary";
+import { useOrganizationSettings } from "../../hooks/UseOrganizationSettings";
+import DataDashboardTable from "../../components/DataDashboard/DataDashboardTable/DataDashboardTable";
+
+function DataDashboard() {
+    const { data: orgDetails } = useOrganizationSettings();
+    const { description } = orgDetails || {};
+    return (
+        <div className="data-dashboard">
+            <header className="usa-header usa-header--extended bg-primary-darker text-white">
+                <GridContainer className="margin-left-7 margin-right-7 padding-top-8 padding-bottom-8 rs-max-width-100-important">
+                    <div className="font-sans-lg text-blue-30 width-full">
+                        {description}
+                    </div>
+                    <div className="font-sans-2xl">Data Dashboard</div>
+                    <hr className="margin-bottom-3" />
+                    <div className="font-sans-lg">
+                        All the labs, facilities, aggregators, etc. that have
+                        ever contributed data to your reports.
+                    </div>
+                    <hr className="margin-bottom-3" />
+                    <div className="font-sans-2xs">
+                        Jump to:{" "}
+                        <USNavLink href="/data-dashboard/facilities-providers">
+                            All facilities & providers
+                        </USNavLink>
+                    </div>
+                    <hr />
+                </GridContainer>
+            </header>
+            <GridContainer className="margin-left-7 margin-right-7 padding-top-8 padding-bottom-8 rs-max-width-100-important">
+                <Helmet>
+                    <title>{FeatureName.DATA_DASHBOARD}</title>
+                </Helmet>
+                <article>
+                    {withCatchAndSuspense(<DataDashboardTable />)}
+                </article>
+            </GridContainer>
+        </div>
+    );
+}
+
+export function DataDashboardWithAuth() {
+    return (
+        <AuthElement
+            element={<DataDashboard />}
+            requiredUserType={MemberType.RECEIVER}
+        />
+    );
+}
