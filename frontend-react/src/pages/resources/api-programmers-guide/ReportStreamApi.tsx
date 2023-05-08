575ddda68ac10b05b2fab92a7b0b412132c7a7a3
Joseph Andersen <josephandersen@navapbc.com>
2023-05-08 11:14:33 -0500

    sidenav

--- frontend-react/src/pages/resources/api-programmers-guide/ReportStreamApi.tsx
+++ frontend-react/src/pages/resources/api-programmers-guide/ReportStreamApi.tsx
@@ -1,9 +1,10 @@
 import { MarkdownLayout } from "../../../components/Content/MarkdownLayout";
 import ReportStreamAPIMarkdown from "../../../content/resources/api-programmers-guide/ReportStreamApi.mdx";
+import Sidenav from "../../../content/resources/api-programmers-guide/Sidenav.mdx";
 
 export function ReportStreamAPIPage() {
     return (
-        <MarkdownLayout sidenav={<></>}>
+        <MarkdownLayout sidenav={<Sidenav />}>
             <ReportStreamAPIMarkdown />
         </MarkdownLayout>
     );
