b392368309b02cf4db2a676881ddf4f84718a6ea
Joseph Andersen <josephandersen@navapbc.com>
2023-05-24 14:39:51 -0500

    Support Page has 2 broken links (#9600)
    Fixes #9599

--- frontend-react/src/AppRouter.tsx
+++ frontend-react/src/AppRouter.tsx
@@ -186,6 +186,13 @@ export const appRoutes: RouteObject[] = [
                             isContentPage: true,
                         },
                     },
+                    {
+                        path: "*",
+                        element: <Support />,
+                        handle: {
+                            isContentPage: true,
+                        },
+                    },
                 ],
             },
             {
