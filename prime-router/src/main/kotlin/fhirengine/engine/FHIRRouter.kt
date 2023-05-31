f14a2aa0c18c369d3c702be81334d6bde5cabc8a
Luis Pabon <TurkeyFried@users.noreply.github.com>
2023-05-31 11:30:42 -0400

    Update prime-router/src/main/kotlin/fhirengine/engine/FHIRRouter.kt
    Co-authored-by: jimmyfagan <90421499+jimmyfagan@users.noreply.github.com>

--- prime-router/src/main/kotlin/fhirengine/engine/FHIRRouter.kt
+++ prime-router/src/main/kotlin/fhirengine/engine/FHIRRouter.kt
@@ -574,7 +574,7 @@ class FHIRRouter(
     ) {
         var filteredTrackingElement = bundle.identifier.value ?: ""
         if (focusResource is Observation) {
-            // for Observation-type elements, we use the code if property when available
+            // for Observation-type elements, we use the code property when available
             // if more elements need specific logic, consider extending the FHIR libraries
             // instead of adding more if/else statements
             val coding = focusResource.code.coding.firstOrNull()
