b89e13a786ec0c79d04e0ba52f63a9aa2ca903e6
Michael Kalish <michael.kalish@focusconsulting.io>
2023-07-05 14:44:13 -0400

    Up the smoke test polling timeout to 10 min

--- prime-router/src/main/kotlin/cli/tests/TestReportStream.kt
+++ prime-router/src/main/kotlin/cli/tests/TestReportStream.kt
@@ -469,7 +469,7 @@ abstract class CoolTest {
     suspend fun pollForStepResult(
         reportId: ReportId,
         taskAction: TaskAction,
-        maxPollSecs: Int = 480,
+        maxPollSecs: Int = 600,
         pollSleepSecs: Int = 20,
     ): Map<UUID, DetailedSubmissionHistory?> {
         var timeElapsedSecs = 0
@@ -660,7 +660,7 @@ abstract class CoolTest {
         totalItems: Int,
         filterOrgName: Boolean = false,
         silent: Boolean = false,
-        maxPollSecs: Int = 480,
+        maxPollSecs: Int = 600,
         pollSleepSecs: Int = 30, // I had this as every 5 secs, but was getting failures.  The queries run unfastly.
         asyncProcessMode: Boolean = false,
         isUniversalPipeline: Boolean = false
