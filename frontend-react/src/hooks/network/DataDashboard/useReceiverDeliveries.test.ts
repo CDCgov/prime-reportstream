d0a66db9a9266148286fa4e23faf8ca33853de96
Penny Lischer <penny.lischer@agile6.com>
2023-07-11 21:26:10 -0700

    9400 - Added useReceiverDeliveries unit tests

---
+++ frontend-react/src/hooks/network/DataDashboard/useReceiverDeliveries.test.ts
@@ -0,0 +1,80 @@
+import { renderHook, waitFor } from "@testing-library/react";
+
+import {
+    dataDashboardServer,
+    makeRSReceiverDeliveryResponseFixture,
+} from "../../../__mocks__/DataDashboardMockServer";
+import { mockSessionContext } from "../../../contexts/__mocks__/SessionContext";
+import { AppWrapper } from "../../../utils/CustomRenderUtils";
+import { MemberType } from "../../UseOktaMemberships";
+
+import useReceiverDeliveries from "./UseReceiverDeliveries";
+
+describe("useReceiverDeliveries", () => {
+    beforeAll(() => dataDashboardServer.listen());
+    afterEach(() => dataDashboardServer.resetHandlers());
+    afterAll(() => dataDashboardServer.close());
+
+    describe("with no Organization name", () => {
+        beforeEach(() => {
+            mockSessionContext.mockReturnValue({
+                oktaToken: {
+                    accessToken: "TOKEN",
+                },
+                activeMembership: undefined,
+                dispatch: () => {},
+                initialized: true,
+                isUserAdmin: false,
+                isUserReceiver: false,
+                isUserSender: false,
+                environment: "test",
+            });
+        });
+
+        test("returns undefined", async () => {
+            const { result } = renderHook(() => useReceiverDeliveries(), {
+                wrapper: AppWrapper(),
+            });
+            await waitFor(() =>
+                expect(result.current.fetchResults).toEqual(undefined)
+            );
+            expect(result.current.isDeliveriesLoading).toEqual(true);
+        });
+    });
+
+    describe("with Organization and service name", () => {
+        beforeEach(() => {
+            mockSessionContext.mockReturnValue({
+                oktaToken: {
+                    accessToken: "TOKEN",
+                },
+                activeMembership: {
+                    memberType: MemberType.RECEIVER,
+                    parsedName: "testOrg",
+                    service: "testReceiverService",
+                },
+                dispatch: () => {},
+                initialized: true,
+                isUserAdmin: false,
+                isUserReceiver: true,
+                isUserSender: false,
+                environment: "test",
+            });
+        });
+
+        test("returns receiver deliveries", async () => {
+            const { result } = renderHook(
+                () => useReceiverDeliveries("testServiceName"),
+                {
+                    wrapper: AppWrapper(),
+                }
+            );
+            await waitFor(() =>
+                expect(result.current.fetchResults).toEqual(
+                    makeRSReceiverDeliveryResponseFixture(5)
+                )
+            );
+            expect(result.current.isDeliveriesLoading).toEqual(false);
+        });
+    });
+});
