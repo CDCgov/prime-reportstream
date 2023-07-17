0cd82b6847a5a79713205ad1c9a4428c55b4c782
KeithNava <134446588+KeithNava@users.noreply.github.com>
2023-07-17 12:17:09 -0400

    feat: convert legacy markdown files into MDX (#10431)

--- frontend-react/src/pages/resources/markdown-adapters.tsx
+++ frontend-react/src/pages/resources/markdown-adapters.tsx
@@ -1,12 +1,9 @@
-import { MarkdownRenderer } from "../../components/Content/MarkdownRenderer";
-import ReferralGuide from "../../content/resources/referral-guide.md?url";
-import GettingStartedSubmittingData from "../../content/resources/getting-started-submitting-data.md?url";
+import ReferralGuideMDX from "../../content/resources/referral-guide.mdx";
+import GettingStartedSubmittingData from "../../content/resources/getting-started-submitting-data.mdx";
 
 /** Markdown adapters. These are what are required to render MD as a page */
-export const ReferralGuideMd = () => (
-    <MarkdownRenderer markdownUrl={ReferralGuide} />
-);
+export const ReferralGuideMd = () => <ReferralGuideMDX />;
 
 export const GettingStartedSubmittingDataMd = () => (
-    <MarkdownRenderer markdownUrl={GettingStartedSubmittingData} />
+    <GettingStartedSubmittingData />
 );
