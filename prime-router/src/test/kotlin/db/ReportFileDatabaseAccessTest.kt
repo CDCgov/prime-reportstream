a8a071a127125fbbcc0249fb45efd44ffd5ce5be
Michael Kalish <michael.kalish@focusconsulting.io>
2023-07-18 16:48:07 -0400

    9455: use test containers to test reportfile database access

--- prime-router/src/test/kotlin/db/ReportFileDatabaseAccessTest.kt
+++ prime-router/src/test/kotlin/db/ReportFileDatabaseAccessTest.kt
@@ -4,11 +4,56 @@ import assertk.assertThat
 import assertk.assertions.contains
 import assertk.assertions.isNotNull
 import assertk.assertions.isNull
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+import gov.cdc.prime.router.azure.DatabaseAccess
 import gov.cdc.prime.router.azure.MockHttpRequestMessage
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
 
+@Testcontainers
 class ReportFileDatabaseAccessTest {
+
+    @Container
+    val postgresDatabase = PostgreSQLContainer("postgres:11-alpine")
+
+    @Test
+    fun `Test loads data`() {
+        val config = HikariConfig()
+        config.jdbcUrl = postgresDatabase.jdbcUrl
+        config.username = postgresDatabase.username
+        config.password = postgresDatabase.password
+        config.addDataSourceProperty(
+            "dataSourceClassName",
+            "org.postgresql.ds.PGSimpleDataSource"
+        )
+        config.addDataSourceProperty("cachePrepStmts", "true")
+        config.addDataSourceProperty("prepStmtCacheSize", "250")
+        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048")
+        config.addDataSourceProperty(
+            "connectionTimeout",
+            "60000"
+        ) // Default is 30000 (30 seconds)
+
+        // See this info why these are a good value
+        //  https://github.com/brettwooldridge/HikariCP/wiki/About-Pool-Sizing
+        config.minimumIdle = 2
+        config.maximumPoolSize = 25
+        // This strongly recommended to be set "be several seconds shorter than any database or
+        // infrastructure
+        // imposed connection time limit". Not sure what value is but have observed that
+        // connection are closed
+        // after about 10 minutes
+        config.maxLifetime = 180000
+        val datasource = HikariDataSource(config)
+        val databaseAccess = DatabaseAccess(datasource)
+        val reportFileDatabaseAccess = ReportFileDatabaseAccess(databaseAccess)
+        val rows = reportFileDatabaseAccess.getReports(ReportFileApiSearch(emptyList(), null))
+        assertThat(rows.results.isEmpty())
+    }
 
     @Nested
     inner class ReportFileApiSearchTest() {
