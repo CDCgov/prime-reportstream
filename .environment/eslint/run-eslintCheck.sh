d6ecd7264e2e6a6e840b3b2bf4d228b7d5f115d0
Stephen Nesman <stephen.nesman@semanticbits.com>
2023-07-18 13:41:16 -0400

    lint

--- .environment/eslint/run-eslintCheck.sh
+++ .environment/eslint/run-eslintCheck.sh
@@ -10,7 +10,7 @@ function js_fmt_check() {
     MODIFIED_JS_FILES_COUNT=$(git status --porcelain | grep "\.js$" | wc -l)
     if [ ${MODIFIED_TSX_FILES_COUNT?} != 0 ] || [ ${MODIFIED_JS_FILES_COUNT?} != 0 ]; then
         yarn install --ignore-platform
-        yarn run lint --ignore-platform
+        yarn run lint
     else
         note "Skipping this check, you made no changes to JavaScript or TypeScript files..."
     fi
