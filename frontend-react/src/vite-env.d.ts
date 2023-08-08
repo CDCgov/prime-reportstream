31569d8d349f32d351980973ccac66a42687df0a
Joseph Andersen <12385932+jpandersen87@users.noreply.github.com>
2023-08-08 11:33:00 -0500

    Experience/10726-Update-MarkdownLayout-for-new-features (#10728)
    * Update MarkdownLayout for new features
    Fixes #10726

--- frontend-react/src/vite-env.d.ts
+++ frontend-react/src/vite-env.d.ts
@@ -1,2 +1,20 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
+
+declare module "*.mdx" {
+    export const frontmatter: {
+        sidenav?: string;
+        breadcrumbs?: Array<{ label: string; href: string }>;
+        title?: string;
+        subtitle?: string | string[];
+        callToAction?: Array<{
+            label: string;
+            href: string;
+            icon: string;
+            style: string;
+        }>;
+        lastUpdated?: string;
+        toc?: boolean | { depth?: number };
+        backToTop?: boolean;
+    };
+}
