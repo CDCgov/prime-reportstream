c1851c75a2de408f2f93c894cdc5f07e3a3a204d
etanb <etan.berkowitz@gmail.com>
2023-08-08 17:25:22 -0700

    beginnings of new navbar

---
+++ frontend-react/src/components/navbar/ReportStreamNavbar.stories.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import { ReportStreamNavbar } from "./ReportStreamNavbar";
+
+export default {
+    title: "Components/Navbar",
+    component: ReportStreamNavbar,
+};
+
+export const RsNavbar = (): React.ReactElement => <ReportStreamNavbar />;
---
+++ frontend-react/src/components/navbar/ReportStreamNavbar.tsx
@@ -0,0 +1,120 @@
+import {
+    Button,
+    Header,
+    Menu,
+    NavDropDownButton,
+    PrimaryNav,
+    Title,
+} from "@trussworks/react-uswds";
+import { useState } from "react";
+
+export const ReportStreamNavbar = () => {
+    const [openMenuItem, setOpenMenuItem] = useState<null | string>(null);
+    const setMenu = (menuName: string) => {
+        if (openMenuItem === menuName) {
+            setOpenMenuItem(null);
+        } else {
+            setOpenMenuItem(menuName);
+        }
+    };
+
+    const Dropdown = ({
+        menuName,
+        dropdownList,
+    }: {
+        menuName: string;
+        dropdownList: React.ReactElement[];
+    }) => {
+        return (
+            <>
+                <NavDropDownButton
+                    menuId={menuName.toLowerCase()}
+                    isOpen={openMenuItem === menuName}
+                    label={menuName}
+                    onToggle={() => {
+                        setMenu(menuName);
+                    }}
+                />
+                <Menu
+                    items={dropdownList}
+                    isOpen={openMenuItem === menuName}
+                    id={`${menuName}Dropdown`}
+                />
+            </>
+        );
+    };
+    const testItemsMenu = [
+        <Dropdown
+            menuName="About"
+            dropdownList={[
+                <a href="#linkOne" key="one">
+                    Current link
+                </a>,
+                <a href="#linkTwo" key="two">
+                    Simple link Two
+                </a>,
+            ]}
+        />,
+        <Dropdown
+            menuName="Getting started"
+            dropdownList={[
+                <a href="#linkOne" key="one">
+                    Current link
+                </a>,
+                <a href="#linkTwo" key="two">
+                    Simple link Two
+                </a>,
+            ]}
+        />,
+        <Dropdown
+            menuName="Developer resources"
+            dropdownList={[
+                <a href="#linkOne" key="one">
+                    Current link
+                </a>,
+                <a href="#linkTwo" key="two">
+                    Simple link Two
+                </a>,
+            ]}
+        />,
+        <Dropdown
+            menuName="Managing your connection"
+            dropdownList={[
+                <a href="#linkOne" key="one">
+                    Current link
+                </a>,
+                <a href="#linkTwo" key="two">
+                    Simple link Two
+                </a>,
+            ]}
+        />,
+        <Dropdown
+            menuName="Support"
+            dropdownList={[
+                <a href="#linkOne" key="one">
+                    Current link
+                </a>,
+                <a href="#linkTwo" key="two">
+                    Simple link Two
+                </a>,
+            ]}
+        />,
+    ];
+    return (
+        <Header basic={true} className="bg-white">
+            <div className="usa-nav-container">
+                <div className="usa-navbar">
+                    <Title>ReportStream</Title>
+                </div>
+                <PrimaryNav items={testItemsMenu}>
+                    <div>
+                        <Button outline type="button">
+                            Login
+                        </Button>
+                        <Button type="button">Connect now</Button>
+                    </div>
+                </PrimaryNav>
+            </div>
+        </Header>
+    );
+};
