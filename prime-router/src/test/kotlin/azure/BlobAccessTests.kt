5b9d328bafbefab772577864bc42f3489cc534de
Jack Wang <jack.wang@focusconsulting.io>
2023-08-09 00:07:13 -0400

    additional tests

--- prime-router/src/test/kotlin/azure/BlobAccessTests.kt
+++ prime-router/src/test/kotlin/azure/BlobAccessTests.kt
@@ -4,10 +4,14 @@ import assertk.assertThat
 import assertk.assertions.isEqualTo
 import assertk.assertions.isNotNull
 import assertk.fail
+import com.azure.storage.blob.BlobClient
+import com.azure.storage.blob.BlobClientBuilder
 import com.azure.storage.blob.BlobServiceClientBuilder
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.mockkClass
 import io.mockk.mockkConstructor
+import io.mockk.mockkObject
 import io.mockk.unmockkAll
 import io.mockk.verify
 import org.junit.jupiter.api.Test
@@ -60,6 +64,59 @@ class BlobAccessTests {
     }
 
     @Test
+    fun `copy blob`() {
+        mockkClass(BlobAccess::class)
+        mockkObject(BlobAccess.Companion)
+
+        val testUrl = "http://testurl/testfile"
+        val testFile = BlobAccess.BlobInfo.getBlobFilename(testUrl)
+
+        every { BlobAccess.Companion.downloadBlob(testUrl) }.returns("testblob".toByteArray())
+        every {
+            BlobAccess.Companion.uploadBlob(
+                testFile,
+                "testblob".toByteArray(),
+                "testcontainer",
+                "testenvvar"
+            )
+        }.returns("http://testurl2")
+
+        val result = BlobAccess.copyBlob(testUrl, "testcontainer", "testenvvar")
+
+        verify(exactly = 1) { BlobAccess.Companion.downloadBlob(testUrl) }
+        verify(exactly = 1) {
+            BlobAccess.Companion.uploadBlob(
+                testFile,
+                "testblob".toByteArray(),
+                "testcontainer",
+                "testenvvar"
+            )
+        }
+        assertThat(result).isEqualTo("http://testurl2")
+        unmockkAll()
+    }
+
+    @Test
+    fun `delete blob`() {
+        val mockedBlobClient = mockkClass(BlobClient::class)
+        every { mockedBlobClient.delete() } answers { }
+        mockkConstructor(BlobClientBuilder::class)
+        every { anyConstructed<BlobClientBuilder>().connectionString(any()) } answers
+            { BlobClientBuilder() }
+        every { anyConstructed<BlobClientBuilder>().endpoint(any()) } answers
+            { BlobClientBuilder() }
+        every { anyConstructed<BlobClientBuilder>().buildClient() } returns mockedBlobClient
+
+        BlobAccess.deleteBlob("http://127.0.0.1")
+
+        verify(exactly = 1) { BlobClientBuilder().connectionString(any()) }
+        verify(exactly = 1) { BlobClientBuilder().endpoint(any()) }
+        verify(exactly = 1) { BlobClientBuilder().buildClient() }
+        verify(exactly = 1) { mockedBlobClient.delete() }
+        unmockkAll()
+    }
+
+    @Test
     fun `check connection`() {
         mockkConstructor(BlobServiceClientBuilder::class)
         every { anyConstructed<BlobServiceClientBuilder>().connectionString(any()) } answers
@@ -67,7 +124,8 @@ class BlobAccessTests {
         every { anyConstructed<BlobServiceClientBuilder>().buildClient() } returns mockk()
 
         BlobAccess.checkConnection("test")
-        verify(exactly = 1) { BlobServiceClientBuilder().connectionString(any()) }
+
+        verify(exactly = 1) { BlobServiceClientBuilder().connectionString(System.getenv("test")) }
         verify(exactly = 1) { BlobServiceClientBuilder().buildClient() }
         unmockkAll()
     }
