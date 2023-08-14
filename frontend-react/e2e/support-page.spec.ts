0c68c1a0637122aee516769c2809ab40819b6c4b
Keith WIlliams <keithwilliams@navapbc.com>
2023-08-14 11:19:46 -0400

    feat: add more pages to e2e

---
+++ frontend-react/e2e/support-page.spec.ts
@@ -0,0 +1,38 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("Support page", () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto("/support");
+    });
+
+    test("should have correct title", async ({ page }) => {
+        await expect(page).toHaveTitle(/Support/);
+    });
+
+    test.describe("Card navigation", () => {
+        const cardLinks = [
+            {
+                name: "Frequently asked questions",
+                url: "/support/faq",
+            },
+            {
+                name: "Service request",
+                url: "/support/service-request",
+            },
+            {
+                name: "Contact",
+                url: "/support/contact",
+            },
+        ];
+
+        for (const cardLink of cardLinks) {
+            test(`should have ${cardLink["name"]} link`, async ({ page }) => {
+                await page
+                    .getByRole("link", { name: cardLink["name"] })
+                    .click();
+
+                await expect(page).toHaveURL(cardLink["url"]);
+            });
+        }
+    });
+});
