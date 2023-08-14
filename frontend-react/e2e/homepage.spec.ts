c340f4d80739e90c6643050992aa4b54c39ac1a1
Keith WIlliams <keithwilliams@navapbc.com>
2023-08-14 11:19:46 -0400

    feat: add more pages to e2e

--- frontend-react/e2e/homepage.spec.ts
+++ frontend-react/e2e/homepage.spec.ts
@@ -1,32 +1,39 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('Has correct title', async ({ page }) => {
-  await page.goto('/');
+test("Has correct title", async ({ page }) => {
+    await page.goto("/");
 
-  await expect(page).toHaveTitle(/CDC Prime ReportStream/);
+    await expect(page).toHaveTitle(/CDC Prime ReportStream/);
 });
 
-test('has Product link', async ({ page }) => {
-  await page.goto('/');
+test("has Product link", async ({ page }) => {
+    await page.goto("/");
 
-  await page.getByRole('link', { name: 'Product' }).click();
+    await page
+        .getByTestId("header")
+        .getByRole("link", { name: "Product" })
+        .click();
 
-  await expect(page).toHaveURL(/.*product\/overview/);
+    await expect(page).toHaveURL(/.*product\/overview/);
 });
 
-test('has Resources link', async ({ page }) => {
-  await page.goto('/');
+test("has Resources link", async ({ page }) => {
+    await page.goto("/");
 
-  await page.getByRole('link', { name: 'Resources' }).click();
+    await page
+        .getByTestId("header")
+        .getByRole("link", { name: "Resources" })
+        .click();
 
-  await expect(page).toHaveURL(/.*resources/);
+    await expect(page).toHaveURL(/.*resources/);
 });
 
-test('has Support link', async ({ page }) => {
-  await page.goto('/');
+test("has Support link", async ({ page }) => {
+    await page.goto("/");
 
-  await page.getByRole('link', { name: 'Support' }).click();
-
-  await expect(page).toHaveURL(/.*support/);
+    await page
+        .getByTestId("header")
+        .getByRole("link", { name: "Support" })
+        .click();
+    await expect(page).toHaveURL(/.*support/);
 });
-
--- frontend-react/e2e/product-page.spec.ts
+++ frontend-react/e2e/product-page.spec.ts
@@ -1,33 +1,49 @@
 import { test, expect } from "@playwright/test";
 
-beforeEach(async ({ page }) => {
-    await page.goto("/product/overview");
-});
+test.describe("Product page", () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto("/product/overview");
+    });
 
-test("Has correct title", async ({ page }) => {
-    await expect(page).toHaveTitle(/Product/);
-});
+    test("should have correct title", async ({ page }) => {
+        await expect(page).toHaveTitle(/Product/);
+    });
 
-test("has Product link", async ({ page }) => {
-    await page.getByRole("link", { name: "Overview" }).click();
+    test.describe("Side navigation", () => {
+        test("should have Overview link", async ({ page }) => {
+            await page
+                .getByTestId("sidenav")
+                .getByRole("link", { name: "Overview" })
+                .click();
 
-    await expect(page).toHaveURL(/.*product\/overview/);
-});
+            await expect(page).toHaveURL(/.*product\/overview/);
+        });
 
-test("has Live link", async ({ page }) => {
-    await page.getByRole("link", { name: /Where we\'re live/ }).click();
+        test("should have Live link", async ({ page }) => {
+            await page
+                .getByTestId("sidenav")
+                .getByRole("link", { name: /Where we\'re live/ })
+                .click();
 
-    await expect(page).toHaveURL(/.*product\/where-were-live/);
-});
+            await expect(page).toHaveURL(/.*product\/where-were-live/);
+        });
 
-test("has Release notes link", async ({ page }) => {
-    await page.getByRole("link", { name: /Release notes/ }).click();
+        test("should have Release notes link", async ({ page }) => {
+            await page
+                .getByTestId("sidenav")
+                .getByRole("link", { name: /Release notes/ })
+                .click();
 
-    await expect(page).toHaveURL(/.*product\/release-notes/);
-});
+            await expect(page).toHaveURL(/.*product\/release-notes/);
+        });
 
-test("has About link", async ({ page }) => {
-    await page.getByRole("link", { name: /Release notes/ }).click();
+        test("should have About link", async ({ page }) => {
+            await page
+                .getByTestId("sidenav")
+                .getByRole("link", { name: /About/ })
+                .click();
 
-    await expect(page).toHaveURL(/.*product\/release-notes/);
+            await expect(page).toHaveURL(/.*product\/about/);
+        });
+    });
 });
