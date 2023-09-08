0884af0aea1d92eb978e6765d074462289a370be
Joseph Andersen <12385932+jpandersen87@users.noreply.github.com>
2023-09-08 13:49:52 -0500

    accordion item hash onclick

--- frontend-react/src/shared/Accordion/Accordion.tsx
+++ frontend-react/src/shared/Accordion/Accordion.tsx
@@ -2,6 +2,27 @@ import { Accordion as OrigAccordion } from "@trussworks/react-uswds";
 import { useId } from "react";
 import { useLocation } from "react-router";
 
+/**
+ * Ensure our accordion urls being added to history is only ever
+ * page + 1 by utilizing a property in history state.
+ *
+ * Ex: User goes to /support page and interacts with multiple accordion items.
+ * Top of history will be: /support, /support#lastAccordionItemOpened. User can
+ * press back to go to /support and then navigate elsewhere to naturally remove
+ * the hash link from history. This example doesn't take into account if the user
+ * clicks any hash links on the page, in which case the history will include all
+ * links clicked (default browser behavior) + last accordion item opened.
+ */
+function appendHashToURL(hash: string) {
+    const currentURL = window.location.href;
+    const newURL = currentURL.split("#")[0] + hash; // Remove any existing anchor and append the new one
+    if (window.history.state.isAccordion) {
+        window.history.replaceState(window.history.state, "", newURL);
+    } else {
+        window.history.pushState({ isAccordion: true }, "", newURL);
+    }
+}
+
 export interface AccordionProps
     extends React.ComponentProps<typeof OrigAccordion> {}
 
@@ -23,11 +44,22 @@ export function Accordion({ items, ...props }: AccordionProps) {
         expanded: i.id === hashId,
     }));
     return (
-        <OrigAccordion
-            key={`${id}-${location.key}`}
-            {...props}
-            items={finalItems}
-        />
+        <div
+            onClick={(event) => {
+                // TODO: Figure out how to get event.target to return proper type
+                appendHashToURL(
+                    `#${(event.target as HTMLElement).getAttribute(
+                        "aria-controls",
+                    )}`,
+                );
+            }}
+        >
+            <OrigAccordion
+                key={`${id}-${location.key}`}
+                {...props}
+                items={finalItems}
+            />
+        </div>
     );
 }
 
