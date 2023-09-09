59dc3db89fe33a148cdb6f15314e32353438785c
Joseph Andersen <12385932+jpandersen87@users.noreply.github.com>
2023-09-08 19:21:46 -0500

    style fixes

--- frontend-react/src/shared/Accordion/Accordion.tsx
+++ frontend-react/src/shared/Accordion/Accordion.tsx
@@ -29,12 +29,18 @@ export interface AccordionProps
 /**
  * Enhanced Accordion proxy.
  *
+ * Will automatically wrap titles with provided id for item,
+ * while replacing the id used for the content (so that url
+ * hash scrolling will go to title).
+ *
  * Will automatically expand item matching hash name in url.
  * Creates a unique accordion key from a unique id + location key
  * in order to force remounting when a new hash is selected (
  * original accordion does not honor changes to item expand
- * property). Will also update history will the last accordion item
- * hash link interacted with.
+ * property).
+ *
+ * Will also update history will the last accordion item
+ * id interacted with.
  */
 export function Accordion({ items, ...props }: AccordionProps) {
     const location = useLocation();
@@ -42,17 +48,25 @@ export function Accordion({ items, ...props }: AccordionProps) {
     const hashId = location.hash.replace("#", "");
     const finalItems = items.map((i) => ({
         ...i,
+        id: `${i.id}--content`,
+        title: <span id={i.id}>{i.title}</span>,
         expanded: i.id === hashId,
     }));
     return (
         <div
             onClick={(event) => {
                 // TODO: Figure out how to get event.target to return proper type
-                appendHashToURL(
-                    `#${(event.target as HTMLElement).getAttribute(
-                        "aria-controls",
-                    )}`,
-                );
+                const target = event.target as HTMLElement;
+                // Depending on DOM tree structure inside button, target will differ
+                // depending on area clicked. Check if the target itself has an id,
+                // otherwise try to find a descendant with one.
+                const id =
+                    target.getAttribute("id") ??
+                    target.querySelector("[id]")?.id;
+
+                if (id) {
+                    appendHashToURL(`#${id}`);
+                }
             }}
         >
             <OrigAccordion
