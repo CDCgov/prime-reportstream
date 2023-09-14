5e30b0f3d5a5fdeea9fb1f5d2e423627d10e5fc8
etanb <etan.berkowitz@gmail.com>
2023-09-14 14:37:39 -0700

    baseline component

--- frontend-react/src/shared/ReleaseNote/ReleaseNote.tsx
+++ frontend-react/src/shared/ReleaseNote/ReleaseNote.tsx
@@ -1,14 +1,26 @@
 import styles from "./ReleaseNote.module.scss";
 
+type Section = {
+    title: string;
+    tag: "feature" | "bug" | "announcement" | "improvement";
+    text: HTMLElementTagNameMap["p"];
+};
+
 interface ReleaseNoteProps {
     header: string;
-    sections: Array<{
-        title: string;
-        tag: "feature" | "bug" | "announcement" | "improvement";
-        text: HTMLElementTagNameMap["p"];
-    }>;
+    sections: Array<Section>;
 }
 
-export const ReleaseNote = ({ header, sections }): ReleaseNoteProps => {
-    return <></>;
-};
+export function ReleaseNote({ header, sections }: ReleaseNoteProps) {
+    return (
+        <div>
+            <p>{header}</p>
+            {sections.map((section: Section) => (
+                <div>
+                    <p>{section.title}</p>
+                    <div></div>
+                </div>
+            ))}
+        </div>
+    );
+}
