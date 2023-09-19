6c5f2fb643cbe77d42ce8d392fbce03212920325
etanb <etan.berkowitz@gmail.com>
2023-09-18 18:18:24 -0700

    bare bones of ReleaseNote

--- frontend-react/src/shared/ReleaseNote/ReleaseNote.tsx
+++ frontend-react/src/shared/ReleaseNote/ReleaseNote.tsx
@@ -1,9 +1,12 @@
+import { Tag } from "@trussworks/react-uswds";
+import { ReactNode } from "react";
+
 import styles from "./ReleaseNote.module.scss";
 
 type Section = {
     title: string;
     tag: "feature" | "bug" | "announcement" | "improvement";
-    text: HTMLElementTagNameMap["p"];
+    body: ReactNode;
 };
 
 interface ReleaseNoteProps {
@@ -11,14 +14,30 @@ interface ReleaseNoteProps {
     sections: Array<Section>;
 }
 
+function NoteTag({ tag }: { tag: Section["tag"] }) {
+    switch (tag) {
+        case "feature":
+            return <Tag className="tag tag--feature">New feature</Tag>;
+        case "bug":
+            return <Tag className="tag tag--bug">Bug fix</Tag>;
+        case "announcement":
+            return <Tag className="tag tag--announcement">Announcement</Tag>;
+        case "improvement":
+            return <Tag className="tag tag--improvement">Improvement</Tag>;
+    }
+}
+
 export function ReleaseNote({ header, sections }: ReleaseNoteProps) {
     return (
-        <div>
+        <div className={styles.ReleaseNote}>
             <p>{header}</p>
             {sections.map((section: Section) => (
                 <div>
                     <p>{section.title}</p>
-                    <div></div>
+                    <div className="section-container">
+                        <NoteTag tag={section.tag} />
+                        {section.body}
+                    </div>
                 </div>
             ))}
         </div>
