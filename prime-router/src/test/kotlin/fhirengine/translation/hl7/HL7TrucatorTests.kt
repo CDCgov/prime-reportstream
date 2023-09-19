8ed2078ede3d9a5626197a85da18eaf305623835
Jamie Albinson <jamie.albinson@focusconsulting.io>
2023-09-19 14:56:38 -0400

    11288 enable truncation for any HL7 message type

--- prime-router/src/test/kotlin/fhirengine/translation/hl7/HL7TrucatorTests.kt
+++ prime-router/src/test/kotlin/fhirengine/translation/hl7/HL7TrucatorTests.kt
@@ -3,6 +3,8 @@ package gov.cdc.prime.router.fhirengine.translation.hl7
 import assertk.assertThat
 import assertk.assertions.isEqualTo
 import assertk.assertions.isNull
+import ca.uhn.hl7v2.model.v251.message.ADT_A01
+import ca.uhn.hl7v2.model.v251.message.OML_O21
 import ca.uhn.hl7v2.model.v251.message.ORU_R01
 import ca.uhn.hl7v2.util.Terser
 import gov.cdc.prime.router.fhirengine.translation.hl7.config.TruncationConfig
@@ -12,15 +14,18 @@ import kotlin.test.assertEquals
 class HL7TrucatorTests {
 
     private val emptyTerser = Terser(ORU_R01())
-    private val hl7Truncator = HL7Truncator()
+    private val covidHL7Truncator = CovidPipelineHL7Truncator()
+    private val upHL7Truncator = UniversalPipelineHL7Truncator()
 
     @Test
     fun `test setTruncationLimitWithEncoding`() {
         val testValueWithSpecialChars = "Test & Value ~ Text ^ String"
         val testValueNoSpecialChars = "Test Value Text String"
         val testLimit = 20
-        val newLimitWithSpecialChars = hl7Truncator.getTruncationLimitWithEncoding(testValueWithSpecialChars, testLimit)
-        val newLimitNoSpecialChars = hl7Truncator.getTruncationLimitWithEncoding(testValueNoSpecialChars, testLimit)
+        val newLimitWithSpecialChars = covidHL7Truncator
+            .getTruncationLimitWithEncoding(testValueWithSpecialChars, testLimit)
+        val newLimitNoSpecialChars = covidHL7Truncator
+            .getTruncationLimitWithEncoding(testValueNoSpecialChars, testLimit)
 
         assertEquals(newLimitWithSpecialChars, 16)
         assertEquals(newLimitNoSpecialChars, testLimit)
@@ -29,25 +34,144 @@ class HL7TrucatorTests {
     @Test
     fun `test getHl7MaxLength`() {
         // Test the ordering provider id has the right length
-        assertThat(hl7Truncator.getHl7MaxLength("ORC-12-1", emptyTerser)).isEqualTo(15)
-        assertThat(hl7Truncator.getHl7MaxLength("OBR-16-1", emptyTerser)).isEqualTo(15)
+        assertThat(covidHL7Truncator.getHl7MaxLength("ORC-12-1", emptyTerser)).isEqualTo(15)
+        assertThat(covidHL7Truncator.getHl7MaxLength("OBR-16-1", emptyTerser)).isEqualTo(15)
         // Test that MSH returns reasonable values
-        assertThat(hl7Truncator.getHl7MaxLength("MSH-7", emptyTerser)).isEqualTo(26)
-        assertThat(hl7Truncator.getHl7MaxLength("MSH-4-1", emptyTerser)).isEqualTo(20)
-        assertThat(hl7Truncator.getHl7MaxLength("MSH-3-1", emptyTerser)).isEqualTo(20)
-        assertThat(hl7Truncator.getHl7MaxLength("MSH-4-2", emptyTerser)).isEqualTo(199)
-        assertThat(hl7Truncator.getHl7MaxLength("MSH-1", emptyTerser)).isEqualTo(1)
+        assertThat(covidHL7Truncator.getHl7MaxLength("MSH-7", emptyTerser)).isEqualTo(26)
+        assertThat(covidHL7Truncator.getHl7MaxLength("MSH-4-1", emptyTerser)).isEqualTo(20)
+        assertThat(covidHL7Truncator.getHl7MaxLength("MSH-3-1", emptyTerser)).isEqualTo(20)
+        assertThat(covidHL7Truncator.getHl7MaxLength("MSH-4-2", emptyTerser)).isEqualTo(199)
+        assertThat(covidHL7Truncator.getHl7MaxLength("MSH-1", emptyTerser)).isEqualTo(1)
         // Test that OBX returns reasonable values
-        assertThat(hl7Truncator.getHl7MaxLength("OBX-2", emptyTerser)).isEqualTo(2)
-        assertThat(hl7Truncator.getHl7MaxLength("OBX-5", emptyTerser)).isEqualTo(99999)
-        assertThat(hl7Truncator.getHl7MaxLength("OBX-11", emptyTerser)).isEqualTo(1)
+        assertThat(covidHL7Truncator.getHl7MaxLength("OBX-2", emptyTerser)).isEqualTo(2)
+        assertThat(covidHL7Truncator.getHl7MaxLength("OBX-5", emptyTerser)).isEqualTo(99999)
+        assertThat(covidHL7Truncator.getHl7MaxLength("OBX-11", emptyTerser)).isEqualTo(1)
         // This component limit is smaller than the enclosing field. This inconsistency was fixed by v2.9
-        assertThat(hl7Truncator.getHl7MaxLength("OBX-18", emptyTerser)).isEqualTo(22)
-        assertThat(hl7Truncator.getHl7MaxLength("OBX-18-1", emptyTerser)).isEqualTo(199)
-        assertThat(hl7Truncator.getHl7MaxLength("OBX-19", emptyTerser)).isEqualTo(26)
-        assertThat(hl7Truncator.getHl7MaxLength("OBX-23-1", emptyTerser)).isEqualTo(50)
+        assertThat(covidHL7Truncator.getHl7MaxLength("OBX-18", emptyTerser)).isEqualTo(22)
+        assertThat(covidHL7Truncator.getHl7MaxLength("OBX-18-1", emptyTerser)).isEqualTo(199)
+        assertThat(covidHL7Truncator.getHl7MaxLength("OBX-19", emptyTerser)).isEqualTo(26)
+        assertThat(covidHL7Truncator.getHl7MaxLength("OBX-23-1", emptyTerser)).isEqualTo(50)
         // Test that a subcomponent returns null
-        assertThat(hl7Truncator.getHl7MaxLength("OBR-16-1-2", emptyTerser)).isNull()
+        assertThat(covidHL7Truncator.getHl7MaxLength("OBR-16-1-2", emptyTerser)).isNull()
+    }
+
+    @Test
+    fun `test UP getHl7MaxLength for ORU_R01`() {
+        // Test the ordering provider id has the right length
+        assertThat(
+            upHL7Truncator.getHl7MaxLength("/PATIENT_RESULT/ORDER_OBSERVATION/ORC-12-1", emptyTerser)
+        ).isEqualTo(15)
+        assertThat(
+            upHL7Truncator.getHl7MaxLength("/PATIENT_RESULT/ORDER_OBSERVATION/OBR-16-1", emptyTerser)
+        ).isEqualTo(15)
+        // Test that MSH returns reasonable values
+        assertThat(
+            upHL7Truncator.getHl7MaxLength("MSH-7", emptyTerser)
+        ).isEqualTo(26)
+        assertThat(
+            upHL7Truncator.getHl7MaxLength("MSH-4-1", emptyTerser)
+        ).isEqualTo(20)
+        assertThat(
+            upHL7Truncator.getHl7MaxLength("MSH-3-1", emptyTerser)
+        ).isEqualTo(20)
+        assertThat(
+            upHL7Truncator.getHl7MaxLength("MSH-4-2", emptyTerser)
+        ).isEqualTo(199)
+        assertThat(
+            upHL7Truncator.getHl7MaxLength("MSH-1", emptyTerser)
+        ).isEqualTo(1)
+        // Test that OBX returns reasonable values
+        assertThat(
+            upHL7Truncator.getHl7MaxLength("/PATIENT_RESULT/ORDER_OBSERVATION/SPECIMEN/OBX-2", emptyTerser)
+        ).isEqualTo(2)
+        assertThat(
+            upHL7Truncator.getHl7MaxLength("/PATIENT_RESULT/ORDER_OBSERVATION/SPECIMEN/OBX-5", emptyTerser)
+        ).isEqualTo(99999)
+        assertThat(
+            upHL7Truncator.getHl7MaxLength("/PATIENT_RESULT/ORDER_OBSERVATION/SPECIMEN/OBX-11", emptyTerser)
+        ).isEqualTo(1)
+        // This component limit is smaller than the enclosing field. This inconsistency was fixed by v2.9
+        assertThat(
+            upHL7Truncator.getHl7MaxLength("/PATIENT_RESULT/ORDER_OBSERVATION/SPECIMEN/OBX-18", emptyTerser)
+        ).isEqualTo(22)
+        assertThat(
+            upHL7Truncator.getHl7MaxLength("/PATIENT_RESULT/ORDER_OBSERVATION/SPECIMEN/OBX-18-1", emptyTerser)
+        ).isEqualTo(199)
+        assertThat(
+            upHL7Truncator.getHl7MaxLength("/PATIENT_RESULT/ORDER_OBSERVATION/SPECIMEN/OBX-19", emptyTerser)
+        ).isEqualTo(26)
+        assertThat(
+            upHL7Truncator.getHl7MaxLength("/PATIENT_RESULT/ORDER_OBSERVATION/SPECIMEN/OBX-23-1", emptyTerser)
+        ).isEqualTo(50)
+        // Test that a subcomponent returns null
+        assertThat(
+            upHL7Truncator.getHl7MaxLength("/PATIENT_RESULT/ORDER_OBSERVATION/OBR-16-1-2", emptyTerser)
+        ).isNull()
+    }
+
+    @Test
+    fun `test UP getHl7MaxLength for OML_O21`() {
+        val terser = Terser(OML_O21())
+
+        // Test the ordering provider id has the right length
+        assertThat(upHL7Truncator.getHl7MaxLength("/ORDER/ORC-12-1", terser)).isEqualTo(15)
+        assertThat(upHL7Truncator.getHl7MaxLength("/ORDER/OBSERVATION_REQUEST/OBR-16-1", terser)).isEqualTo(15)
+        // Test that MSH returns reasonable values
+        assertThat(upHL7Truncator.getHl7MaxLength("MSH-7", terser)).isEqualTo(26)
+        assertThat(upHL7Truncator.getHl7MaxLength("MSH-4-1", terser)).isEqualTo(20)
+        assertThat(upHL7Truncator.getHl7MaxLength("MSH-3-1", terser)).isEqualTo(20)
+        assertThat(upHL7Truncator.getHl7MaxLength("MSH-4-2", terser)).isEqualTo(199)
+        assertThat(upHL7Truncator.getHl7MaxLength("MSH-1", terser)).isEqualTo(1)
+        // Test that OBX returns reasonable values
+        assertThat(
+            upHL7Truncator.getHl7MaxLength("/ORDER/OBSERVATION_REQUEST/OBSERVATION/OBX-2", terser)
+        ).isEqualTo(2)
+        assertThat(
+            upHL7Truncator.getHl7MaxLength("/ORDER/OBSERVATION_REQUEST/OBSERVATION/OBX-5", terser)
+        ).isEqualTo(99999)
+        assertThat(
+            upHL7Truncator.getHl7MaxLength("/ORDER/OBSERVATION_REQUEST/OBSERVATION/OBX-11", terser)
+        ).isEqualTo(1)
+        // This component limit is smaller than the enclosing field. This inconsistency was fixed by v2.9
+        assertThat(
+            upHL7Truncator.getHl7MaxLength("/ORDER/OBSERVATION_REQUEST/OBSERVATION/OBX-18", terser)
+        ).isEqualTo(22)
+        assertThat(
+            upHL7Truncator.getHl7MaxLength("/ORDER/OBSERVATION_REQUEST/OBSERVATION/OBX-18-1", terser)
+        ).isEqualTo(199)
+        assertThat(
+            upHL7Truncator.getHl7MaxLength("/ORDER/OBSERVATION_REQUEST/OBSERVATION/OBX-19", terser)
+        ).isEqualTo(26)
+        assertThat(
+            upHL7Truncator.getHl7MaxLength("/ORDER/OBSERVATION_REQUEST/OBSERVATION/OBX-23-1", terser)
+        ).isEqualTo(50)
+        // Test that a subcomponent returns null
+        assertThat(
+            upHL7Truncator.getHl7MaxLength("/ORDER/OBSERVATION_REQUEST/OBR-16-1-2", terser)
+        ).isNull()
+    }
+
+    @Test
+    fun `test UP getHl7MaxLength for ADT_A01`() {
+        val terser = Terser(ADT_A01())
+
+        // Test that MSH returns reasonable values
+        assertThat(upHL7Truncator.getHl7MaxLength("MSH-7", terser)).isEqualTo(26)
+        assertThat(upHL7Truncator.getHl7MaxLength("MSH-4-1", terser)).isEqualTo(20)
+        assertThat(upHL7Truncator.getHl7MaxLength("MSH-3-1", terser)).isEqualTo(20)
+        assertThat(upHL7Truncator.getHl7MaxLength("MSH-4-2", terser)).isEqualTo(199)
+        assertThat(upHL7Truncator.getHl7MaxLength("MSH-1", terser)).isEqualTo(1)
+        // Test that OBX returns reasonable values
+        assertThat(upHL7Truncator.getHl7MaxLength("OBX-2", terser)).isEqualTo(2)
+        assertThat(upHL7Truncator.getHl7MaxLength("OBX-5", terser)).isEqualTo(99999)
+        assertThat(upHL7Truncator.getHl7MaxLength("OBX-11", terser)).isEqualTo(1)
+        // This component limit is smaller than the enclosing field. This inconsistency was fixed by v2.9
+        assertThat(upHL7Truncator.getHl7MaxLength("OBX-18", terser)).isEqualTo(22)
+        assertThat(upHL7Truncator.getHl7MaxLength("OBX-18-1", terser)).isEqualTo(199)
+        assertThat(upHL7Truncator.getHl7MaxLength("OBX-19", terser)).isEqualTo(26)
+        assertThat(upHL7Truncator.getHl7MaxLength("OBX-23-1", terser)).isEqualTo(50)
+        // Test that a subcomponent returns null
+        assertThat(upHL7Truncator.getHl7MaxLength("OBX-23-1-2", terser)).isNull()
     }
 
     @Test
@@ -64,7 +188,7 @@ class HL7TrucatorTests {
         )
 
         inputAndExpected.forEach { (input, expected) ->
-            val actual = hl7Truncator.trimAndTruncateValue(
+            val actual = covidHL7Truncator.trimAndTruncateValue(
                 input,
                 "MSH-4-1",
                 emptyTerser,
@@ -95,7 +219,7 @@ class HL7TrucatorTests {
         )
 
         inputAndExpected.forEach { (input, expected) ->
-            val actual = hl7Truncator.trimAndTruncateValue(
+            val actual = covidHL7Truncator.trimAndTruncateValue(
                 input,
                 "MSH-4-1",
                 emptyTerser,
@@ -120,7 +244,7 @@ class HL7TrucatorTests {
         )
 
         inputAndExpected.forEach { (input, expected) ->
-            val actual = hl7Truncator.trimAndTruncateValue(
+            val actual = covidHL7Truncator.trimAndTruncateValue(
                 input,
                 "MSH-4-1",
                 emptyTerser,
@@ -143,7 +267,7 @@ class HL7TrucatorTests {
         )
 
         inputAndExpected.forEach { (input, expected) ->
-            val actual = hl7Truncator.trimAndTruncateValue(
+            val actual = covidHL7Truncator.trimAndTruncateValue(
                 input,
                 "MSH-4-1",
                 emptyTerser,
