29642ad8cb8dc89ee9993b11b4f9285ee30630f5
Penny Lischer <penny.lischer@agile6.com>
2023-09-27 14:43:05 -0700

    11460 - removed alt text

--- frontend-react/src/components/alerts/NoServicesAlert.tsx
+++ frontend-react/src/components/alerts/NoServicesAlert.tsx
@@ -7,10 +7,7 @@ export const NoServicesBanner = () => {
     return (
         <>
             <section className="rs-text-align-center">
-                <img
-                    src="/assets/folder-search.svg"
-                    alt="Map of states using ReportStream"
-                />
+                <img src="/assets/folder-search.svg" alt="" />
                 <h3 className="margin-top-0">No available data</h3>
                 <p>
                     If you need more help finding what you're looking for,{" "}
