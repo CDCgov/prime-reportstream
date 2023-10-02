50143daeb401f1d636a3b98f7762d2f5b95ee7dd
Michael Kalish <michael.kalish@focusconsulting.io>
2023-10-02 14:15:33 -0400

    fixup! 11549: finish NTE -> Annotation mappings

--- prime-router/src/testIntegration/kotlin/datatests/mappinginventory/nte_note/NTETests.kt
+++ prime-router/src/testIntegration/kotlin/datatests/mappinginventory/nte_note/NTETests.kt
@@ -4,13 +4,14 @@ import gov.cdc.prime.router.datatests.mappinginventory.translateAndCompareFHIRTo
 import gov.cdc.prime.router.datatests.mappinginventory.translateAndCompareHL7ToFHIR
 import org.junit.jupiter.api.Test
 
-class NTETests {
-
+// This test asserts against a sample HL7 that an NTE segment associated with a PID, OBR and OBX and verifies
+// that the no data is loss between each step.  The NTE segments associated with PID and OBX both have NTE.5 populated
+class HL7ToFhirToHL7 {
     @Test
     fun `can accurately map NTE segment to annotation resource`() {
         assert(
             translateAndCompareHL7ToFHIR(
-                "mappinginventory/nte_note/nte_segment_test_file.hl7",
+                "mappinginventory/NTE/nte_segment_test_file.hl7",
                 "mappinginventory/nte_note/annotation_resource_test_file.fhir"
             ).passed
         )
@@ -20,8 +21,25 @@ class NTETests {
     fun `can accurately map Annotation resource to NTE segment`() {
         assert(
             translateAndCompareFHIRToHL7(
-                "mappinginventory/nte_note/annotation_resource_test_file.fhir",
+                "mappinginventory/NTE/annotation_resource_test_file.fhir",
                 "mappinginventory/nte_note/nte_segment_test_file.hl7"
+            ).passed
+        )
+    }
+}
+
+// FHIR supports an authorString in addition to the authorReference that is generated in the HL7->FHIR conversion
+// which does not have a direct mapping to HL7.  If provided, the mapping will append "Authored by: {authorString}"
+// to the note comment
+
+class FhirToHL7 {
+
+    @Test
+    fun `can acccurately map an authorString fhir attribute to an NTE segment`() {
+        assert(
+            translateAndCompareFHIRToHL7(
+                "mappinginventory/NTE/annotation_author_string_resource_test_file.fhir",
+                "mappinginventory/nte_note/nte_segment_author_string_test_file.hl7"
             ).passed
         )
     }
