2d0c4deae933b58cd40c75dcaf70213a530bb01d
Michael Kalish <michael.kalish@focusconsulting.io>
2023-10-12 15:08:43 -0400

    fixup! 11113: XON to Organization to XON

--- prime-router/src/testIntegration/kotlin/datatests/mappinginventory/xon/XONToOrganization.kt
+++ prime-router/src/testIntegration/kotlin/datatests/mappinginventory/xon/XONToOrganization.kt
@@ -21,8 +21,8 @@ class XONToOrganization {
 
         assert(
             translateAndCompareFHIRToHL7(
-                "mappinginventory/xon/xon-to-organization-xon10-populated.fhir",
-                "mappinginventory/xon/xon-to-organization-xon10-populated.hl7",
+                "mappinginventory/xon/xon-to-organization.fhir",
+                "mappinginventory/xon/xon-to-organization.hl7",
             ).passed
         )
     }
@@ -40,6 +40,23 @@ class XONToOrganization {
             translateAndCompareFHIRToHL7(
                 "mappinginventory/xon/xon-to-organization-xon10-populated.fhir",
                 "mappinginventory/xon/xon-to-organization-xon10-populated.hl7",
+            ).passed
+        )
+    }
+
+    @Test
+    fun `test translate to HL7 to FHIR to HL7 when XON-10 is populated and XON-3 is empty`() {
+        assert(
+            translateAndCompareHL7ToFHIR(
+                "mappinginventory/xon/xon-to-organization-xon10-populated-xon3-empty.hl7",
+                "mappinginventory/xon/xon-to-organization-xon10-populated-xon3-empty.fhir",
+            ).passed
+        )
+
+        assert(
+            translateAndCompareFHIRToHL7(
+                "mappinginventory/xon/xon-to-organization-xon10-populated-xon3-empty.fhir",
+                "mappinginventory/xon/xon-to-organization-xon10-populated-xon3-empty.hl7",
             ).passed
         )
     }
