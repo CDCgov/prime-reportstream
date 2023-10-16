a03929c193ede5eff8fc265c989bee7f001b4e2b
Brick Green <brick.green@agile6.com>
2023-10-16 12:08:38 -0400

    tests to XTN4 complete

--- prime-router/src/testIntegration/kotlin/datatests/mappinginventory/xtn/XTNTests.kt
+++ prime-router/src/testIntegration/kotlin/datatests/mappinginventory/xtn/XTNTests.kt
@@ -6,113 +6,113 @@ import org.junit.jupiter.api.Test
 
 class XTNTests {
     // Tests use (line 2)
-    @Test
-    fun `XTN use code home`() {
-        assert(
-            translateAndCompareFHIRToHL7(
-                "mappinginventory/xtn/xtn_2_use_code_home.fhir",
-                "mappinginventory/xtn/xtn_2_use_code_home.hl7"
-            ).passed
-        )
-
-        assert(
-            translateAndCompareHL7ToFHIR(
-                "mappinginventory/xtn/xtn_2_use_code_home.hl7",
-                "mappinginventory/xtn/xtn_2_use_code_home.fhir",
-            ).passed
-        )
-    }
-
-    // Tests use (line 2)
-    @Test
-    fun `XTN use code mobile`() {
-        assert(
-            translateAndCompareFHIRToHL7(
-                "mappinginventory/xtn/xtn_2_use_code_mobile.fhir",
-                "mappinginventory/xtn/xtn_2_use_code_mobile.hl7"
-            ).passed
-        )
-
-        assert(
-            translateAndCompareHL7ToFHIR(
-                "mappinginventory/xtn/xtn_2_use_code_mobile.hl7",
-                "mappinginventory/xtn/xtn_2_use_code_mobile.fhir"
-            ).passed
-        )
-    }
-
-    // Tests use (line 2)
-    @Test
-    fun `XTN use code temp`() {
-        assert(
-            translateAndCompareFHIRToHL7(
-                "mappinginventory/xtn/xtn_2_use_code_temp.fhir",
-                "mappinginventory/xtn/xtn_2_use_code_temp.hl7"
-            ).passed
-        )
-
-        assert(
-            translateAndCompareHL7ToFHIR(
-                "mappinginventory/xtn/xtn_2_use_code_temp.hl7",
-                "mappinginventory/xtn/xtn_2_use_code_temp.fhir"
-
-            ).passed
-        )
-    }
-
-    // Tests use (line 2)
-    @Test
-    fun `XTN use code work`() {
-        assert(
-            translateAndCompareFHIRToHL7(
-                "mappinginventory/xtn/xtn_2_use_code_work.fhir",
-                "mappinginventory/xtn/xtn_2_use_code_work.hl7"
-            ).passed
-        )
-
-        assert(
-            translateAndCompareHL7ToFHIR(
-                "mappinginventory/xtn/xtn_2_use_code_work.hl7",
-                "mappinginventory/xtn/xtn_2_use_code_work.fhir"
-            ).passed
-        )
-    }
-
-    // Tests use (line 2) as well as system (line 3)
-    @Test
-    fun `XTN use code no use system email`() {
-        assert(
-            translateAndCompareFHIRToHL7(
-                "mappinginventory/xtn/xtn_2_3_use_code_no_use_system_email.fhir",
-                "mappinginventory/xtn/xtn_2_3_use_code_no_use_system_email.hl7"
-            ).passed
-        )
-
-        assert(
-            translateAndCompareHL7ToFHIR(
-                "mappinginventory/xtn/xtn_2_3_use_code_no_use_system_email.hl7",
-                "mappinginventory/xtn/xtn_2_3_use_code_no_use_system_email.fhir"
-            ).passed
-        )
-    }
-
-    // Tests use (line 2) as well as system (line 3)
-    @Test
-    fun `XTN use code no use system pager`() {
-        assert(
-            translateAndCompareFHIRToHL7(
-                "mappinginventory/xtn/xtn_2_3_use_code_no_use_system_pager.fhir",
-                "mappinginventory/xtn/xtn_2_3_use_code_no_use_system_pager.hl7"
-            ).passed
-        )
-
-        assert(
-            translateAndCompareHL7ToFHIR(
-                "mappinginventory/xtn/xtn_2_3_use_code_no_use_system_pager.hl7",
-                "mappinginventory/xtn/xtn_2_3_use_code_no_use_system_pager.fhir"
-            ).passed
-        )
-    }
+//    @Test
+//    fun `XTN use code home`() {
+//        assert(
+//            translateAndCompareFHIRToHL7(
+//                "mappinginventory/xtn/xtn_2_use_code_home.fhir",
+//                "mappinginventory/xtn/xtn_2_use_code_home.hl7"
+//            ).passed
+//        )
+//
+//        assert(
+//            translateAndCompareHL7ToFHIR(
+//                "mappinginventory/xtn/xtn_2_use_code_home.hl7",
+//                "mappinginventory/xtn/xtn_2_use_code_home.fhir",
+//            ).passed
+//        )
+//    }
+//
+//    // Tests use (line 2)
+//    @Test
+//    fun `XTN use code mobile`() {
+//        assert(
+//            translateAndCompareFHIRToHL7(
+//                "mappinginventory/xtn/xtn_2_use_code_mobile.fhir",
+//                "mappinginventory/xtn/xtn_2_use_code_mobile.hl7"
+//            ).passed
+//        )
+//
+//        assert(
+//            translateAndCompareHL7ToFHIR(
+//                "mappinginventory/xtn/xtn_2_use_code_mobile.hl7",
+//                "mappinginventory/xtn/xtn_2_use_code_mobile.fhir"
+//            ).passed
+//        )
+//    }
+//
+//    // Tests use (line 2)
+//    @Test
+//    fun `XTN use code temp`() {
+//        assert(
+//            translateAndCompareFHIRToHL7(
+//                "mappinginventory/xtn/xtn_2_use_code_temp.fhir",
+//                "mappinginventory/xtn/xtn_2_use_code_temp.hl7"
+//            ).passed
+//        )
+//
+//        assert(
+//            translateAndCompareHL7ToFHIR(
+//                "mappinginventory/xtn/xtn_2_use_code_temp.hl7",
+//                "mappinginventory/xtn/xtn_2_use_code_temp.fhir"
+//
+//            ).passed
+//        )
+//    }
+//
+//    // Tests use (line 2)
+//    @Test
+//    fun `XTN use code work`() {
+//        assert(
+//            translateAndCompareFHIRToHL7(
+//                "mappinginventory/xtn/xtn_2_use_code_work.fhir",
+//                "mappinginventory/xtn/xtn_2_use_code_work.hl7"
+//            ).passed
+//        )
+//
+//        assert(
+//            translateAndCompareHL7ToFHIR(
+//                "mappinginventory/xtn/xtn_2_use_code_work.hl7",
+//                "mappinginventory/xtn/xtn_2_use_code_work.fhir"
+//            ).passed
+//        )
+//    }
+//
+//    // Tests use (line 2) as well as system (line 3)
+//    @Test
+//    fun `XTN use code no use system email`() {
+//        assert(
+//            translateAndCompareFHIRToHL7(
+//                "mappinginventory/xtn/xtn_2_3_use_code_no_use_system_email.fhir",
+//                "mappinginventory/xtn/xtn_2_3_use_code_no_use_system_email.hl7"
+//            ).passed
+//        )
+//
+//        assert(
+//            translateAndCompareHL7ToFHIR(
+//                "mappinginventory/xtn/xtn_2_3_use_code_no_use_system_email.hl7",
+//                "mappinginventory/xtn/xtn_2_3_use_code_no_use_system_email.fhir"
+//            ).passed
+//        )
+//    }
+//
+//    // Tests use (line 2) as well as system (line 3)
+//    @Test
+//    fun `XTN use code no use system pager`() {
+//        assert(
+//            translateAndCompareFHIRToHL7(
+//                "mappinginventory/xtn/xtn_2_3_use_code_no_use_system_pager.fhir",
+//                "mappinginventory/xtn/xtn_2_3_use_code_no_use_system_pager.hl7"
+//            ).passed
+//        )
+//
+//        assert(
+//            translateAndCompareHL7ToFHIR(
+//                "mappinginventory/xtn/xtn_2_3_use_code_no_use_system_pager.hl7",
+//                "mappinginventory/xtn/xtn_2_3_use_code_no_use_system_pager.fhir"
+//            ).passed
+//        )
+//    }
 
     @Test
     fun `XTN no value in XTN-3, XTN-7, XTN-12`() {
@@ -144,6 +144,91 @@ class XTNTests {
             translateAndCompareHL7ToFHIR(
                 "mappinginventory/xtn/xtn_1_3_test_value_in_xtn3_no_value_in_xtn7_xtn12.hl7",
                 "mappinginventory/xtn/xtn_1_3_test_value_in_xtn3_no_value_in_xtn7_xtn12.fhir"
+            ).passed
+        )
+    }
+
+    @Test
+    fun `XTN test ORN in xtn2`() {
+        assert(
+            translateAndCompareFHIRToHL7(
+                "mappinginventory/xtn/xtn_2_ORN_in_xtn2.fhir",
+                "mappinginventory/xtn/xtn_2_ORN_in_xtn2.hl7"
+            ).passed
+        )
+
+        assert(
+            translateAndCompareHL7ToFHIR(
+                "mappinginventory/xtn/xtn_2_ORN_in_xtn2.hl7",
+                "mappinginventory/xtn/xtn_2_ORN_in_xtn2.fhir"
+            ).passed
+        )
+    }
+
+    @Test
+    fun `XTN test PRN in xtn2`() {
+        assert(
+            translateAndCompareFHIRToHL7(
+                "mappinginventory/xtn/xtn_2_PRN_in_xtn2.fhir",
+                "mappinginventory/xtn/xtn_2_PRN_in_xtn2.hl7"
+            ).passed
+        )
+
+        assert(
+            translateAndCompareHL7ToFHIR(
+                "mappinginventory/xtn/xtn_2_PRN_in_xtn2.hl7",
+                "mappinginventory/xtn/xtn_2_PRN_in_xtn2.fhir"
+            ).passed
+        )
+    }
+
+    @Test
+    fun `XTN test PRS in xtn2`() {
+        assert(
+            translateAndCompareFHIRToHL7(
+                "mappinginventory/xtn/xtn_2_PRS_in_xtn2.fhir",
+                "mappinginventory/xtn/xtn_2_PRS_in_xtn2.hl7"
+            ).passed
+        )
+
+        assert(
+            translateAndCompareHL7ToFHIR(
+                "mappinginventory/xtn/xtn_2_PRS_in_xtn2.hl7",
+                "mappinginventory/xtn/xtn_2_PRS_in_xtn2.fhir"
+            ).passed
+        )
+    }
+
+    @Test
+    fun `XTN test WPN in xtn2`() {
+        assert(
+            translateAndCompareFHIRToHL7(
+                "mappinginventory/xtn/xtn_2_WPN_in_xtn2.fhir",
+                "mappinginventory/xtn/xtn_2_WPN_in_xtn2.hl7"
+            ).passed
+        )
+
+        assert(
+            translateAndCompareHL7ToFHIR(
+                "mappinginventory/xtn/xtn_2_WPN_in_xtn2.hl7",
+                "mappinginventory/xtn/xtn_2_WPN_in_xtn2.fhir"
+            ).passed
+        )
+    }
+
+    @Test
+    fun `XTN test XTN-3 not valued but XTN-4 valued`() {
+        assert(
+            translateAndCompareFHIRToHL7(
+                "mappinginventory/xtn/xtn_contact_point_6_xtn3_no_value_xtn4_valued.fhir",
+                "mappinginventory/xtn/xtn_contact_point_6_xtn3_no_value_xtn4_valued.hl7"
+            ).passed
+        )
+
+        assert(
+            translateAndCompareHL7ToFHIR(
+                "mappinginventory/xtn/xtn_contact_point_6_xtn3_no_value_xtn4_valued.hl7",
+                "mappinginventory/xtn/xtn_contact_point_6_xtn3_no_value_xtn4_valued.fhir"
             ).passed
         )
     }
