b0c7097162e5976db090025f21ecda5e5db82855
Brick Green <brick.green@agile6.com>
2023-10-18 15:39:28 -0400

    transition test files to use NK1.40

--- prime-router/src/testIntegration/kotlin/datatests/mappinginventory/xtn/XTNTests.kt
+++ prime-router/src/testIntegration/kotlin/datatests/mappinginventory/xtn/XTNTests.kt
@@ -118,15 +118,15 @@ class XTNTests {
     fun `XTN no value in XTN-3, XTN-7, XTN-12`() {
         assert(
             translateAndCompareFHIRToHL7(
-                "mappinginventory/xtn/xtn_1_no_value_in_xtn3_xtn7_xtn12.fhir",
-                "mappinginventory/xtn/xtn_1_no_value_in_xtn3_xtn7_xtn12.hl7"
+                "mappinginventory/xtn/contact-point/xtn_3_no_value_in_xtn3_xtn7_xtn12.fhir",
+                "mappinginventory/xtn/contact-point/xtn_3_no_value_in_xtn3_xtn7_xtn12.hl7"
             ).passed
         )
 
         assert(
             translateAndCompareHL7ToFHIR(
-                "mappinginventory/xtn/xtn_1_no_value_in_xtn3_xtn7_xtn12.hl7",
-                "mappinginventory/xtn/xtn_1_no_value_in_xtn3_xtn7_xtn12.fhir"
+                "mappinginventory/xtn/contact-point/xtn_3_no_value_in_xtn3_xtn7_xtn12.hl7",
+                "mappinginventory/xtn/contact-point/xtn_3_no_value_in_xtn3_xtn7_xtn12.fhir"
             ).passed
         )
     }
@@ -135,83 +135,83 @@ class XTNTests {
     fun `XTN test value in XTN-3 and no value in XTN-7, XTN-12`() {
         assert(
             translateAndCompareFHIRToHL7(
-                "mappinginventory/xtn/xtn_1_3_test_value_in_xtn3_no_value_in_xtn7_xtn12.fhir",
-                "mappinginventory/xtn/xtn_1_3_test_value_in_xtn3_no_value_in_xtn7_xtn12.hl7"
+                "mappinginventory/xtn/contact-point/xtn_3_5_test_value_in_xtn3_no_value_in_xtn7_xtn12.fhir",
+                "mappinginventory/xtn/contact-point/xtn_3_5_test_value_in_xtn3_no_value_in_xtn7_xtn12.hl7"
             ).passed
         )
 
         assert(
             translateAndCompareHL7ToFHIR(
-                "mappinginventory/xtn/xtn_1_3_test_value_in_xtn3_no_value_in_xtn7_xtn12.hl7",
-                "mappinginventory/xtn/xtn_1_3_test_value_in_xtn3_no_value_in_xtn7_xtn12.fhir"
+                "mappinginventory/xtn/contact-point/xtn_3_5_test_value_in_xtn3_no_value_in_xtn7_xtn12.hl7",
+                "mappinginventory/xtn/contact-point/xtn_3_5_test_value_in_xtn3_no_value_in_xtn7_xtn12.fhir"
             ).passed
         )
     }
 
     @Test
-    fun `XTN test ORN in xtn2`() {
+    fun `XTN test ORN in XTN-2`() {
         assert(
             translateAndCompareFHIRToHL7(
-                "mappinginventory/xtn/xtn_2_ORN_in_xtn2.fhir",
-                "mappinginventory/xtn/xtn_2_ORN_in_xtn2.hl7"
+                "mappinginventory/xtn/contact-point/xtn_4_ORN_in_xtn2.fhir",
+                "mappinginventory/xtn/contact-point/xtn_4_ORN_in_xtn2.hl7"
             ).passed
         )
 
         assert(
             translateAndCompareHL7ToFHIR(
-                "mappinginventory/xtn/xtn_2_ORN_in_xtn2.hl7",
-                "mappinginventory/xtn/xtn_2_ORN_in_xtn2.fhir"
+                "mappinginventory/xtn/contact-point/xtn_4_ORN_in_xtn2.hl7",
+                "mappinginventory/xtn/contact-point/xtn_4_ORN_in_xtn2.fhir"
             ).passed
         )
     }
 
     @Test
-    fun `XTN test PRN in xtn2`() {
+    fun `XTN test PRN in XTN-2`() {
         assert(
             translateAndCompareFHIRToHL7(
-                "mappinginventory/xtn/xtn_2_PRN_in_xtn2.fhir",
-                "mappinginventory/xtn/xtn_2_PRN_in_xtn2.hl7"
+                "mappinginventory/xtn/contact-point/xtn_4_PRN_in_xtn2.fhir",
+                "mappinginventory/xtn/contact-point/xtn_4_PRN_in_xtn2.hl7"
             ).passed
         )
 
         assert(
             translateAndCompareHL7ToFHIR(
-                "mappinginventory/xtn/xtn_2_PRN_in_xtn2.hl7",
-                "mappinginventory/xtn/xtn_2_PRN_in_xtn2.fhir"
+                "mappinginventory/xtn/contact-point/xtn_4_PRN_in_xtn2.hl7",
+                "mappinginventory/xtn/contact-point/xtn_4_PRN_in_xtn2.fhir"
             ).passed
         )
     }
 
     @Test
-    fun `XTN test PRS in xtn2`() {
+    fun `XTN test PRS in XTN-2`() {
         assert(
             translateAndCompareFHIRToHL7(
-                "mappinginventory/xtn/xtn_2_PRS_in_xtn2.fhir",
-                "mappinginventory/xtn/xtn_2_PRS_in_xtn2.hl7"
+                "mappinginventory/xtn/contact-point/xtn_4_PRS_in_xtn2.fhir",
+                "mappinginventory/xtn/contact-point/xtn_4_PRS_in_xtn2.hl7"
             ).passed
         )
 
         assert(
             translateAndCompareHL7ToFHIR(
-                "mappinginventory/xtn/xtn_2_PRS_in_xtn2.hl7",
-                "mappinginventory/xtn/xtn_2_PRS_in_xtn2.fhir"
+                "mappinginventory/xtn/contact-point/xtn_4_PRS_in_xtn2.hl7",
+                "mappinginventory/xtn/contact-point/xtn_4_PRS_in_xtn2.fhir"
             ).passed
         )
     }
 
     @Test
-    fun `XTN test WPN in xtn2`() {
+    fun `XTN test WPN in XTN-2`() {
         assert(
             translateAndCompareFHIRToHL7(
-                "mappinginventory/xtn/xtn_2_WPN_in_xtn2.fhir",
-                "mappinginventory/xtn/xtn_2_WPN_in_xtn2.hl7"
+                "mappinginventory/xtn/contact-point/xtn_4_WPN_in_xtn2.fhir",
+                "mappinginventory/xtn/contact-point/xtn_4_WPN_in_xtn2.hl7"
             ).passed
         )
 
         assert(
             translateAndCompareHL7ToFHIR(
-                "mappinginventory/xtn/xtn_2_WPN_in_xtn2.hl7",
-                "mappinginventory/xtn/xtn_2_WPN_in_xtn2.fhir"
+                "mappinginventory/xtn/contact-point/xtn_4_WPN_in_xtn2.hl7",
+                "mappinginventory/xtn/contact-point/xtn_4_WPN_in_xtn2.fhir"
             ).passed
         )
     }
@@ -220,15 +220,15 @@ class XTNTests {
     fun `XTN test XTN-3 not valued but XTN-4 valued`() {
         assert(
             translateAndCompareFHIRToHL7(
-                "mappinginventory/xtn/xtn_contact_point_6_xtn3_no_value_xtn4_valued.fhir",
-                "mappinginventory/xtn/xtn_contact_point_6_xtn3_no_value_xtn4_valued.hl7"
+                "mappinginventory/xtn/contact-point/xtn_6_xtn3_no_value_xtn4_valued.fhir",
+                "mappinginventory/xtn/contact-point/xtn_6_xtn3_no_value_xtn4_valued.hl7"
             ).passed
         )
 
         assert(
             translateAndCompareHL7ToFHIR(
-                "mappinginventory/xtn/xtn_contact_point_6_xtn3_no_value_xtn4_valued.hl7",
-                "mappinginventory/xtn/xtn_contact_point_6_xtn3_no_value_xtn4_valued.fhir"
+                "mappinginventory/xtn/contact-point/xtn_6_xtn3_no_value_xtn4_valued.hl7",
+                "mappinginventory/xtn/contact-point/xtn_6_xtn3_no_value_xtn4_valued.fhir"
             ).passed
         )
     }
@@ -237,15 +237,15 @@ class XTNTests {
     fun `XTN test XTN-3 is Internet and XTN-4 has a value`() {
         assert(
             translateAndCompareFHIRToHL7(
-                "mappinginventory/xtn/xtn_contact_point_8_xtn3_internet_xtn4_test_email.fhir",
-                "mappinginventory/xtn/xtn_contact_point_8_xtn3_internet_xtn4_test_email.hl7"
+                "mappinginventory/xtn/contact-point/xtn_8_xtn3_internet_xtn4_test_email.fhir",
+                "mappinginventory/xtn/contact-point/xtn_8_xtn3_internet_xtn4_test_email.hl7"
             ).passed
         )
 
         assert(
             translateAndCompareHL7ToFHIR(
-                "mappinginventory/xtn/xtn_contact_point_8_xtn3_internet_xtn4_test_email.hl7",
-                "mappinginventory/xtn/xtn_contact_point_8_xtn3_internet_xtn4_test_email.fhir"
+                "mappinginventory/xtn/contact-point/xtn_8_xtn3_internet_xtn4_test_email.hl7",
+                "mappinginventory/xtn/contact-point/xtn_8_xtn3_internet_xtn4_test_email.fhir"
             ).passed
         )
     }
@@ -254,15 +254,15 @@ class XTNTests {
     fun `XTN test value of 1 in XTN-5`() {
         assert(
             translateAndCompareFHIRToHL7(
-                "mappinginventory/xtn/xtn_9_10_xtn5_value_of_1.fhir",
-                "mappinginventory/xtn/xtn_9_10_xtn5_value_of_1.hl7"
+                "mappinginventory/xtn/contact-point/xtn_9_10_xtn5_value_of_1.fhir",
+                "mappinginventory/xtn/contact-point/xtn_9_10_xtn5_value_of_1.hl7"
             ).passed
         )
 
         assert(
             translateAndCompareHL7ToFHIR(
-                "mappinginventory/xtn/xtn_9_10_xtn5_value_of_1.hl7",
-                "mappinginventory/xtn/xtn_9_10_xtn5_value_of_1.fhir"
+                "mappinginventory/xtn/contact-point/xtn_9_10_xtn5_value_of_1.hl7",
+                "mappinginventory/xtn/contact-point/xtn_9_10_xtn5_value_of_1.fhir"
             ).passed
         )
     }
@@ -271,15 +271,15 @@ class XTNTests {
     fun `XTN test value XTN-6`() {
         assert(
             translateAndCompareFHIRToHL7(
-                "mappinginventory/xtn/xtn_11_xtn6_has_value.fhir",
-                "mappinginventory/xtn/xtn_11_xtn6_has_value.hl7"
+                "mappinginventory/xtn/contact-point/xtn_11_xtn6_has_value.fhir",
+                "mappinginventory/xtn/contact-point/xtn_11_xtn6_has_value.hl7"
             ).passed
         )
 
         assert(
             translateAndCompareHL7ToFHIR(
-                "mappinginventory/xtn/xtn_11_xtn6_has_value.hl7",
-                "mappinginventory/xtn/xtn_11_xtn6_has_value.fhir"
+                "mappinginventory/xtn/contact-point/xtn_11_xtn6_has_value.hl7",
+                "mappinginventory/xtn/contact-point/xtn_11_xtn6_has_value.fhir"
             ).passed
         )
     }
@@ -288,15 +288,15 @@ class XTNTests {
     fun `XTN test value in XTN-7 and XTN-3 is Internet`() {
         assert(
             translateAndCompareFHIRToHL7(
-                "mappinginventory/xtn/xtn_12_xtn7_has_value_xtn3_is_Internet.fhir",
-                "mappinginventory/xtn/xtn_12_xtn7_has_value_xtn3_is_Internet.hl7"
+                "mappinginventory/xtn/contact-point/xtn_12_xtn7_has_value_xtn3_is_Internet.fhir",
+                "mappinginventory/xtn/contact-point/xtn_12_xtn7_has_value_xtn3_is_Internet.hl7"
             ).passed
         )
 
         assert(
             translateAndCompareHL7ToFHIR(
-                "mappinginventory/xtn/xtn_12_xtn7_has_value_xtn3_is_Internet.hl7",
-                "mappinginventory/xtn/xtn_12_xtn7_has_value_xtn3_is_Internet.fhir"
+                "mappinginventory/xtn/contact-point/xtn_12_xtn7_has_value_xtn3_is_Internet.hl7",
+                "mappinginventory/xtn/contact-point/xtn_12_xtn7_has_value_xtn3_is_Internet.fhir"
             ).passed
         )
     }
@@ -305,15 +305,15 @@ class XTNTests {
     fun `XTN test value in XTN-8`() {
         assert(
             translateAndCompareFHIRToHL7(
-                "mappinginventory/xtn/xtn_13_xtn8_has_value.fhir",
-                "mappinginventory/xtn/xtn_13_xtn8_has_value.hl7"
+                "mappinginventory/xtn/contact-point/xtn_13_xtn8_has_value.fhir",
+                "mappinginventory/xtn/contact-point/xtn_13_xtn8_has_value.hl7"
             ).passed
         )
 
         assert(
             translateAndCompareHL7ToFHIR(
-                "mappinginventory/xtn/xtn_13_xtn8_has_value.hl7",
-                "mappinginventory/xtn/xtn_13_xtn8_has_value.fhir"
+                "mappinginventory/xtn/contact-point/xtn_13_xtn8_has_value.hl7",
+                "mappinginventory/xtn/contact-point/xtn_13_xtn8_has_value.fhir"
             ).passed
         )
     }
@@ -322,15 +322,15 @@ class XTNTests {
     fun `XTN test value in XTN-9`() {
         assert(
             translateAndCompareFHIRToHL7(
-                "mappinginventory/xtn/xtn_14_xtn9_has_value.fhir",
-                "mappinginventory/xtn/xtn_14_xtn9_has_value.hl7"
+                "mappinginventory/xtn/contact-point/xtn_14_xtn9_has_value.fhir",
+                "mappinginventory/xtn/contact-point/xtn_14_xtn9_has_value.hl7"
             ).passed
         )
 
         assert(
             translateAndCompareHL7ToFHIR(
-                "mappinginventory/xtn/xtn_14_xtn9_has_value.hl7",
-                "mappinginventory/xtn/xtn_14_xtn9_has_value.fhir"
+                "mappinginventory/xtn/contact-point/xtn_14_xtn9_has_value.hl7",
+                "mappinginventory/xtn/contact-point/xtn_14_xtn9_has_value.fhir"
             ).passed
         )
     }
@@ -339,15 +339,15 @@ class XTNTests {
     fun `XTN test value in XTN-7 and XTN-3 is PH`() {
         assert(
             translateAndCompareFHIRToHL7(
-                "mappinginventory/xtn/xtn_12_xtn7_has_value_xtn3_is_PH.fhir",
-                "mappinginventory/xtn/xtn_12_xtn7_has_value_xtn3_is_PH.hl7"
+                "mappinginventory/xtn/contact-point/xtn_12_xtn7_has_value_xtn3_is_PH.fhir",
+                "mappinginventory/xtn/contact-point/xtn_12_xtn7_has_value_xtn3_is_PH.hl7"
             ).passed
         )
 
         assert(
             translateAndCompareHL7ToFHIR(
-                "mappinginventory/xtn/xtn_12_xtn7_has_value_xtn3_is_PH.hl7",
-                "mappinginventory/xtn/xtn_12_xtn7_has_value_xtn3_is_PH.fhir"
+                "mappinginventory/xtn/contact-point/xtn_12_xtn7_has_value_xtn3_is_PH.hl7",
+                "mappinginventory/xtn/contact-point/xtn_12_xtn7_has_value_xtn3_is_PH.fhir"
             ).passed
         )
     }
@@ -356,15 +356,15 @@ class XTNTests {
     fun `XTN test value in XTN-12 and XTN-3 is Internet`() {
         assert(
             translateAndCompareFHIRToHL7(
-                "mappinginventory/xtn/xtn_17_xtn12_has_value_xtn3_is_Internet.fhir",
-                "mappinginventory/xtn/xtn_17_xtn12_has_value_xtn3_is_Internet.hl7"
+                "mappinginventory/xtn/contact-point/xtn_17_xtn12_has_value_xtn3_is_Internet.fhir",
+                "mappinginventory/xtn/contact-point/xtn_17_xtn12_has_value_xtn3_is_Internet.hl7"
             ).passed
         )
 
         assert(
             translateAndCompareHL7ToFHIR(
-                "mappinginventory/xtn/xtn_17_xtn12_has_value_xtn3_is_Internet.hl7",
-                "mappinginventory/xtn/xtn_17_xtn12_has_value_xtn3_is_Internet.fhir"
+                "mappinginventory/xtn/contact-point/xtn_17_xtn12_has_value_xtn3_is_Internet.hl7",
+                "mappinginventory/xtn/contact-point/xtn_17_xtn12_has_value_xtn3_is_Internet.fhir"
             ).passed
         )
     }
@@ -373,15 +373,15 @@ class XTNTests {
     fun `XTN test value in XTN-12 and XTN-3 is PH`() {
         assert(
             translateAndCompareFHIRToHL7(
-                "mappinginventory/xtn/xtn_17_xtn12_has_value_xtn3_is_PH.fhir",
-                "mappinginventory/xtn/xtn_17_xtn12_has_value_xtn3_is_PH.hl7"
+                "mappinginventory/xtn/contact-point/xtn_17_xtn12_has_value_xtn3_is_PH.fhir",
+                "mappinginventory/xtn/contact-point/xtn_17_xtn12_has_value_xtn3_is_PH.hl7"
             ).passed
         )
 
         assert(
             translateAndCompareHL7ToFHIR(
-                "mappinginventory/xtn/xtn_17_xtn12_has_value_xtn3_is_PH.hl7",
-                "mappinginventory/xtn/xtn_17_xtn12_has_value_xtn3_is_PH.fhir"
+                "mappinginventory/xtn/contact-point/xtn_17_xtn12_has_value_xtn3_is_PH.hl7",
+                "mappinginventory/xtn/contact-point/xtn_17_xtn12_has_value_xtn3_is_PH.fhir"
             ).passed
         )
     }
@@ -390,15 +390,15 @@ class XTNTests {
     fun `XTN test value in XTN-13`() {
         assert(
             translateAndCompareFHIRToHL7(
-                "mappinginventory/xtn/xtn_18_xtn13_has_value.fhir",
-                "mappinginventory/xtn/xtn_18_xtn13_has_value.hl7"
+                "mappinginventory/xtn/contact-point/xtn_18_xtn13_has_value.fhir",
+                "mappinginventory/xtn/contact-point/xtn_18_xtn13_has_value.hl7"
             ).passed
         )
 
         assert(
             translateAndCompareHL7ToFHIR(
-                "mappinginventory/xtn/xtn_18_xtn13_has_value.hl7",
-                "mappinginventory/xtn/xtn_18_xtn13_has_value.fhir"
+                "mappinginventory/xtn/contact-point/xtn_18_xtn13_has_value.hl7",
+                "mappinginventory/xtn/contact-point/xtn_18_xtn13_has_value.fhir"
             ).passed
         )
     }
@@ -407,15 +407,15 @@ class XTNTests {
     fun `XTN test value in XTN-14`() {
         assert(
             translateAndCompareFHIRToHL7(
-                "mappinginventory/xtn/xtn_19_xtn14_has_value.fhir",
-                "mappinginventory/xtn/xtn_19_xtn14_has_value.hl7"
+                "mappinginventory/xtn/contact-point/xtn_19_xtn14_has_value.fhir",
+                "mappinginventory/xtn/contact-point/xtn_19_xtn14_has_value.hl7"
             ).passed
         )
 
         assert(
             translateAndCompareHL7ToFHIR(
-                "mappinginventory/xtn/xtn_19_xtn14_has_value.hl7",
-                "mappinginventory/xtn/xtn_19_xtn14_has_value.fhir"
+                "mappinginventory/xtn/contact-point/xtn_19_xtn14_has_value.hl7",
+                "mappinginventory/xtn/contact-point/xtn_19_xtn14_has_value.fhir"
             ).passed
         )
     }
@@ -424,15 +424,15 @@ class XTNTests {
     fun `XTN test positive value in XTN-18`() {
         assert(
             translateAndCompareFHIRToHL7(
-                "mappinginventory/xtn/xtn_23_xtn18_has_positive_value.fhir",
-                "mappinginventory/xtn/xtn_23_xtn18_has_positive_value.hl7"
+                "mappinginventory/xtn/contact-point/xtn_23_xtn18_has_positive_value.fhir",
+                "mappinginventory/xtn/contact-point/xtn_23_xtn18_has_positive_value.hl7"
             ).passed
         )
 
         assert(
             translateAndCompareHL7ToFHIR(
-                "mappinginventory/xtn/xtn_23_xtn18_has_positive_value.hl7",
-                "mappinginventory/xtn/xtn_23_xtn18_has_positive_value.fhir"
+                "mappinginventory/xtn/contact-point/xtn_23_xtn18_has_positive_value.hl7",
+                "mappinginventory/xtn/contact-point/xtn_23_xtn18_has_positive_value.fhir"
             ).passed
         )
     }
@@ -441,15 +441,15 @@ class XTNTests {
 //    fun `XTN test negative value in XTN-18`() {
 //        assert(
 //            !translateAndCompareFHIRToHL7(
-//                "mappinginventory/xtn/xtn_23_xtn18_has_negative_value.fhir",
-//                "mappinginventory/xtn/xtn_23_xtn18_has_negative_value.hl7"
+//                "mappinginventory/xtn/contact-point/xtn_23_xtn18_has_negative_value.fhir",
+//                "mappinginventory/xtn/contact-point/xtn_23_xtn18_has_negative_value.hl7"
 //            ).passed
 //        )
 //
 //        assert(
 //            !translateAndCompareHL7ToFHIR(
-//                "mappinginventory/xtn/xtn_23_xtn18_has_negative_value.hl7",
-//                "mappinginventory/xtn/xtn_23_xtn18_has_negative_value.fhir"
+//                "mappinginventory/xtn/contact-point/xtn_23_xtn18_has_negative_value.hl7",
+//                "mappinginventory/xtn/contact-point/xtn_23_xtn18_has_negative_value.fhir"
 //            ).passed
 //        )
 //    }
