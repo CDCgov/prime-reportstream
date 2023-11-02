0fc0ad5186d5a6a425ca9ee16bca33297ce6a065
jimmyfagan <jimmyfagan@navapbc.com>
2023-11-02 15:00:48 -0400

    Split out DTM to dateTime/date tests, update handling of PID.29

--- prime-router/src/testIntegration/kotlin/datatests/mappinginventory/dtm/DTMTests.kt
+++ prime-router/src/testIntegration/kotlin/datatests/mappinginventory/dtm/DTMTests.kt
@@ -7,14 +7,43 @@ class DTMTests {
 
     @Test
     fun `test dtm to instant`() {
-        assert(verifyHL7ToFHIRToHL7Mapping("dtm/dtm-year-precision-to-instant").passed)
-        assert(verifyHL7ToFHIRToHL7Mapping("dtm/dtm-month-precision-to-instant").passed)
-        assert(verifyHL7ToFHIRToHL7Mapping("dtm/dtm-day-precision-to-instant").passed)
-        assert(verifyHL7ToFHIRToHL7Mapping("dtm/dtm-minute-precision-to-instant").passed)
-        assert(verifyHL7ToFHIRToHL7Mapping("dtm/dtm-second-precision-to-instant").passed)
-        assert(verifyHL7ToFHIRToHL7Mapping("dtm/dtm-dsec-precision-to-instant").passed)
-        assert(verifyHL7ToFHIRToHL7Mapping("dtm/dtm-csec-precision-to-instant").passed)
-        assert(verifyHL7ToFHIRToHL7Mapping("dtm/dtm-msec-precision-to-instant").passed)
-        assert(verifyHL7ToFHIRToHL7Mapping("dtm/dtm-mmsec-precision-to-instant").passed)
+        assert(verifyHL7ToFHIRToHL7Mapping("dtm/instant/dtm-year-precision-to-instant").passed)
+        assert(verifyHL7ToFHIRToHL7Mapping("dtm/instant/dtm-zero-year-to-instant").passed)
+        assert(verifyHL7ToFHIRToHL7Mapping("dtm/instant/dtm-month-precision-to-instant").passed)
+        assert(verifyHL7ToFHIRToHL7Mapping("dtm/instant/dtm-day-precision-to-instant").passed)
+        assert(verifyHL7ToFHIRToHL7Mapping("dtm/instant/dtm-minute-precision-to-instant").passed)
+        assert(verifyHL7ToFHIRToHL7Mapping("dtm/instant/dtm-second-precision-to-instant").passed)
+        assert(verifyHL7ToFHIRToHL7Mapping("dtm/instant/dtm-dsec-precision-to-instant").passed)
+        assert(verifyHL7ToFHIRToHL7Mapping("dtm/instant/dtm-csec-precision-to-instant").passed)
+        assert(verifyHL7ToFHIRToHL7Mapping("dtm/instant/dtm-msec-precision-to-instant").passed)
+        assert(verifyHL7ToFHIRToHL7Mapping("dtm/instant/dtm-mmsec-precision-to-instant").passed)
+    }
+
+    @Test
+    fun `test dtm to dateTime`() {
+        assert(verifyHL7ToFHIRToHL7Mapping("dtm/dateTime/dtm-year-precision-to-dateTime").passed)
+        assert(verifyHL7ToFHIRToHL7Mapping("dtm/dateTime/dtm-zero-year-to-dateTime").passed)
+        assert(verifyHL7ToFHIRToHL7Mapping("dtm/dateTime/dtm-month-precision-to-dateTime").passed)
+        assert(verifyHL7ToFHIRToHL7Mapping("dtm/dateTime/dtm-day-precision-to-dateTime").passed)
+        assert(verifyHL7ToFHIRToHL7Mapping("dtm/dateTime/dtm-minute-precision-to-dateTime").passed)
+        assert(verifyHL7ToFHIRToHL7Mapping("dtm/dateTime/dtm-second-precision-to-dateTime").passed)
+        assert(verifyHL7ToFHIRToHL7Mapping("dtm/dateTime/dtm-dsec-precision-to-dateTime").passed)
+        assert(verifyHL7ToFHIRToHL7Mapping("dtm/dateTime/dtm-csec-precision-to-dateTime").passed)
+        assert(verifyHL7ToFHIRToHL7Mapping("dtm/dateTime/dtm-msec-precision-to-dateTime").passed)
+        assert(verifyHL7ToFHIRToHL7Mapping("dtm/dateTime/dtm-mmsec-precision-to-dateTime").passed)
+    }
+
+    @Test
+    fun `test dtm to date`() {
+        assert(verifyHL7ToFHIRToHL7Mapping("dtm/date/dtm-year-precision-to-date").passed)
+        assert(verifyHL7ToFHIRToHL7Mapping("dtm/date/dtm-zero-year-to-date").passed)
+        assert(verifyHL7ToFHIRToHL7Mapping("dtm/date/dtm-month-precision-to-date").passed)
+        assert(verifyHL7ToFHIRToHL7Mapping("dtm/date/dtm-day-precision-to-date").passed)
+        assert(verifyHL7ToFHIRToHL7Mapping("dtm/date/dtm-minute-precision-to-date").passed)
+        assert(verifyHL7ToFHIRToHL7Mapping("dtm/date/dtm-second-precision-to-date").passed)
+        assert(verifyHL7ToFHIRToHL7Mapping("dtm/date/dtm-dsec-precision-to-date").passed)
+        assert(verifyHL7ToFHIRToHL7Mapping("dtm/date/dtm-csec-precision-to-date").passed)
+        assert(verifyHL7ToFHIRToHL7Mapping("dtm/date/dtm-msec-precision-to-date").passed)
+        assert(verifyHL7ToFHIRToHL7Mapping("dtm/date/dtm-mmsec-precision-to-date").passed)
     }
 }
