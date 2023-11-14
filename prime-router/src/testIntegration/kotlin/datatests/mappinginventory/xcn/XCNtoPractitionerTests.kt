23c347693a2f848cc00eb5ab43bdec640bdfa673
Jack Wang <jack.wang@focusconsulting.io>
2023-11-14 17:57:46 -0500

    fixes

---
+++ prime-router/src/testIntegration/kotlin/datatests/mappinginventory/xcn/XCNtoPractitionerTests.kt
@@ -0,0 +1,12 @@
+package gov.cdc.prime.router.datatests.mappinginventory.xcn
+
+import gov.cdc.prime.router.datatests.mappinginventory.verifyHL7ToFHIRToHL7Mapping
+import org.junit.jupiter.api.Test
+
+class XCNtoPractitionerTests {
+    @Test
+    fun `test translate to HL7 to FHIR to HL7`() {
+        assert(verifyHL7ToFHIRToHL7Mapping("xcn/xcn-to-practitioner-xcn19-20-populated-xcn17-empty").passed)
+        assert(verifyHL7ToFHIRToHL7Mapping("xcn/xcn-to-practitioner-xcn17-populated-xcn19-20-empty").passed)
+    }
+}
