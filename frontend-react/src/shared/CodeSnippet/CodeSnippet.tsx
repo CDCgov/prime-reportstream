ebbdcab0a8e65d97a9439755f311dcda830154e9
Joseph Andersen <12385932+jpandersen87@users.noreply.github.com>
2023-11-14 18:10:19 -0600

    copyString as required prop

--- frontend-react/src/shared/CodeSnippet/CodeSnippet.tsx
+++ frontend-react/src/shared/CodeSnippet/CodeSnippet.tsx
@@ -1,43 +1,15 @@
-import React, { ReactNode, useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { Icon, Tooltip } from "@trussworks/react-uswds";
 import classnames from "classnames";
 
 import styles from "./CodeSnippet.module.scss";
 
-/**
- * Gets the text from the ReactNode object.
- */
-const getNodeText = (node: React.ReactNode): string => {
-    if (node == null) return "";
-
-    switch (typeof node) {
-        case "string":
-        case "number":
-            return node.toString();
-
-        case "boolean":
-            return "";
-
-        case "object": {
-            if (node instanceof Array) return node.map(getNodeText).join("");
-
-            if ("props" in node) return getNodeText(node.props.children);
-            return "";
-        }
-
-        default:
-            console.warn("Unresolved `node` of type:", typeof node, node);
-            return "";
-    }
-};
-
-interface CodeSnippetProps {
-    children?: ReactNode;
+interface CodeSnippetProps extends React.PropsWithChildren {
+    copyString: string;
 }
 
-export const CodeSnippet = ({ children }: CodeSnippetProps) => {
+export const CodeSnippet = ({ children, copyString }: CodeSnippetProps) => {
     const [isCopied, setIsCopied] = useState(false);
-    const copyString = getNodeText(children);
 
     /**
      * Cached component that renders tooltip so that changing isCopied status
