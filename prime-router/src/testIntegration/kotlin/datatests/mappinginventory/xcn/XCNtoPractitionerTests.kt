d6e146bbbd525cc59e5658aef5ca561a4a110f03
Jack Wang <jack.wang@focusconsulting.io>
2023-11-20 14:33:22 -0500

    address pr feedback and update tests to latest master

--- prime-router/src/testIntegration/kotlin/datatests/mappinginventory/xcn/XCNtoPractitionerTests.kt
+++ prime-router/src/testIntegration/kotlin/datatests/mappinginventory/xcn/XCNtoPractitionerTests.kt
@@ -7,7 +7,20 @@ class XCNtoPractitionerTests {
     @Test
     fun `test translate to HL7 to FHIR to HL7`() {
         assert(verifyHL7ToFHIRToHL7Mapping("xcn/xcn-to-practitioner-xcn19-20-populated-xcn17-empty").passed)
+    }
+
+    @Test
+    fun `test translate to HL7 to FHIR to HL7 when XCN-17 is populated`() {
         assert(verifyHL7ToFHIRToHL7Mapping("xcn/xcn-to-practitioner-xcn17-populated-xcn19-20-empty").passed)
+    }
+
+    @Test
+    fun `test translate to HL7 to FHIR to HL7 with organization assigning authority`() {
+        assert(verifyHL7ToFHIRToHL7Mapping("xcn/xcn-to-practitioner-dns-assigning-authority").passed)
+    }
+
+    @Test
+    fun `test translate to HL7 to FHIR to HL7 with system assigning authority`() {
         assert(verifyHL7ToFHIRToHL7Mapping("xcn/xcn-to-practitioner-uuid-assigning-authority").passed)
         assert(verifyHL7ToFHIRToHL7Mapping("xcn/xcn-to-practitioner-uuid-assigning-authority-no-namespace").passed)
         assert(verifyHL7ToFHIRToHL7Mapping("xcn/xcn-to-practitioner-iso-assigning-authority-no-namespace").passed)
