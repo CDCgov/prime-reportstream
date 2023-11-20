9fad84770ffe8eb3e7db88c5b4fb501a6fb6cec6
Stephen Nesman <94193373+snesm@users.noreply.github.com>
2023-11-20 14:11:25 -0500

    Bump io.projectreactor.netty:reactor-netty-http@1.0.38 to io.projectreactor.netty:reactor-netty-http:1.0.39 in /prime-router (#12267)
    * pin reactor-netty-http:1.0.39

--- prime-router/build.gradle.kts
+++ prime-router/build.gradle.kts
@@ -801,6 +801,8 @@ dependencies {
     implementation("com.microsoft.azure.functions:azure-functions-java-library:3.0.0")
     implementation("com.azure:azure-core:1.45.0")
     implementation("com.azure:azure-core-http-netty:1.13.10")
+    // pin io.projectreactor.netty:reactor-netty-http@1.0.39
+    implementation("io.projectreactor.netty:reactor-netty-http:1.0.39")
     implementation("com.azure:azure-storage-blob:12.25.0") {
         exclude(group = "com.azure", module = "azure-core")
     }
