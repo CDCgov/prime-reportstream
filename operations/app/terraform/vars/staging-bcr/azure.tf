1801fc520fbac4d72d63cd86243918bf00f37c14
Denis Krylov <dkrylovsb@users.noreply.github.com>
2023-11-21 14:09:10 -0500

    Switched to the `prime-data-hub-staging-bcr` RG.

--- operations/app/terraform/vars/staging-bcr/azure.tf
+++ operations/app/terraform/vars/staging-bcr/azure.tf
@@ -13,7 +13,7 @@ terraform {
   # NOTE: injected at init time by calling terraform init with the "--backend-config=<path to .tfbackend file>"
   # See configurations directory
   backend "azurerm" {
-    resource_group_name  = "prime-data-hub-staging"
+    resource_group_name  = "prime-data-hub-staging-bcr"
     storage_account_name = "pdhstagingnewterraform"
     container_name       = "terraformstate"
     key                  = "staging.terraform.tfstate"
@@ -27,5 +27,5 @@ provider "azurerm" {
     }
   }
   skip_provider_registration = true
-  subscription_id            = "7d1e3999-6577-4cd5-b296-f518e5c8e677"
+  subscription_id            = "e72f6092-24af-40fe-888c-7379b9c6748a"
 }
