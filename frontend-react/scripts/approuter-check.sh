799000c3e699ca835fe83adb49ab1f06ea66ba76
etanb <etan.berkowitz@gmail.com>
2023-11-30 13:28:20 -0800

    add script to check AppRouter changes

---
+++ frontend-react/scripts/approuter-check.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+APPROUTER_FILE="AppRouter.tsx"
+MODIFIED_FILES=$(git diff --name-only)
+
+# Check if the AppRouter file is in the list of modified files
+if echo "$MODIFIED_FILES" | grep -q "$APPROUTER_FILE"; then
+    read -r -p "You modified $APPROUTER_FILE, did you also need to update the sitemap.xml? (Y/n): " response
+
+    # Convert to lowercase and trim whitespace
+    response=$(echo "$response" | tr -d '[:space:]' | tr '[:upper:]' '[:lower:]')
+
+    if [[ "$response" =~ ^(yes|y)$ ]]; then
+        echo "Proceeding with the rest of the script..."
+    else
+        echo "Exiting script."
+        exit 0
+    fi
+fi
