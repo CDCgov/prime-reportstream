8a87a686f1fc5d2d6439d14b608c4d6b008c2d95
Michael Kalish <michael.kalish@focusconsulting.io>
2023-12-04 15:32:09 -0500

    fixup! 12130: ORC/OBR to ServiceRequest/DiagnosticReport

--- prime-router/src/testIntegration/kotlin/datatests/mappinginventory/orcobr/ORCOBRToServiceRequestDiagnosticReportTests.kt
+++ prime-router/src/testIntegration/kotlin/datatests/mappinginventory/orcobr/ORCOBRToServiceRequestDiagnosticReportTests.kt
@@ -9,4 +9,9 @@ class ORCOBRToServiceRequestDiagnosticReportTests {
     fun `test ORC populated`() {
         assert(verifyHL7ToFHIRToHL7Mapping("orcobr/orc_obr-to-servicerequest_diagnosticreport-orc-populated").passed)
     }
+
+    @Test
+    fun `test OBR populated`() {
+        assert(verifyHL7ToFHIRToHL7Mapping("orcobr/orc_obr-to-servicerequest_diagnosticreport-obr-populated").passed)
+    }
 }
