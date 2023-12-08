c88dd979a449bff3fdcab1ebe0fcba2aa8526d14
Jessica Willis <jessicawillis@navapbc.com>
2023-12-08 10:50:14 -0500

    First pass

--- prime-router/build.gradle.kts
+++ prime-router/build.gradle.kts
@@ -830,6 +830,7 @@ dependencies {
     implementation("tech.tablesaw:tablesaw-core:0.43.1")
     implementation("com.github.ajalt.clikt:clikt-jvm:3.5.4")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.6.0")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:$jacksonVersion")
     implementation("com.fasterxml.jackson.core:jackson-databind:$jacksonVersion")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonVersion")
