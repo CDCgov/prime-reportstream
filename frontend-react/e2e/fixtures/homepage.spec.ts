91cc74c1de69cdb8aea4ec14573c5b20c454f25b
Penny Lischer <penny.lischer@agile6.com>
2023-12-14 16:55:46 -0800

    12307 - added checks for missing internal links

--- frontend-react/e2e/fixtures/homepage.spec.ts
+++ frontend-react/e2e/fixtures/homepage.spec.ts
@@ -4,6 +4,10 @@ import { Header } from "../pages/header";
 import { OurNetwork } from "../pages/our-network";
 import { ExternalLinks } from "../helpers/external-links";
 import { Utils } from "../helpers/utils";
+import { Security } from "../pages/security";
+import { GettingStarted } from "../pages/getting-started";
+import { Support } from "../pages/support";
+import { ManagingYourConnection } from "../pages/managing-your-connection";
 
 test.describe("Homepage", () => {
     test.beforeEach(async ({ page }) => {
@@ -51,6 +55,68 @@ test.describe("Homepage", () => {
     }) => {
         const externalLinks = new ExternalLinks(page);
         await externalLinks.clickOnConnect("footer", "Connect now");
+    });
+
+    test("opens the Getting started page on 'how to set up transfer' click", async ({
+        page,
+    }) => {
+        const header = new Header(page);
+        const gettingStarted = new GettingStarted(page);
+
+        await page
+            .getByRole("link", { name: "how to set up transfer" })
+            .click();
+        await gettingStarted.onLoad();
+        // Go back to the homepage
+        await header.clickOnHome();
+    });
+
+    test("opens the Security page on 'security of your data' click", async ({
+        page,
+    }) => {
+        const header = new Header(page);
+        const security = new Security(page);
+
+        await page.getByRole("link", { name: "security of your data" }).click();
+        await security.onLoad();
+        // Go back to the homepage
+        await header.clickOnHome();
+    });
+
+    test("opens the Support page on 'expert support team' click", async ({
+        page,
+    }) => {
+        const header = new Header(page);
+        const support = new Support(page);
+
+        await page.getByRole("link", { name: "expert support team" }).click();
+        await support.onLoad();
+        // Go back to the homepage
+        await header.clickOnHome();
+    });
+
+    test("opens the managing-your-connection page on 'our tools' click", async ({
+        page,
+    }) => {
+        const header = new Header(page);
+        const managingYourConnection = new ManagingYourConnection(page);
+
+        await page.getByRole("link", { name: "our tools" }).click();
+        await managingYourConnection.onLoad();
+        // Go back to the homepage
+        await header.clickOnHome();
+    });
+
+    test("opens Our Network page on 'See our full network' click", async ({
+        page,
+    }) => {
+        const ourNetwork = new OurNetwork(page);
+        const header = new Header(page);
+
+        await page.getByRole("link", { name: "See our full network" }).click();
+        await ourNetwork.onLoad();
+        // Go back to the homepage
+        await header.clickOnHome();
     });
 
     test("has clickable Where were live map", async ({ page }) => {
