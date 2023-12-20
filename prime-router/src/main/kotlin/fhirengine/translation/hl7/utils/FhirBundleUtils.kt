be95f2a9150736d4ca4115ebdd9586aef118008a
Jessica Willis <jessicawillis@navapbc.com>
2023-12-20 12:52:07 -0500

    Fixed the issue where it could not handle Piped targetTypes

--- prime-router/src/main/kotlin/fhirengine/translation/hl7/utils/FhirBundleUtils.kt
+++ prime-router/src/main/kotlin/fhirengine/translation/hl7/utils/FhirBundleUtils.kt
@@ -41,33 +41,61 @@ object FhirBundleUtils : Logging {
      * Converts a [value] of type [sourceType] into a compatible Base of type [targetType]. Returns the original value
      * and logs an error if the conversion is not supported.
      */
-    fun convertFhirType(value: Base, sourceType: String, targetType: String, logger: KotlinLogger = this.logger): Base {
+    fun convertFhirType(
+        value: Base,
+        sourceType: String,
+        targetType: String,
+        logger: KotlinLogger = this.logger,
+    ): Base? {
         return if (sourceType == targetType || targetType == "*") {
             value
-        } else if (StringCompatibleType.values().any { it.typeAsString == sourceType }) {
-            when (targetType) {
-                StringCompatibleType.Base64Binary.typeAsString -> Base64BinaryType(value.primitiveValue())
-                StringCompatibleType.Canonical.typeAsString -> CanonicalType(value.primitiveValue())
-                StringCompatibleType.Code.typeAsString -> CodeType(value.primitiveValue())
-                StringCompatibleType.Date.typeAsString -> DateType(value.primitiveValue())
-                StringCompatibleType.DateTime.typeAsString -> DateTimeType(value.primitiveValue())
-                StringCompatibleType.Id.typeAsString -> IdType(value.primitiveValue())
-                StringCompatibleType.Instant.typeAsString -> InstantType(value.primitiveValue())
-                StringCompatibleType.Markdown.typeAsString -> MarkdownType(value.primitiveValue())
-                StringCompatibleType.Oid.typeAsString -> OidType(value.primitiveValue())
-                StringCompatibleType.String.typeAsString -> StringType(value.primitiveValue())
-                StringCompatibleType.Time.typeAsString -> TimeType(value.primitiveValue())
-                StringCompatibleType.Uri.typeAsString -> UriType(value.primitiveValue())
-                StringCompatibleType.Url.typeAsString -> UrlType(value.primitiveValue())
-                StringCompatibleType.Uuid.typeAsString -> UuidType(value.primitiveValue())
-                else -> {
-                    logger.error("Conversion between $sourceType and $targetType not supported.")
-                    value
+        } else if (targetType.contains("|")) {
+            val targetTypes = targetType.split("|")
+            targetTypes.forEach {
+                if (sourceType == it || it == "*") {
+                    return value
+                }
+                val attemptedValue = getValue(it, value, logger, sourceType)
+                if (attemptedValue != null) {
+                    return attemptedValue
                 }
             }
+            value
+        } else if (StringCompatibleType.values().any { it.typeAsString == sourceType }) {
+            getValue(targetType, value, logger, sourceType) ?: value
         } else {
             logger.error("Conversion between $sourceType and $targetType not yet implemented.")
             value
         }
+    }
+
+    private fun getValue(
+        targetType: String,
+        value: Base,
+        logger: KotlinLogger,
+        sourceType: String,
+    ) = try {
+        when (targetType) {
+            StringCompatibleType.Base64Binary.typeAsString -> Base64BinaryType(value.primitiveValue())
+            StringCompatibleType.Canonical.typeAsString -> CanonicalType(value.primitiveValue())
+            StringCompatibleType.Code.typeAsString -> CodeType(value.primitiveValue())
+            StringCompatibleType.Date.typeAsString -> DateType(value.primitiveValue())
+            StringCompatibleType.DateTime.typeAsString -> DateTimeType(value.primitiveValue())
+            StringCompatibleType.Id.typeAsString -> IdType(value.primitiveValue())
+            StringCompatibleType.Instant.typeAsString -> InstantType(value.primitiveValue())
+            StringCompatibleType.Markdown.typeAsString -> MarkdownType(value.primitiveValue())
+            StringCompatibleType.Oid.typeAsString -> OidType(value.primitiveValue())
+            StringCompatibleType.String.typeAsString -> StringType(value.primitiveValue())
+            StringCompatibleType.Time.typeAsString -> TimeType(value.primitiveValue())
+            StringCompatibleType.Uri.typeAsString -> UriType(value.primitiveValue())
+            StringCompatibleType.Url.typeAsString -> UrlType(value.primitiveValue())
+            StringCompatibleType.Uuid.typeAsString -> UuidType(value.primitiveValue())
+            else -> {
+                logger.error("Conversion between $sourceType and $targetType not supported.")
+                null
+            }
+        }
+    } catch (e: Exception) {
+        null
     }
 }
