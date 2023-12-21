fd43cbfa225b85d8e3d4906512186d558aaab241
Jessica Willis <jessicawillis@navapbc.com>
2023-12-21 13:05:23 -0500

    Added tests for old and new processEvent messages

--- prime-router/src/test/kotlin/azure/EventTest.kt
+++ prime-router/src/test/kotlin/azure/EventTest.kt
@@ -2,6 +2,7 @@ package gov.cdc.prime.router.azure
 
 import assertk.assertThat
 import assertk.assertions.isEqualTo
+import gov.cdc.prime.router.Options
 import java.time.OffsetDateTime
 import java.util.UUID
 import kotlin.test.Test
@@ -66,6 +67,16 @@ class EventTest {
     @Test
     fun `test batchEvent encode and decode as empty with time`() {
         val event = BatchEvent(Event.EventAction.BATCH, "test", true, OffsetDateTime.now())
+        val message = event.toQueueMessage()
+        val returnEvent = Event.parseQueueMessage(message)
+        assertThat(returnEvent).isEqualTo(event)
+    }
+
+    @Test
+    fun `test processEvent encode and decode with time`() {
+        val at = OffsetDateTime.now()
+        val reportId = UUID.randomUUID()
+        val event = ProcessEvent(Event.EventAction.PROCESS, reportId, Options.None, emptyMap(), emptyList(), at)
         val message = event.toQueueMessage()
         val returnEvent = Event.parseQueueMessage(message)
         assertThat(returnEvent).isEqualTo(event)
@@ -139,6 +150,17 @@ class EventTest {
         val at = OffsetDateTime.now()
         val event = BatchEvent(Event.EventAction.BATCH, "test", true, at)
         val message = "receiver&BATCH&test&true&$at"
+        val returnEvent = Event.parseQueueMessage(message)
+        assertThat(returnEvent).isEqualTo(event)
+    }
+
+    @Test
+    fun `test old processEvent encode and decode with time`() {
+        val at = OffsetDateTime.now()
+        val reportId = UUID.randomUUID()
+        val event = ProcessEvent(Event.EventAction.PROCESS, reportId, Options.None, emptyMap(), emptyList(), at)
+        val message =
+            "process&PROCESS&$reportId&${Options.None}&&${emptyList<String>()}&$at"
         val returnEvent = Event.parseQueueMessage(message)
         assertThat(returnEvent).isEqualTo(event)
     }
