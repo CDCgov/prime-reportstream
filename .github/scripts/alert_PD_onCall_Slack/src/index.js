eda7816432e90191c0b95d8f7a626aa913a636dd
Supriya Addagada <supriya.addagada@geha.com>
2023-12-27 10:12:08 -0600

    pager duty on call notification

---
+++ .github/scripts/alert_PD_onCall_Slack/src/index.js
@@ -0,0 +1,51 @@
+const pd = require("@pagerduty/pdjs");
+const core = require("@actions/core");
+
+async function run() {
+  // parse action inputs
+  const pdToken = core.getInput("token");
+  const scheduleId = core.getInput("schedule-id");
+  const startDate = core.getInput("start-date");
+  const endDate = core.getInput("end-date");
+
+  if (startDate && !endDate) {
+    core.setFailed("An end date is required when a start date is passed in");
+  }
+
+  // set up API client
+  const pdClient = pd.api({ token: pdToken });
+  const params = {
+    "schedule_ids[]": scheduleId,
+    since: startDate,
+    until: endDate,
+    limit: 1,
+  };
+  const queryParams = Object.entries(params)
+    .map(([k, v]) => `${k}=${v}`)
+    .join("&");
+
+  pdClient
+    .get(`/oncalls?${queryParams}`)
+    .then(({ resource }) => {
+      // `resource` should be a list of oncall entries
+      if (resource.length > 0) {
+        core.debug(`Oncalls found: ${JSON.stringify(resource)}`);
+
+        const person = resource[0]["user"]["summary"];
+
+        if (typeof person !== "undefined") {
+          core.info(`🎉 On-call person found: ${person}`);
+          core.setOutput("person", person);
+        } else {
+          core.setFailed("❓ Could not parse on-call entry");
+        }
+      } else {
+        core.setFailed("❓ No one is on the schedule");
+      }
+    })
+    .catch((error) => {
+      core.setFailed(`❌ Unable to fetch on-call data: ${error}`);
+    });
+}
+
+run();
