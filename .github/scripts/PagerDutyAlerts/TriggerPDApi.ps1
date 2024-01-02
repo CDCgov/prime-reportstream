166cb1654e7c922031b5a3e7122833f73a0cfd9b
Supriya Addagada <supriya.addagada@geha.com>
2024-01-02 15:57:58 -0600

    PD API alerts

--- .github/scripts/PagerDutyAlerts/TriggerPDApi.ps1
+++ .github/scripts/PagerDutyAlerts/TriggerPDApi.ps1
@@ -1,23 +1,17 @@
-
+param (
+    [string]$PD_KEY
+)
 $endpoint = "https://api.pagerduty.com/schedules?Accept=application/vnd.pagerduty+json;version=2&Content-Type=application/json"
 
-# $headers = @{
-#     'Authorization' = "Token token=${ secrets.PD_ROTATION_SLACK_NOTIFICATION }"
-#     'Accept'        = 'application/vnd.pagerduty+json;version=2'
-#     'Content-Type'  = 'application/json'
-# }
-
 $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
-$headers.Add("Authorization","Token token=${ secrets.PD_ROTATION_SLACK_NOTIFICATION }")
-# $headers.Add("Accept", "application/vnd.pagerduty+json;version=2")
-# $headers.Add("Content-Type", "application/json")
+$headers.Add("Authorization","Token token=$PD_KEY")
+$headers.Add("Accept", "application/vnd.pagerduty+json;version=2")
+$headers.Add("Content-Type", "application/json")
 
 # $BasicCreds = Get-BasicAuthCreds -Token ${ secrets.PD_ROTATION_SLACK_NOTIFICATION }
 
 $val = Invoke-RestMethod -Uri $endpoint -Headers $headers -Method Get
 
-
-
-$json = $val | ConvertFrom-JSON
-Write-Host $json
+$jsonstring=$val.schedules | ConvertTo-Json -Compress -Depth 100
+Write-Host $jsonstring
 # $limit = [datetime]::Now.AddDays(-90)
