b5af3708c597f74c8c826bd6315d1282a413b7cf
Supriya Addagada <supriya.addagada@geha.com>
2024-01-03 10:29:15 -0600

    PD API on call details

--- .github/scripts/PagerDutyAlerts/TriggerPDApi.ps1
+++ .github/scripts/PagerDutyAlerts/TriggerPDApi.ps1
@@ -1,8 +1,7 @@
 param (
     [string]$PD_KEY
 )
-$endpoint = "https://api.pagerduty.com//oncalls?include[]=users&schedule_ids[]=PE8NLRU&earliest=true&Accept=application/vnd.pagerduty+json;version=2&Content-Type=application/json"
-
+$endpoint = "https://api.pagerduty.com//oncalls?include[]=users&schedule_ids[]=PE8NLRU&earliest=true"
 $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 $headers.Add("Authorization","Token token=$PD_KEY")
 $headers.Add("Accept", "application/vnd.pagerduty+json;version=2")
@@ -13,5 +12,6 @@ $headers.Add("Content-Type", "application/json")
 $val = Invoke-RestMethod -Uri $endpoint -Headers $headers -Method Get
 
 $jsonstring=$val.oncalls | ConvertTo-Json -Compress -Depth 100
-Write-Host $jsonstring
+Write-Host "current on call person name" $jsonstring.user.name
+Write-Host "current on call person email" $jsonstring.user.email
 # $limit = [datetime]::Now.AddDays(-90)
