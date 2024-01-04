66c53f758bd4ec58b0e5eef87d1a0a0ac638b47f
Supriya Addagada <supriya.addagada@geha.com>
2024-01-04 11:05:56 -0600

    PD on call alert

--- .github/scripts/PagerDutyAlerts/TriggerPDApi.ps1
+++ .github/scripts/PagerDutyAlerts/TriggerPDApi.ps1
@@ -42,8 +42,5 @@ $json1 = $data | ConvertTo-Json
 $jsonstring=$json1 | ConvertFrom-Json | ConvertTo-Json -Compress -Depth 100
  Write-Host $jsonstring
 
- $Object = [String]::Join(",",(ConvertFrom-Json $json1))
-
-
-echo "OnCallPerson=$jsonstring"  | Out-File -FilePath $Env:GITHUB_ENV -Encoding utf8 -Append
-echo "OnCallDetails=$Object"  | Out-File -FilePath $Env:GITHUB_ENV -Encoding utf8 -Append
+echo "PersonName=$val.oncalls.user.name"  | Out-File -FilePath $Env:GITHUB_ENV -Encoding utf8 -Append
+echo "NextPersonName=$Nextoncallval.oncalls.user.name"  | Out-File -FilePath $Env:GITHUB_ENV -Encoding utf8 -Append
