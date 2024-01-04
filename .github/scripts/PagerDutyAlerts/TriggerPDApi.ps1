307f13f0dc3f1f97b65086d29511465f006703a4
Supriya Addagada <supriya.addagada@geha.com>
2024-01-04 08:54:01 -0600

    PD API

--- .github/scripts/PagerDutyAlerts/TriggerPDApi.ps1
+++ .github/scripts/PagerDutyAlerts/TriggerPDApi.ps1
@@ -42,7 +42,7 @@ $json1 = $data | ConvertTo-Json
 $jsonstring=$json1 | ConvertFrom-Json | ConvertTo-Json -Compress -Depth 100
  Write-Host $jsonstring
 
- $Object = [String]::Join(",",(ConvertFrom-Json $json))
+ $Object = [String]::Join(",",(ConvertFrom-Json $json1))
 
 
 echo "OnCallPerson=$jsonstring"  | Out-File -FilePath $Env:GITHUB_ENV -Encoding utf8 -Append
