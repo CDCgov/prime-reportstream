2b46a288d335514e47e7489233f4a56c640951c2
Supriya Addagada <supriya.addagada@geha.com>
2024-01-03 21:19:39 -0600

    PD on call

--- .github/scripts/PagerDutyAlerts/TriggerPDApi.ps1
+++ .github/scripts/PagerDutyAlerts/TriggerPDApi.ps1
@@ -1,17 +1,45 @@
 param (
     [string]$PD_KEY
 )
+
 $endpoint = "https://api.pagerduty.com//oncalls?include[]=users&schedule_ids[]=PE8NLRU&earliest=true"
 $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 $headers.Add("Authorization","Token token=$PD_KEY")
 $headers.Add("Accept", "application/vnd.pagerduty+json;version=2")
 $headers.Add("Content-Type", "application/json")
 
-# $BasicCreds = Get-BasicAuthCreds -Token ${ secrets.PD_ROTATION_SLACK_NOTIFICATION }
 
 $val = Invoke-RestMethod -Uri $endpoint -Headers $headers -Method Get
 
 $jsonstring=$val.oncalls | ConvertTo-Json -Compress -Depth 100
-Write-Host "current on call person name" $jsonstring.user.name
-Write-Host "current on call person email" $jsonstring.user.email
-# $limit = [datetime]::Now.AddDays(-90)
+Write-Host "current on call person name - " $val.oncalls.user.name
+Write-Host "current on call person email - " $val.oncalls.user.email
+$enddate = [DateTime]$val.oncalls.end
+
+$NextOncallStart=$enddate.AddDays(1).ToString('dd-MM-yyyy')
+$NextOncallend=$enddate.AddDays(7).ToString('dd-MM-yyyy')
+
+$Nextoncallendpoint = "https://api.pagerduty.com//oncalls?include[]=users&schedule_ids[]=PE8NLRU&earliest=true&until=$NextOncallend&since=$NextOncallStart"
+
+$Nextoncallval = Invoke-RestMethod -Uri $Nextoncallendpoint -Headers $headers -Method Get
+
+Write-Host "Next on call person name - " ($Nextoncallval.oncalls.user.name -join ', ') 
+Write-Host "Next on call person email - " ($Nextoncallval.oncalls.user.email -join ', ') 
+
+$data = [pscustomobject]@{
+    OnCallDetails = @()
+}
+
+$data.OnCallDetails += @{
+PersonName       = $val.oncalls.user.name
+PersonEmail      = $val.oncalls.user.email
+NextPersonName   = $Nextoncallval.oncalls.user.name
+user             = $Nextoncallval.oncalls.user.email
+}
+
+$json1 = $data | ConvertTo-Json
+
+$jsonstring=$json1 | ConvertFrom-Json | ConvertTo-Json -Compress -Depth 100
+ Write-Host $jsonstring
+
+echo "OnCallPerson=$jsonstring"  | Out-File -FilePath $Env:GITHUB_ENV -Encoding utf8 -Append
