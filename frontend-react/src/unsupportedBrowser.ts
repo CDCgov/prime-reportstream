809571b38aad7721bb4ab5a5bc0904c86dbd4e3a
Stephen Nesman <94193373+snesm@users.noreply.github.com>
2024-02-06 11:41:58 -0500

    gratuitous changes

--- frontend-react/src/unsupportedBrowser.ts
+++ frontend-react/src/unsupportedBrowser.ts
@@ -1,4 +1,5 @@
 import { minimumBrowsersRegex } from "./utils/SupportedBrowsers";
+//
 
 if (!minimumBrowsersRegex.test(navigator.userAgent)) {
     // eslint-disable-next-line no-restricted-globals
