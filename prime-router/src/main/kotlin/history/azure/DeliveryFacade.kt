966054cffa3abb6761f1e1f05fbd3bd37ced4bab
Penny Lischer <penny.lischer@agile6.com>
2024-02-07 16:38:42 -0800

    12774 - Updated logic when sending an invalid reportId Updates to unit tests.

--- prime-router/src/main/kotlin/history/azure/DeliveryFacade.kt
+++ prime-router/src/main/kotlin/history/azure/DeliveryFacade.kt
@@ -1,10 +1,8 @@
 package gov.cdc.prime.router.history.azure
 
 import com.microsoft.azure.functions.HttpRequestMessage
-import com.microsoft.azure.functions.HttpStatus
 import gov.cdc.prime.router.ReportId
 import gov.cdc.prime.router.azure.DatabaseAccess
-import gov.cdc.prime.router.azure.HttpException
 import gov.cdc.prime.router.azure.db.tables.pojos.Action
 import gov.cdc.prime.router.common.BaseEngine
 import gov.cdc.prime.router.history.DeliveryFacility
@@ -62,11 +60,11 @@ class DeliveryFacade(
             "End date must be after start date."
         }
 
-        var reportId: UUID?
+        var reportId: UUID? = null
         try {
             reportId = if (reportIdStr != null) UUID.fromString(reportIdStr) else null
         } catch (e: IllegalArgumentException) {
-            throw HttpException("Invalid format for report ID: $reportIdStr", HttpStatus.UNPROCESSABLE_ENTITY)
+            logger.error("Invalid format for report ID: $reportIdStr", e)
         }
 
         return dbDeliveryAccess.fetchActions(
