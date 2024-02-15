fee42328727e59962525f2f44c0a17ddc8b5dc66
Penelope Lischer <102491809+penny-lischer@users.noreply.github.com>
2024-02-15 10:47:55 -0800

    12336 - daily data e2e (#12897)
    * 12336 - daily data e2e
    
    * 12336 - daily data e2e updates
    
    * fix okta for tests, e2e test fixes
    
    * 12336 - daily data e2e admin auth updates
    
    * 12336 - removed class from e2e files and added export to functions
    
    * 12336 - added sender and receiver e2e tests
    
    * 12336 - updated pagination snapshot
    
    * 12336 - changed folder name from fixtures to spec
    
    * 12336 - Clean up
    
    * 12336 - Added waitForLoadState when logging in
    
    * generate login objects with errors if env vars missing
    
    * quote env vars
    
    * env
    
    * store e2e results
    
    * fix path
    
    * 12336 - added screenshot prop
    
    * 12336 - added waituntil when calling goto
    
    * 12336 - added waitfor when going to Org Settings
    
    * optimize auth setup, fix backend url, optimize outputs
    
    * ci mode
    
    * fixes
    
    * 12336 - added waitForResponse when going to Org Settings
    
    * ci config fixes
    
    * 12336 - Added check to validate API response before asserting expectations.
    
    ---------
    
    Co-authored-by: Joseph Andersen <12385932+jpandersen87@users.noreply.github.com>

--- frontend-react/vite.config.ts
+++ frontend-react/vite.config.ts
@@ -10,22 +10,32 @@ import remarkFrontmatter from "remark-frontmatter";
 import remarkMdxFrontmatter from "remark-mdx-frontmatter";
 import { checker } from "vite-plugin-checker";
 
-const LOCAL_MODES = ["preview", "development", "test", "csp"];
+const LOCAL_BACKEND_MODES = ["preview", "development", "test", "csp", "ci"];
+const LOCAL_PROXY_MODES = ["preview", "development", "test", "csp"];
 const DEMO_MODES = /^demo\d+$/;
 const TRIALFRONTEND_MODES = /^trialfrontend\d+$/;
 
 /**
  * Determine the backend url based on mode.
  */
+function createProxyUrl(mode: string) {
+    if (LOCAL_PROXY_MODES.includes(mode)) return "http://127.0.0.1:7071";
+    const subdomain =
+        mode === "production" ? "" : mode === "ci" ? "staging" : mode;
+    return `https://${subdomain ? subdomain + "." : ""}prime.cdc.gov`;
+}
+
 function createBackendUrl(mode: string) {
     const port = getPort(mode);
-    if (LOCAL_MODES.includes(mode))
+    if (LOCAL_BACKEND_MODES.includes(mode))
         return `http://localhost${port ? ":" + port.toString() : ""}`;
     return `https://${mode !== "production" ? mode + "." : ""}prime.cdc.gov`;
 }
 
 function getPort(mode: string) {
     switch (mode) {
+        case "ci":
+        case "test":
         case "csp":
         case "preview":
             return 4173;
@@ -39,6 +49,7 @@ function getPort(mode: string) {
  */
 function loadRedirectedEnv(mode: string) {
     let redirectedMode = mode;
+    if (mode === "ci") redirectedMode = "test";
     if (DEMO_MODES.exec(mode)) redirectedMode = "demo";
     else if (TRIALFRONTEND_MODES.exec(mode)) redirectedMode = "trialfrontend";
 
@@ -50,12 +61,12 @@ export default defineConfig(async ({ mode, isPreview }) => {
     const env = loadRedirectedEnv(mode);
     const isCsp = mode === "csp";
     const port = getPort(mode);
+    const backendUrl = env.VITE_BACKEND_URL ?? createBackendUrl(mode);
+    const proxyUrl = env.PROXY_URL ?? createProxyUrl(mode);
 
     return {
         define: {
-            "import.meta.env.VITE_BACKEND_URL": JSON.stringify(
-                env.VITE_BACKEND_URL ?? createBackendUrl(mode),
-            ),
+            "import.meta.env.VITE_BACKEND_URL": JSON.stringify(backendUrl),
         },
         optimizeDeps: {
             include: ["react/jsx-runtime"],
@@ -87,7 +98,7 @@ export default defineConfig(async ({ mode, isPreview }) => {
             // Proxy localhost/api to local prime-router
             proxy: {
                 "/api": {
-                    target: "http://127.0.0.1:7071",
+                    target: proxyUrl,
                     changeOrigin: true,
                 },
             },
