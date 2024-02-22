f8347565065b6717f28b1ec4a70bbe8cf5823e25
Jamie Albinson <jamie.albinson@focusconsulting.io>
2024-02-22 15:11:04 -0500

    add test

---
+++ prime-router/src/test/kotlin/azure/observability/event/AzureEventUtilsTest.kt
@@ -0,0 +1,28 @@
+package gov.cdc.prime.router.azure.observability.event
+
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import gov.cdc.prime.router.fhirengine.utils.FhirTranscoder
+import java.io.File
+import kotlin.test.Test
+
+class AzureEventUtilsTest {
+
+    private val VALID_FHIR_URL = "src/test/resources/fhirengine/engine/routing/valid.fhir"
+
+    @Test
+    fun `get all conditions from bundle and map them correctly`() {
+        val fhirData = File(VALID_FHIR_URL).readText()
+        val bundle = FhirTranscoder.decode(fhirData)
+
+        val expected = setOf(
+            ConditionSummary(
+                "840539006",
+                "Disease caused by severe acute respiratory syndrome coronavirus 2 (disorder)"
+            )
+        )
+        val actual = AzureEventUtils.getConditions(bundle)
+
+        assertThat(actual).isEqualTo(expected)
+    }
+}
