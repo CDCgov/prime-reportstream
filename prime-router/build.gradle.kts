d3aaa377346857a69dd766af4130802274450318
Michael Kalish <michael.kalish@focusconsulting.io>
2024-02-26 17:34:53 -0500

    fixup! 10491: sync schemas workflow

--- prime-router/build.gradle.kts
+++ prime-router/build.gradle.kts
@@ -442,6 +442,13 @@ tasks.register("testS2S") {
     finalizedBy("primeCLI")
 }
 
+tasks.register("testSyncS") {
+    group = rootProject.description ?: ""
+    description = "Run sync test"
+    project.extra["cliArgs"] = listOf("test", "--run", "sync-translation-schemas")
+    finalizedBy("primeCLI")
+}
+
 tasks.register("generateDocs") {
     group = rootProject.description ?: ""
     description = "Generate the schema documentation in markup format"
@@ -568,7 +575,7 @@ dockerCompose {
 //    projectName = "prime-router" // docker-composer has this setter broken as of 0.16.4
     setProjectName("prime-router") // this is a workaround for the broken setter for projectName
     useComposeFiles.addAll("docker-compose.yml")
-    startedServices.addAll("sftp", "soap-webservice", "rest-webservice", "vault", "azurite")
+    startedServices.addAll("sftp", "soap-webservice", "rest-webservice", "vault", "azurite", "azurite-stage")
     stopContainers.set(false)
     waitForTcpPorts.set(false)
     // Starting in version 0.17 the plugin changed the default to true, meaning our docker compose yaml files
