ab4d975f3d9e87bb8880b8b655d442e74f09b696
Denis Krylov <dkrylovsb@users.noreply.github.com>
2024-02-29 15:20:05 -0500

    Fixed app_service_plan.main.

--- operations/app/terraform/modules/app_service_plan/main.tf
+++ operations/app/terraform/modules/app_service_plan/main.tf
@@ -1,12 +1,10 @@
 resource "azurerm_service_plan" "service_plan" {
-  name                = "${var.resource_prefix}-serviceplan"
-  location            = var.location
-  resource_group_name = var.resource_group
-  #kind                = "Linux"
-  #reserved            = true
-  sku_name            = "P3v2"
-  os_type             = "Linux"
-  tags = {
-    environment = var.environment
-  }
+    name                = "${var.resource_prefix}-serviceplan"
+    location            = var.location
+    resource_group_name = var.resource_group
+    sku_name            = var.app_size
+    os_type             = "Linux"
+    tags                = {
+        environment = var.environment
+    }
 }
