36590740a3bc9ed7f8f7cc12a38df314d65b6213
Denis Krylov <dkrylovsb@users.noreply.github.com>
2024-02-29 14:48:35 -0500

    Removed extra input parametrs from ~inputs.tf in function_app.

--- operations/app/terraform/modules/function_app/~inputs.tf
+++ operations/app/terraform/modules/function_app/~inputs.tf
@@ -79,9 +79,6 @@ variable "sa_partner_connection_string" {
 variable "client_config_key_vault_id" {}
 variable "app_config_key_vault_id" {}
 variable "app_config_key_vault_name" {}
-variable "function_app" {}
-variable "function_app_id" {}
-variable "service_plan" {}
 variable "dns_ip" {}
 variable "okta_base_url" {}
 variable "OKTA_authKey" {}
