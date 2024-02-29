3bb13724138e2fbf727e4ee69c165e22a6498534
Joseph Andersen <12385932+jpandersen87@users.noreply.github.com>
2024-02-29 11:14:25 -0600

    add createMeta customizer - customizer takes empty strings into account

--- frontend-react/src/layouts/Markdown/utils.ts
+++ frontend-react/src/layouts/Markdown/utils.ts
@@ -1,6 +1,17 @@
-import { merge } from "lodash";
+import { mergeWith } from "lodash";
 import { AppConfig } from "../../config";
 
+/**
+ * Deep merge default meta with frontmatter meta. Has customizer that takes empty strings into account.
+ */
 export function createMeta(config: AppConfig, frontmatter: Frontmatter) {
-    return merge({}, config.PAGE_META.defaults, frontmatter.meta);
+    return mergeWith(
+        {},
+        config.PAGE_META.defaults,
+        frontmatter.meta,
+        (a, b) => {
+            if (typeof b === "string" && b === "") return a;
+            return undefined;
+        },
+    );
 }
