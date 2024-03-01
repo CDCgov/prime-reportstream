d10a458afc2103817af23543c899d8fe9e6538f6
Penelope Lischer <102491809+penny-lischer@users.noreply.github.com>
2024-03-01 12:40:26 -0800

    12891 - Implement preview images to ReportStream website pages (#13498)
    * 12891 - Implement preview images to ReportStream website pages
    
    * 12891 - Updated structure of openGraph object and renamed vars
    
    * opengraph meta tweaks
    - `src` instead of `url`
    - create `openGraphImage` object that's either provided or default
    - add `META` property to app config
    - markdown layout always has meta in helmet due to opengraph image
    - opengraph img folder nit
    - custom og image added to managing your connection index page
    - fix lazyroutemarkdown test
    
    * createMeta util function
    - function to do deep merge between app config meta defaults and frontmatter
    - includes test
    - app config property adjustments
    - remove title/description from html templates
    - use createMeta in markdownlayout
    
    * memo markdownlayout meta
    
    * 12891 - Added alt text
    
    * 12891 - updated img path to opengraph
    
    * add createMeta customizer
    - customizer takes empty strings into account
    
    * compromise and use defaults in templates until non-spa
    
    * 12891 - fixed lint errors
    
    * 12891 - skipped failing unit tests
    
    * 12891 - merged with master.
    
    * 12891 - merged with master.
    
    ---------
    
    Co-authored-by: Joseph Andersen <12385932+jpandersen87@users.noreply.github.com>

--- frontend-react/src/pages/admin/AdminLastMileFailures.tsx
+++ frontend-react/src/pages/admin/AdminLastMileFailures.tsx
@@ -11,6 +11,14 @@ export function AdminLastMileFailuresPage() {
         <GridContainer>
             <Helmet>
                 <title>Last mile failures</title>
+                <meta
+                    property="og:image"
+                    content="/assets/img/opengraph/reportstream.png"
+                />
+                <meta
+                    property="og:image:alt"
+                    content='"ReportStream" surrounded by an illustration of lines and boxes connected by colorful dots.'
+                />
             </Helmet>
             <article className="margin-bottom-5">
                 <h3 className="margin-bottom-0">
