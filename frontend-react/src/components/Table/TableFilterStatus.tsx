a5c77e39bedcaf2bb7f1351828858c9413a88117
etanb <etan.berkowitz@gmail.com>
2024-03-04 17:04:54 -0800

    add single result logic

--- frontend-react/src/components/Table/TableFilterStatus.tsx
+++ frontend-react/src/components/Table/TableFilterStatus.tsx
@@ -11,10 +11,11 @@ function TableFilterStatus({ filterStatus }: TableFilterStatusProps) {
     return (
         <div className="margin-left-2">
             <p className="display-inline">
-                Showing (
-                {filterStatus.resultLength ? filterStatus.resultLength : 0})
-                results for:{" "}
+                Showing
+                {` (${filterStatus.resultLength ?? 0}) ${filterStatus.resultLength === 1 ? "result" : "results"} `}
+                for:{" "}
             </p>
+
             <p className="display-inline text-bold">
                 {filterStatus.activeFilters.filter((item) => item).join(", ")}
             </p>
