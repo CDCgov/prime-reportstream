7d606d9533adb833b74b348ddc37b48b8a64292a
Denis Krylov <dkrylovsb@users.noreply.github.com>
2024-03-12 15:01:23 -0400

    Added allow_nested_items_to_be_public = false to storage.tf.

--- operations/app/terraform/modules/sftp/storage.tf
+++ operations/app/terraform/modules/sftp/storage.tf
@@ -6,6 +6,7 @@ resource "azurerm_storage_account" "sftp" {
   account_tier             = "Standard"
   account_replication_type = "GRS"
   min_tls_version          = "TLS1_2"
+  allow_nested_items_to_be_public = false
 
   network_rules {
     default_action = "Allow"
