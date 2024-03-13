b7f3b600686962dfb99c30b7c45402246d648e3f
Denis Krylov <dkrylovsb@users.noreply.github.com>
2024-03-13 15:47:39 -0400

    Fixing checkov findings.

--- operations/app/terraform/modules/app_service_plan/main.tf
+++ operations/app/terraform/modules/app_service_plan/main.tf
@@ -1,10 +1,16 @@
 resource "azurerm_service_plan" "service_plan" {
-  name                = "${var.resource_prefix}-serviceplan"
-  location            = var.location
-  resource_group_name = var.resource_group
-  sku_name            = var.app_size
-  os_type             = "Linux"
+  name                   = "${var.resource_prefix}-serviceplan"
+  location               = var.location
+  resource_group_name    = var.resource_group
+  sku_name               = var.app_size
+  os_type                = "Linux"
+  zone_balancing_enabled = true
   tags = {
     environment = var.environment
+  }
+  lifecycle {
+    ignore_changes = [
+      zone_balancing_enabled
+    ]
   }
 }
