18b292cc0fa1e1964f6533887a806a2179a76862
Joseph Andersen <12385932+jpandersen87@users.noreply.github.com>
2024-04-01 17:11:32 -0500

    BUG: No way to control date range for TableFilters component (#13886)
    * BUG: No way to control date range for TableFilters component
    Fixes #13885

--- frontend-react/src/hooks/filters/filters.fixtures.ts
+++ frontend-react/src/hooks/filters/filters.fixtures.ts
@@ -1,15 +1,28 @@
 import { Dispatch } from "react";
 
-import { RangeSettingsAction } from "../UseDateRange";
-import { FilterManager } from "../UseFilterManager";
-import { PageSettingsAction } from "../UsePages";
-import { SortSettingsAction } from "../UseSortOrder";
+import { CursorManager } from "./UseCursorManager";
+import { RangeSettingsAction } from "./UseDateRange";
+import { FilterManager } from "./UseFilterManager";
+import { PageSettingsAction } from "./UsePages";
+import { SortSettingsAction } from "./UseSortOrder";
+
+export const cursorManagerFixture: CursorManager = {
+    cursors: {
+        current: "",
+        next: "",
+        history: [],
+    },
+    hasPrev: false,
+    hasNext: false,
+    // eslint-disable-next-line no-console
+    update: () => void 0,
+};
 
 const fakeDispatch = <T>(): Dispatch<T> => {
     return (_v: T) => void 0;
 };
 
-export const mockFilterManager: FilterManager = {
+export const filterManagerFixture: FilterManager = {
     rangeSettings: {
         from: new Date("2022-01-01").toISOString(),
         to: new Date("2022-12-31").toISOString(),
