054fbd12e2d68af7f108363b437e460b2ab6e1ee
Denis Krylov <dkrylovsb@users.noreply.github.com>
2024-04-05 14:37:10 -0400

    Removed code that was replacing "Microsoft.Insights" with lower case.

--- operations/app/terraform/modules/application_insights/main.tf
+++ operations/app/terraform/modules/application_insights/main.tf
@@ -86,12 +86,12 @@ resource "azurerm_monitor_action_group" "action_group_dummy" {
 
 # Prevent TF changes where Microsoft.Insights is forced lowercase
 locals {
-  action_group_id = try(replace(azurerm_monitor_action_group.action_group[0].id, "Microsoft.Insights", "microsoft.insights"), "")
+  action_group_id = try(azurerm_monitor_action_group.action_group[0].id, "")
   action_group_slack_id = (local.alerting_enabled == 1 ?
-    try(replace(azurerm_monitor_action_group.action_group_slack.id, "Microsoft.Insights", "microsoft.insights"), "") :
+    try(azurerm_monitor_action_group.action_group_slack.id, "") :
   azurerm_monitor_action_group.action_group_slack.id)
-  app_insights_id = replace(azurerm_application_insights.app_insights.id, "Microsoft.Insights", "microsoft.insights")
+  app_insights_id = azurerm_application_insights.app_insights.id
   action_group_metabase_id = (local.alerting_enabled == 1 ?
-    try(replace(azurerm_monitor_action_group.action_group_metabase.id, "Microsoft.Insights", "microsoft.insights"), "") :
+    try(azurerm_monitor_action_group.action_group_metabase.id, "") :
   azurerm_monitor_action_group.action_group_metabase.id)
 }
