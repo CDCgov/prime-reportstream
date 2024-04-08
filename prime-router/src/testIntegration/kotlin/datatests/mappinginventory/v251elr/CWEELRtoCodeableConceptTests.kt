ac4cec5340301990b29e643eb07fd7489f7281a5
Brick Green <brick.green@agile6.com>
2024-04-08 12:58:04 -0600

    add cwe_elr and xon_elr test

--- prime-router/src/testIntegration/kotlin/datatests/mappinginventory/v251elr/CWEELRtoCodeableConceptTests.kt
+++ prime-router/src/testIntegration/kotlin/datatests/mappinginventory/v251elr/CWEELRtoCodeableConceptTests.kt
@@ -17,66 +17,144 @@ class CWEELRtoCodeableConceptTests {
 
     @Test
     fun `test value in CWE-2`() {
-        assert(verifyHL7ToFHIRToHL7Mapping("cwe/codeable-concept/cwe-4-test-value-cwe2").passed)
+        assert(
+            verifyHL7ToFHIRToHL7Mapping(
+                "v251-elr/cwe/codeable-concept/cwe-4-test-value-cwe2",
+                profile = "./metadata/HL7/v251-elr",
+                outputSchema = "classpath:/metadata/hl7_mapping/v251-elr/ORU_R01.yml",
+            ).passed
+        )
     }
 
     @Test
     fun `test value in CWE-3`() {
-        assert(verifyHL7ToFHIRToHL7Mapping("cwe/codeable-concept/cwe-5-test-value-cwe3").passed)
+        assert(
+            verifyHL7ToFHIRToHL7Mapping(
+                "v251-elr/cwe/codeable-concept/cwe-5-test-value-cwe3",
+                profile = "./metadata/HL7/v251-elr",
+                outputSchema = "classpath:/metadata/hl7_mapping/v251-elr/ORU_R01.yml",
+            ).passed
+        )
     }
 
     @Test
     fun `test value in CWE-4`() {
-        assert(verifyHL7ToFHIRToHL7Mapping("cwe/codeable-concept/cwe-6-test-value-cwe4").passed)
+        assert(
+            verifyHL7ToFHIRToHL7Mapping(
+                "v251-elr/cwe/codeable-concept/cwe-6-test-value-cwe4",
+                profile = "./metadata/HL7/v251-elr",
+                outputSchema = "classpath:/metadata/hl7_mapping/v251-elr/ORU_R01.yml",
+            ).passed
+        )
     }
 
     @Test
     fun `test value in CWE-5`() {
-        assert(verifyHL7ToFHIRToHL7Mapping("cwe/codeable-concept/cwe-7-test-value-cwe5").passed)
+        assert(
+            verifyHL7ToFHIRToHL7Mapping(
+                "v251-elr/cwe/codeable-concept/cwe-7-test-value-cwe5",
+                profile = "./metadata/HL7/v251-elr",
+                outputSchema = "classpath:/metadata/hl7_mapping/v251-elr/ORU_R01.yml",
+            ).passed
+        )
     }
 
     @Test
     fun `test value in CWE-6`() {
-        assert(verifyHL7ToFHIRToHL7Mapping("cwe/codeable-concept/cwe-8-test-value-cwe6").passed)
+        assert(
+            verifyHL7ToFHIRToHL7Mapping(
+                "v251-elr/cwe/codeable-concept/cwe-8-test-value-cwe6",
+                profile = "./metadata/HL7/v251-elr",
+                outputSchema = "classpath:/metadata/hl7_mapping/v251-elr/ORU_R01.yml",
+            ).passed
+        )
     }
 
     @Test
     fun `test value in CWE-7`() {
-        assert(verifyHL7ToFHIRToHL7Mapping("cwe/codeable-concept/cwe-9-test-value-cwe7").passed)
+        assert(
+            verifyHL7ToFHIRToHL7Mapping(
+                "v251-elr/cwe/codeable-concept/cwe-9-test-value-cwe7",
+                profile = "./metadata/HL7/v251-elr",
+                outputSchema = "classpath:/metadata/hl7_mapping/v251-elr/ORU_R01.yml",
+            ).passed
+        )
     }
 
     @Test
     fun `test value in CWE-8`() {
-        assert(verifyHL7ToFHIRToHL7Mapping("cwe/codeable-concept/cwe-10-test-value-cwe8").passed)
+        assert(
+            verifyHL7ToFHIRToHL7Mapping(
+                "v251-elr/cwe/codeable-concept/cwe-10-test-value-cwe8",
+                profile = "./metadata/HL7/v251-elr",
+                outputSchema = "classpath:/metadata/hl7_mapping/v251-elr/ORU_R01.yml",
+            ).passed
+        )
     }
 
     @Test
     fun `test value in CWE-9`() {
-        assert(verifyHL7ToFHIRToHL7Mapping("cwe/codeable-concept/cwe-11-test-value-cwe9").passed)
+        assert(
+            verifyHL7ToFHIRToHL7Mapping(
+                "v251-elr/cwe/codeable-concept/cwe-11-test-value-cwe9",
+                profile = "./metadata/HL7/v251-elr",
+                outputSchema = "classpath:/metadata/hl7_mapping/v251-elr/ORU_R01.yml",
+            ).passed
+        )
     }
 
     @Test
     fun `test value in CWE-10`() {
-        assert(verifyHL7ToFHIRToHL7Mapping("cwe/codeable-concept/cwe-12-test-value-cwe10").passed)
+        assert(
+            verifyHL7ToFHIRToHL7Mapping(
+                "v251-elr/cwe/codeable-concept/cwe-12-test-value-cwe10",
+                profile = "./metadata/HL7/v251-elr",
+                outputSchema = "classpath:/metadata/hl7_mapping/v251-elr/ORU_R01.yml",
+            ).passed
+        )
     }
 
     @Test
     fun `test value in CWE-11`() {
-        assert(verifyHL7ToFHIRToHL7Mapping("cwe/codeable-concept/cwe-13-test-value-cwe11").passed)
+        assert(
+            verifyHL7ToFHIRToHL7Mapping(
+                "v251-elr/cwe/codeable-concept/cwe-13-test-value-cwe11",
+                profile = "./metadata/HL7/v251-elr",
+                outputSchema = "classpath:/metadata/hl7_mapping/v251-elr/ORU_R01.yml",
+            ).passed
+        )
     }
 
     @Test
     fun `test value in CWE-12`() {
-        assert(verifyHL7ToFHIRToHL7Mapping("cwe/codeable-concept/cwe-14-test-value-cwe12").passed)
+        assert(
+            verifyHL7ToFHIRToHL7Mapping(
+                "v251-elr/cwe/codeable-concept/cwe-14-test-value-cwe12",
+                profile = "./metadata/HL7/v251-elr",
+                outputSchema = "classpath:/metadata/hl7_mapping/v251-elr/ORU_R01.yml",
+            ).passed
+        )
     }
 
     @Test
     fun `test value in CWE-13`() {
-        assert(verifyHL7ToFHIRToHL7Mapping("cwe/codeable-concept/cwe-15-test-value-cwe13").passed)
+        assert(
+            verifyHL7ToFHIRToHL7Mapping(
+                "v251-elr/cwe/codeable-concept/cwe-15-test-value-cwe13",
+                profile = "./metadata/HL7/v251-elr",
+                outputSchema = "classpath:/metadata/hl7_mapping/v251-elr/ORU_R01.yml",
+            ).passed
+        )
     }
 
     @Test
     fun `test value in CWE-14`() {
-        assert(verifyHL7ToFHIRToHL7Mapping("cwe/codeable-concept/cwe-16-test-value-cwe14").passed)
+        assert(
+            verifyHL7ToFHIRToHL7Mapping(
+                "v251-elr/cwe/codeable-concept/cwe-16-test-value-cwe14",
+                profile = "./metadata/HL7/v251-elr",
+                outputSchema = "classpath:/metadata/hl7_mapping/v251-elr/ORU_R01.yml",
+            ).passed
+        )
     }
 }
