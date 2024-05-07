e653a30d8a6ee7eba39a2b833331b6fb6f8e23a6
Jamie Albinson <jamie.albinson@focusconsulting.io>
2024-05-07 16:16:04 -0400

    fix comments

--- prime-router/src/main/kotlin/config/validation/JsonSchemaValidationService.kt
+++ prime-router/src/main/kotlin/config/validation/JsonSchemaValidationService.kt
@@ -5,8 +5,14 @@ import gov.cdc.prime.router.common.JacksonMapperUtilities
 import java.io.File
 import java.io.InputStream
 
+/**
+ * Service used to validate YAML files against a JSON schema
+ */
 class JsonSchemaValidationService {
 
+    /**
+     * Validate the YAML structure of a file
+     */
     fun validateYAMLStructure(
         configType: ConfigurationType,
         file: File,
@@ -14,12 +20,19 @@ class JsonSchemaValidationService {
         return validateYAMLStructure(configType, file.inputStream())
     }
 
+    /**
+     * Validate the YAML structure of a string
+     */
     fun validateYAMLStructure(
         configType: ConfigurationType,
         yamlString: String,
     ): Set<ValidationMessage> {
         return validateYAMLStructure(configType, yamlString.byteInputStream())
     }
+
+    /**
+     * Validate the YAML structure of an input stream
+     */
 
     fun validateYAMLStructure(
         configType: ConfigurationType,
