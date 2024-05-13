b26fd322bf0adee4916e2c3bcc743fc2622b0836
Denis Krylov <dkrylovsb@users.noreply.github.com>
2024-05-13 16:15:15 -0400

    Added an `exports` module.

---
+++ operations/app/terraform/modules/exports/~inputs.tf
@@ -0,0 +1,14 @@
+variable "storage_account_name" {
+    type        = string
+    description = "Storage Account"
+}
+
+variable "resource_group_name" {
+    type        = string
+    description = "Resource Group Name"
+}
+
+variable "resource_prefix" {
+    type        = string
+    description = "Resource Prefix"
+}
