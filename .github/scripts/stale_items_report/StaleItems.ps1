751a813788bb550e4da066d9cf4afabf38e73e4a
Josiah Siegel <josiah.siegel@semanticbits.com>
2024-05-15 15:21:56 -0400

    summarize items per user

--- .github/scripts/stale_items_report/StaleItems.ps1
+++ .github/scripts/stale_items_report/StaleItems.ps1
@@ -9,8 +9,7 @@ function Get-StaleItems {
     param(
         [string]$ItemType,
         [string]$Endpoint,
-        [int]$StaleDays,
-        [string[]]$ExcludeList
+        [int]$StaleDays
     )
 
     $BasicCreds = Get-BasicAuthCreds -Username "repo" -Password ${ secrets.GITHUB_TOKEN }
@@ -18,46 +17,37 @@ function Get-StaleItems {
     $json = $val | ConvertFrom-JSON
     $limit = [datetime]::Now.AddDays(-$StaleDays)
 
-    $bulletPointList = ""
+    $staleItems = $json | Where-Object { $_.updated_at -lt $limit }
 
-    foreach($obj in $json)
-    {
-        if($obj.updated_at -lt $limit)
-        {
-            $title = $obj.title -replace '`', ''
-
-            if($ItemType -eq "Branch" -and $obj.name -notin $ExcludeList)
-            {
-                $BranchName = $obj.name
-                $BasicCreds1 = Get-BasicAuthCreds -Username "repo" -Password ${ secrets.GITHUB_TOKEN }
-                $endpoint1 = "https://api.github.com/repos/CDCgov/prime-reportstream/branches/$BranchName"
-                $Branch = Invoke-WebRequest -Uri $endpoint1 -Headers @{"Authorization"="Basic $BasicCreds1"}
-                $jsonBranch = $Branch | ConvertFrom-JSON
-            
-                if($obj.commit.commit.author.date -lt $limit)
-                {
-                    $bulletPointList += "- [" + $jsonBranch.name.SubString(0,[math]::min(10,$jsonBranch.name.length)) + "..." + $jsonBranch.name.Substring([math]::max(0,$jsonBranch.name.Length - 10)) + "]($($jsonBranch._links.html)): | $($jsonBranch.commit.commit.author.name)`n"
-                }
-            }
-            elseif($ItemType -ne "Branch")
-            {
-                $bulletPointList += "- [#$($obj.number)]($($obj.html_url)): " + $title.SubString(0,[math]::min(10,$title.length)) + "..." + $title.Substring([math]::max(0,$title.Length - 10)) + " | $($obj.user.login)`n"
-            }
-        }
-    }
-
-    Write-Host $bulletPointList
-
-    $encodedBulletPointList = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($bulletPointList))
-    Write-Output "STALE_$($ItemType.ToUpper().Replace(" ", "_"))_LIST=$encodedBulletPointList" >> $env:GITHUB_ENV
+    return $staleItems
 }
 
-# Stale Pull Requests
-Get-StaleItems -ItemType "Pull Request" -Endpoint "https://api.github.com/repos/CDCgov/prime-reportstream/pulls?direction=asc&sort=updated&state=open" -StaleDays 90
+# Get stale pull requests
+$stalePullRequests = Get-StaleItems -ItemType "Pull Request" -Endpoint "https://api.github.com/repos/CDCgov/prime-reportstream/pulls?direction=asc&sort=updated&state=open" -StaleDays 90
+$stalePullRequestCount = $stalePullRequests.Count
 
-# Stale Branches
+# Get stale branches
 $ExcludeBranchesList = @(Get-Content .\.github\scripts\stale_items_report\excludebrancheslist.txt)
-Get-StaleItems -ItemType "Branch" -Endpoint "https://api.github.com/repos/CDCgov/prime-reportstream/branches?direction=asc&sort=updated" -StaleDays 90 -ExcludeList $ExcludeBranchesList
+$staleBranches = Get-StaleItems -ItemType "Branch" -Endpoint "https://api.github.com/repos/CDCgov/prime-reportstream/branches?direction=asc&sort=updated" -StaleDays 90 | Where-Object { $_.name -notin $ExcludeBranchesList }
+$staleBranchCount = $staleBranches.Count
 
-# Stale Issues
-Get-StaleItems -ItemType "Issue" -Endpoint "https://api.github.com/repos/CDCgov/prime-reportstream/issues?state=open&per_page=100&direction=asc&sort=updated" -StaleDays 90
+# Get stale issues
+$staleIssues = Get-StaleItems -ItemType "Issue" -Endpoint "https://api.github.com/repos/CDCgov/prime-reportstream/issues?state=open&per_page=100&direction=asc&sort=updated" -StaleDays 90
+$staleIssueCount = $staleIssues.Count
+
+# Count issues older than 90 days for each issue author
+$issueAuthorCounts = $staleIssues | Group-Object -Property user.login | Select-Object Name, Count
+
+$slackMessage = @"
+*Stale GitHub Items Summary*
+
+- Stale Pull Requests (>90 days): $stalePullRequestCount
+- Stale Branches (>90 days): $staleBranchCount
+- Stale Issues (>90 days): $staleIssueCount
+
+*Stale Issues by Author*
+$($issueAuthorCounts | ForEach-Object { "- $($_.Name): $($_.Count)" })
+"@
+
+$encodedSlackMessage = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($slackMessage))
+Write-Output "SLACK_MESSAGE=$encodedSlackMessage" >> $env:GITHUB_ENV
