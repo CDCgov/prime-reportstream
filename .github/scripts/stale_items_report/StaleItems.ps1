324e5f78a41a4dc1b1c563be0f1c43f05e0462e0
Josiah Siegel <josiah.siegel@semanticbits.com>
2024-05-16 11:35:08 -0400

    adjust stale items formatting

--- .github/scripts/stale_items_report/StaleItems.ps1
+++ .github/scripts/stale_items_report/StaleItems.ps1
@@ -1,7 +1,11 @@
+param (
+    [string]$pass
+)
+
 function Get-BasicAuthCreds {
-    param([string]$Username,[string]$Password)
-    $AuthString = "{0}:{1}" -f $Username,$Password
-    $AuthBytes  = [System.Text.Encoding]::Ascii.GetBytes($AuthString)
+    param([string]$Username, [string]$Password)
+    $AuthString = "{0}:{1}" -f $Username, $Password
+    $AuthBytes = [System.Text.Encoding]::Ascii.GetBytes($AuthString)
     return [Convert]::ToBase64String($AuthBytes)
 }
 
@@ -12,64 +16,160 @@ function Get-StaleItems {
         [int]$StaleDays
     )
 
-    $BasicCreds = Get-BasicAuthCreds -Username "repo" -Password ${ secrets.RUNLEAKS_TOKEN }
-    $val = Invoke-WebRequest -Uri $Endpoint -Headers @{"Authorization"="Basic $BasicCreds"}
-    $json = $val | ConvertFrom-JSON
+    $BasicCreds = Get-BasicAuthCreds -Username "repo" -Password $pass
+    $headers = @{"Authorization" = "Basic $BasicCreds"}
     $limit = [datetime]::Now.AddDays(-$StaleDays)
 
-    $staleItems = $json | Where-Object { $_.updated_at -lt $limit }
+    $json = Invoke-WebRequest -Uri $Endpoint -Headers $headers | ConvertFrom-JSON
+
+    $staleItems = switch ($ItemType) {
+        "Issue" {
+            $json | Where-Object { [datetime]::Parse($_.updated_at) -lt $limit }
+        }
+        "Branch" {
+            $json | ForEach-Object {
+                $branchJson = Invoke-WebRequest -Uri $_.commit.url -Headers $headers | ConvertFrom-JSON
+                $updatedAt = [datetime]::Parse($branchJson.commit.author.date)
+                if ($updatedAt -lt $limit -and $branchJson.author.login) {
+                    $branchJson
+                }
+            }
+        }
+        "PullRequest" {
+            $json | Where-Object { [datetime]::Parse($_.updated_at) -lt $limit }
+        }
+        default {
+            Write-Error "Invalid item type: $ItemType"
+            return
+        }
+    }
 
     return $staleItems
 }
 
-function Get-AllStaleIssues {
+function Get-AllStaleItems {
     param(
+        [string]$ItemType,
         [string]$Endpoint,
         [int]$StaleDays
     )
 
-    $staleIssues = @()
+    $staleItems = @()
     $page = 1
     $perPage = 100
 
     do {
         $url = "$Endpoint&page=$page&per_page=$perPage"
-        $issues = Get-StaleItems -ItemType "Issue" -Endpoint $url -StaleDays $StaleDays
-        $staleIssues += $issues
+        $items = Get-StaleItems -ItemType $ItemType -Endpoint $url -StaleDays $StaleDays
+        $staleItems += $items
         $page++
-    } while ($issues.Count -eq $perPage)
+    } while ($items.Count -eq $perPage)
 
-    return $staleIssues
+    return $staleItems
 }
 
-# Get stale pull requests
-$stalePullRequests = Get-StaleItems -ItemType "Pull Request" -Endpoint "https://api.github.com/repos/CDCgov/prime-reportstream/pulls?direction=asc&sort=updated&state=open" -StaleDays 90
-$stalePullRequestCount = $stalePullRequests.Count
+function Format-AuthorCounts {
+    param(
+        [object[]]$Items,
+        [string]$ItemType,
+        [int]$TopAuthors,
+        [string]$AuthorProperty,
+        [switch]$IncludeLink
+    )
 
-# Get stale branches
-$ExcludeBranchesList = @(Get-Content .\.github\scripts\stale_items_report\excludebrancheslist.txt)
-$staleBranches = Get-StaleItems -ItemType "Branch" -Endpoint "https://api.github.com/repos/CDCgov/prime-reportstream/branches?direction=asc&sort=updated" -StaleDays 90 | Where-Object { $_.name -notin $ExcludeBranchesList }
-$staleBranchCount = $staleBranches.Count
+    $authorCounts = $Items |
+        Select-Object @{Name = 'UserLogin'; Expression = { $_.$AuthorProperty.login.replace('[bot]', '') } } |
+        Group-Object -Property UserLogin -NoElement |
+        Sort-Object Count -Descending |
+        Select-Object -First $TopAuthors
 
-# Get all stale issues using pagination
-$staleIssues = Get-AllStaleIssues -Endpoint "https://api.github.com/repos/CDCgov/prime-reportstream/issues?state=open&direction=asc&sort=updated" -StaleDays 90
-$staleIssueCount = $staleIssues.Count
+    $authorCountsFormatted = $authorCounts | ForEach-Object {
+        $authorLogin = $_.Name
+        if ($IncludeLink) {
+            $itemTypeUrl = if ($ItemType -eq "PullRequest") { "pulls" } else { $ItemType.ToLower() + "s" }
+            $authorItemsLink = "https://github.com/CDCgov/prime-reportstream/${itemTypeUrl}?q=is:open+sort:created-asc+author:$authorLogin"
+            "- [$authorLogin]($authorItemsLink): $($_.Count)"
+        } else {
+            "- ${authorLogin}: $($_.Count)"
+        }
+    }
 
-# Count issues older than 90 days for each issue author
-$issueAuthorCounts = $staleIssues | Group-Object -Property user.login | Select-Object Name, Count | Where-Object { $_.Name -ne $null }
+    return $authorCountsFormatted
+}
 
-$issueAuthorCountsFormatted = $issueAuthorCounts | ForEach-Object { "- $($_.Name): $($_.Count)" }
+function Get-StaleItemCounts {
+    param(
+        [string]$ItemType,
+        [string]$Endpoint,
+        [int]$StaleDays,
+        [int]$TopAuthors,
+        [string]$AuthorProperty,
+        [switch]$IncludeLink
+    )
 
-$slackMessage = @"
-*Stale GitHub Items Summary*
+    $staleItems = Get-AllStaleItems -ItemType $ItemType -Endpoint $Endpoint -StaleDays $StaleDays
+    $staleItemsFiltered = if ($ItemType -eq "Issue") { $staleItems | Where-Object { $_.html_url -notmatch "/pull/" } } else { $staleItems }
+    $authorCountsFormatted = Format-AuthorCounts -Items $staleItemsFiltered -ItemType $ItemType -TopAuthors $TopAuthors -AuthorProperty $AuthorProperty -IncludeLink:$IncludeLink
 
-- Stale Pull Requests (>90 days): $stalePullRequestCount
-- Stale Branches (>90 days): $staleBranchCount
-- Stale Issues (>90 days): $staleIssueCount
+    return @{
+        Count = $staleItemsFiltered.Count
+        AuthorCounts = $authorCountsFormatted
+    }
+}
 
-*Stale Issues by Author*
-$($issueAuthorCountsFormatted -join "`n")
+function Get-SlackMessage {
+    param(
+        [string]$Title,
+        [int]$Count,
+        [string[]]$AuthorCounts
+    )
+
+    return @"
+*$Title*
+---
+$($AuthorCounts -join "`n")
+
+"@
+}
+
+# Configuration
+$topPullRequestAuthors = 5
+$stalePullRequestDays = 180
+$topBranchAuthors = 5
+$staleBranchDays = 180
+$topIssueAuthors = 15
+$staleIssueDays = 180
+
+# Get stale item counts and author counts
+$stalePullRequests = Get-StaleItemCounts -ItemType "PullRequest" -Endpoint "https://api.github.com/repos/CDCgov/prime-reportstream/pulls?direction=asc&sort=updated&state=open" -StaleDays $stalePullRequestDays -TopAuthors $topPullRequestAuthors -AuthorProperty "user" -IncludeLink
+$staleBranches = Get-StaleItemCounts -ItemType "Branch" -Endpoint "https://api.github.com/repos/CDCgov/prime-reportstream/branches?direction=asc&sort=updated&protected=false" -StaleDays $staleBranchDays -TopAuthors $topBranchAuthors -AuthorProperty "author"
+$staleIssues = Get-StaleItemCounts -ItemType "Issue" -Endpoint "https://api.github.com/repos/CDCgov/prime-reportstream/issues?state=open&direction=asc&sort=updated" -StaleDays $staleIssueDays -TopAuthors $topIssueAuthors -AuthorProperty "user" -IncludeLink
+
+# Prepare Slack messages
+$slackMessageSummary = @"
+
+- Stale Pull Requests (>$stalePullRequestDays days): $($stalePullRequests.Count)
+- Stale Branches (>$staleBranchDays days): $($staleBranches.Count)
+- Stale Issues (>$staleIssueDays days): $($staleIssues.Count)
+
 "@
 
-$encodedSlackMessage = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($slackMessage))
-Write-Output "SLACK_MESSAGE=$encodedSlackMessage" >> $env:GITHUB_ENV
+$slackMessagePullRequests = Get-SlackMessage -Title "Stale Pull Requests by Author (Top $topPullRequestAuthors)" -Count $stalePullRequests.Count -AuthorCounts $stalePullRequests.AuthorCounts
+$slackMessageBranches = Get-SlackMessage -Title "Stale Branches by Author (Top $topBranchAuthors)" -Count $staleBranches.Count -AuthorCounts $staleBranches.AuthorCounts
+$slackMessageIssues = Get-SlackMessage -Title "Stale Issues by Author (Top $topIssueAuthors)" -Count $staleIssues.Count -AuthorCounts $staleIssues.AuthorCounts
+
+# Output Slack messages
+Write-Host $slackMessageSummary
+Write-Host $slackMessagePullRequests
+Write-Host $slackMessageBranches
+Write-Host $slackMessageIssues
+
+# Set GitHub environment variables
+$encodedslackMessageSummary = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($slackMessageSummary))
+$encodedslackMessagePullRequests = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($slackMessagePullRequests))
+$encodedslackMessageBranches = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($slackMessageBranches))
+$encodedslackMessageIssues = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($slackMessageIssues))
+Write-Output "SLACK_MESSAGE_SUMMARY=$encodedslackMessageSummary" >> $env:GITHUB_ENV
+Write-Output "SLACK_MESSAGE_PULL_REQUESTS=$encodedslackMessagePullRequests" >> $env:GITHUB_ENV
+Write-Output "SLACK_MESSAGE_BRANCHES=$encodedslackMessageBranches" >> $env:GITHUB_ENV
+Write-Output "SLACK_MESSAGE_ISSUES=$encodedslackMessageIssues" >> $env:GITHUB_ENV
