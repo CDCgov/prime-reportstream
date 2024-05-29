13088c99c1a5ec56a6c942a6309756d28f182f05
Josiah Siegel <josiah.siegel@semanticbits.com>
2024-05-29 09:51:37 -0400

    validate ignore_changes

--- operations/app/terraform/modules/data_factory/private_endpoint.tf
+++ operations/app/terraform/modules/data_factory/private_endpoint.tf
@@ -4,12 +4,6 @@ resource "azurerm_data_factory_managed_private_endpoint" "sftp_share" {
   target_resource_id = var.sftp_storage.id
   subresource_name   = "file"
 
-  lifecycle {
-    ignore_changes = [
-      fqdns
-    ]
-  }
-
   timeouts {}
 }
 
