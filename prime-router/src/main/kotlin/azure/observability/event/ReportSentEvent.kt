4bc462dafe361bb5ad5553a6b1f84ac5ff05fc9a
Brick Green <brick.green@agile6.com>
2024-06-05 12:12:33 -0600

    add documentation for ReportSentEvent

--- prime-router/src/main/kotlin/azure/observability/event/ReportSentEvent.kt
+++ prime-router/src/main/kotlin/azure/observability/event/ReportSentEvent.kt
@@ -3,6 +3,10 @@ package gov.cdc.prime.router.azure.observability.event
 import gov.cdc.prime.router.ReportId
 import gov.cdc.prime.router.Topic
 
+/**
+ * Event definition for when a report has been successfully sent to a receiver.
+ * [trasnportType] is nullable as some receivers will purposefully not have transports configured
+ */
 data class ReportSentEvent(
     val sentReportId: ReportId,
     val reportId: ReportId,
