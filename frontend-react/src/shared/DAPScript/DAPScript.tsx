3bf943e33e0617cbb0d4c0faa524e47d0ff10fad
Penny Lischer <penny.lischer@agile6.com>
2024-06-12 11:46:56 -0700

    14518 - Fixed CSP Header Issues

--- frontend-react/src/shared/DAPScript/DAPScript.tsx
+++ frontend-react/src/shared/DAPScript/DAPScript.tsx
@@ -21,6 +21,7 @@ const DAPScript = ({ pathname }: DAPScriptProps) => {
         <Helmet>
             <script
                 async
+                crossOrigin="use-credentials"
                 type="text/javascript"
                 src="https://dap.digitalgov.gov/Universal-Federated-Analytics-Min.js?agency=HHS&subagency=CDC"
                 id="_fed_an_ua_tag"
