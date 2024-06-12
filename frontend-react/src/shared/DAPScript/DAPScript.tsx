c9ba0d76fc549cdbbbcc21d0a524027d975d9a6d
Penny Lischer <penny.lischer@agile6.com>
2024-06-12 14:57:54 -0700

    14518 - Reverted props on script

--- frontend-react/src/shared/DAPScript/DAPScript.tsx
+++ frontend-react/src/shared/DAPScript/DAPScript.tsx
@@ -21,8 +21,6 @@ const DAPScript = ({ pathname }: DAPScriptProps) => {
         <Helmet>
             <script
                 async
-                crossOrigin="use-credentials"
-                referrerPolicy="same-origin"
                 type="text/javascript"
                 src="https://dap.digitalgov.gov/Universal-Federated-Analytics-Min.js?agency=HHS&subagency=CDC"
                 id="_fed_an_ua_tag"
