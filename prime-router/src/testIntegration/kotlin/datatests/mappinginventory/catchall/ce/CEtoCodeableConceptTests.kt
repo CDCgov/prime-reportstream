65faec0f08072f996bba0d20eed2fae14b5f5cfe
Josh Fisk <joshuafisk@navapbc.com>
2024-06-18 12:19:19 -0700

    code review feedback

--- prime-router/src/testIntegration/kotlin/datatests/mappinginventory/catchall/ce/CEtoCodeableConceptTests.kt
+++ prime-router/src/testIntegration/kotlin/datatests/mappinginventory/catchall/ce/CEtoCodeableConceptTests.kt
@@ -5,32 +5,7 @@ import org.junit.jupiter.api.Test
 
 class CEtoCodeableConceptTests {
     @Test
-    fun `test value in CE-1`() {
-        assert(verifyHL7ToFHIRToHL7Mapping("catchall/ce/ce-3-test-value-ce1").passed)
-    }
-
-    @Test
-    fun `test value in CE-2`() {
-        assert(verifyHL7ToFHIRToHL7Mapping("catchall/ce/ce-4-test-value-ce2").passed)
-    }
-
-    @Test
-    fun `test value in CE-3`() {
-        assert(verifyHL7ToFHIRToHL7Mapping("catchall/ce/ce-5-test-value-ce3").passed)
-    }
-
-    @Test
-    fun `test value in CE-4`() {
-        assert(verifyHL7ToFHIRToHL7Mapping("catchall/ce/ce-6-test-value-ce4").passed)
-    }
-
-    @Test
-    fun `test value in CE-5`() {
-        assert(verifyHL7ToFHIRToHL7Mapping("catchall/ce/ce-7-test-value-ce5").passed)
-    }
-
-    @Test
-    fun `test value in CE-6`() {
-        assert(verifyHL7ToFHIRToHL7Mapping("catchall/ce/ce-8-test-value-ce6").passed)
+    fun `test values in CE`() {
+        assert(verifyHL7ToFHIRToHL7Mapping("catchall/ce/ce-test-values").passed)
     }
 }
