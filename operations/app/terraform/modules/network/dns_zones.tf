32fa74f16228b7c35b898336bd17ce9ed4a9f589
Denis Krylov <dkrylovsb@users.noreply.github.com>
2024-06-21 15:46:56 -0400

    Fixed dns_zones.tf

--- operations/app/terraform/modules/network/dns_zones.tf
+++ operations/app/terraform/modules/network/dns_zones.tf
@@ -14,7 +14,7 @@ resource "time_sleep" "wait_dns_zone" {
 resource "azurerm_private_dns_zone_virtual_network_link" "dns_zone_private_link" {
   for_each = { for entry in local.zone_vnets : "${var.resource_prefix}-${substr(entry.vnet_name, -14,-1)}-${entry.dns_zone}" => entry }
 
-  name                  = each.value.vnet_name == data.azurerm_virtual_network.vnet["default"].name ? each.value.dns_zone : "${each.key}"
+  name                  = each.value.vnet_name == data.azurerm_virtual_network.vnet["app-vnet"].name ? each.value.dns_zone : "${each.key}"
   private_dns_zone_name = each.value.dns_zone
   resource_group_name   = var.resource_group
   virtual_network_id    = each.value.vnet_id
