bbab44ae5d3d80b30a7585178479702be85ccf4d
Angela DeGolier <angeladegolier@navapbc.com>
2024-06-21 15:15:48 -0400

    Put Unknown and empty strings into a constant

--- prime-router/src/main/kotlin/azure/observability/event/CodeSummary.kt
+++ prime-router/src/main/kotlin/azure/observability/event/CodeSummary.kt
@@ -3,18 +3,21 @@ package gov.cdc.prime.router.azure.observability.event
 import org.hl7.fhir.r4.model.Coding
 
 data class CodeSummary(
-    val system: String = "",
-    val code: String = "",
-    val display: String = "",
+    val system: String = EMPTY_STRING,
+    val code: String = EMPTY_STRING,
+    val display: String = EMPTY_STRING,
 ) {
     companion object {
+        const val UNKNOWN = "Unknown"
+        const val EMPTY_STRING = ""
+
         /**
          * Create an instance of [CodeSummary] from a [Coding]
          */
         fun fromCoding(coding: Coding) = CodeSummary(
-            coding.system ?: "Unknown",
-            coding.code ?: "Unknown",
-            coding.display ?: "Unknown",
+            coding.system ?: UNKNOWN,
+            coding.code ?: UNKNOWN,
+            coding.display ?: UNKNOWN,
         )
     }
 }
