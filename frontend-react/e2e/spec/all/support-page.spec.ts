bfe8c0868dfd9395a635ae137910a79a59041df8
etanb <etan.berkowitz@gmail.com>
2024-07-03 19:36:35 -0700

    reneable and more robust support e2e

--- frontend-react/e2e/spec/all/support-page.spec.ts
+++ frontend-react/e2e/spec/all/support-page.spec.ts
@@ -1,39 +1,62 @@
 import { expect, test } from "@playwright/test";
+import fs from "fs";
+import path from "path";
 import * as support from "../../pages/support";
-// eslint-disable-next-line playwright/no-skipped-test
-test.describe.skip("Support page", () => {
+
+const cards = [
+    {
+        name: "Welcome to ReportStream",
+        anchorID: "welcome-to-reportstream-1",
+    },
+    {
+        name: "Getting started",
+        anchorID: "getting-started-1",
+    },
+    {
+        name: "Using ReportStream",
+        anchorID: "using-reportstream-1",
+    },
+];
+
+test.describe("Support page", () => {
     test.beforeEach(async ({ page }) => {
         await support.goto(page);
     });
 
-    test("should have correct title", async ({ page }) => {
-        await expect(page).toHaveURL(/support/);
-        await expect(page).toHaveTitle(/ReportStream support/);
+    test("Should have the correct page header", async ({ page }) => {
+        const header = page;
+
+        await await expect(header).toHaveText("h1", "Support");
     });
 
-    test("Card navigation", () => {
-        const cardLinks = [
-            {
-                name: "Frequently asked questions",
-                url: "/support/faq",
-            },
-            {
-                name: "Service request",
-                url: "/support/service-request",
-            },
-            {
-                name: "Contact",
-                url: "/support/contact",
-            },
-        ];
+    test("Should have a way of contacting support", async ({ page }) => {
+        // Importing the actual URL of our contact form to match
+        // what we use in production and only need to make updates
+        // in a single place
+        const jsonPath = path.resolve("./src/content/site.json");
+        const site = JSON.parse(await fs.promises.readFile(jsonPath, "utf8"));
+        const contactLink = page
+            .locator(`a[href="${site.forms.contactUs.url}"]`)
+            .first();
 
-        for (const cardLink of cardLinks) {
-            // eslint-disable-next-line playwright/expect-expect
-            test(`should have ${cardLink.name} link`, async ({ page }) => {
-                await page.getByRole("link", { name: cardLink.name }).click();
+        await contactLink.scrollIntoViewIfNeeded();
+        await expect(contactLink).toBeVisible();
+    });
 
-                await expect(page).toHaveURL(cardLink.url);
+    for (const card of cards) {
+        // eslint-disable-next-line playwright/expect-expect
+        test(`should have ${card.name} link`, async ({ page }) => {
+            const cardHeader = page.locator(".usa-card__header", {
+                hasText: card.name,
             });
-        }
-    });
+
+            await expect(cardHeader).toBeVisible();
+
+            const cardContainer = cardHeader.locator("..");
+            const viewAllLink = cardContainer.locator("a").last();
+
+            await viewAllLink.click();
+            await expect(page.locator(`#${card.anchorID}`)).toBeVisible();
+        });
+    }
 });
