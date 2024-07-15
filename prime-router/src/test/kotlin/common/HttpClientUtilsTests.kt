904723ffec1bbcfafafdc2d055bf5b06a1b933b9
David Holiday <davidholiday@Davids-MacBook-Pro.local>
2024-07-15 15:05:19 -0600

    another test

--- prime-router/src/test/kotlin/common/HttpClientUtilsTests.kt
+++ prime-router/src/test/kotlin/common/HttpClientUtilsTests.kt
@@ -5,25 +5,47 @@ import junit.framework.TestCase.assertEquals
 import java.lang.reflect.Field
 import kotlin.test.Test
 
-
 class HttpClientUtilsTests {
 
     @Test
     fun `test default values for clients and auth hash are null`() {
         // httpClient should be initialized as null
-        var httpClientField: Field = HttpClientUtils::class.java.getDeclaredField("httpClient")
+        val httpClientField: Field = HttpClientUtils::class.java.getDeclaredField("httpClient")
         httpClientField.trySetAccessible()
         assertEquals(null, httpClientField.get(HttpClient::class))
 
         // httpClientWithAuth should be initialized as null
-        var httpClientWithAuth: Field = HttpClientUtils::class.java.getDeclaredField("httpClientWithAuth")
+        val httpClientWithAuth: Field = HttpClientUtils::class.java.getDeclaredField("httpClientWithAuth")
         httpClientWithAuth.trySetAccessible()
         assertEquals(null, httpClientWithAuth.get(HttpClient::class))
 
-        // accessTokenHash should be initialized as null
-        var accessTokenHash: Field = HttpClientUtils::class.java.getDeclaredField("accessTokenHash")
+        // accessTokenHash should be initialized as -1
+        val accessTokenHash: Field = HttpClientUtils::class.java.getDeclaredField("accessTokenHash")
         accessTokenHash.trySetAccessible()
         assertEquals(-1, accessTokenHash.get(Int::class))
     }
 
+    @Test
+    fun `test httpClient is a singleton and instantiating it does not impact value of field accessTokenHash`() {
+        // create the httpClient singleton
+        val httpClient1 = HttpClientUtils.getDefaultHttpClient(null)
+        val httpClientObjRef1 = System.identityHashCode(httpClient1)
+
+        // request the httpClient obj again
+        val httpClient2 = HttpClientUtils.getDefaultHttpClient(null)
+        val httpClientObjRef2 = System.identityHashCode(httpClient2)
+
+        // should still be the same object
+        assertEquals(httpClientObjRef1, httpClientObjRef2)
+
+        // httpClientWithAuth should still be null
+        val httpClientWithAuth: Field = HttpClientUtils::class.java.getDeclaredField("httpClientWithAuth")
+        httpClientWithAuth.trySetAccessible()
+        assertEquals(null, httpClientWithAuth.get(HttpClient::class))
+
+        // accessTokenHash should still be -1
+        val accessTokenHash: Field = HttpClientUtils::class.java.getDeclaredField("accessTokenHash")
+        accessTokenHash.trySetAccessible()
+        assertEquals(-1, accessTokenHash.get(Int::class))
+    }
 }
