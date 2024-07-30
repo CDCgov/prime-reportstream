f7a84086850b51e5c3ee37767d565b5288596237
Angela DeGolier <angeladegolier@navapbc.com>
2024-07-29 23:04:01 -0400

    Updated tests

--- prime-router/src/test/kotlin/validation/MarsOtcElrOnboardingValidatorTests.kt
+++ prime-router/src/test/kotlin/validation/MarsOtcElrOnboardingValidatorTests.kt
@@ -2,6 +2,7 @@ package gov.cdc.prime.router.validation
 
 import assertk.assertThat
 import assertk.assertions.isFalse
+import assertk.assertions.isTrue
 import gov.cdc.prime.router.ActionLogger
 import gov.cdc.prime.router.fhirengine.utils.HL7Reader
 import org.junit.jupiter.api.Test
@@ -13,11 +14,22 @@ class MarsOtcElrOnboardingValidatorTests {
     @Test
     fun `test can detect an invalid RADxMARS message`() {
         val sampleMessageInputStream =
-            this.javaClass.classLoader.getResourceAsStream("validation/marsotcelr/sample_2.hl7")
+            this.javaClass.classLoader.getResourceAsStream("validation/marsotcelr/fail_onboarding_pass_prod.hl7")
 
         val sampleMessage = sampleMessageInputStream!!.bufferedReader().use { it.readText() }
         val messages = HL7Reader(ActionLogger()).getMessages(sampleMessage)
         val report = validator.validate(messages[0])
         assertThat(report.isValid()).isFalse()
+    }
+
+    @Test
+    fun `test a valid RADxMARS message`() {
+        val sampleMessageInputStream =
+            this.javaClass.classLoader.getResourceAsStream("validation/marsotcelr/valid.hl7")
+
+        val sampleMessage = sampleMessageInputStream!!.bufferedReader().use { it.readText() }
+        val messages = HL7Reader(ActionLogger()).getMessages(sampleMessage)
+        val report = validator.validate(messages[0])
+        assertThat(report.isValid()).isTrue()
     }
 }
