e337685c164d13e9792c64760c7e6636ca4922c5
Brick Green <brick.green@agile6.com>
2024-08-06 13:00:30 -0400

    add flyway script tup update report_file

---
+++ prime-router/src/main/resources/db/migration/V72_alter_reportfile_schemaname_tschematopic_to_nullable.sql
@@ -0,0 +1,14 @@
+/*
+* The Flyway tool applies this migration to create the database.
+*
+* Follow this style guide https://about.gitlab.com/handbook/business-ops/data-team/platform/sql-style-guide/
+* use VARCHAR(63) for names in organization and schema
+*
+* Copy a version of this comment into the next migration
+*
+*/
+
+-- Alter the table to make schema_name and schema_topic nullable
+ALTER TABLE report_file
+    ALTER COLUMN schema_name DROP NOT NULL,
+    ALTER COLUMN schema_topic DROP NOT NULL;
