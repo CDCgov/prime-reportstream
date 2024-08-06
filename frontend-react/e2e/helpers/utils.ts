d5d750c2945c0587e75fd30c136b295b62b692c3
Penny Lischer <penny.lischer@agile6.com>
2024-08-06 13:31:51 -0700

    15211 - testing

--- frontend-react/e2e/helpers/utils.ts
+++ frontend-react/e2e/helpers/utils.ts
@@ -4,7 +4,7 @@ import { fromDateWithTime, toDateWithTime } from "../pages/daily-data";
 
 export const TEST_ORG_IGNORE = "ignore";
 export const TEST_ORG_AK = "ak-phd";
-export const TEST_ORG_UP_RECEIVER_FULL_ELR = "SETTINGS_TEST";
+export const TEST_ORG_UP_RECEIVER_FULL_ELR = "FULL_ELR";
 export const TEST_ORG_CP_RECEIVER_CSV = "CSV";
 export const TEST_ORG_ELIMS_RECEIVER_ELR = "ELR_ELIMS";
 export const TEST_ORG_AK_RECEIVER = "elr";
