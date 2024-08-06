ec69efb0f27baeb39d5e82e88d274060e24569ca
Arnej <118766341+arnejduranovic@users.noreply.github.com>
2024-08-06 14:55:21 -0500

    Revert "Platform/13925/singleton http client" (#15522)

--- prime-router/build.gradle.kts
+++ prime-router/build.gradle.kts
@@ -882,7 +882,6 @@ dependencies {
     implementation("io.ktor:ktor-client-core:$ktorVersion")
     implementation("io.ktor:ktor-client-cio:$ktorVersion")
     implementation("io.ktor:ktor-client-apache:$ktorVersion")
-    implementation("io.ktor:ktor-client-apache5:$ktorVersion")
     implementation("io.ktor:ktor-client-logging:$ktorVersion")
     implementation("io.ktor:ktor-client-encoding:$ktorVersion")
     implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
