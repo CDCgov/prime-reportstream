632c945e4d79f91c8c14d4278711e6c780b38d69
etanb <etan.berkowitz@gmail.com>
2024-08-23 16:52:12 -0700

    POMify message details

--- frontend-react/e2e/pages/authenticated/message-details.ts
+++ frontend-react/e2e/pages/authenticated/message-details.ts
@@ -1,11 +1,35 @@
-import { Page } from "@playwright/test";
-
+import { MOCK_GET_MESSAGE } from "../../mocks/messages";
 import { MESSAGE_ID } from "../../pages/authenticated/message-id-search";
+import * as messageIdSearch from "../../pages/authenticated/message-id-search";
 
-export const URL_MESSAGE_DETAILS = `/message-details/${MESSAGE_ID}`;
+import { BasePage, BasePageTestArgs, RouteHandlerFulfillEntry } from "../BasePage";
 
-export async function goto(page: Page) {
-    await page.goto(URL_MESSAGE_DETAILS, {
-        waitUntil: "domcontentloaded",
-    });
+export class MessageDetailsPage extends BasePage {
+    static readonly URL_MESSAGE_DETAILS = `/message-details/${MESSAGE_ID}`;
+
+    constructor(testArgs: BasePageTestArgs) {
+        super(
+            {
+                url: MessageDetailsPage.URL_MESSAGE_DETAILS,
+                title: "ReportStream - CDC's free, interoperable data transfer platform",
+                heading: testArgs.page.getByRole("heading", {
+                    name: "Message ID Search",
+                }),
+            },
+            testArgs,
+        );
+
+        this.addMockRouteHandlers([this.createMessageIDSearchAPIHandler()]);
+    }
+
+    createMessageIDSearchAPIHandler(): RouteHandlerFulfillEntry {
+        return [
+            messageIdSearch.API_MESSAGE,
+            () => {
+                return {
+                    json: MOCK_GET_MESSAGE,
+                };
+            },
+        ];
+    }
 }
