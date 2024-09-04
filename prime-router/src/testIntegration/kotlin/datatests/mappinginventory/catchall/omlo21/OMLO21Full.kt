033ad64d4d2acb55d40ed5685758e3c6efa7c10d
Josh Fisk <joshuafisk@navapbc.com>
2024-09-04 14:52:44 -0700

    Working integration tests

--- prime-router/src/testIntegration/kotlin/datatests/mappinginventory/catchall/omlo21/OMLO21Full.kt
+++ prime-router/src/testIntegration/kotlin/datatests/mappinginventory/catchall/omlo21/OMLO21Full.kt
@@ -2,20 +2,36 @@ package gov.cdc.prime.router.datatests.mappinginventory.omlo21
 
 import gov.cdc.prime.router.datatests.mappinginventory.verifyHL7ToFHIRToHL7Mapping
 import org.junit.jupiter.api.Test
-// import kotlin.test.Ignore
 
 class OMLO21Full {
 
      // not working yet, now that it is switched over to OML output schema, lots of errors as far as the eye can see
     @Test
-    fun `test OML_O21 all segments`() {
-        // eventually these should all be true
+    fun `test OML_O21 all segments HL7 to FHIR`() {
+        assert(verifyHL7ToFHIRToHL7Mapping("catchall/omlo21/oml_o21-full", false, true, true).passed)
+    }
+
+    @Test
+    fun `test OML_O21 all segments HL7 to FHIR to HL7`() {
         assert(
             verifyHL7ToFHIRToHL7Mapping(
-                "catchall/omlo21/oml_o21-full",
+                "catchall/omlo21/oml_o21-fullh2f2h",
                 true,
                 true,
                 false,
+                outputSchema = "classpath:/metadata/hl7_mapping/OML_O21/OML_O21-test.yml"
+            ).passed
+        )
+    }
+
+    @Test
+    fun `test OML_O21 all segments FHIR to HL7`() {
+        assert(
+            verifyHL7ToFHIRToHL7Mapping(
+                "catchall/omlo21/oml_o21-fullf2h",
+                true,
+                false,
+                true,
                 outputSchema = "classpath:/metadata/hl7_mapping/OML_O21/OML_O21-test.yml"
             ).passed
         )
