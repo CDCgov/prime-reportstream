a12be52eac354fa5a01b5082151cc7c952b6bbd3
etanb <etan.berkowitz@gmail.com>
2024-09-05 09:42:22 -0700

    testing re enabling file download

--- frontend-react/e2e/spec/chromium-only/authenticated/daily-data-page-user-flow.spec.ts
+++ frontend-react/e2e/spec/chromium-only/authenticated/daily-data-page-user-flow.spec.ts
@@ -373,7 +373,7 @@ test.describe(
                         await dailyDataPage.page.locator(".usa-table tbody").waitFor({ state: "visible" });
                     });
 
-                    test.skip("downloads the file", async ({ dailyDataPage }) => {
+                    test("downloads the file", async ({ dailyDataPage }) => {
                         await setDate(dailyDataPage.page, "#start-date", 14);
                         await setDate(dailyDataPage.page, "#end-date", 0);
 
