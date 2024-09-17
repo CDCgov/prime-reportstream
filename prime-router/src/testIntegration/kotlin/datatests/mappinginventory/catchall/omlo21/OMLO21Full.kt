6db4493513b8017c0c73f772dbc1058fe37a7100
Josh Fisk <joshuafisk@navapbc.com>
2024-09-17 13:39:57 -0700

    small updates

--- prime-router/src/testIntegration/kotlin/datatests/mappinginventory/catchall/omlo21/OMLO21Full.kt
+++ prime-router/src/testIntegration/kotlin/datatests/mappinginventory/catchall/omlo21/OMLO21Full.kt
@@ -18,29 +18,29 @@ class OMLO21Full {
         )
     }
 
-//    @Test
-//    fun `test OML_O21 all segments HL7 to FHIR to HL7 O L D`() {
-//        assert(
-//            verifyHL7ToFHIRToHL7Mapping(
-//                "catchall/omlo21/oml_o21-fullh2f2h",
-//                true,
-//                true,
-//                false,
-//                outputSchema = "classpath:/metadata/hl7_mapping/OML_O21/OML_O21-test_NEW.yml"
-//            ).passed
-//        )
-//    }
+    @Test
+    fun `test OML_O21 all segments HL7 to FHIR to HL7 O L D`() {
+        assert(
+            verifyHL7ToFHIRToHL7Mapping(
+                "catchall/omlo21/oml_o21-fullh2f2h",
+                true,
+                true,
+                false,
+                outputSchema = "classpath:/metadata/hl7_mapping/OML_O21/OML_O21-test_NEW.yml"
+            ).passed
+        )
+    }
 
-//    @Test
-//    fun `test OML_O21 all segments FHIR to HL7 O L D`() {
-//        assert(
-//            verifyHL7ToFHIRToHL7Mapping(
-//                "catchall/omlo21/oml_o21-fullf2h",
-//                true,
-//                false,
-//                true,
-//                outputSchema = "classpath:/metadata/hl7_mapping/OML_O21/OML_O21-test_NEW.yml"
-//            ).passed
-//        )
-//    }
+    @Test
+    fun `test OML_O21 all segments FHIR to HL7 O L D`() {
+        assert(
+            verifyHL7ToFHIRToHL7Mapping(
+                "catchall/omlo21/oml_o21-fullf2h",
+                true,
+                false,
+                true,
+                outputSchema = "classpath:/metadata/hl7_mapping/OML_O21/OML_O21-test.yml"
+            ).passed
+        )
+    }
 }
