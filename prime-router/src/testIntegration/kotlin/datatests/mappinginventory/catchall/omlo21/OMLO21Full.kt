c3b9ab0ffdf1b09603944fdc23a5dba1be8b849a
Josh Fisk <joshuafisk@navapbc.com>
2024-09-17 13:12:23 -0700

    some cleanup

--- prime-router/src/testIntegration/kotlin/datatests/mappinginventory/catchall/omlo21/OMLO21Full.kt
+++ prime-router/src/testIntegration/kotlin/datatests/mappinginventory/catchall/omlo21/OMLO21Full.kt
@@ -10,8 +10,8 @@ class OMLO21Full {
         assert(
             verifyHL7ToFHIRToHL7Mapping(
                 "catchall/omlo21/oml_o21-fullh2f2h_NEW",
-                false,
-                false,
+                true,
+                true,
                 false,
                 outputSchema = "classpath:/metadata/hl7_mapping/OML_O21/OML_O21-test_NEW.yml"
             ).passed
@@ -26,11 +26,11 @@ class OMLO21Full {
 //                true,
 //                true,
 //                false,
-//                outputSchema = "classpath:/metadata/hl7_mapping/OML_O21/OML_O21-test.yml"
+//                outputSchema = "classpath:/metadata/hl7_mapping/OML_O21/OML_O21-test_NEW.yml"
 //            ).passed
 //        )
 //    }
-//
+
 //    @Test
 //    fun `test OML_O21 all segments FHIR to HL7 O L D`() {
 //        assert(
@@ -39,23 +39,8 @@ class OMLO21Full {
 //                true,
 //                false,
 //                true,
-//                outputSchema = "classpath:/metadata/hl7_mapping/OML_O21/OML_O21-test.yml"
+//                outputSchema = "classpath:/metadata/hl7_mapping/OML_O21/OML_O21-test_NEW.yml"
 //            ).passed
 //        )
 //    }
-
-    // philosophy of this ticket should be to doc how the mappings currently work, not how they should work.
-    // follow up ticket for how they should work
-
-    // probably will have to split this test out into three
-    // uses cases: FHIR -> OML; OML -> OML; ORM -> OML
-    // each use case may need it's own test here because of what is/isn't mapped at different parts.
-    // The approach that needs to be tried is to pare down fields in the hl7 to find what is actually possible in an
-    // hl7 to fhir to hl7 scenario. This should be the first OML integration test and should reflect what is currently
-    // mapped and not what *should* be mapped. Follow up tickets need to be logged for missing fields.
-
-    // datatests/mappinginventory/catchall/omlo21/maximal_oml_preserved.hl7 represents a preserved fully mapped out
-    // OML message. This was derived from starting at the original FHIR->HL7 OML mappings and creating a message
-    // that had every field populated. Likely a lot of these fields aren't captured going from HL7 to FHIR and can be
-    // removed.
 }
