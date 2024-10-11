5a3038adf824727b80d3e0ad72695afc15ed904f
Gabriel Dorsch <gabriel.dorsch@gmail.com>
2024-10-11 13:50:37 -0400

    Added mocks to fix 2 test failures

--- prime-router/src/test/kotlin/history/azure/SubmissionFunctionTests.kt
+++ prime-router/src/test/kotlin/history/azure/SubmissionFunctionTests.kt
@@ -22,6 +22,7 @@ import gov.cdc.prime.router.TranslatorConfiguration
 import gov.cdc.prime.router.azure.DatabaseAccess
 import gov.cdc.prime.router.azure.MockHttpRequestMessage
 import gov.cdc.prime.router.azure.MockSettings
+import gov.cdc.prime.router.azure.SubmissionTableService
 import gov.cdc.prime.router.azure.WorkflowEngine
 import gov.cdc.prime.router.azure.db.enums.TaskAction
 import gov.cdc.prime.router.azure.db.tables.pojos.Action
@@ -439,6 +440,12 @@ class SubmissionFunctionTests : Logging {
         mockkObject(AuthenticatedClaims.Companion)
         every { AuthenticatedClaims.authenticate(any()) } returns
             AuthenticatedClaims.generateTestClaims()
+
+        val submissionTableService = mockk<SubmissionTableService>()
+        every { submissionTableService.getSubmission(any(), any()) } returns null
+
+        mockkObject(SubmissionTableService.Companion)
+        every { SubmissionTableService.getInstance() } returns submissionTableService
 
         // Invalid id:  not a UUID nor a Long
         var response = function.getReportDetailedHistory(mockRequest, "bad")
