3403b457cf3290576e04e2d56e21fb916300cae7
Jamie Albinson <jamie.albinson@focusconsulting.io>
2024-10-21 14:52:53 -0400

    add basic healthcheck test

---
+++ auth/src/test/kotlin/gov/cdc/prime/reportstream/auth/controller/HealthControllerTest.kt
@@ -0,0 +1,32 @@
+package gov.cdc.prime.reportstream.auth.controller
+
+import gov.cdc.prime.reportstream.auth.AuthApplicationConstants
+import gov.cdc.prime.reportstream.auth.model.ApplicationStatus
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.security.test.web.reactive.server.SecurityMockServerConfigurers.csrf
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.test.web.reactive.server.WebTestClient
+import kotlin.test.Test
+
+@ExtendWith(SpringExtension::class)
+@SpringBootTest
+@AutoConfigureWebTestClient
+class HealthControllerTest @Autowired constructor(
+    private val webTestClient: WebTestClient
+) {
+
+    @Test
+    fun `successful healthcheck`() {
+        webTestClient
+            .mutateWith(csrf())
+            .get()
+            .uri(AuthApplicationConstants.Endpoints.HEALTHCHECK_ENDPOINT_V1)
+            .exchange()
+            .expectStatus().isOk
+            .expectBody(ApplicationStatus::class.java)
+    }
+
+}
