6dbf124c35726605e22d497cedd13d412c5f1194
Matt Szubrycht <matt.szubrycht@aquia.io>
2024-11-04 15:35:09 -0800

    skip linter on net logger

--- frontend-react/pa11y-net-logger.js
+++ frontend-react/pa11y-net-logger.js
@@ -1,13 +1,15 @@
 /* eslint-env node */
 /* eslint-disable no-undef */
 /* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/require-await */
 
 module.exports = async (page) => {
-  page.on('request', (request) => {
-    console.log(`Request: ${request.method()} ${request.url()}`);
-  });
-
-  page.on('response', (response) => {
-    console.log(`Response: ${response.status()} ${response.url()}`);
-  });
-};
+    page.on('request', (request) => {
+      console.log(`Request: ${request.method()} ${request.url()}`);
+    });
+  
+    page.on('response', (response) => {
+      console.log(`Response: ${response.status()} ${response.url()}`);
+    });
+  };
+  
