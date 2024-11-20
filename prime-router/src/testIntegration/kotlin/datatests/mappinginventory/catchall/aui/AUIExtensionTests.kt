70fe3c72ec5868db1b4292e4055a2364049bb302
David Holiday <davidholiday@Davids-MacBook-Pro.local>
2024-11-20 16:29:35 -0700

    working

--- prime-router/src/testIntegration/kotlin/datatests/mappinginventory/catchall/aui/AUIExtensionTests.kt
+++ prime-router/src/testIntegration/kotlin/datatests/mappinginventory/catchall/aui/AUIExtensionTests.kt
@@ -6,12 +6,14 @@ import org.junit.jupiter.api.Test
 class AUIExtensionTests {
     @Test
     fun `test correctly handles ISO universal id type`() {
-        assert(verifyHL7ToFHIRToHL7Mapping(
+        assert(
+            verifyHL7ToFHIRToHL7Mapping(
             "catchall/aui/aui",
             false,
             false,
             false,
             outputSchema = "classpath:/metadata/hl7_mapping/OML_O21/OML_O21-base.yml"
-        ).passed)
+        ).passed
+        )
     }
 }
