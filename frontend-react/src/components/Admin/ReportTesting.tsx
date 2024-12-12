50a8133085b44c761b305b88835a8beea61186d4
etanb <etan.berkowitz@gmail.com>
2024-12-12 11:15:02 -0800

    simple view message

--- frontend-react/src/components/Admin/ReportTesting.tsx
+++ frontend-react/src/components/Admin/ReportTesting.tsx
@@ -1,4 +1,4 @@
-import { Form, GridContainer } from "@trussworks/react-uswds";
+import { Button, GridContainer } from "@trussworks/react-uswds";
 import { Helmet } from "react-helmet-async";
 import { useParams } from "react-router";
 import { AdminFormWrapper } from "./AdminFormWrapper";
@@ -7,6 +7,7 @@ import useReportTesting from "../../hooks/api/reports/UseReportTesting";
 import AdminFetchAlert from "../alerts/AdminFetchAlert";
 import Spinner from "../Spinner";
 import Title from "../Title";
+import { Icon } from "../../shared";
 
 function ReportTesting() {
     const { orgname, receivername } = useParams<EditReceiverSettingsParams>();
@@ -16,9 +17,25 @@ function ReportTesting() {
     }
     if (isLoading || !testMessages) return <Spinner />;
 
-    const RadioField = ({ title, index }: { title: string; index: number }) => {
+    const RadioField = ({ title, body, index }: { title: string; body: string; index: number }) => {
+        const openJsonInNewTab = () => {
+            // Your JSON object or string
+            const jsonString = JSON.stringify(JSON.parse(body), null, 2);
+
+            // Create a Blob from the JSON string
+            const blob = new Blob([jsonString], { type: "application/json" });
+
+            // Create an object URL for the Blob
+            const url = URL.createObjectURL(blob);
+
+            // Open the URL in a new browser tab
+            window.open(url, "_blank");
+
+            // Revoke the URL after some time to free up memory
+            setTimeout(() => URL.revokeObjectURL(url), 1000);
+        };
         return (
-            <div className="usa-radio bg-base-lighter padding-2 border-bottom-1px border-gray-30">
+            <div className="usa-radio bg-base-lightest padding-2 border-bottom-1px border-gray-30">
                 <input
                     className="usa-radio__input"
                     id={`message-${index}`}
@@ -26,8 +43,11 @@ function ReportTesting() {
                     name="message-test-form"
                     value={title}
                 />
-                <label className="usa-radio__label margin-0" htmlFor={`message-${index}`}>
-                    {title}
+                <label className="usa-radio__label" htmlFor={`message-${index}`}>
+                    {title}{" "}
+                    <Button type="button" unstyled onClick={openJsonInNewTab}>
+                        View message <Icon name="Visibility" />
+                    </Button>
                 </label>
             </div>
         );
@@ -61,13 +81,13 @@ function ReportTesting() {
                     </p>
                     <hr />
                     <p className="font-sans-xl text-bold">Test message bank</p>
-                    <Form className="width-full maxw-full">
-                        <fieldset className="usa-fieldset bg-base-lighter padding-3">
+                    <form>
+                        <fieldset className="usa-fieldset bg-base-lightest padding-3">
                             {testMessages?.map((item, index) => (
-                                <RadioField key={index} title={item.fileName} index={index} />
+                                <RadioField key={index} index={index} title={item.fileName} body={item.reportBody} />
                             ))}
                         </fieldset>
-                    </Form>
+                    </form>
                 </GridContainer>
             </AdminFormWrapper>
         </>
