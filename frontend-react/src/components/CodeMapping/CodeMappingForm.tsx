444fb4d466c50a5c6f1c1f253aaba168e0f85f58
Joseph Andersen <12385932+jpandersen87@users.noreply.github.com>
2025-02-10 02:04:58 -0600

    Develop front-end pages & states for code mapping tool (UPLOAD flow) Fixes #16758 * first pass, no style match * skeletons of todo functions * sitemap updated

--- frontend-react/src/components/CodeMapping/CodeMappingForm.tsx
+++ frontend-react/src/components/CodeMapping/CodeMappingForm.tsx
@@ -1,44 +1,64 @@
-import { Button, ButtonGroup, FileInput } from "@trussworks/react-uswds";
-import { ChangeEvent, FormEventHandler, MouseEventHandler, useCallback } from "react";
-import site from "../../content/site.json";
+import { Button, FileInput } from "@trussworks/react-uswds";
+import { FormEventHandler, MouseEventHandler, type PropsWithChildren, useCallback } from "react";
+import CodeMappingResults from "./CodeMappingResults";
+import useCodeMappingFormSubmit from "../../hooks/api/UseCodeMappingFormSubmit/UseCodeMappingFormSubmit";
+import Spinner from "../Spinner";
 
-interface CodeMappingFormProps {
-    onSubmit: FormEventHandler<HTMLFormElement>;
-    setFile: (event: ChangeEvent<HTMLInputElement>) => void;
-}
+export type CodeMappingFormProps = PropsWithChildren;
 
-const CodeMappingForm = ({ onSubmit, setFile }: CodeMappingFormProps) => {
+const CodeMappingForm = (props: CodeMappingFormProps) => {
+    const { data, isPending, mutate } = useCodeMappingFormSubmit();
+    const onSubmitHandler = useCallback<FormEventHandler<HTMLFormElement>>(
+        (ev) => {
+            ev.preventDefault();
+            mutate();
+            return false;
+        },
+        [mutate],
+    );
     const onBackHandler = useCallback<MouseEventHandler>((_ev) => {
         window.history.back();
     }, []);
 
     return (
         <>
-            <p className="font-sans-lg">
+            <p className="todo">
                 Check that your LOINC and SNOMED codes are mapped to conditions so ReportStream can accurately transform
                 your data
             </p>
             <p>
-                Follow <a href="/developer-resources/api/getting-started#2_4">these instructions</a> and use{" "}
-                <a href={site.assets.codeMapTemplate.path}>our template</a> to format your result and organism codes to
-                LOINC or SNOMED. Note: Local codes cannot be automatically mapped.
+                Follow <a href="todo">these instructions</a> and use <a href="todo">our template</a> to format your
+                result and organism codes to LOINC or SNOMED. Note: Local codes cannot be automatically mapped.
             </p>
-
-            <form onSubmit={onSubmit}>
-                <label className="usa-label" htmlFor="file-input-specific">
-                    Upload CSV file
-                </label>
-                <span className="usa-hint" id="file-input-specific-hint">
-                    Make sure your file has a .csv extension
-                </span>
-                <FileInput id={""} name={"file"} className="maxw-full" accept=".csv" onChange={setFile} />
-                <ButtonGroup className="margin-top-5">
+            {data && <CodeMappingResults />}
+            {isPending && (
+                <>
+                    <Spinner />
+                    <p>
+                        Checking your file for any unmapped codes that will prevent data from being reported
+                        successfully.
+                    </p>
+                </>
+            )}
+            {!data && !isPending && (
+                <form onSubmit={onSubmitHandler}>
+                    <label className="usa-label" htmlFor="file-input-specific">
+                        Upload CSV file
+                    </label>
+                    <span className="usa-hint" id="file-input-specific-hint">
+                        Make sure your file has a .csv extension
+                    </span>
+                    <FileInput id={""} name={"file"} />
                     <Button type={"button"} outline onClick={onBackHandler}>
                         Back
                     </Button>
                     <Button type={"submit"}>Submit</Button>
-                </ButtonGroup>
-            </form>
+                </form>
+            )}
+            {props.children}
+            <p>
+                Questions or feedback? Please email <a href="todo">reportstream@cdc.gov</a>
+            </p>
         </>
     );
 };
