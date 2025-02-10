ce6e9702c1dd5263efa5802bc9a3a436584fe8bd
Joseph Andersen <12385932+jpandersen87@users.noreply.github.com>
2025-02-10 17:13:34 -0600

    style pass * styling added to match design * proper paths for urls added * fileinput configured for ".csv"

--- frontend-react/src/components/CodeMapping/CodeMappingForm.tsx
+++ frontend-react/src/components/CodeMapping/CodeMappingForm.tsx
@@ -1,6 +1,7 @@
-import { Button, FileInput } from "@trussworks/react-uswds";
+import { Button, ButtonGroup, FileInput } from "@trussworks/react-uswds";
 import { FormEventHandler, MouseEventHandler, type PropsWithChildren, useCallback } from "react";
 import CodeMappingResults from "./CodeMappingResults";
+import site from "../../content/site.json";
 import useCodeMappingFormSubmit from "../../hooks/api/UseCodeMappingFormSubmit/UseCodeMappingFormSubmit";
 import Spinner from "../Spinner";
 
@@ -22,13 +23,14 @@ const CodeMappingForm = (props: CodeMappingFormProps) => {
 
     return (
         <>
-            <p className="todo">
+            <p className="font-sans-lg">
                 Check that your LOINC and SNOMED codes are mapped to conditions so ReportStream can accurately transform
                 your data
             </p>
             <p>
-                Follow <a href="todo">these instructions</a> and use <a href="todo">our template</a> to format your
-                result and organism codes to LOINC or SNOMED. Note: Local codes cannot be automatically mapped.
+                Follow <a href="/developer-resources/api/getting-started#2_4">these instructions</a> and use{" "}
+                <a href={site.assets.codeMapTemplate.path}>our template</a> to format your result and organism codes to
+                LOINC or SNOMED. Note: Local codes cannot be automatically mapped.
             </p>
             {data && <CodeMappingResults />}
             {isPending && (
@@ -48,16 +50,18 @@ const CodeMappingForm = (props: CodeMappingFormProps) => {
                     <span className="usa-hint" id="file-input-specific-hint">
                         Make sure your file has a .csv extension
                     </span>
-                    <FileInput id={""} name={"file"} />
-                    <Button type={"button"} outline onClick={onBackHandler}>
-                        Back
-                    </Button>
-                    <Button type={"submit"}>Submit</Button>
+                    <FileInput id={""} name={"file"} className="maxw-full" accept=".csv" />
+                    <ButtonGroup className="margin-top-5">
+                        <Button type={"button"} outline onClick={onBackHandler}>
+                            Back
+                        </Button>
+                        <Button type={"submit"}>Submit</Button>
+                    </ButtonGroup>
                 </form>
             )}
             {props.children}
-            <p>
-                Questions or feedback? Please email <a href="todo">reportstream@cdc.gov</a>
+            <p className="margin-top-9">
+                Questions or feedback? Please email <a href="mailto:reportstream@cdc.gov">reportstream@cdc.gov</a>
             </p>
         </>
     );
