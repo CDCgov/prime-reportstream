0a400fd7f28b7b068a165a72b78f751ae8b029f5
Abi Philip <abiphilip@navapbc.com>
2025-02-14 15:53:20 -0600

    update suppress-device-information.yml

--- prime-router/src/main/kotlin/cli/helpers/HL7DiffHelper.kt
+++ prime-router/src/main/kotlin/cli/helpers/HL7DiffHelper.kt
@@ -201,7 +201,7 @@ class HL7DiffHelper {
                     return listOf(
                         Hl7Diff(
                             segmentIndex,
-                            "Difference in number of components. Input: $inputComponents, Output: $outputComponents ",
+                            " $inputComponents # $outputComponents ",
                             "",
                             fieldNumber,
                             secondaryFieldNum,
@@ -213,8 +213,7 @@ class HL7DiffHelper {
                     return listOf(
                         Hl7Diff(
                             segmentIndex,
-                            """Difference in number of extra components. 
-                                |Input: $inputExtraComponents Output: $outputExtraComponents
+                            """  $inputExtraComponents # $outputExtraComponents
                             """.trimMargin(),
                             "",
                             fieldNumber,
@@ -251,7 +250,7 @@ class HL7DiffHelper {
                 return listOf(
                     Hl7Diff(
                         segmentIndex,
-                        "Difference in type of field, ${input.javaClass}, ${output.javaClass}.",
+                        " ${input.javaClass} # ${output.javaClass}.",
                         "",
                         fieldNumber,
                         secondaryFieldNum,
@@ -315,7 +314,7 @@ class HL7DiffHelper {
             val outputText = if (output.isEmpty()) {
                 ""
             } else {
-                ", $output"
+                "# $output."
             }
 
             val tertiaryFieldNumberText = if (tertiaryFieldNumber == null) {
@@ -334,7 +333,7 @@ class HL7DiffHelper {
 
             return "$segmentIndex." +
                 "$fieldNumberText$secondaryFieldNumberText$tertiaryFieldNumberText" +
-                " , $input$outputText"
+                " # $input$outputText"
         }
     }
 
