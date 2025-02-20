642c61afdb0ee6fdae6db83ece853afb21f03006
etanb <etan.berkowitz@gmail.com>
2025-02-20 11:18:44 -0800

    hoist business logic to CodeMapPage

--- frontend-react/src/pages/onboarding/CodeMappingPage.tsx
+++ frontend-react/src/pages/onboarding/CodeMappingPage.tsx
@@ -1,14 +1,65 @@
-import type { PropsWithChildren } from "react";
+import { Button, GridContainer } from "@trussworks/react-uswds";
+import { FormEventHandler, MouseEventHandler, useCallback, useState } from "react";
+import { Helmet } from "react-helmet-async";
 import CodeMappingForm from "../../components/CodeMapping/CodeMappingForm";
+import CodeMappingResults from "../../components/CodeMapping/CodeMappingResults";
+import Spinner from "../../components/Spinner";
+import { USLink } from "../../components/USLink";
+import useCodeMappingFormSubmit from "../../hooks/api/UseCodeMappingFormSubmit/UseCodeMappingFormSubmit";
 
-export type CodeMappingPageProps = PropsWithChildren;
+enum CodeMappingSteps {
+    StepOne = "CodeMapFileSelect",
+    StepTwo = "CodeMapResult",
+}
 
-const CodeMappingPage = (props: CodeMappingPageProps) => {
+const CodeMappingPage = () => {
+    const { data, isPending, mutate } = useCodeMappingFormSubmit();
+    const [currentCodeMapStep, setCurrentCodeMapStep] = useState<CodeMappingSteps>(CodeMappingSteps.StepOne);
+    const onCancelHandler = useCallback<MouseEventHandler>((_ev) => {
+        // Don't have a proper mechanism to cancel in-flight requests so refresh page
+        window.location.reload();
+    }, []);
+    const onSubmitHandler = useCallback<FormEventHandler<HTMLFormElement>>(
+        (ev) => {
+            ev.preventDefault();
+            mutate();
+            return false;
+        },
+        [mutate],
+    );
     return (
         <>
-            <h1>Code mapping tool</h1>
-            <CodeMappingForm />
-            {props.children}
+            <Helmet>
+                <title>Code mapping tool - ReportStream</title>
+            </Helmet>
+
+            <GridContainer>
+                <h1>Code mapping tool</h1>
+                {isPending ? (
+                    <>
+                        <Spinner />
+                        <p className="text-center">
+                            Checking your file for any unmapped codes that will <br /> prevent data from being reported
+                            successfully
+                        </p>
+                        <Button type={"button"} outline onClick={onCancelHandler}>
+                            Cancel
+                        </Button>
+                    </>
+                ) : (
+                    <>
+                        {currentCodeMapStep === CodeMappingSteps.StepOne && (
+                            <CodeMappingForm onSubmitHandler={onSubmitHandler} />
+                        )}
+                        {currentCodeMapStep === CodeMappingSteps.StepTwo && <CodeMappingResults />}
+                    </>
+                )}
+
+                <p className="margin-top-9">
+                    Questions or feedback? Please email{" "}
+                    <USLink href="mailto:reportstream@cdc.gov">reportstream@cdc.gov</USLink>
+                </p>
+            </GridContainer>
         </>
     );
 };
