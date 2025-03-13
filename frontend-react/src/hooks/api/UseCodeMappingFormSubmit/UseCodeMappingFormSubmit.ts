05c93b3345e04749b6cf586528d1dd95aeb9708b
etanb <etan.berkowitz@gmail.com>
2025-03-13 13:28:45 -0700

    first pass implementing API + refactor of getting Client name code

--- frontend-react/src/hooks/api/UseCodeMappingFormSubmit/UseCodeMappingFormSubmit.ts
+++ frontend-react/src/hooks/api/UseCodeMappingFormSubmit/UseCodeMappingFormSubmit.ts
@@ -1,43 +1,57 @@
-import { useMutation } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
+import { useCallback, useState } from "react";
+import { ConditionCodeData, conditionCodeEndpoints } from "../../../config/endpoints/conditionCode";
+import useSessionContext from "../../../contexts/Session/useSessionContext";
 
-export interface CodeMapData {
-    "test code": string;
-    "test description": string;
-    "coding system": string;
-    mapped: string;
-}
-
-export const sampArr = [
-    {
-        "test code": "97097-0",
-        "test description": "SARS-CoV-2 (COVID-19) Ag [Presence] in Upper respiratory specimen by Rapid  immunoassay",
-        "coding system": "LOINC",
-        mapped: "Y",
-    },
-    {
-        "test code": "80382-5",
-        "test description": "Influenza virus A Ag [Presence] in Upper respiratory specimen by Rapid immunoassay",
-        "coding system": "LOINC",
-        mapped: "Y",
-    },
-    {
-        "test code": "12345",
-        "test description": "Flu B",
-        "coding system": "LOCAL",
-        mapped: "N",
-    },
-];
+const { mapSenderCode } = conditionCodeEndpoints;
 
 const useCodeMappingFormSubmit = () => {
-    const fn = async () => {
-        // Simulate network request
-        await new Promise((resolve) => setTimeout(resolve, 2500));
+    const { activeMembership, authorizedFetch } = useSessionContext();
+    const [requestBody, setRequestBody] = useState<File | null>(null);
+    const [client, setClient] = useState("");
 
-        // Return sample JSON
-        return sampArr;
+    const fetchData = useCallback(async () => {
+        try {
+            // Attempt the fetch
+            console.log("requestBody = ", requestBody);
+            const result = await authorizedFetch<ConditionCodeData[]>(
+                {
+                    data: requestBody,
+                    headers: {
+                        "Content-Type": "text/csv",
+                        client: client,
+                    },
+                },
+                mapSenderCode,
+            );
+
+            return result;
+        } catch (err) {
+            // Ensure we're rejecting with an actual Error object
+            if (err instanceof Error) {
+                return Promise.reject(err);
+            } else {
+                return Promise.reject(new Error(String(err)));
+            }
+        }
+    }, [authorizedFetch, requestBody]);
+
+    const useQueryResult = useQuery<ConditionCodeData[], Error>({
+        queryKey: [mapSenderCode.queryKey, activeMembership, requestBody],
+        queryFn: fetchData,
+        enabled: Boolean(requestBody),
+        staleTime: 0,
+        gcTime: 0,
+    });
+
+    const { data } = useQueryResult;
+
+    return {
+        ...useQueryResult,
+        data: data ?? [],
+        setRequestBody,
+        setClient,
     };
-
-    return useMutation({ mutationFn: fn });
 };
 
 export default useCodeMappingFormSubmit;
