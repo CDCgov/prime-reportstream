49ad6713982e662ad3808c77adcaae2fba9ac7a2
Josh Fisk <joshuafisk@navapbc.com>
2025-04-15 12:30:27 -0700

    Adding swagger annotations bump back in, it was not the problem child

--- prime-router/build.gradle.kts
+++ prime-router/build.gradle.kts
@@ -955,7 +955,7 @@ dependencies {
     // force jsoup since skrapeit-html-parser@1.2.1+ has not updated
     implementation("org.jsoup:jsoup:1.19.1")
     // https://mvnrepository.com/artifact/io.swagger/swagger-annotations
-    implementation("io.swagger:swagger-annotations:1.6.14")
+    implementation("io.swagger:swagger-annotations:1.6.15")
     implementation("io.swagger.core.v3:swagger-jaxrs2:2.2.23")
     // https://mvnrepository.com/artifact/javax.ws.rs/javax.ws.rs-api
     implementation("javax.ws.rs:javax.ws.rs-api:2.1.1")
