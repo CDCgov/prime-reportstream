00bd709070686daa0493b1c5d605296272231fe8
bethbeza <beth.beza@aquia.io>
2025-04-18 13:22:20 -0400

    migration (#17881)
    Co-authored-by: matts <22215332+devopsmatt@users.noreply.github.com>

---
+++ terraform_np/modules/vault_migration/terraform_np/modules/my_vault_migration/data.tf
@@ -0,0 +1,32 @@
+data "azurerm_client_config" "current" {}
+
+# Demo1 Vaults
+data "azurerm_key_vault" "pdhdemo1_appconfigs5m" {
+  name                = var.pdhdemo1_appconfigs5m_name
+  resource_group_name = var.pdhdemo1_appconfigs5m_rg
+}
+
+data "azurerm_key_vault" "pdhdemo1_clientconfigs5m" {
+  name                = var.pdhdemo1_clientconfigs5m_name
+  resource_group_name = var.pdhdemo1_clientconfigs5m_rg
+}
+
+data "azurerm_key_vault" "pdhdemo1_keyvaults5m" {
+  name                = var.pdhdemo1_keyvaults5m_name
+  resource_group_name = var.pdhdemo1_keyvaults5m_rg
+}
+
+# Demo2 Vaults
+data "azurerm_key_vault" "pdhdemo2_appconfig1wu" {
+  name                = var.pdhdemo2_appconfig1wu_name
+  resource_group_name = var.pdhdemo2_appconfig1wu_rg
+}
+
+data "azurerm_key_vault" "pdhdemo2_clientconfig1wu" {
+  name                = var.pdhdemo2_clientconfig1wu_name
+  resource_group_name = var.pdhdemo2_clientconfig1wu_rg
+}
+
+data "azurerm_key_vault" "pdhdemo2_keyvault1wu" {
+  name                = var.pdhd
+
