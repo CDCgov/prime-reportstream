6b9aeba2062960d8410d632d9239216d40c0b876
Jeremy Jones <jeremyjones@navapbc.com>
2025-04-21 19:58:50 -0400

    revert kotlin/validation changes

--- prime-router/src/main/kotlin/validation/MarsOtcElrOnboardingValidator.kt
+++ prime-router/src/main/kotlin/validation/MarsOtcElrOnboardingValidator.kt
@@ -1,12 +1,12 @@
 package gov.cdc.prime.router.validation
 
-import org.hl7.fhir.instance.model.api.IBaseResource
+import org.hl7.fhir.r4.model.Bundle
 
 class MarsOtcElrOnboardingValidator : AbstractItemValidator() {
 
     override val hl7ConformanceProfileLocation: String = "metadata/hl7_validation/v251/radxmars/onboarding"
 
-    override fun validateFHIR(resource: IBaseResource): IItemValidationResult = NoopItemValidationResult()
+    override fun validateFHIR(bundle: Bundle): IItemValidationResult = NoopItemValidationResult()
 
     override val validatorProfileName: String = "RADx MARS Onboarding"
 }
