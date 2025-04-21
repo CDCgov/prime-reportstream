8e70d6e1a962389b986a7d531ef0fcef895ea65b
Jeremy Jones <jeremyjones@navapbc.com>
2025-04-21 19:58:50 -0400

    revert kotlin/validation changes

--- prime-router/src/main/kotlin/validation/IItemValidator.kt
+++ prime-router/src/main/kotlin/validation/IItemValidator.kt
@@ -9,7 +9,6 @@ import gov.nist.validation.report.Entry
 import gov.nist.validation.report.Report
 import hl7.v2.validation.SyncHL7Validator
 import hl7.v2.validation.ValidationContextBuilder
-import org.hl7.fhir.instance.model.api.IBaseResource
 import org.hl7.fhir.r4.model.Bundle
 
 interface IItemValidator {
@@ -126,10 +125,10 @@ abstract class AbstractItemValidator : IItemValidator {
         return HL7ValidationResult(report)
     }
 
-    open fun validateFHIR(resource: IBaseResource): IItemValidationResult {
+    open fun validateFHIR(bundle: Bundle): IItemValidationResult {
         val ctx = FhirContext.forR4()
         val validator = ctx.newValidator()
-        val result = validator.validateWithResult(resource)
+        val result = validator.validateWithResult(bundle)
         return FHIRValidationResult(result)
     }
 }
