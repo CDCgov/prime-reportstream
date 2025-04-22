9139e56e166f279202c1b8e681937a805726f822
Beth Beza <beth.beza@aquia.io>
2025-04-21 23:14:21 -0400

    add prod vaults

--- operations/app/terraform_np/np_main.tf
+++ operations/app/terraform_np/np_main.tf
@@ -29,4 +29,56 @@ module "pdhstaging_serviceplan" {
     managed-by  = "terraform"
   }
 }
+resource "azurerm_key_vault" "pdhprod_vault" {
+  name                        = "pdhprod-admin-functionapp"
+  location                    = "East US"
+  resource_group_name         = "ddphss-prim-prd-moderate-app-vnet"
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  sku_name                    = "standard"
+  purge_protection_enabled    = true
+  soft_delete_enabled         = true
+  enabled_for_deployment      = false
+  enabled_for_disk_encryption = false
+  enabled_for_template_deployment = false
+
+  tags = {
+    environment = "prod"
+    managed-by  = "terraform"
+  }
+}
+resource "azurerm_key_vault" "pdhprod_appconfig" {
+  name                        = "pdhprod-appconfig"
+  location                    = "East US"
+  resource_group_name         = "ddphss-prim-prd-moderate-app-vnet"
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  sku_name                    = "premium"
+  purge_protection_enabled    = true
+  soft_delete_enabled         = true
+  enabled_for_deployment      = false
+  enabled_for_disk_encryption = false
+  enabled_for_template_deployment = false
+
+  tags = {
+    environment = "prod"
+    managed-by  = "terraform"
+  }
+}
+
+resource "azurerm_key_vault" "pdhprod_clientconfig" {
+  name                        = "pdhprod-clientconfig"
+  location                    = "East US"
+  resource_group_name         = "ddphss-prim-prd-moderate-app-vnet"
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  sku_name                    = "premium"
+  purge_protection_enabled    = true
+  soft_delete_enabled         = true
+  enabled_for_deployment      = false
+  enabled_for_disk_encryption = false
+  enabled_for_template_deployment = false
+
+  tags = {
+    environment = "prod"
+    managed-by  = "terraform"
+  }
+}
 
