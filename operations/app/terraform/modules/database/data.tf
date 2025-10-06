e36a0b844e8aeed968e5f18c032f1bd9a657e80b
matts <22215332+devopsmatt@users.noreply.github.com>
2025-10-06 10:27:34 -0700

    Tf fixes (#18608)
    * Fix terraform database module configuration for prod and staging
    
    * Upgrade terraform plan comment action to borchero/terraform-plan-comment@v2
    
    * bump python
    
    * force tf run
    
    * Fix database module initialization in terraform
    
    * staging db fixes
    
    * Add database module data sources and enable staging database lookup
    
    * Remove unsupported azurerm_postgresql_database data source

--- operations/app/terraform/modules/database/data.tf
+++ operations/app/terraform/modules/database/data.tf
@@ -1,4 +1,4 @@
-# --- Data Sources for Existing Postgres Servers and Databases ---
+# --- Data Sources for Existing Postgres Servers ---
 
 # Look up the primary PostgreSQL server.
 data "azurerm_postgresql_server" "primary" {
@@ -12,15 +12,4 @@ data "azurerm_postgresql_server" "replicas" {
 
   name                = each.key
   resource_group_name = var.database_resource_group_name
-}
-
-# Look up the required databases on the primary server.
-data "azurerm_postgresql_database" "dbs" {
-  for_each = toset(var.database_names)
-
-  name                = each.key
-  resource_group_name = var.database_resource_group_name
-  server_name         = data.azurerm_postgresql_server.primary.name
-
-  depends_on = [data.azurerm_postgresql_server.primary]
 }
