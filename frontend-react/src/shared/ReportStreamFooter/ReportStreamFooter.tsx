26a9a4cc557e5c6df52b7e46f718924e463fa6b4
Tino Gutierrez <tino.gutierrez@focusconsulting.io>
2025-12-12 16:54:15 -0500

    Experience-18722: Remove About ReportStream and Terms of service interal links from ReportStreamFooter.tsx

--- frontend-react/src/shared/ReportStreamFooter/ReportStreamFooter.tsx
+++ frontend-react/src/shared/ReportStreamFooter/ReportStreamFooter.tsx
@@ -21,45 +21,23 @@ export const ReportStreamFooter = () => {
             <Identifier className="grid-container">
                 <div className="grid-row">
                     <div className="grid-col-8">
-                        <IdentifierMasthead
-                            className="margin-bottom-4 padding-top-0"
-                            aria-label="Agency identifier"
-                        >
+                        <IdentifierMasthead className="margin-bottom-4 padding-top-0" aria-label="Agency identifier">
                             <IdentifierLogos>
-                                <IdentifierLogo
-                                    href="https://cdc.gov"
-                                    target="_blank"
-                                    rel="noopener"
-                                >
-                                    <img
-                                        src="/assets/cdc-logo.svg"
-                                        alt="CDC logo, links to main CDC website"
-                                    />
+                                <IdentifierLogo href="https://cdc.gov" target="_blank" rel="noopener">
+                                    <img src="/assets/cdc-logo.svg" alt="CDC logo, links to main CDC website" />
                                 </IdentifierLogo>
                             </IdentifierLogos>
                             <IdentifierIdentity domain="cdc.gov">
                                 An official website of the{" "}
-                                <Link
-                                    href="https://cdc.gov"
-                                    target="_blank"
-                                    rel="noopener"
-                                >
+                                <Link href="https://cdc.gov" target="_blank" rel="noopener">
                                     Centers for Disease Control and Prevention
                                 </Link>
                             </IdentifierIdentity>
                         </IdentifierMasthead>
-                        <IdentifierLinks
-                            className="column-count-3"
-                            navProps={{ "aria-label": "Important links" }}
-                        >
+                        <IdentifierLinks className="column-count-3" navProps={{ "aria-label": "Important links" }}>
                             <IdentifierLinkItem>
                                 <IdentifierLink href="https://www.cdc.gov/about/cdc/index.html">
                                     About CDC
-                                </IdentifierLink>
-                            </IdentifierLinkItem>
-                            <IdentifierLinkItem>
-                                <IdentifierLink href="/about">
-                                    About ReportStream
                                 </IdentifierLink>
                             </IdentifierLinkItem>
                             <IdentifierLinkItem>
@@ -68,9 +46,7 @@ export const ReportStreamFooter = () => {
                                 </IdentifierLink>
                             </IdentifierLinkItem>
                             <IdentifierLinkItem>
-                                <IdentifierLink href="https://www.cdc.gov/foia/">
-                                    FOIA requests
-                                </IdentifierLink>
+                                <IdentifierLink href="https://www.cdc.gov/foia/">FOIA requests</IdentifierLink>
                             </IdentifierLinkItem>
                             <IdentifierLinkItem>
                                 <IdentifierLink href="https://www.cdc.gov/eeo/nofearact/index.htm">
@@ -78,10 +54,7 @@ export const ReportStreamFooter = () => {
                                 </IdentifierLink>
                             </IdentifierLinkItem>
                             <IdentifierLinkItem>
-                                <IdentifierLink
-                                    href="https://oig.hhs.gov/"
-                                    className="usa-link--external"
-                                >
+                                <IdentifierLink href="https://oig.hhs.gov/" className="usa-link--external">
                                     Office of the Inspector General
                                 </IdentifierLink>
                             </IdentifierLinkItem>
@@ -98,21 +71,13 @@ export const ReportStreamFooter = () => {
                                     Vulnerability disclosure policy
                                 </IdentifierLink>
                             </IdentifierLinkItem>
-                            <IdentifierLinkItem>
-                                <IdentifierLink href="/terms-of-service">
-                                    Terms of service
-                                </IdentifierLink>
-                            </IdentifierLinkItem>
                         </IdentifierLinks>
                         <IdentifierGov
                             className="margin-top-1 padding-bottom-0"
                             aria-label="U.S. government information and services"
                         >
-                            Looking for U.S. government information and
-                            services?{" "}
-                            <USExtLink href="https://www.usa.gov/">
-                                Visit USA.gov
-                            </USExtLink>
+                            Looking for U.S. government information and services?{" "}
+                            <USExtLink href="https://www.usa.gov/">Visit USA.gov</USExtLink>
                         </IdentifierGov>
                     </div>
                     <div className="grid-col-1"></div>
@@ -120,14 +85,10 @@ export const ReportStreamFooter = () => {
                         <h4 className="margin-top-3">Need help?</h4>
                         <hr className="margin-top-2 margin-bottom-2" />
                         <p className="margin-bottom-4 usa-identifier__response">
-                            Our team will respond to your questions or set up a
-                            time to learn more about how we can help you.
+                            Our team will respond to your questions or set up a time to learn more about how we can help
+                            you.
                         </p>
-                        <USLinkButton
-                            href={site.forms.connectWithRS.url}
-                            outline
-                            inverse
-                        >
+                        <USLinkButton href={site.forms.connectWithRS.url} outline inverse>
                             Connect now
                         </USLinkButton>
                     </div>
